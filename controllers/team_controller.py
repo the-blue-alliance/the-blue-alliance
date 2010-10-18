af50c35a46a0caab63a361c2e154d0b4d57cc7c9
Greg Marra <greg.marra@gmail.com>
2010-10-17 23:03:38 -0700

    removes unused ajax endpoints, optional year.

--- controllers/team_controller.py
+++ controllers/team_controller.py
@@ -20,14 +20,17 @@ class TeamList(webapp.RequestHandler):
         
 # The view of a single Team.
 class TeamDetail(webapp.RequestHandler):
-    def get(self, team_number, year):
-        team = Team.all().filter("team_number =", int(team_number))[0]
+    def get(self, team_number, year=None):
+        if not year: year = 2010 #fixme: make this real. -gregmarra 17 Oct 2010
+        team = Team.get_by_key_name("frc" + team_number)
+        
+        #todo 404 handling
                 
         # First, get a list of events attended by this team in the given year.
         events = team.events.filter('year=', int(year))
         
-        participation = []
-        matches = []
+        participation = list()
+        matches = list()
         
         # Return an array of event names and a list of matches from that event that the
         # team was a participant in.
@@ -43,25 +46,5 @@ class TeamDetail(webapp.RequestHandler):
         
         path = os.path.join(os.path.dirname(__file__), '../templates/teams/details.html')
         self.response.out.write(template.render(path, template_values))
-        
-class TeamEvents(webapp.RequestHandler):
-    """
-    An AJAX endpoint to return a list of a team's events given a year.
-    """
-    def get(self, team_number, year):
-        team = Team.all().filter("team_number =", int(team_number))[0]
-        events = team.events.filter("year =", int(year))
-        
-        path = os.path.join(os.path.dirname(__file__), '../templates/teams/teamevents.html')
-        self.response.out.write(template.render(path, { 'events': events }))
 
-class TeamMatches(webapp.RequestHandler):
-    """
-    An AJAX endpoint to return a list of a team's matches given an event ID.
-    """
-    def get(self, event_id):
-        event = Event.all().get_by_id(int(event_id))[0]
-        matches = event.matches
-        
-        path = os.path.join(os.path.dirname(__file__), '../templates/teams/teammatches.html')
-        self.response.out.write(template.render(path, { 'matches': matches }))
+
