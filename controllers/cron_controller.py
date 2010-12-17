1d1dc6769ae40722a5c4547da1dd22f3f1d8c2a2
Greg Marra <greg.marra@gmail.com>
2010-12-16 20:38:37 -0800

    taskqueues no longer labs.

--- controllers/cron_controller.py
+++ controllers/cron_controller.py
@@ -3,7 +3,7 @@ import os
 
 from django.utils import simplejson
 
-from google.appengine.api.labs import taskqueue
+from google.appengine.api import taskqueue
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp import template, util
 
