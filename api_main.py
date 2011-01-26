79a861a47d27ccfe9eecf25b8fd5e3e18daf018d
Greg Marra <greg.marra@gmail.com>
2011-01-25 23:04:29 -0800

    First pass at API

--- api_main.py
+++ api_main.py
@@ -8,8 +8,8 @@ from controllers.api_controller import ApiEventsShow, ApiTeamsShow
 
 
 def main():
-    application = webapp.WSGIApplication([('/api/v1/teams/show', ApiTeams),
-                                          ('/api/v1/events/show', ApiEvents),
+    application = webapp.WSGIApplication([('/api/v1/teams/show', ApiTeamsShow),
+                                          ('/api/v1/events/show', ApiEventsShow),
                                           ],
                                          debug=True)
     util.run_wsgi_app(application)
