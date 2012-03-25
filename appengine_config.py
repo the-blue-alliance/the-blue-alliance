97cc3fe08d3aedc1a788c21e38255a06902cbd95
Joseph Bisch <josephbisch@yahoo.com>
2012-03-25 12:02:35 -0400

    Explicitly set django version in appengine_config.py to avoid error when manually running cron jobs

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,6 @@
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 def webapp_add_wsgi_middleware(app):
    from google.appengine.ext.appstats import recording
    app = recording.appstats_wsgi_middleware(app)
