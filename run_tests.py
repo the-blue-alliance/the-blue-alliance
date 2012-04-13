dc3d95cc13b8b7f0b7ee7841a31a231d9ac413b6
Greg Marra <greg.marra@gmail.com>
2012-04-12 20:41:00 -0700

    Improved flexibility in test running. Can execute single test from command line.

--- run_tests.py
+++ run_tests.py
@@ -11,21 +11,27 @@ The SDK Path is probably /usr/local/google_appengine on Mac OS
 SDK_PATH    Path to the SDK installation"""
 
 
-def main(sdk_path, test_path):
+def main(sdk_path, test_pattern):
     sys.path.insert(0, sdk_path)
     import dev_appserver
     dev_appserver.fix_sys_path()
-    suite = unittest2.loader.TestLoader().discover(test_path)
+    
+    suite = unittest2.loader.TestLoader().discover("tests", test_pattern)
     unittest2.TextTestRunner(verbosity=2).run(suite)
 
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(USAGE)
     options, args = parser.parse_args()
-    if len(args) != 1:
-        print 'Error: Exactly 1 argument required.'
+    if len(args) < 1:
+        print 'Error: Must provide SDK path.'
         parser.print_help()
         sys.exit(1)
-    SDK_PATH = args[0]
-    TEST_PATH = "tests"
-    main(SDK_PATH, TEST_PATH)
+    
+    sdk_path = args[0]
+    
+    test_pattern = "test*.py"
+    if len(args) > 1:
+        test_pattern = args[1]
+    
+    main(sdk_path, test_pattern)
