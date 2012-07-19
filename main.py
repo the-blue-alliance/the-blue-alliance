86e3bb81e7aea70b2973174ec4ee13bf2b23cd5e
Greg Marra <greg.marra@gmail.com>
2012-07-19 13:56:04 -0700

    Move to webapp2. Enable concurrent requests. Fixes #147 . Fixes #146 .

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
-#
-
 import os
+import webapp2
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template, util
-
-from controllers.main_controller import MainHandler, ContactHandler, ThanksHandler, SearchHandler, PageNotFoundHandler, OprHandler
+import tba_config
 
 from controllers.event_controller import EventList, EventDetail, EventRss
+from controllers.main_controller import MainHandler, ContactHandler, ThanksHandler, SearchHandler, PageNotFoundHandler, OprHandler
 from controllers.match_controller import MatchList, MatchDetail
 from controllers.team_controller import TeamList, TeamDetail
 
-
-
-def main():
-    application = webapp.WSGIApplication([('/', MainHandler),
-                                          ('/contact', ContactHandler),
-                                          ('/events', EventList),
-                                          ('/event/(.*)/feed', EventRss),
-                                          ('/events/(.*)', EventList),
-                                          ('/event/(.*)', EventDetail),
-                                          ('/match/list', MatchList),
-                                          ('/match/(.*)', MatchDetail),
-                                          ('/search', SearchHandler),
-                                          ('/teams', TeamList),
-                                          ('/team/([0-9]*)', TeamDetail),
-                                          ('/team/([0-9]*)/(.*)', TeamDetail),
-                                          ('/thanks', ThanksHandler),
-                                          ('/opr', OprHandler),
-                                          ('/.*', PageNotFoundHandler)
-                                          ],
-                                         debug=True)
-    util.run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-    main()
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/contact', ContactHandler),
+                               ('/events', EventList),
+                               ('/event/(.*)/feed', EventRss),
+                               ('/events/(.*)', EventList),
+                               ('/event/(.*)', EventDetail),
+                               ('/match/list', MatchList),
+                               ('/match/(.*)', MatchDetail),
+                               ('/search', SearchHandler),
+                               ('/teams', TeamList),
+                               ('/team/([0-9]*)', TeamDetail),
+                               ('/team/([0-9]*)/(.*)', TeamDetail),
+                               ('/thanks', ThanksHandler),
+                               ('/opr', OprHandler),
+                               ('/.*', PageNotFoundHandler)
+                               ],
+                              debug=tba_config.DEBUG)
