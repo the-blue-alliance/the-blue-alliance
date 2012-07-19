6e5d96ede0a5fa176756503067a471d0e902c4ed
Kunal Mehta <legoktm@gmail.com>
2012-07-19 14:39:01 -0700

    since we now depend the Event db.model, move that to the bottom of the file change get_key_name to include the regional name

--- models.py
+++ models.py
@@ -42,25 +42,6 @@ class Team(db.Model):
     def details_url(self):
         return "/team/%s" % self.team_number
 
-class Award(db.Model):
-    """
-    Awards represent FIRST Robotics Competition awards given out at an event.
-    name is a general name and can be seen in /datafeeds/datafeed_usfirst_awards.py
-    key_name is like '2012sj_rca'
-    """
-    name = db.StringProperty() #general name used for sorting
-    year = db.IntegerProperty() #year it was awarded
-    winner = db.IntegerProperty() #who won the award
-    awardee = db.StringProperty() #who won it
-    
-    def get_key_name(self):
-        """
-        Returns the string of the key_name of the Award object before writing it.
-        """
-        return str(self.year) + self.name
-    
-    def details_url(self):
-        return "/award/%s" % self.get_key_name()
 
     
 
@@ -253,3 +234,31 @@ class Match(db.Model):
     
     def details_url(self):
         return "/match/%s" % self.get_key_name()
+
+
+class Award(db.Model):
+    """
+    Awards represent FIRST Robotics Competition awards given out at an event.
+    name is a general name and can be seen in /datafeeds/datafeed_usfirst_awards.py
+    key_name is like '2012sj_rca'
+    """
+    name = db.StringProperty() #general name used for sorting
+    year = db.IntegerProperty() #year it was awarded
+    winner = db.IntegerProperty() #who won the award
+    awardee = db.StringProperty() #who won it
+    event = db.ReferenceProperty(Event, required=True)
+    
+    def event_key_name(self):
+        return Match.event.get_value_for_datastore(self).name()
+
+
+    def get_key_name(self):
+        """
+        Returns the string of the key_name of the Award object before writing it.
+        """
+        return str(self.year) + self.event_key_name() + '_' + self.name
+    
+    def details_url(self):
+        return "/award/%s" % self.get_key_name()
+
+
