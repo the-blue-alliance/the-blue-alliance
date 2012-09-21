9fa9cbc041002b83fde072ba726316fec4c5349a
Greg Marra <greg.marra@gmail.com>
2012-09-20 23:54:13 -0700

    Adds rest of files for fms_event_list.

---
+++ datafeeds/fms_event_list_parser.py
@@ -0,0 +1,57 @@
+import datetime
+import logging
+
+from BeautifulSoup import BeautifulSoup
+
+from datafeeds.parser_base import ParserBase
+
+class FmsEventListParser(ParserBase):
+    """
+    Facilitates getting information about Events from USFIRST.
+    Reads from FMS data pages, which are mostly tab delimited files wrapped in some HTML.
+    """
+    
+    @classmethod
+    def parse(self, html):
+        """
+        Parse the information table on USFIRSTs site to extract event information.
+        Return a list of dictionaries of event data.
+        """
+        events = list()
+        soup = BeautifulSoup(html,
+                convertEntities=BeautifulSoup.HTML_ENTITIES)
+        
+        for title in soup.findAll('title'):
+            if title.string != "2012 FRC Team/Event List":
+                return None
+        
+        event_rows = soup.findAll("pre")[0].string.split("\n")
+        
+        for line in event_rows[2:]: #first is blank, second is headers.
+            data = line.split("\t")
+            if len(data) > 1:
+                try:
+                    events.append({
+                        "first_eid": data[0],
+                        "name": data[1].strip(),
+                        "venue": data[2],
+                        "location": "%s, %s" % (data[3], data[4]),
+                        "start_date": self.splitDate(data[6]),
+                        "end_date": self.splitDate(data[7]),
+                        "year": int(data[8]),
+                        "event_short": data[11].strip().lower()
+                    })
+                except Exception, e:
+                    logging.warning("Failed to parse event row: %s" % data)
+                    logging.warning(e)
+        
+        return events
+
+    @classmethod
+    def splitDate(self, date):
+        try:
+            (year, month, day) = date.split("-")
+            date =  datetime.datetime(int(year), int(month), int(day))
+            return date
+        except Exception, e:
+            return None
---
+++ tests/test_datafeed_fms_events.py
@@ -0,0 +1,41 @@
+import unittest2
+import datetime
+
+from google.appengine.ext import testbed
+
+from datafeeds.datafeed_fms import DatafeedFms
+
+class TestDatafeedFmsEvents(unittest2.TestCase):
+    
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_urlfetch_stub()
+        
+        self.datafeed = DatafeedFms()
+    
+    def tearDown(self):
+        self.testbed.deactivate()
+    
+    def test_getFmsEventList(self):
+        events = self.datafeed.getFmsEventList()
+        self.findCT(events)
+    
+    def findCT(self, events):
+        found_ct = False
+        for event in events:
+            if event.event_short == "ct":
+                found_ct = True
+                self.assertEqual(event.key_name, "2012ct")
+                self.assertEqual(event.end_date, datetime.datetime(2012, 03, 31))
+                self.assertEqual(event.event_short, "ct")
+                self.assertEqual(event.first_eid, "7627")
+                self.assertEqual(event.location, "Hartford, CT")
+                self.assertEqual(event.name, "Northeast Utilities FIRST Connecticut Regional")
+                self.assertEqual(event.official, True)
+                self.assertEqual(event.start_date, datetime.datetime(2012, 03, 29))
+                self.assertEqual(event.venue, "Connecticut Convention Center")
+                self.assertEqual(event.year, 2012)
+        
+        self.assertTrue(found_ct)
+        self.assertTrue(len(events) > 0)
