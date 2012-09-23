4a9a9d5d34f8d29bbd6b26afc5e8a19c7bde02cc
Greg Marra <greg.marra@gmail.com>
2012-09-23 00:56:03 -0700

    cleaner updateMerge in TeamManipulator

--- helpers/team_manipulator.py
+++ helpers/team_manipulator.py
@@ -12,17 +12,17 @@ class TeamManipulator(ManipulatorBase):
         "old" team that are present in the "new" team, but keep fields from
         the "old" team that are null in the "new" team.
         """
-        #FIXME: There must be a way to do this elegantly. -greg 5/12/2010
-        
-        if new_team.name:
-            old_team.name = new_team.name
-        if new_team.nickname:
-            old_team.nickname = new_team.nickname
-        if new_team.website:
-            old_team.website = new_team.website
-        if new_team.address:
-            old_team.address = new_team.address
-        
+        attrs = [
+            "address",
+            "name",
+            "nickname",
+            "website",
+        ]
+
+        for attr in attrs:
+            if getattr(new_team, attr) is not None:
+                setattr(old_team, attr, getattr(new_team, attr))
+                
         # Take the new tpid and tpid_year iff the year is newer than the old one
         if (new_team.first_tpid_year > old_team.first_tpid_year):
             old_team.first_tpid_year = new_team.first_tpid_year
