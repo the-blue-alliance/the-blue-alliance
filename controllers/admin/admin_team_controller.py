4a1a7b0ba9dadf4b56f4e5bb72223248b906d319
Greg Marra <greg.marra@gmail.com>
2012-10-08 22:56:35 -0700

    EventTeams on admin team details. Fixes #239

--- controllers/admin/admin_team_controller.py
+++ controllers/admin/admin_team_controller.py
@@ -3,6 +3,7 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
+from models.event_team import EventTeam
 from models.team import Team
 
 # The view of a list of teams.
@@ -20,10 +21,12 @@ class AdminTeamList(webapp.RequestHandler):
 # The view of a single Team.
 class AdminTeamDetail(webapp.RequestHandler):
     def get(self, team_number):
-        team = Team.get_by_id("frc" + team_number)                
-        
-        template_values = { 
-            'team' : team,
+        team = Team.get_by_id("frc" + team_number)
+        event_teams = EventTeam.query(EventTeam.team == team.key).fetch(500)
+
+        template_values = {
+            'event_teams': event_teams,
+            'team': team,
         }
         
         path = os.path.join(os.path.dirname(__file__), '../../templates/admin/team_details.html')
