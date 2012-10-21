835cae8c7bb8a9120008657e5641d6fbbdc5782b
Brandon Dean <deanbrandon4@gmail.com>
2012-10-21 00:59:25 -0400

    Work around for SERVER_SOFTWARE not being set

--- tba_config.py
+++ tba_config.py
@@ -1,6 +1,6 @@
 import os
 
-DEBUG = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 # The CONFIG variables should have exactly the same structure between environments
 # Eventually a test environment should be added. -gregmarra 17 Jul 2012
