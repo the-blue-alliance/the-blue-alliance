dde0d89d9a4500e9b0b239aed4a4e474ddd84a12
Eugene Fang <fang.eugene@gmail.com>
2012-10-23 00:01:29 -0700

    group events by week

--- helpers/event_helper.py
+++ helpers/event_helper.py
@@ -1,13 +1,59 @@
 import logging
+import collections
+import datetime
 
 from models.event import Event
 from models.match import Match
 from models.team import Team
 
+CHAMPIONSHIP_EVENTS = set(['arc', 'cur', 'gal', 'new', 'ein', 'cmp'])
+CHAMPIONSHIP_EVENTS_LABEL = 'Championship Event'
+REGIONAL_EVENTS_LABEL = 'Week {} Events'
+OFFSEASON_EVENTS_LABEL = 'Offseason Events'
+
+
 class EventHelper(object):
     """
     Helper class for Events.
     """
+    @classmethod
+    def groupByWeek(self, events):
+        """
+        Events should already be ordered by start_date
+        Works for years 2005 and above
+        """
+        toReturn = collections.OrderedDict()  # key: week_label, value: list of events
+        
+        current_week = 0
+        week_start = None
+        for event in events:
+            start = event.start_date
+
+            if event.event_short in CHAMPIONSHIP_EVENTS:
+                if CHAMPIONSHIP_EVENTS_LABEL in toReturn:
+                    toReturn[CHAMPIONSHIP_EVENTS_LABEL].append(event)
+                else:
+                    toReturn[CHAMPIONSHIP_EVENTS_LABEL] = [event]
+                continue
+            elif (start == None) or (start.month > 4):
+                label = OFFSEASON_EVENTS_LABEL
+                if label in toReturn:
+                    toReturn[label].append(event)
+                else:
+                    toReturn[label] = [event]
+                continue
+            else:
+                if (week_start == None) or (start > week_start + datetime.timedelta(days=6)):
+                    current_week += 1
+                    week_start = start
+                    
+                label = REGIONAL_EVENTS_LABEL.format(current_week)
+                if label in toReturn:
+                    toReturn[label].append(event)
+                else:
+                    toReturn[label] = [event]
+        
+        return toReturn
     
     @classmethod
     def getTeamWLTFromMatches(self, team_key, matches):
