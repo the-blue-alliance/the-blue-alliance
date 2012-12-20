27900a2bba153b6588c18e1f50bacafcbe610630
Eugene Fang <fang.eugene@gmail.com>
2012-12-19 17:53:29 -0800

    Banner for competition season description

--- static/javascript/gameday.js
+++ static/javascript/gameday.js
@@ -69,6 +69,7 @@ function setupViews() {
 	  layout_0();
 	  setChat(true);
 	  setupView(0, $("#2013kickoff-1"));
+	  $("#nav-alert-container").html('<div class="alert alert-success nav-alert"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Welcome!</strong> Remember to come back during the competition season for webcasts, scores, and more!</div>');
   }
 }
 
