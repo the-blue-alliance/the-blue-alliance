201a698920235c3b54154c1a2b39dd3d63dfca73
Greg Marra <gmarra@fb.com>
2012-12-27 20:42:22 -0500

    Moves Insights to CacheableHandler.

--- controllers/insights_controller.py
+++ controllers/insights_controller.py
@@ -8,41 +8,47 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 import tba_config
-from base_controller import BaseHandler
+from base_controller import BaseHandler, CacheableHandler
 
 from models.insight import Insight
 
 VALID_YEARS = [2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002]
 
-class InsightsOverview(BaseHandler):
+class InsightsOverview(CacheableHandler):
     """
     Show Insights Overview
     """
-    def get(self):
-        memcache_key = "insights_overview"
-        html = memcache.get(memcache_key)
-        
-        if html is None:
-            template_values = {
-                'valid_years': VALID_YEARS,
-            }
-            
-            insight_futures = [Insight.get_by_id_async(Insight.renderKeyName(0, insight_name)) for insight_name in Insight.INSIGHT_NAMES.values()]
-            for insight_future in insight_futures:
-                insight = insight_future.get_result()
-                if insight:
-                    template_values[insight.name] = insight
-                            
-            path = os.path.join(os.path.dirname(__file__), '../templates/insights.html')
-            html = template.render(path, template_values)
-            if tba_config.CONFIG["memcache"]: memcache.set(memcache_key, html, 60 * 60 * 24) 
-        
-        self.response.out.write(html)
+    def __init__(self, *args, **kw):
+        super(InsightsOverview, self).__init__(*args, **kw)
+        self._cache_expiration = 60 * 60 * 24
+        self._cache_key = "insights_overview"
+        self._cache_version = 1
 
-class InsightsDetail(BaseHandler):
+    def _render(self):
+        template_values = {
+            'valid_years': VALID_YEARS,
+        }
+        
+        insight_futures = [Insight.get_by_id_async(Insight.renderKeyName(0, insight_name)) for insight_name in Insight.INSIGHT_NAMES.values()]
+        for insight_future in insight_futures:
+            insight = insight_future.get_result()
+            if insight:
+                template_values[insight.name] = insight
+                        
+        path = os.path.join(os.path.dirname(__file__), '../templates/insights.html')
+        return template.render(path, template_values)
+
+
+class InsightsDetail(CacheableHandler):
     """
     Show Insights for a particular year
     """
+    def __init__(self, *args, **kw):
+        super(InsightsDetail, self).__init__(*args, **kw)
+        self._cache_expiration = 60 * 60 * 24
+        self._cache_key = "insight_detail_{}" # (year)
+        self._cache_version = 1
+
     def get(self, year):
         if year == '':
             return self.redirect("/insights")
@@ -51,24 +57,22 @@ class InsightsDetail(BaseHandler):
         year = int(year)
         if year not in VALID_YEARS:
             return self.redirect("/error/404")
+
+        self._cache_key = self._cache_key.format(year)
+        super(InsightsDetail, self).get(year)
+
+    def _render(self, year):
+        template_values = {
+            'valid_years': VALID_YEARS,
+            'selected_year': year,
+        }
         
-        memcache_key = "insights_detail_%s" % year
-        html = memcache.get(memcache_key)
+        insight_futures = [Insight.get_by_id_async(Insight.renderKeyName(year, insight_name)) for insight_name in Insight.INSIGHT_NAMES.values()]
+        for insight_future in insight_futures:
+            insight = insight_future.get_result()
+            if insight:
+                template_values[insight.name] = insight
         
-        if html is None:
-            template_values = {
-                'valid_years': VALID_YEARS,
-                'selected_year': year,
-            }
-            
-            insight_futures = [Insight.get_by_id_async(Insight.renderKeyName(year, insight_name)) for insight_name in Insight.INSIGHT_NAMES.values()]
-            for insight_future in insight_futures:
-                insight = insight_future.get_result()
-                if insight:
-                    template_values[insight.name] = insight
-            
-            path = os.path.join(os.path.dirname(__file__), '../templates/insights_details.html')
-            html = template.render(path, template_values)
-            if tba_config.CONFIG["memcache"]: memcache.set(memcache_key, html, 86400) 
-        
-        self.response.out.write(html)
+        path = os.path.join(os.path.dirname(__file__), '../templates/insights_details.html')
+        return template.render(path, template_values)
+        if tba_config.CONFIG["memcache"]: memcache.set(memcache_key, html, 86400) 
