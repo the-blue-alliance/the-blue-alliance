610e40782cb0848b19f26c740d34ebfe2fc64864
Greg Marra <greg.marra@gmail.com>
2013-01-01 20:28:07 -0800

    Fix webcasts to be about 2013.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -209,10 +209,10 @@ class WebcastsHandler(CacheableHandler):
         super(CacheableHandler, self).__init__(*args, **kw)
         self._cache_expiration = 60 * 60 * 24 * 7
         self._cache_key = "main_webcasts"
-        self._cache_version = 1
+        self._cache_version = 2
 
     def _render(self, *args, **kw):
-        events = Event.query(Event.year == 2010).order(Event.start_date).fetch(500)
+        events = Event.query(Event.year == 2013).order(Event.start_date).fetch(500)
 
         template_values = {
             'events': events,
