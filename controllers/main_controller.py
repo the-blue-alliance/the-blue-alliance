313a3c70af8b8978e9eada5f0e093f25eeef4f81
David Thomas <dev@synth3.tk>
2013-02-18 13:25:24 -0500

    Added controller and template

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -231,3 +231,14 @@ class WebcastsHandler(CacheableHandler):
         
         path = os.path.join(os.path.dirname(__file__), '../templates/webcasts.html')
         return template.render(path, template_values)
+
+class HowToRecordHandler(CacheableHandler):
+    def __init__(self, *args, **kw):
+        super(CacheableHandler, self).__init__(*args, **kw)
+        self._cache_expiration = 60 * 60 * 24 * 7
+        self._cache_key = "main_howto_record"
+        self._cache_version = 1
+
+    def _render(self, *args, **kw):
+        path = os.path.join(os.path.dirname(__file__), "../templates/howto_record.html")
+        return template.render(path, {})
