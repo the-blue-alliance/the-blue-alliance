d271edfb12fc1f0e8ecde096ffced4011a5aa48d
Greg Marra <gmarra@fb.com>
2013-03-01 08:49:25 -0800

    Reduce caching time.

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -51,7 +51,7 @@ class MainBuildseasonHandler(CacheableHandler):
 class MainCompetitionseasonHandler(CacheableHandler):
     def __init__(self, *args, **kw):
         super(CacheableHandler, self).__init__(*args, **kw)
-        self._cache_expiration = 60 * 60 * 24 * 7
+        self._cache_expiration = 60 * 60
         self._cache_key = "main_competitionseason"
         self._cache_version = 5
 
