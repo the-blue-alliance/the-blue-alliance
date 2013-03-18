61c773a99ef08fe22466018404d9c98c6b45250f
Greg Marra <greg.marra@gmail.com>
2013-03-17 19:27:37 -0700

    Adds Create Test Events button to Admin index page.
    This button does a lot.
    - It sets up a Past Test Event with complete qualifications.
    - It sets up a Present Test Event with some complete and some incomplete qualifications, as well as a webcast.
    - It sets up a Future Test Event
    
    For each Event
    - The Event has EventTeams generated from Teams already in the dev instance, so you can actually click around the site pretty normally.
    - Since there are matches, you can test things like Insights, Match pages, etc.
    - The webcast lets you check the watch now buttons on teams, event pages, and the homepage
    
    The button does not work in the prod environment.
    
    This diff also refactors some of how testing is done to use the new objects that generate this test data.

--- helpers/event/event_test_creator.py
+++ helpers/event/event_test_creator.py
@@ -1,29 +1,36 @@
 import datetime
+import logging
 
 from helpers.event_manipulator import EventManipulator
+from helpers.event_team.event_team_test_creator import EventTeamTestCreator
+from helpers.match.match_test_creator import MatchTestCreator
 from models.event import Event
 
 class EventTestCreator(object):
     @classmethod
-    def createFutureEvent(self):
+    def createFutureEvent(self, only_event=False):
         event = Event(
             id = "{}testfuture".format(datetime.datetime.now().year),
-            end_date = datetime.datetime.today() + datetime.timedelta(days=5),
+            end_date = datetime.datetime.today() + datetime.timedelta(days=12),
             event_short = "testfuture",
             event_type = "Regional",
             first_eid = "5561",
             name = "Future Test Event",
-            start_date = datetime.datetime.today() + datetime.timedelta(days=1),
+            start_date = datetime.datetime.today() + datetime.timedelta(days=8),
             year = datetime.datetime.now().year,
             venue_address = "123 Fake Street, California, USA",
             website = "http://www.google.com"
         )
-        return EventManipulator.createOrUpdate(event)
+        event = EventManipulator.createOrUpdate(event)
+        if not only_event:
+            EventTeamTestCreator.createEventTeams(event)
+        return event
 
     @classmethod
-    def createPresentEvent(self):
+    def createPresentEvent(self, only_event=False):
+        id_string = "{}testpresent".format(datetime.datetime.now().year)
         event = Event(
-            id = "{}testpresent".format(datetime.datetime.now().year),
+            id = id_string,
             end_date = datetime.datetime.today() + datetime.timedelta(days=1),
             event_short = "testpresent",
             event_type = "Regional",
@@ -32,22 +39,40 @@ class EventTestCreator(object):
             start_date = datetime.datetime.today() - datetime.timedelta(days=2),
             year = datetime.datetime.now().year,
             venue_address = "123 Fake Street, California, USA",
-            website = "http://www.google.com"
+            website = "http://www.google.com",
+            webcast_json = """[{"type":"ustream","channel":"6540154"}]"""
         )
-        return EventManipulator.createOrUpdate(event)
+        event = EventManipulator.createOrUpdate(event)
+        if not only_event:
+            EventTeamTestCreator.createEventTeams(event)
+
+            event.prepTeams()
+
+            mtc = MatchTestCreator(event)
+            mtc.createCompleteQuals()
+            mtc.createIncompleteQuals()
+        return event
 
     @classmethod
-    def createPastEvent(self):
+    def createPastEvent(self, only_event=False):
         event = Event(
             id = "{}testpast".format(datetime.datetime.now().year),
-            end_date = datetime.datetime.today() - datetime.timedelta(days=1),
+            end_date = datetime.datetime.today() - datetime.timedelta(days=8),
             event_short = "testpast",
             event_type = "Regional",
             first_eid = "5561",
             name = "Past Test Event",
-            start_date = datetime.datetime.today() - datetime.timedelta(days=5),
+            start_date = datetime.datetime.today() - datetime.timedelta(days=12),
             year = datetime.datetime.now().year,
             venue_address = "123 Fake Street, California, USA",
             website = "http://www.google.com"
         )
-        return EventManipulator.createOrUpdate(event)
+        event =  EventManipulator.createOrUpdate(event)
+        if not only_event:
+            EventTeamTestCreator.createEventTeams(event)
+
+            event.prepTeams()
+
+            mtc = MatchTestCreator(event)
+            mtc.createCompleteQuals()
+        return event
---
+++ helpers/match/match_test_creator.py
@@ -0,0 +1,70 @@
+import datetime
+import json
+import random
+
+from helpers.match_manipulator import MatchManipulator
+from models.match import Match
+
+class MatchTestCreator(object):
+
+    def __init__(self, event):
+        self.event = event
+
+    def buildTestMatch(self, comp_level, set_number, match_number, complete):
+        teams = random.sample(self.event.teams, 6)
+
+        if complete:
+            red_score = random.randint(0,100)
+            blue_score = random.randint(0,100)
+        else:
+            red_score = -1
+            blue_score = -1
+
+        alliances = {
+            "red": {
+                "teams": [team.key_name for team in teams[:3]],
+                "score": red_score
+            },
+            "blue": {
+                "teams": [team.key_name for team in teams[3:]],
+                "score": blue_score
+            }
+        }
+
+        if comp_level == "qm":
+            id_string = "{}_{}{}".format(
+                self.event.key_name,
+                comp_level,
+                match_number)
+        else:
+            id_string = "{}_{}{}m{}".format(
+                self.event.key_name,
+                comp_level,
+                set_number,
+                match_number)
+
+        return Match(
+            id = id_string,
+            alliances_json = json.dumps(alliances),
+            comp_level = comp_level,
+            event = self.event.key,
+            game = "frc_2012_rebr",
+            set_number = set_number,
+            match_number = match_number,
+            team_key_names = [team.key_name for team in teams]
+        )
+
+    def createCompleteQuals(self):
+        comp_level = "qm"
+        set_number = 1
+        complete = True
+        matches = [self.buildTestMatch(comp_level, set_number, match_number, complete) for match_number in range(1,10)]
+        MatchManipulator.createOrUpdate(matches)
+
+    def createIncompleteQuals(self):
+        comp_level = "qm"
+        set_number = 1
+        complete = False
+        matches = [self.buildTestMatch(comp_level, set_number, match_number, complete) for match_number in range(11,20)]
+        MatchManipulator.createOrUpdate(matches)
+
