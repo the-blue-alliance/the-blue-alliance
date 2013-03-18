593c103b30ba365c3a59566f69ba8763800058aa
Greg Marra <greg.marra@gmail.com>
2013-03-17 19:27:37 -0700

    Adds Create Test Events button to Admin index page.
    This button does a lot.
    - It sets up a Past Test Event with complete qualifications.
    - It sets up a Present Test Event with some complete and some incomplete qualifications, as well as a webcast.
    - It sets up a Future Test Event
    
    For each Event
    - The Event has EventTeams generated from Teams already in the dev instance, so you can actually click around the site pretty normally.
    - Since there are matches, you can test things like Insights, Match pages, etc.
    - The webcast lets you check the watch now buttons on teams, event pages, and the homepage
    
    The button does not work in the prod environment.
    
    This diff also refactors some of how testing is done to use the new objects that generate this test data.

--- tests/test_event.py
+++ tests/test_event.py
@@ -14,24 +14,22 @@ class TestEventManipulator(unittest2.TestCase):
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
-        self.future_event = EventTestCreator.createFutureEvent()
-        self.present_event = EventTestCreator.createPresentEvent()
-        self.past_event = EventTestCreator.createPastEvent()
+        self.future_event = EventTestCreator.createFutureEvent(only_event=True)
+        self.present_event = EventTestCreator.createPresentEvent(only_event=True)
+        self.past_event = EventTestCreator.createPastEvent(only_event=True)
         
     def tearDown(self):
         self.testbed.deactivate()
 
     def test_datesFuture(self):
         self.assertFalse(self.future_event.now)
-        self.assertTrue(self.future_event.within_a_day)
-        self.assertFalse(self.future_event.withinDays(0,3))
-        self.assertTrue(self.future_event.withinDays(-3,0))
+        self.assertFalse(self.future_event.withinDays(0,8))
+        self.assertTrue(self.future_event.withinDays(-8,0))
 
     def test_datesPast(self):
         self.assertFalse(self.past_event.now)
-        self.assertTrue(self.past_event.within_a_day)
-        self.assertTrue(self.past_event.withinDays(0,3))
-        self.assertFalse(self.past_event.withinDays(-3,0))
+        self.assertTrue(self.past_event.withinDays(0,8))
+        self.assertFalse(self.past_event.withinDays(-8,0))
 
     def test_datesPresent(self):
         self.assertTrue(self.present_event.now)
