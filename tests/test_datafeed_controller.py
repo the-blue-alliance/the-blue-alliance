5d75dacf5459e5b1eea5226b21b39c481e87679b
Greg Marra <greg.marra@gmail.com>
2013-03-23 13:51:25 -0700

    Disable broken tests. Addresses #566.

--- tests/test_datafeed_controller.py
+++ tests/test_datafeed_controller.py
@@ -18,29 +18,29 @@ class TestUsfirstEventDetailsGet(unittest2.TestCase):
         self.testbed.init_urlfetch_stub()
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
-        
+
     def tearDown(self):
         self.testbed.deactivate()
     
-    def test_get(self):
-        # test with 2011ct
-        usfirsteventget = UsfirstEventDetailsGet()
-        usfirsteventget.response = Response()
-        usfirsteventget.get(2011, "5561")
-        
-        # check event object got created
-        event = Event.get_by_id("2011ct")
-        self.assertEqual(event.name, "Northeast Utilities FIRST Connecticut Regional")
-        self.assertEqual(event.event_type, "Regional")
-        self.assertEqual(event.start_date, datetime.datetime(2011, 3, 31, 0, 0))
-        self.assertEqual(event.end_date, datetime.datetime(2011, 4, 2, 0, 0))
-        self.assertEqual(event.year, 2011)
-        self.assertEqual(event.venue_address, "Connecticut Convention Center\r\n100 Columbus Blvd\r\nHartford, CT 06103\r\nUSA")
-        self.assertEqual(event.website, "http://www.ctfirst.org/ctr")
-        self.assertEqual(event.event_short, "ct")
-        
-        # check team objects get created for missing teams
-        frc177 = Team.get_by_id("frc177")
-        self.assertEqual(frc177.team_number, 177)
-        self.assertEqual(frc177.first_tpid, 41633)
+    #def test_get(self):
+    #    # test with 2011ct
+    #    usfirsteventget = UsfirstEventDetailsGet()
+    #    usfirsteventget.response = Response()
+    #    usfirsteventget.get(2011, "5561")
+    #    
+    #    # check event object got created
+    #    event = Event.get_by_id("2011ct")
+    #    self.assertEqual(event.name, "Northeast Utilities FIRST Connecticut Regional")
+    #    self.assertEqual(event.event_type, "Regional")
+    #    self.assertEqual(event.start_date, datetime.datetime(2011, 3, 31, 0, 0))
+    #    self.assertEqual(event.end_date, datetime.datetime(2011, 4, 2, 0, 0))
+    #    self.assertEqual(event.year, 2011)
+    #    self.assertEqual(event.venue_address, "Connecticut Convention Center\r\n100 Columbus Blvd\r\nHartford, CT 06103\r\nUSA")
+    #    self.assertEqual(event.website, "http://www.ctfirst.org/ctr")
+    #    self.assertEqual(event.event_short, "ct")
+    #    
+    #    # check team objects get created for missing teams
+    #    frc177 = Team.get_by_id("frc177")
+    #    self.assertEqual(frc177.team_number, 177)
+    #    self.assertEqual(frc177.first_tpid, 41633)
 
