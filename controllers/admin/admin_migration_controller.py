890d2c34646f2d9fb45f4385e4a9f86b45a7eb84
Eugene Fang <fang.eugene@gmail.com>
2013-07-22 23:56:26 -0700

    remove used event type migration utils. fixes #660

--- controllers/admin/admin_migration_controller.py
+++ controllers/admin/admin_migration_controller.py
@@ -12,26 +12,3 @@ class AdminMigration(LoggedInHandler):
     self._require_admin()
     path = os.path.join(os.path.dirname(__file__), '../../templates/admin/migration.html')
     self.response.out.write(template.render(path, self.template_values))
-
-class AdminMigrationUpdateEventType(LoggedInHandler):
-  def get(self):
-    self._require_admin()
-    
-    events = Event.query().order(Event.year).order(Event.start_date).fetch(10000)
-    for event in events:
-      event.new_event_type = EventType.type_names[EventHelper.parseEventType(event.event_type)]
-    
-    self.template_values.update({"events": events})
-    path = os.path.join(os.path.dirname(__file__), '../../templates/admin/event_type_migration.html')
-    self.response.out.write(template.render(path, self.template_values))
-
-class AdminMigrationUpdateEventTypeAccept(LoggedInHandler):
-  def post(self):
-    self._require_admin()
-    
-    events = Event.query().order(Event.year).order(Event.start_date).fetch(10000)
-    for event in events:
-      event.event_type_enum = EventHelper.parseEventType(event.event_type)
-    ndb.put_multi(events)
-    
-    self.redirect("/admin/events")
