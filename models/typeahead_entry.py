3ef9788c745ddcca363e19be64b509ce145ba9ee
Eugene Fang <fang.eugene@gmail.com>
2013-07-27 21:31:48 -0700

    comment for clarity

--- models/typeahead_entry.py
+++ models/typeahead_entry.py
@@ -3,6 +3,9 @@ from google.appengine.ext import ndb
 
 class TypeaheadEntry(ndb.Model):
     """
+    Model for storing precomputed typeahead entries as keys and values, where
+    TypeaheadEntry.id (set in cron_controller.TypeaheadCalcDo) is the key and
+    TypeaheadEntry.data_json is the value.
     """
     data_json = ndb.StringProperty(required=True, indexed=False)
 
