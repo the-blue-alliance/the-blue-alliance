aa9291b2c7b310b26d595f3bc43b963be133d6ee
Greg Marra <greg.marra@gmail.com>
2013-07-27 17:25:16 -0700

    Revert "build and version info on admin page. fixes #667"
    This reverts commit 098e2aa912ea68a94e0e52f68a5b589fad26bc4a.

--- controllers/admin/admin_main_controller.py
+++ controllers/admin/admin_main_controller.py
@@ -1,6 +1,4 @@
 import os
-import json
-import re
 
 from google.appengine.ext.webapp import template
 
@@ -9,19 +7,6 @@ from controllers.base_controller import LoggedInHandler
 class AdminMain(LoggedInHandler):
     def get(self):
         self._require_admin()
-        
-        # version info
-        fname = os.path.join(os.path.dirname(__file__), '../../version_info.json')
-        with open(fname, 'r') as f:
-            data = json.loads(f.read())
-        self.template_values['git_branch_name'] = data['git_branch_name']
-        commit_hash, commit_author, commit_date, commit_msg, _ = re.split("[\r\n]+", data['git_last_commit'])
-        self.template_values['commit_hash'] = commit_hash
-        self.template_values['commit_author'] = commit_author
-        self.template_values['commit_date'] = commit_date
-        self.template_values['commit_msg'] = commit_msg
-        self.template_values['build_time'] = data['build_time']
-        
         path = os.path.join(os.path.dirname(__file__), '../../templates/admin/index.html')
         self.response.out.write(template.render(path, self.template_values))
 
