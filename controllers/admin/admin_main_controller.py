003b19b89083303cf4dc3d58ff488623b1182a77
Eugene Fang <fang.eugene@gmail.com>
2013-07-29 21:18:34 -0700

    try/catch build info on admin page

--- controllers/admin/admin_main_controller.py
+++ controllers/admin/admin_main_controller.py
@@ -11,16 +11,19 @@ class AdminMain(LoggedInHandler):
         self._require_admin()
         
         # version info
-        fname = os.path.join(os.path.dirname(__file__), '../../version_info.json')
-        with open(fname, 'r') as f:
-            data = json.loads(f.read())
-        self.template_values['git_branch_name'] = data['git_branch_name']
-        commit_hash, commit_author, commit_date, commit_msg, _ = re.split("[\r\n]+", data['git_last_commit'])
-        self.template_values['commit_hash'] = commit_hash
-        self.template_values['commit_author'] = commit_author
-        self.template_values['commit_date'] = commit_date
-        self.template_values['commit_msg'] = commit_msg
-        self.template_values['build_time'] = data['build_time']
+        try:
+            fname = os.path.join(os.path.dirname(__file__), '../../version_info.json')
+            with open(fname, 'r') as f:
+                data = json.loads(f.read())
+            self.template_values['git_branch_name'] = data['git_branch_name']
+            commit_hash, commit_author, commit_date, commit_msg, _ = re.split("[\r\n]+", data['git_last_commit'])
+            self.template_values['commit_hash'] = commit_hash
+            self.template_values['commit_author'] = commit_author
+            self.template_values['commit_date'] = commit_date
+            self.template_values['commit_msg'] = commit_msg
+            self.template_values['build_time'] = data['build_time']
+        except:
+            pass
         
         path = os.path.join(os.path.dirname(__file__), '../../templates/admin/index.html')
         self.response.out.write(template.render(path, self.template_values))
