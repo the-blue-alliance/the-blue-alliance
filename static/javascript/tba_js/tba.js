e15a12f5c2200bb5abe35865275fa49d22801dd6
Eugene Fang <fang.eugene@gmail.com>
2013-08-10 23:55:57 -0700

    smoothscroll to top for phones

--- static/javascript/tba_js/tba.js
+++ static/javascript/tba_js/tba.js
@@ -48,22 +48,29 @@ $('#auth-logoutlink').click(function(){
 // General JS for all pages
 $(document).ready(function(){
 	// Jumping to page section
-    $('.smooth-scroll').bind('click',function(event){
-        var $anchor = $(this);
-        var $navbar_position = $('.navbar').css('top');
-        var $navbar_height = parseInt($('.navbar').css('height'));
-        var $offset = 0;
-        
-        // Takes care of changing navbar size/position due to @media width
-        if ($navbar_position == '0px') {
-          $offset = $navbar_height;
-        }
- 
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - $offset
-        }, 250);
-        event.preventDefault();
-    });
+  $('.smooth-scroll').bind('click',function(event){
+    var $anchor = $(this);
+    var $navbar_position = $('.navbar').css('top');
+    var $navbar_height = parseInt($('.navbar').css('height'));
+    var $offset = 0;
+    
+    // Takes care of changing navbar size/position due to @media width
+    if ($navbar_position == '0px') {
+      $offset = $navbar_height + 10;
+    }
+    
+    var target_offset = $($anchor.attr('href')).offset();
+    if (target_offset == null) {
+      var pixels = 0;
+    } else {
+      var pixels = target_offset.top - $offset;
+    }
+  
+    $('html, body').stop().animate({
+        scrollTop: pixels
+    }, 250);
+    event.preventDefault();
+  });
 	
 	// Fancybox
 	$(".fancybox").fancybox();
