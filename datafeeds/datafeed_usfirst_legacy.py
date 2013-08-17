a6b294899f7bcd822f9951c404a23699d9fa3c20
Eugene Fang <fang.eugene@gmail.com>
2013-08-16 17:01:58 -0700

    usfirst legacy datafeed inherits from ustfirst datafeed

--- datafeeds/datafeed_usfirst_legacy.py
+++ datafeeds/datafeed_usfirst_legacy.py
@@ -6,42 +6,20 @@ from google.appengine.api import urlfetch
 
 import tba_config
 
-from helpers.team_helper import TeamTpidHelper
-
-from datafeeds.datafeed_base import DatafeedBase
+from datafeeds.datafeed_usfirst import DatafeedUsfirst
 from datafeeds.usfirst_legacy_event_details_parser import UsfirstLegacyEventDetailsParser
-from datafeeds.usfirst_event_list_parser import UsfirstEventListParser
-from datafeeds.usfirst_event_rankings_parser import UsfirstEventRankingsParser
-from datafeeds.usfirst_event_awards_parser import UsfirstEventAwardsParser
 from datafeeds.usfirst_legacy_event_teams_parser import UsfirstLegacyEventTeamsParser
-from datafeeds.usfirst_matches_parser import UsfirstMatchesParser
 from datafeeds.usfirst_legacy_team_details_parser import UsfirstLegacyTeamDetailsParser
 
 from models.event import Event
-from models.award import Award
-from models.match import Match
 from models.team import Team
 
 
-class DatafeedUsfirstLegacy(DatafeedBase):
-    EVENT_AWARDS_URL_PATTERN = "http://www2.usfirst.org/%scomp/events/%s/awards.html"  # % (year, event_short)
-    EVENT_DETAILS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=event_details&eid=%s&-session=myarea:%s"
-    EVENT_LIST_REGIONALS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?event_type=FRC&season_FRC=%s"
-    EVENT_RANKINGS_URL_PATTERN = "http://www2.usfirst.org/%scomp/events/%s/rankings.html"  # % (year, event_short)
-    EVENT_TEAMS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=event_teamlist&results_size=250&eid=%s&-session=myarea:%s"
-    EVENT_SHORT_EXCEPTIONS = {
-        "arc": "Archimedes",
-        "cur": "Curie",
-        "gal": "Galileo",
-        "new": "Newton",
-    }
-
-    MATCH_RESULTS_URL_PATTERN = "http://www2.usfirst.org/%scomp/events/%s/matchresults.html"  # % (year, event_short)
-    MATCH_SCHEDULE_QUAL_URL_PATTERN = "http://www2.usfirst.org/%scomp/events/%s/schedulequal.html"
-    MATCH_SCHEDULE_ELIMS_URL_PATTERN = "http://www2.usfirst.org/%scomp/events/%s/scheduleelim.html"
-
+class DatafeedUsfirstLegacy(DatafeedUsfirst):
     SESSION_KEY_GENERATING_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=searchresults&programs=FRC&reports=teams&omit_searchform=1&season_FRC=%s"
 
+    EVENT_DETAILS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=event_details&eid=%s&-session=myarea:%s"
+    EVENT_TEAMS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=event_teamlist&results_size=250&eid=%s&-session=myarea:%s"
     TEAM_DETAILS_URL_PATTERN = "https://my.usfirst.org/myarea/index.lasso?page=team_details&tpid=%s&-session=myarea:%s"
 
     def __init__(self, *args, **kw):
@@ -101,49 +79,6 @@ class DatafeedUsfirstLegacy(DatafeedBase):
             year=event.get("year", None)
         )
 
-    def getEventList(self, year):
-        if type(year) is not int:
-            raise TypeError("year must be an integer")
-        url = self.EVENT_LIST_REGIONALS_URL_PATTERN % year
-        events, _ = self.parse(url, UsfirstEventListParser)
-
-        return [Event(
-            event_type_enum=event.get("event_type_enum", None),
-            event_short="???",
-            first_eid=event.get("first_eid", None),
-            name=event.get("name", None),
-            year=int(year)
-            )
-            for event in events]
-
-    def getEventRankings(self, event):
-        url = self.EVENT_RANKINGS_URL_PATTERN % (event.year,
-            self.EVENT_SHORT_EXCEPTIONS.get(event.event_short, event.event_short))
-        return self.parse(url, UsfirstEventRankingsParser)
-
-    def getEventAwards(self, event):
-
-        def _getTeamKey(award):
-            team = Team.get_by_id('frc' + str(award.get('team_number', None)))
-            if team is not None:
-                return team.key
-            else:
-                return None
-
-        url = self.EVENT_AWARDS_URL_PATTERN % (event.year,
-            self.EVENT_SHORT_EXCEPTIONS.get(event.event_short, event.event_short))
-        awards, _ = self.parse(url, UsfirstEventAwardsParser)
-
-        return [Award(
-            id=Award.renderKeyName(event.key_name, award.get('name')),
-            name=award.get('name', None),
-            team=_getTeamKey(award),
-            awardee=award.get('awardee', None),
-            year=event.year,
-            official_name=award.get('official_name', None),
-            event=event.key)
-            for award in awards]
-
     def getEventTeams(self, year, first_eid):
         """
         Returns a list of team_numbers attending a particular Event
@@ -160,28 +95,6 @@ class DatafeedUsfirstLegacy(DatafeedBase):
             team_number=team.get("team_number", None)
             )
             for team in teams]
-
-    def getMatches(self, event):
-        url = self.MATCH_RESULTS_URL_PATTERN % (event.year,
-            self.EVENT_SHORT_EXCEPTIONS.get(event.event_short, event.event_short))
-        matches, _ = self.parse(url, UsfirstMatchesParser)
-
-        return [Match(
-            id=Match.renderKeyName(
-                event,
-                match.get("comp_level", None),
-                match.get("set_number", 0),
-                match.get("match_number", 0)),
-            event=event.key,
-            game=Match.FRC_GAMES_BY_YEAR.get(event.year, "frc_unknown"),
-            set_number=match.get("set_number", 0),
-            match_number=match.get("match_number", 0),
-            comp_level=match.get("comp_level", None),
-            team_key_names=match.get("team_key_names", None),
-            time_string=match.get("time_string", None),
-            alliances_json=match.get("alliances_json", None)
-            )
-            for match in matches]
 
     def getTeamDetails(self, team):
         if hasattr(team, 'first_tpid'):
@@ -204,13 +117,3 @@ class DatafeedUsfirstLegacy(DatafeedBase):
 
         logging.warning('Null TPID for team %s' % team.team_number)
         return None
-
-    def getTeamsTpids(self, year, skip=0):
-        """
-        Calling this function once establishes all of the Team objects in the Datastore.
-        It does this by calling up USFIRST to search for Tpids. We have to do this in
-        waves to avoid going over the GAE timeout.
-        """
-        # FIXME: This is not proper Datafeed form. -gregmarra 2012 Aug 26
-        # TeamTpidHelper actually creates Team objects.
-        TeamTpidHelper.scrapeTpids(skip, year)
