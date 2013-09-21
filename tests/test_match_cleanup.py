53d4f81ea13c66dc227487506ae8e238e0ee3f2b
Greg Marra <greg.marra@gmail.com>
2013-09-21 13:46:41 -0700

    Rearrange test to try to avoid Travis flakiness.

--- tests/test_match_cleanup.py
+++ tests/test_match_cleanup.py
@@ -1,6 +1,5 @@
 import unittest2
 
-from google.appengine.ext import db
 from google.appengine.ext import testbed
 
 from helpers.match_helper import MatchHelper
@@ -8,43 +7,44 @@ from datafeeds.offseason_matches_parser import OffseasonMatchesParser
 from models.event import Event
 from models.match import Match
 
-
-def setupMatches(csv):
-    with open(csv, 'r') as f:
-        event = Event(
+class TestMatchCleanup(unittest2.TestCase):
+    def setUp(self):
+        self.event = Event(
           id="2013test",
           event_short="test",
           year=2013
         )
 
-        parsed_matches = OffseasonMatchesParser.parse(f.read())
-        matches = [Match(
-            id=Match.renderKeyName(
-                event,
-                match.get("comp_level", None),
-                match.get("set_number", 0),
-                match.get("match_number", 0)),
-            event=event.key,
-            game=Match.FRC_GAMES_BY_YEAR.get(event.year, "frc_unknown"),
-            set_number=match.get("set_number", 0),
-            match_number=match.get("match_number", 0),
-            comp_level=match.get("comp_level", None),
-            team_key_names=match.get("team_key_names", None),
-            alliances_json=match.get("alliances_json", None)
-            )
-            for match in parsed_matches]
-        return matches
-
-
-class TestMatchCleanup(unittest2.TestCase):
-    def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def setupMatches(self, csv):
+        with open(csv, 'r') as f:
+            parsed_matches = OffseasonMatchesParser.parse(f.read())
+            matches = [Match(
+                id=Match.renderKeyName(
+                    self.event,
+                    match.get("comp_level", None),
+                    match.get("set_number", 0),
+                    match.get("match_number", 0)),
+                event=self.event.key,
+                game=Match.FRC_GAMES_BY_YEAR.get(self.event.year, "frc_unknown"),
+                set_number=match.get("set_number", 0),
+                match_number=match.get("match_number", 0),
+                comp_level=match.get("comp_level", None),
+                team_key_names=match.get("team_key_names", None),
+                alliances_json=match.get("alliances_json", None)
+                )
+                for match in parsed_matches]
+            return matches
+
     def test_cleanup(self):
-        matches = setupMatches('test_data/cleanup_matches.csv')
+        matches = self.setupMatches('test_data/cleanup_matches.csv')
         cleaned_matches = MatchHelper.deleteInvalidMatches(matches)
         indices = [9, 12, 26]
         for index in sorted(indices, reverse=True):  # need to delete in reverse order so indices don't get messed up
