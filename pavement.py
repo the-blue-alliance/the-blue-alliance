4a5350c6fabf858856859a6d189f99aace118058
Eugene Fang <fang.eugene@gmail.com>
2013-10-15 10:46:13 -0700

    add more tests to test_fast

--- pavement.py
+++ pavement.py
@@ -74,6 +74,8 @@ def test_fast():
   sh("python run_tests.py --test_pattern=test_event.py")
   sh("python run_tests.py --test_pattern=test_match_cleanup.py")
   sh("python run_tests.py --test_pattern=test_event_group_by_week.py")
+  sh("python run_tests.py --test_pattern=test_event_team_repairer.py")
+  sh("python run_tests.py --test_pattern=test_event_team_updater.py")
 
 
 def setup_function():
