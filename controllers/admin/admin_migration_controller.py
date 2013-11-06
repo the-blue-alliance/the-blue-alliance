d390c7c2a667eb0aa1190ccf3054779ad0577664
Eugene Fang <fang.eugene@gmail.com>
2013-11-05 22:05:10 -0800

    migration to reindex accounts

--- controllers/admin/admin_migration_controller.py
+++ controllers/admin/admin_migration_controller.py
@@ -5,6 +5,7 @@ from consts.event_type import EventType
 from controllers.base_controller import LoggedInHandler
 from models.event import Event
 from helpers.event_helper import EventHelper
+from models.account import Account
 
 
 class AdminMigration(LoggedInHandler):
@@ -12,3 +13,12 @@ class AdminMigration(LoggedInHandler):
     self._require_admin()
     path = os.path.join(os.path.dirname(__file__), '../../templates/admin/migration.html')
     self.response.out.write(template.render(path, self.template_values))
+
+
+class AdminMigrationReindexAccount(LoggedInHandler):
+  def get(self):
+    self._require_admin()
+    account_keys = Account.query().fetch(1000, keys_only=True)
+    accounts = ndb.get_multi(account_keys)
+    ndb.put_multi(accounts)
+    self.response.out.write("DONE!")
