c3b3cb3e00c6daa4e6d91fc62eb41a604f80150f
Eugene Fang <fang.eugene@gmail.com>
2013-11-09 11:26:10 -0800

    fix facebook buttons

--- static/javascript/tba_js/tba.js
+++ static/javascript/tba_js/tba.js
@@ -1,12 +1,3 @@
-// Load the SDK Asynchronously
-(function(d){
-	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement('script'); js.id = id; js.async = true;
-	js.src = "//connect.facebook.net/en_US/all.js";
-	ref.parentNode.insertBefore(js, ref);
-}(document));
-
 // General JS for all pages
 $(document).ready(function(){
 	// Jumping to page section
