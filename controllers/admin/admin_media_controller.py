08de67b7376ee5e277b2cf583248a72b9bfd4114
Eugene Fang <fang.eugene@gmail.com>
2013-11-10 10:17:56 -0800

    refactor MediaParser for clarity and easier testing

--- controllers/admin/admin_media_controller.py
+++ controllers/admin/admin_media_controller.py
@@ -6,7 +6,7 @@ from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
 
 from controllers.base_controller import LoggedInHandler
-from helpers.media_helper import MediaHelper
+from helpers.media_helper import MediaParser
 from helpers.media_manipulator import MediaManipulator
 from models.media import Media
 from models.team import Team
@@ -36,22 +36,23 @@ class AdminMediaAdd(LoggedInHandler):
     def post(self):
         self._require_admin()
 
-        media_dict = MediaHelper.partial_media_dict_from_url(self.request.get('media_url').strip())
-        year_str = self.request.get('year')
-        if year_str == '':
-            year = None
-        else:
-            year = int(year_str.strip())
+        media_dict = MediaParser.partial_media_dict_from_url(self.request.get('media_url').strip())
+        if media_dict is not None:
+            year_str = self.request.get('year')
+            if year_str == '':
+                year = None
+            else:
+                year = int(year_str.strip())
 
-        media = Media(
-            id=Media.render_key_name(media_dict['media_type_enum'], media_dict['media_id']),
-            media_id=media_dict['media_id'],
-            media_type_enum=media_dict['media_type_enum'],
-            details_json=media_dict.get('details_json', None),
-            year=year,
-            references=[ndb.Key(self.REFERENCE_MAP[self.request.get('reference_type')],
-                                self.request.get('reference_key'))],
-        )
-        MediaManipulator.createOrUpdate(media)
+            media = Media(
+                id=Media.render_key_name(media_dict['media_type_enum'], media_dict['media_id']),
+                media_id=media_dict['media_id'],
+                media_type_enum=media_dict['media_type_enum'],
+                details_json=media_dict.get('details_json', None),
+                year=year,
+                references=[ndb.Key(self.REFERENCE_MAP[self.request.get('reference_type')],
+                                    self.request.get('reference_key'))],
+            )
+            MediaManipulator.createOrUpdate(media)
 
         self.redirect(self.request.get('originating_url'))
--- helpers/media_helper.py
+++ helpers/media_helper.py
@@ -8,9 +8,9 @@ from BeautifulSoup import BeautifulSoup
 from consts.media_type import MediaType
 
 
-class MediaHelper(object):
+class MediaParser(object):
     @classmethod
-    def partial_media_dict_from_url(self, url):
+    def partial_media_dict_from_url(cls, url):
         """
         Takes a url, and turns it into a partial Media object dict
         """
@@ -18,37 +18,69 @@ class MediaHelper(object):
 
         if 'chiefdelphi.com/media/photos/' in url:
             media_dict['media_type_enum'] = MediaType.CD_PHOTO_THREAD
-            media_dict['media_id'] = re.match(r'.*chiefdelphi.com\/media\/photos\/(\d+)', url).group(1)
+            media_id = cls._parse_cdphotothread_media_id(url)
+            if media_id is None:
+                logging.warning("Failed to determine media_id from url: {}".format(url))
+                return None
+            media_dict['media_id'] = media_id
 
             urlfetch_result = urlfetch.fetch(url, deadline=10)
             if urlfetch_result.status_code != 200:
                 logging.warning('Unable to retrieve url: {}'.format(url))
                 return None
 
-            # parse html for the image url
-            soup = BeautifulSoup(urlfetch_result.content,
-                                 convertEntities=BeautifulSoup.HTML_ENTITIES)
-            image_url = "http://www.chiefdelphi.com{}".format(soup.find('a', {'target': 'cdmLargePic'})['href'])
-            link_url = "http://www.chiefdelphi.com/media/photos/{}".format(media_dict['media_id'])
+            image_url = cls._parse_cdphotothread_image_url(urlfetch_result.content)
+            if image_url is None:
+                logging.warning("Failed to determine image_url from the page: {}".format(url))
+                return None
             details = {'image_url': image_url,
-                       'link_url': link_url}
+                       'link_url': "http://www.chiefdelphi.com/media/photos/{}".format(media_dict['media_id'])}
             media_dict['details_json'] = json.dumps(details)
         elif 'youtube.com' in url:
             media_dict['media_type_enum'] = MediaType.YOUTUBE
-
-            youtube_id = None
-            regex1 = re.match(r".*youtu\.be\/(.*)", url)
-            if regex1 is not None:
-                youtube_id = regex1.group(1)
-            else:
-                regex2 = re.match(r".*v=([a-zA-Z0-9_-]*)", url)
-                if regex2 is not None:
-                    youtube_id = regex2.group(1)
-            if youtube_id is None:
-                logging.warning("Unable to get YouTube id from url: {}".format(url))
-            media_dict['media_id'] = youtube_id
+            media_id = cls._parse_youtube_media_id(url)
+            if media_id is None:
+                logging.warning("Failed to determine media_id from url: {}".format(url))
+                return None
+            media_dict['media_id'] = media_id
         else:
-            logging.warning("Failed to determine media type from url: ".format(url))
+            logging.warning("Failed to determine media type from url: {}".format(url))
             return None
 
         return media_dict
+
+    @classmethod
+    def _parse_cdphotothread_media_id(cls, url):
+        regex1 = re.match(r'.*chiefdelphi.com\/media\/photos\/(\d+)', url)
+        if regex1 is not None:
+            return regex1.group(1)
+        else:
+            return None
+
+    @classmethod
+    def _parse_cdphotothread_image_url(cls, html):
+        # parse html for the image url
+        soup = BeautifulSoup(html,
+                             convertEntities=BeautifulSoup.HTML_ENTITIES)
+        element = soup.find('a', {'target': 'cdmLargePic'})
+        if element is not None:
+            partial_url = element['href']
+        else:
+            return None
+        return "http://www.chiefdelphi.com{}".format(partial_url)
+
+    @classmethod
+    def _parse_youtube_media_id(cls, url):
+        youtube_id = None
+        regex1 = re.match(r".*youtu\.be\/(.*)", url)
+        if regex1 is not None:
+            youtube_id = regex1.group(1)
+        else:
+            regex2 = re.match(r".*v=([a-zA-Z0-9_-]*)", url)
+            if regex2 is not None:
+                youtube_id = regex2.group(1)
+
+        if youtube_id is None:
+            return None
+        else:
+            return youtube_id
