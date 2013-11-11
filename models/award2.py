8a6471449ef0dd4104b1b1da32ed3ad4edfaa4de
Eugene Fang <fang.eugene@gmail.com>
2013-11-10 23:50:05 -0800

    Add comments

--- models/award2.py
+++ models/award2.py
@@ -11,16 +11,19 @@ from models.team import Team
 class Award2(ndb.Model):
     """
     Awards represent FIRST Robotics Competition awards given out at an event.
+    key_name is formatted as: <event_key_name>_<award_type_enum>
+    If multiple recipients win the same award at the same event (such as
+    Winner or Dean's List), they show up under the repeated properties.
     """
 
-    name_str = ndb.StringProperty(required=True, indexed=False)
+    name_str = ndb.StringProperty(required=True, indexed=False)  # award name that shows up on USFIRST Pages. May vary for the same award type.
     award_type_enum = ndb.IntegerProperty(required=True)
     year = ndb.IntegerProperty(required=True)  # year the award was awarded
     event = ndb.KeyProperty(kind=Event, required=True)  # event at which the award was awarded
     event_type_enum = ndb.IntegerProperty(required=True)  # needed to query for awards from events of a certain event type
 
-    team_list = ndb.KeyProperty(kind=Team, repeated=True)  # team(s) that won the award (if applicable)
-    recipient_json_list = ndb.StringProperty(repeated=True)  # JSON dict(s) with team number and/or awardee
+    team_list = ndb.KeyProperty(kind=Team, repeated=True)  # key of team(s) that won the award (if applicable)
+    recipient_json_list = ndb.StringProperty(repeated=True)  # JSON dict(s) with team_number and/or awardee
 
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
