f87db5881dd1a621dab7c1b824b6d5f087cfdfb4
Eugene Fang <fang.eugene@gmail.com>
2013-11-23 11:49:38 -0800

    Use same year picker on insights pages as event list pages

--- controllers/insights_controller.py
+++ controllers/insights_controller.py
@@ -12,7 +12,8 @@ from base_controller import CacheableHandler
 
 from models.insight import Insight
 
-VALID_YEARS = [2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002]
+MAX_YEAR = 2013
+VALID_YEARS = list(reversed(range(1992, MAX_YEAR + 1)))
 
 
 class InsightsOverview(CacheableHandler):
