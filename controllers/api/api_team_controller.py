9faa6e7b0eb5c26b113790633b5ec0025b6016b3
Eugene Fang <fang.eugene@gmail.com>
2013-11-25 23:58:48 -0800

    refactor complex queries for team details

--- controllers/api/api_team_controller.py
+++ controllers/api/api_team_controller.py
@@ -4,18 +4,13 @@ import webapp2
 
 from datetime import datetime
 
-from google.appengine.api import memcache
-from google.appengine.ext import ndb
-
-import tba_config
 from controllers.api.api_base_controller import ApiBaseController
 
 from helpers.model_to_dict import ModelToDict
+from helpers.data_fetcher import DataFetcher
 
-from models.award import Award
-from models.event_team import EventTeam
-from models.match import Match
 from models.team import Team
+
 
 class ApiTeamController(ApiBaseController):
     LONG_CACHE_EXPIRATION = 60 * 60 * 24
@@ -36,52 +31,19 @@ class ApiTeamController(ApiBaseController):
     def _render(self, team_key, year=None):
         self._write_cache_headers(61)
 
-        @ndb.tasklet
-        def get_event_matches_async(event_team_key):
-            event_team = yield event_team_key.get_async()
-            if (event_team.year == self.year):
-                event = yield event_team.event.get_async()
-                if not event.start_date:
-                    event.start_date = datetime.datetime(self.year, 12, 31)  # unknown goes last
-                matches_keys = yield Match.query(
-                  Match.event == event.key, Match.team_key_names == self.team.key_name).fetch_async(500, keys_only=True)
-                matches = yield ndb.get_multi_async(matches_keys)
-                raise ndb.Return((event, matches))
-            raise ndb.Return(None)
-
-        @ndb.tasklet
-        def get_events_matches_async():
-            event_team_keys = yield EventTeam.query(EventTeam.team == self.team.key).fetch_async(1000, keys_only=True)
-            events_matches = yield map(get_event_matches_async, event_team_keys)
-            events_matches = filter(None, events_matches)
-            raise ndb.Return(events_matches)
-
-        @ndb.tasklet
-        def get_awards_async():
-            award_keys = yield Award.query(Award.year == self.year, Award.team_list == self.team.key).fetch_async(500, keys_only=True)
-            awards = yield ndb.get_multi_async(award_keys)
-            raise ndb.Return(awards)
-
-        @ndb.toplevel
-        def get_events_matches_awards():
-            events_matches, awards = yield get_events_matches_async(), get_awards_async()
-            raise ndb.Return(events_matches, awards)
-
         self.team = Team.get_by_id(self.team_key)
         if self.team is None:
             self._errors = json.dumps({"404": "%s team not found" % self.team_key})
             self.abort(404)
-        events_matches, awards = get_events_matches_awards()
-        events_matches = sorted(events_matches, key=lambda (e, _): e.start_date)
+
+        events_sorted, matches_by_event_key, awards_by_event_key, _ = DataFetcher.fetch_team_data(self.team, self.year)
         team_dict = ModelToDict.teamConverter(self.team)
 
-
         team_dict["events"] = list()
-        for event, matches in events_matches:
-            event_dict = ModelToDict.eventConverter(event)
-            event_dict["matches"] = [ModelToDict.matchConverter(match) for match in matches if match.event == event.key]
-            event_dict["awards"] = [ModelToDict.awardConverter(award) for award in awards if award.event == event.key]
+        for e in events_sorted:
+            event_dict = ModelToDict.eventConverter(e)
+            event_dict["matches"] = [ModelToDict.matchConverter(match) for match in matches_by_event_key.get(e.key, [])]
+            event_dict["awards"] = [ModelToDict.awardConverter(award) for award in awards_by_event_key.get(e.key, [])]
             team_dict["events"].append(event_dict)
-
 
         return json.dumps(team_dict, ensure_ascii=True)
--- controllers/team_controller.py
+++ controllers/team_controller.py
@@ -12,6 +12,7 @@ from base_controller import CacheableHandler
 from helpers.event_helper import EventHelper
 from helpers.match_helper import MatchHelper
 from helpers.award_helper import AwardHelper
+from helpers.data_fetcher import DataFetcher
 from models.event import Event
 from models.event_team import EventTeam
 from models.match import Match
@@ -121,59 +122,11 @@ class TeamDetail(CacheableHandler):
         super(TeamDetail, self).get(team_number, year, explicit_year)
 
     def _render(self, team_number, year=None, explicit_year=False):
-        @ndb.tasklet
-        def get_events_and_matches_async():
-            event_team_keys = yield EventTeam.query(EventTeam.team == team.key).fetch_async(1000, keys_only=True)
-            event_teams = yield ndb.get_multi_async(event_team_keys)
-            event_keys = []
-            for event_team in event_teams:
-                years.add(event_team.year)  # years is a "global" variable (defined below). Doing this removes the complexity of having to propagate the years up through the tasklet call chain.
-                if event_team.year == year:
-                    event_keys.append(event_team.event)
-            events, matches = yield ndb.get_multi_async(event_keys), get_matches_async(event_keys)
-            raise ndb.Return((events, matches))
-
-        @ndb.tasklet
-        def get_matches_async(event_keys):
-            if event_keys == []:
-                raise ndb.Return([])
-            match_keys = yield Match.query(
-                Match.event.IN(event_keys), Match.team_key_names == team.key_name).fetch_async(500, keys_only=True)
-            matches = yield ndb.get_multi_async(match_keys)
-            raise ndb.Return(matches)
-
-        @ndb.tasklet
-        def get_awards_async():
-            award_keys = yield Award.query(Award.year == year, Award.team_list == team.key).fetch_async(500, keys_only=True)
-            awards = yield ndb.get_multi_async(award_keys)
-            raise ndb.Return(awards)
-
-        @ndb.toplevel
-        def get_events_matches_awards():
-            (events, matches), awards = yield get_events_and_matches_async(), get_awards_async()
-            raise ndb.Return(events, matches, awards)
-
         team = Team.get_by_id("frc" + team_number)
         if not team:
             return self.redirect("/error/404")
 
-        years = set()
-        events, matches, awards = get_events_matches_awards()
-        years = sorted(years)
-        events = sorted(events, key=lambda e: e.start_date if e.start_date else datetime.datetime(year, 12, 31))  # unknown goes last
-
-        matches_by_event_key = {}
-        for match in matches:
-            if match.event in matches_by_event_key:
-                matches_by_event_key[match.event].append(match)
-            else:
-                matches_by_event_key[match.event] = [match]
-        awards_by_event_key = {}
-        for award in awards:
-            if award.event in awards_by_event_key:
-                awards_by_event_key[award.event].append(award)
-            else:
-                awards_by_event_key[award.event] = [award]
+        events_sorted, matches_by_event_key, awards_by_event_key, valid_years = DataFetcher.fetch_team_data(team, year, return_valid_years=True)
 
         participation = []
         year_wlt_list = []
@@ -181,7 +134,7 @@ class TeamDetail(CacheableHandler):
         current_event = None
         matches_upcoming = None
         short_cache = False
-        for e in events:
+        for e in events_sorted:
             event_matches = matches_by_event_key.get(e.key, [])
             event_awards = AwardHelper.organizeAwards(awards_by_event_key.get(e.key, []))
             matches_organized = MatchHelper.organizeMatches(event_matches)
@@ -225,7 +178,7 @@ class TeamDetail(CacheableHandler):
                             "team": team,
                             "participation": participation,
                             "year": year,
-                            "years": years,
+                            "years": valid_years,
                             "year_wlt": year_wlt,
                             "current_event": current_event,
                             "matches_upcoming": matches_upcoming}
