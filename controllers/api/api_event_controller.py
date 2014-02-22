d074f6131754e020780a73dd9b2d4f66362964f2
Brandon Dean <deanbrandon4@gmail.com>
2014-02-22 15:37:36 -0500

    Event teams and matches API requests

--- controllers/api/api_event_controller.py
+++ controllers/api/api_event_controller.py
@@ -2,12 +2,8 @@ import json
 import logging
 import webapp2
 
-from datetime import datetime
-
-from google.appengine.api import memcache
 from google.appengine.ext import ndb
 
-import tba_config
 from controllers.api.api_base_controller import ApiBaseController
 
 from helpers.model_to_dict import ModelToDict
@@ -29,17 +25,64 @@ class ApiEventController(ApiBaseController):
     def _validators(self):
         return [("event_id_validator", self.event_key)]
 
+    def set_event(self):
+        event = Event.get_by_id(self.event_key)
+        if event is None:
+            self._errors = json.dumps({"404": "%s event not found" % self.event_key})
+            self.abort(404)
+        self.event = event
+
+
     def _track_call(self, event_key):
         self._track_call_defer('event', event_key)
 
     def _render(self, event_key):
-        self._set_cache_header_length(61)
-
-        self.event = Event.get_by_id(self.event_key)
-        if self.event is None:
-            self._errors = json.dumps({"404": "%s event not found" % self.event_key})
-            self.abort(404)
+        self._set_cache_header_length(60 * 60)
+        self.set_event()
 
         event_dict = ModelToDict.eventConverter(self.event)
 
         return json.dumps(event_dict, ensure_ascii=True)
+
+class ApiEventTeamController(ApiEventController):
+    LONG_CACHE_EXPIRATION = 60 * 60 * 24
+    SHORT_CACHE_EXPIRATION = 60 * 5
+
+    def __init__(self, *args, **kw):
+        super(ApiEventTeamController, self).__init__(*args, **kw)
+        self._cache_key = "apiv2_event_team_controller_{}".format(self.event_key)
+        self._cache_expiration = self.LONG_CACHE_EXPIRATION
+        self._cache_version = 2
+
+    def _render(self, event_key):
+        self._set_cache_header_length(60 * 60)
+        self.set_event()
+
+        teams = self.event.teams
+        team_dicts = list()
+        for team in teams:
+            team_dicts.append(ModelToDict.teamConverter(team))
+
+        return json.dumps(team_dicts, ensure_ascii=True)
+
+class ApiEventMatchController(ApiEventController):
+    LONG_CACHE_EXPIRATION = 60 * 60 * 24
+    SHORT_CACHE_EXPIRATION = 60 * 5
+
+    def __init__(self, *args, **kw):
+        super(ApiEventMatchController, self).__init__(*args, **kw)
+        self._cache_key = "apiv2_event_match_controller_{}".format(self.event_key)
+        self._cache_expiration = self.SHORT_CACHE_EXPIRATION
+        self._cache_version = 2
+
+
+    def _render(self, event_key):
+        self._set_cache_header_length(61)
+        self.set_event()
+
+        matches = self.event.matches
+        match_dicts = list()
+        for match in matches:
+            match_dicts.append(ModelToDict.matchConverter(match))
+
+        return json.dumps(match_dicts, ensure_ascii=True)
--- tests/test_apiv2_event_controller.py
+++ tests/test_apiv2_event_controller.py
@@ -11,6 +11,8 @@ from google.appengine.ext import testbed
 from consts.event_type import EventType
 
 from controllers.api.api_event_controller import ApiEventController
+from controllers.api.api_event_controller import ApiEventTeamController
+from controllers.api.api_event_controller import ApiEventMatchController
 
 from models.event import Event
 from models.event_team import EventTeam
@@ -61,4 +63,131 @@ class TestEventApiController(unittest2.TestCase):
 
         event_dict = json.loads(response.body)
         self.assertEventJson(event_dict)
+
+class TestEventTeamApiController(unittest2.TestCase):
+
+    def setUp(self):
+        app = webapp2.WSGIApplication([webapp2.Route(r'/<event_key:>', ApiEventTeamController, methods=['GET'])], debug=True)
+        self.testapp = webtest.TestApp(app)
+
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_urlfetch_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_taskqueue_stub()
+
+        self.event = Event(
+                id="2010sc",
+                name="Palmetto Regional",
+                event_type_enum=EventType.REGIONAL,
+                short_name="Palmetto",
+                event_short="sc",
+                year=2010,
+                end_date=datetime(2010, 03, 27),
+                official=True,
+                location='Clemson, SC',
+                start_date=datetime(2010, 03, 24),
+        )
+        self.event.put()
+
+        self.team = Team(
+                id="frc281",
+                name="Michelin / Caterpillar / Greenville Technical College /\
+                jcpenney / Baldor / ASME / Gastroenterology Associates /\
+                Laserflex South & Greenville County Schools & Greenville\
+                Technical Charter High School",
+                team_number=281,
+                nickname="EnTech GreenVillians",
+                address="Greenville, SC, USA",
+                website="www.entech.org",
+        )
+        self.team.put()
+
+        self.event_team = EventTeam(
+                team=self.team.key,
+                event=self.event.key,
+                year=datetime.now().year
+        )
+        self.event_team.put()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def assertTeamJson(self, team):
+        team = team[0]
+        self.assertEqual(team["key"], self.team.key_name)
+        self.assertEqual(team["team_number"], self.team.team_number)
+        self.assertEqual(team["nickname"], self.team.nickname)
+        self.assertEqual(team["location"], self.team.location)
+        self.assertEqual(team["locality"], "Greenville")
+        self.assertEqual(team["country_name"], "USA")
+        self.assertEqual(team["region"], "SC")
+        self.assertEqual(team["website"], self.team.website)
+
+
+    def testEventTeamApi(self):
+        response = self.testapp.get('/2010sc', headers={"X-TBA-App-Id": "tba-tests:event-controller-test:v01"})
+
+        team_dict = json.loads(response.body)
+        self.assertTeamJson(team_dict)
+
+class TestEventMatchApiController(unittest2.TestCase):
+
+    def setUp(self):
+        app = webapp2.WSGIApplication([webapp2.Route(r'/<event_key:>', ApiEventMatchController, methods=['GET'])], debug=True)
+        self.testapp = webtest.TestApp(app)
+
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_urlfetch_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_taskqueue_stub()
+
+        self.event = Event(
+                id="2010sc",
+                name="Palmetto Regional",
+                event_type_enum=EventType.REGIONAL,
+                short_name="Palmetto",
+                event_short="sc",
+                year=2010,
+                end_date=datetime(2010, 03, 27),
+                official=True,
+                location='Clemson, SC',
+                start_date=datetime(2010, 03, 24),
+        )
+        self.event.put()
+
+        self.match = Match(
+            id="2010sc_qm1",
+            alliances_json="""{"blue": {"score": 57, "teams": ["frc3464", "frc20", "frc1073"]}, "red": {"score": 74, "teams": ["frc281", "frc571", "frc176"]}}""",
+            comp_level="qm",
+            event=self.event.key,
+            game="frc_2012_rebr",
+            set_number=1,
+            match_number=1,
+            team_key_names=[u'frc281', u'frc571', u'frc176', u'frc3464', u'frc20', u'frc1073'],
+
+        )
+        self.match.put()
+
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def assertMatchJson(self, matches):
+        match = matches[0]
+        self.assertEqual(str(match["key"]), self.match.key.string_id())
+        self.assertEqual(match["comp_level"], self.match.comp_level)
+        self.assertEqual(match["event_key"], self.match.event.string_id())
+        self.assertEqual(match["set_number"], self.match.set_number)
+        self.assertEqual(match["match_number"], self.match.match_number)
+
+
+    def testEventMatchApi(self):
+        response = self.testapp.get('/2010sc', headers={"X-TBA-App-Id": "tba-tests:event-controller-test:v01"})
+
+        match_json = json.loads(response.body)
+        self.assertMatchJson(match_json)
 
