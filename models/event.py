eaece2ba127fae25066f1315f5d8c29fd53ad670
Eugene Fang <fang.eugene@gmail.com>
2014-02-23 15:03:40 -0800

    Fix event validator

--- models/event.py
+++ models/event.py
@@ -204,7 +204,7 @@ class Event(ndb.Model):
 
     @classmethod
     def validate_key_name(self, event_key):
-        key_name_regex = re.compile(r'^[1-9]\d{3}[a-z]+$')
+        key_name_regex = re.compile(r'^[1-9]\d{3}[a-z]+[1-9]?$')
         match = re.match(key_name_regex, event_key)
         return True if match else False
 
