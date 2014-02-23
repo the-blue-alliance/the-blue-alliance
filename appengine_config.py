4a018e45fd70f0f29b78b2a3cadabdcb46a49479
Eugene Fang <fang.eugene@gmail.com>
2014-02-23 01:43:07 -0800

    cleanup

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,7 @@
-import os
-
 appstats_CALC_RPC_COSTS = True
 
 
 def webapp_add_wsgi_middleware(app):
-   from google.appengine.ext.appstats import recording
-   app = recording.appstats_wsgi_middleware(app)
-   return app
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
