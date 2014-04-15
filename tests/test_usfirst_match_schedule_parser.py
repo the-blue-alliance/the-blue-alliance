76607c7668cfc6e42d1ed68b815aae76439ff18f
Eugene Fang <fang.eugene@gmail.com>
2014-04-15 11:29:41 -0700

    2014 champs parser tests. fixes #978

--- tests/test_usfirst_match_schedule_parser.py
+++ tests/test_usfirst_match_schedule_parser.py
@@ -33,3 +33,18 @@ class TestUsfirstMatchScheduleParser(unittest2.TestCase):
         self.assertEqual(match["team_key_names"], [u'frc192', u'frc604', u'frc971', u'frc649', u'frc114', u'frc1388'])
         self.assertEqual(match["alliances_json"], """{"blue": {"score": -1, "teams": ["frc649", "frc114", "frc1388"]}, "red": {"score": -1, "teams": ["frc192", "frc604", "frc971"]}}""")
         self.assertEqual(match["time_string"], "2:58 PM")
+
+    def test_parse_2014cmp_elim(self):
+        with open('test_data/usfirst_html/usfirst_event_match_schedule_2014cmp_elim.html', 'r') as f:
+            matches, _ = UsfirstMatchScheduleParser.parse(f.read())
+
+        self.assertEqual(len(matches), 7)
+
+        # Test 2014cmp_sf2-2
+        match = matches[3]
+        self.assertEqual(match["comp_level"], "sf")
+        self.assertEqual(match["set_number"], 2)
+        self.assertEqual(match["match_number"], 2)
+        self.assertEqual(match["team_key_names"], [u'frc16', u'frc5', u'frc9', u'frc2', u'frc40', u'frc1'])
+        self.assertEqual(match["alliances_json"], """{"blue": {"score": -1, "teams": ["frc2", "frc40", "frc1"]}, "red": {"score": -1, "teams": ["frc16", "frc5", "frc9"]}}""")
+        self.assertEqual(match["time_string"], "5:29 PM")
