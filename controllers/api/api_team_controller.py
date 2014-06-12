7593ba09ddc76fca32bc62ed759833157a117c19
Eugene Fang <fang.eugene@gmail.com>
2014-06-12 12:15:37 -0700

    Only return team model for team API call. Towards #1012

--- controllers/api/api_team_controller.py
+++ controllers/api/api_team_controller.py
@@ -14,6 +14,7 @@ from helpers.media_helper import MediaHelper
 from models.media import Media
 from models.team import Team
 
+
 class ApiTeamController(ApiBaseController):
     CACHE_KEY_FORMAT = "apiv2_team_controller_{}_{}"  # (team_key, year)
     CACHE_VERSION = 0
@@ -42,27 +43,19 @@ class ApiTeamController(ApiBaseController):
             self._errors = json.dumps({"404": "%s team not found" % self.team_key})
             self.abort(404)
 
-        events_sorted, matches_by_event_key, awards_by_event_key, _ = TeamDetailsDataFetcher.fetch(self.team, self.year)
         team_dict = ModelToDict.teamConverter(self.team)
-
-        team_dict["events"] = list()
-        for event in events_sorted:
-            event_dict = ModelToDict.eventConverter(event)
-            event_dict["matches"] = [ModelToDict.matchConverter(match) for match in matches_by_event_key.get(event.key, [])]
-            event_dict["awards"] = [ModelToDict.awardConverter(award) for award in awards_by_event_key.get(event.key, [])]
-            team_dict["events"].append(event_dict)
 
         return json.dumps(team_dict, ensure_ascii=True)
 
-class ApiTeamMediaController(ApiBaseController):
 
+class ApiTeamMediaController(ApiBaseController):
     CACHE_KEY_FORMAT = "apiv2_team_media_controller_{}_{}"  # (team, year)
     CACHE_VERSION = 0
 
     def __init__(self, *args, **kw):
         super(ApiTeamMediaController, self).__init__(*args, **kw)
         self.team_key = self.request.route_kwargs["team_key"]
-        self.year = int(self.request.route_kwargs.get("year") or datetime.now().year)     
+        self.year = int(self.request.route_kwargs.get("year") or datetime.now().year)
         self._cache_key = self.CACHE_KEY_FORMAT.format(self.team_key, self.year)
 
     @property
@@ -72,21 +65,21 @@ class ApiTeamMediaController(ApiBaseController):
     def _track_call(self, team_key, year=None):
         api_label = team_key
         if year is not None:
-            api_label += '/{}'.format(year) 
+            api_label += '/{}'.format(year)
         self._track_call_defer('team/media', api_label)
 
     def _render(self, team_key, year=None):
         self.team = Team.get_by_id(team_key)
         if self.team is None:
             self._errors = json.dumps({"404": "%s team not found" % team_key})
-            self.abort(404) 
+            self.abort(404)
 
         if year is None:
             year = self.year
         else:
             year = int(year)
-        
+
         media_keys = Media.query(Media.references == self.team.key, Media.year == year).fetch(500, keys_only=True)
         medias = ndb.get_multi(media_keys)
-        media_list = [ModelToDict.mediaConverter(media) for media in medias] 
+        media_list = [ModelToDict.mediaConverter(media) for media in medias]
         return json.dumps(media_list, ensure_ascii=True)
--- tests/test_apiv2_team_controller.py
+++ tests/test_apiv2_team_controller.py
@@ -22,8 +22,8 @@ from models.match import Match
 from models.media import Media
 from models.team import Team
 
-class TestTeamApiController(unittest2.TestCase):
 
+class TestTeamApiController(unittest2.TestCase):
     def setUp(self):
         app = webapp2.WSGIApplication([webapp2.Route(r'/<team_key:>', ApiTeamController, methods=['GET'])], debug=True)
         self.testapp = webtest.TestApp(app)
@@ -49,52 +49,6 @@ class TestTeamApiController(unittest2.TestCase):
 
         self.team.put()
 
-        self.event = Event(
-                id="2010sc",
-                name="Palmetto Regional",
-                event_type_enum=EventType.REGIONAL,
-                short_name="Palmetto",
-                event_short="sc",
-                year=datetime.now().year,
-                end_date=datetime(2010, 03, 27),
-                official=True,
-                location='Clemson, SC',
-                start_date=datetime(2010, 03, 24),
-        )
-
-        self.event.put()
-
-        self.event_team = EventTeam(
-                team=self.team.key,
-                event=self.event.key,
-                year=datetime.now().year
-        )
-        self.event_team.put()
-
-        self.match = Match(
-            id="2010sc_qm1",
-            alliances_json="""{"blue": {"score": 57, "teams": ["frc3464", "frc20", "frc1073"]}, "red": {"score": 74, "teams": ["frc281", "frc571", "frc176"]}}""",
-            comp_level="qm",
-            event=self.event.key,
-            game="frc_2012_rebr",
-            set_number=1,
-            match_number=1,
-            team_key_names=[u'frc281', u'frc571', u'frc176', u'frc3464', u'frc20', u'frc1073'],
-
-        )
-        self.match.put()
-
-        self.award = Award(
-            name_str = "Engineering Inspiration",
-            award_type_enum = AwardType.ENGINEERING_INSPIRATION,
-            year=datetime.now().year,
-            event = self.event.key,
-            event_type_enum = EventType.REGIONAL,
-            team_list = [self.team.key],
-            recipient_json_list=[json.dumps({'team_number': 281, 'awardee': None})],
-        )
-        self.award.put()
-
     def tearDown(self):
         self.testbed.deactivate()
 
@@ -108,40 +62,14 @@ class TestTeamApiController(unittest2.TestCase):
         self.assertEqual(team["region"], "SC")
         self.assertEqual(team["website"], self.team.website)
 
-    def assertEventJson(self, event):
-        self.assertEqual(event["key"], self.event.key_name)
-        self.assertEqual(event["name"], self.event.name)
-        self.assertEqual(event["short_name"], self.event.short_name)
-        self.assertEqual(event["official"], self.event.official)
-        self.assertEqual(event["start_date"], self.event.start_date.date().isoformat())
-        self.assertEqual(event["end_date"], self.event.end_date.date().isoformat())
-        self.assertEqual(event["event_type_string"], self.event.event_type_str)
-        self.assertEqual(event["event_type"], self.event.event_type_enum)
-
-    def assertMatchJson(self, match):
-        self.assertEqual(str(match["key"]), self.match.key.string_id())
-        self.assertEqual(match["comp_level"], self.match.comp_level)
-        self.assertEqual(match["event_key"], self.match.event.string_id())
-        self.assertEqual(match["set_number"], self.match.set_number)
-        self.assertEqual(match["match_number"], self.match.match_number)
-
-    def assertAwardJson(self, award):
-        self.assertEqual(award["name"], self.award.name_str)
-        self.assertEqual(award["event_key"], self.award.event.string_id())
-        self.assertEqual(award["recipient_list"], self.award.recipient_list)
-        self.assertEqual(award["year"], self.award.year)
-
     def testTeamApi(self):
         response = self.testapp.get('/frc281', headers={"X-TBA-App-Id": "tba-tests:team-controller-test:v01"})
 
         team_dict = json.loads(response.body)
         self.assertTeamJson(team_dict)
-        self.assertEventJson(team_dict["events"][0])
-        self.assertMatchJson(team_dict["events"][0]["matches"][0])
-        self.assertAwardJson(team_dict["events"][0]["awards"][0])
+
 
 class TestTeamMediaApiController(unittest2.TestCase):
-
     def setUp(self):
         app = webapp2.WSGIApplication([webapp2.Route(r'/<team_key:>/<year:>', ApiTeamMediaController, methods=['GET'])], debug=True)
         self.testapp = webtest.TestApp(app)
@@ -162,36 +90,35 @@ class TestTeamMediaApiController(unittest2.TestCase):
         )
         self.team.put()
 
-
         self.cdmedia = Media(
-                        key=ndb.Key('Media', 'cdphotothread_39894'), 
-                        details_json=u'{"image_partial": "fe3/fe38d320428adf4f51ac969efb3db32c_l.jpg"}', 
-                        foreign_key=u'39894', 
-                        media_type_enum=1, 
-                        references=[ndb.Key('Team', 'frc254')], 
+                        key=ndb.Key('Media', 'cdphotothread_39894'),
+                        details_json=u'{"image_partial": "fe3/fe38d320428adf4f51ac969efb3db32c_l.jpg"}',
+                        foreign_key=u'39894',
+                        media_type_enum=1,
+                        references=[ndb.Key('Team', 'frc254')],
                         year=2014)
         self.cdmedia.put()
         self.cddetails = dict()
-        self.cddetails["image_partial"] = "fe3/fe38d320428adf4f51ac969efb3db32c_l.jpg" 
+        self.cddetails["image_partial"] = "fe3/fe38d320428adf4f51ac969efb3db32c_l.jpg"
 
         self.ytmedia = Media(
-                        key=ndb.Key('Media', 'youtube_aFZy8iibMD0'), 
-                        details_json=None, 
-                        foreign_key=u'aFZy8iibMD0', 
-                        media_type_enum=0, 
-                        references=[ndb.Key('Team', 'frc254')], 
+                        key=ndb.Key('Media', 'youtube_aFZy8iibMD0'),
+                        details_json=None,
+                        foreign_key=u'aFZy8iibMD0',
+                        media_type_enum=0,
+                        references=[ndb.Key('Team', 'frc254')],
                         year=2014)
         self.ytmedia.put()
-        
+
     def tearDown(self):
         self.testbed.deactivate()
 
     def testTeamMediaApi(self):
         response = self.testapp.get('/frc254/2014', headers={"X-TBA-App-Id": "tba-tests:team_media-controller-test:v01"})
         media = json.loads(response.body)
-        
+
         self.assertEqual(len(media), 2)
-        
+
         cd = media[0]
         self.assertEqual(cd["type"], "cdphotothread")
         self.assertEqual(cd["foreign_key"], "39894")
