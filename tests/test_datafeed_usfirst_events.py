4624b4244ab0c6a6887547d6d50a1612c47aeee6
Eugene Fang <fang.eugene@gmail.com>
2014-08-16 13:30:45 -0400

    add missing datastore stub in tests

--- tests/test_datafeed_usfirst_events.py
+++ tests/test_datafeed_usfirst_events.py
@@ -18,6 +18,7 @@ class TestDatafeedUsfirstEvents(unittest2.TestCase):
         self.testbed.activate()
         self.testbed.init_urlfetch_stub()
         self.testbed.init_memcache_stub()
+        self.testbed.init_datastore_v3_stub()
         self.testbed.init_taskqueue_stub(root_path=".")
 
         self.datafeed = DatafeedUsfirst()
