6aef348813620ca53961b2161c23fcaa992c237a
Phil Lopreiato <plnyyanks@gmail.com>
2014-09-16 12:43:47 -0400

    Move notification sending to its own class (to be used for testing later)

--- notifications/base_notification.py
+++ notifications/base_notification.py
@@ -1,17 +1,15 @@
-import hashlib
-import json
 import logging
-import urllib2
 
 from google.appengine.ext import deferred
 
-from controllers.gcm.gcm import GCMConnection, GCMMessage
+from controllers.gcm.gcm import GCMMessage
 from consts.client_type import ClientType
+from helpers.notification_sender import NotificationSender
 
 
 class BaseNotification(object):
 
-    _supported_clients = [ClientType.OS_ANDROID, ClientType.WEBHOOK]  # List of clients this notification type supports (these are default values)
+    _supported_clients = [ClientType.OS_ANDROID, ClientType.WEBHOOK]    # List of clients this notification type supports (these are default values)
                                                                         # Can be overridden by subclasses to only send to some types
 
     """
@@ -32,15 +30,15 @@ class BaseNotification(object):
             if client_type == ClientType.OS_ANDROID and ClientType.OS_ANDROID in self.keys:
                     notification = self._render_android()
                     if len(self.keys[ClientType.OS_ANDROID]) > 0:  # this is after _render because if it's an update fav/subscription notification, then
-                        self.send_gcm(notification)                # we remove the client id that sent the update so it doesn't get notified redundantly
+                        NotificationSender.send_gcm(notification)  # we remove the client id that sent the update so it doesn't get notified redundantly
 
             elif client_type == ClientType.OS_IOS and ClientType.OS_IOS in self.keys:
                 notification = self._render_ios()
-                self.send_ios(notification)
+                NotificationSender.send_ios(notification)
 
             elif client_type == ClientType.WEBHOOK and ClientType.WEBHOOK in self.keys and len(self.keys[ClientType.WEBHOOK]) > 0:
                     notification = self._render_webhook()
-                    self.send_webhook(notification)
+                    NotificationSender.send_webhook(notification, self.keys[ClientType.WEBHOOK])
 
     """
     Subclasses should override this method and return a dict containing the payload of the notification.
@@ -64,42 +62,3 @@ class BaseNotification(object):
 
     def _render_webhook(self):
         return self._build_dict()
-
-    def send_gcm(self, gcm_message):
-        gcm_connection = GCMConnection()
-        gcm_connection.notify_device(gcm_message)
-
-    def send_ios(self, message):
-        pass
-
-    def send_webhook(self, message):
-        payload = json.dumps(message, ensure_ascii=True)
-
-        for client in self.keys[ClientType.WEBHOOK]:
-
-            url = client[0]
-            secret = client[1]
-
-            ch = hashlib.sha1()
-            ch.update(secret)
-            ch.update(payload)
-            checksum = ch.hexdigest()
-
-            request = urllib2.Request(url, payload)
-            request.add_header("X-Tba-Checksum", checksum)
-            try:
-                resp = urllib2.urlopen(request)
-
-            except urllib2.HTTPError, e:
-
-                if e.code == 400:
-                    logging.error('400, Invalid message: ' + repr(gcm_post_json_str))
-                elif e.code == 401:
-                    logging.error('401, Webhook unauthorized')
-                elif e.code == 500:
-                    logging.error('500, Internal error on server sending message')
-                else:
-                    logging.exception('Unexpected HTTPError: ' + str(e.code) + " " + e.msg + " " + e.read())
-
-            except Exception, ex:
-                logging.error("Other Exception: "+str(ex))
