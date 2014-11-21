e16f919d3e580fb4d6630bb5b5a50031bf2f3e25
Phil Lopreiato <plnyyanks@gmail.com>
2014-11-20 20:57:11 -0500

    Add constants for enumeration

---
+++ consts/model_type.py
@@ -0,0 +1,13 @@
+class ModelType(object):
+    """
+    Enums for the differnet model types
+    DO NOT CHANGE EXISTING ONES
+    """
+
+    EVENT = 0
+    TEAM = 1
+    MATCH = 2
+    EVENT_TEAM = 3
+    DISTRICT = 4
+    AWARD = 5
+    MEDIA = 6
