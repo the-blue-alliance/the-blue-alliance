fc4167fb033033a39118d2a8fa52dc823f1bbbf1
Phil Lopreiato <plnyyanks@gmail.com>
2015-01-18 13:49:24 -0500

    Web interface for event firehose Can subscribe to all events in the current year

--- models/subscription.py
+++ models/subscription.py
@@ -2,6 +2,7 @@ import json
 
 from google.appengine.ext import ndb
 
+from consts.client_type import ClientType
 from consts.notification_type import NotificationType
 
 
@@ -15,6 +16,7 @@ class Subscription(ndb.Model):
     model_key = ndb.StringProperty(required=True)
     model_type = ndb.IntegerProperty(required=True)
     notification_types = ndb.IntegerProperty(repeated=True)
+    client_types = ndb.IntegerProperty(repeated=True)
 
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
@@ -22,6 +24,14 @@ class Subscription(ndb.Model):
     def __init__(self, *args, **kw):
         self._settings = None
         super(Subscription, self).__init__(*args, **kw)
+
+    # Since repeated and default are mutually exclusive
+    # Make 'client_types' default to all
+    @property
+    def allowed_clients(self):
+        if not self.client_types:
+            return [ClientType.OS_ANDROID, ClientType.OS_IOS, ClientType.WEBHOOK]
+        return self.client_types
 
     @property
     def notification_names(self):
