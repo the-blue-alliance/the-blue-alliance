e92f1d76a9d2203cdb574cd84daba21de70e1bca
Eugene Fang <fang.eugene@gmail.com>
2015-02-22 12:43:42 -0500

    Revert "update js to use new push format"
    This reverts commit bd38d76027985191007c5e42b827474843725ce7.
    
    Conflicts:
    	static/javascript/tba_js/gameday_matchbar.js

--- static/javascript/tba_js/gameday_matchbar.js
+++ static/javascript/tba_js/gameday_matchbar.js
@@ -1,44 +1,12 @@
-function updateAll() {
-  var eventKeys = {};
-  $('.view-close').each(function () {
-    eventKeys[$(this).attr('event_key')] = true;
-  });
-  
-  for (var eventKey in eventKeys){
-    checkUpdate(eventKey);
-  }
-}
+var eventsRef = new Firebase('https://thebluealliance.firebaseio.com/events/');
 
-var lastTimestamps = {};
-function checkUpdate(eventKey) {
-  $.ajax({
-    url: 'https://thebluealliance.firebaseio.com/events/' + eventKey + '.json',
-    type: 'GET',
-    dataType: 'json',
-    success: function(timestamp) {
-      if (timestamp == null) {
-        return;
-      }
-      if (!(eventKey in lastTimestamps) || timestamp != lastTimestamps[eventKey]) {
-        getUpdate(eventKey, timestamp);
-      }
-      lastTimestamps[eventKey] = timestamp;
-    },
-    contentType: 'application/json'
-  });
-}
+eventsRef.on('child_changed', function(snapshot) {
+  updateMatchbar(snapshot);
+});
 
-function getUpdate(eventKey, timestamp) {
-  $.ajax({
-    url: '/_/live-event/' + eventKey + '/' + timestamp,
-    type: 'GET',
-    dataType: 'json',
-    success: function(data) {
-      updateMatchbar(eventKey, data);
-    },
-    contentType: 'application/json'
-  });
-}
+eventsRef.on('child_added', function(snapshot) {
+  updateMatchbar(snapshot);
+});
 
 // Handle matchbar "Follow" settings
 var following_set = JSON.parse($.cookie("tba-gameday-following"));
@@ -46,39 +14,20 @@ if (following_set == null) {
   following_set = {};
 }
 
-function updateMatchbar(event_key, event_data) {
+function updateMatchbar(snapshot) {
+  var event_key = snapshot.name();
+  var event_data = snapshot.val();
   if (event_data == null) {
     return;
   }
-  
-  var matches = event_data.matches;
-  matches.sort(function(match1, match2){return match1.order - match2.order});
-  
-  var upcoming_matches = [];
-  var last_matches = [];
-  for (var i=0; i<matches.length; i++) {
-    var match = matches[i];
-    
-    if (match.alliances.red.score == -1 || match.alliances.blue.score == -1) {
-      upcoming_matches.push(match);
-    } else {
-      if (match.alliances.red.score > match.alliances.blue.score) {
-        match.winning_alliance = 'red';
-      } else if (match.alliances.blue.score > match.alliances.red.score) {
-        match.winning_alliance = 'blue';
-      } else {
-        match.winning_alliance = '';
-      }
-      last_matches.push(match);
-    }
-  }
-  last_matches.reverse();
 
+  var upcoming_matches = event_data.upcoming_matches;
+  var last_matches = event_data.last_matches;
   var match_bar = $('.' + event_key + '_matches');
 
   match_bar.each(function() { // Because the user might have more than 1 view of a given event open
     var matches = $(this)[0].children;
-    
+
     if (last_matches != null && last_matches[0] != null) {
       var last_match = last_matches[0];
       // Remove old matches up to the last played match
@@ -90,13 +39,13 @@ function updateMatchbar(event_key, event_data) {
           break;
         }
       }
-      
+
       // Render last played match
       var winning_alliance = (last_matches[0].winning_alliance == '') ? 'tie' : last_matches[0].winning_alliance;
       var rendered_match = renderMatch(last_matches[0]).addClass('finished_match_' + winning_alliance);
       $(this).prepend(rendered_match);
     }
-    
+
     if (upcoming_matches != null) {
       for (var i=0; i<upcoming_matches.length; i++) {
         // Render match if not already present in matchbar
@@ -116,7 +65,7 @@ function updateMatchbar(event_key, event_data) {
         }
       }
     }
-    
+
     // Add event code to first match
     var event_code = event_key.replace(/[0-9]/g, '').toUpperCase();
     var match_num = $(this)[0].firstChild.firstChild.innerHTML;
@@ -127,32 +76,34 @@ function updateMatchbar(event_key, event_data) {
 }
 
 function renderMatch(match) {
+  var comp_level = match.comp_level.toUpperCase();
+  comp_level = (comp_level == 'QM') ? 'Q' : comp_level;
+  var match_number = (comp_level == 'QF' || comp_level == 'SF' || comp_level == 'F') ? match.set_number + '-' + match.match_number : match.match_number;
+  var match_label = comp_level + match_number;
+
   var red_teams = match.alliances.red.teams[0].substring(3) + ', ' +
     match.alliances.red.teams[1].substring(3) + ', ' +
     match.alliances.red.teams[2].substring(3);
   var blue_teams = match.alliances.blue.teams[0].substring(3) + ', ' +
     match.alliances.blue.teams[1].substring(3) + ', ' +
     match.alliances.blue.teams[2].substring(3);
-  
+
   var red_score = match.alliances.red.score;
   var blue_score = match.alliances.blue.score;
   red_score = (red_score == -1) ? '' : ' - ' + red_score;
   blue_score = (blue_score == -1) ? '' : ' - ' + blue_score;
 
-  var match_number = $('<div>', {'class': 'match-number', text: match.name});
+  var match_number = $('<div>', {'class': 'match-number', text: match_label});
   var red_score = $('<div>', {'class': 'red', text: red_teams + red_score});
   var blue_score = $('<div>', {'class': 'blue' , text: blue_teams + blue_score});
   var alliances = $('<div>', {'class': 'alliances'});
   alliances.append(red_score).append(blue_score);
-  
+
   var new_match = $('<div>', {'class': 'match', 'id': match.key_name}).append(match_number).append(alliances);
   return new_match;
 }
 
 $(document).ready(function() {
-  // start matchbar updating
-  setInterval(updateAll, 10000);
-  
   // Helper to insert teams in order
   function insertTeam(number) {
     var followed_teams = $("#followed-teams")[0].children;
@@ -168,7 +119,7 @@ $(document).ready(function() {
     if (!added) {
       $("#followed-teams").append(element);
     }
-    
+
     // Attach deletion callback
     $(".remove-following").click(function() {
       var num = $(this).attr("team-num");
@@ -178,7 +129,7 @@ $(document).ready(function() {
       $.cookie("tba-gameday-following", JSON.stringify(following_set));
     });
   }
-  
+
   // Init
   for (var key in following_set) {
     insertTeam(key);
@@ -192,16 +143,16 @@ $(document).ready(function() {
       $("#follow-form")[0].reset();
       return false;
     }
-    
+
     // Add to list if not already present
     if (!following_set[number]) {
       following_set[number] = true;
       insertTeam(number);
     }
-    
+
     // Set cookie
     $.cookie("tba-gameday-following", JSON.stringify(following_set));
-    
+
     // Reset form
     $("#follow-form")[0].reset();
     return false;
