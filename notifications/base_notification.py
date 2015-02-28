4d48b84279a51641ac1f8220a39ae05e4d833cf3
Phil Lopreiato <plnyyanks@gmail.com>
2015-02-28 11:46:31 -0500

    Don't send unless type in in enbaled_notifications

--- notifications/base_notification.py
+++ notifications/base_notification.py
@@ -65,7 +65,7 @@ class BaseNotification(object):
 
     def check_enabled(self):
         var = Sitevar.get_by_id('notifications.enable')
-        return var is None or var.values_json == "true"
+        return (self._type in NotificationType.enabled_notifications) and (var is None or var.values_json == "true")
 
     """
     Subclasses should override this method and return a dict containing the payload of the notification.
