177a4f36c9283fb4c736de86da757ee77bcac45b
flyingcow93 <bwl3@njit.edu>
2015-04-03 00:03:02 -0400

    Update district_helper.py

--- helpers/district_helper.py
+++ helpers/district_helper.py
@@ -26,17 +26,16 @@ class DistrictHelper(object):
     """
     @classmethod
     def inverf(cls, x):
-       
-        if x > 0:
-            s = 1
-        elif x < 0:
-            s = -1
-        else:
-            s = 0
-            
-        a = 0.147
+    	x = 0.3
+    	if x > 0:
+    		s = 1
+    	elif x < 0:
+    		s = -1
+    	else:
+    		s = 0
+    	a = 0.147
         y = s * math.sqrt((math.sqrt((((2 / (math.pi * a)) + ((math.log(1 - x**2)) / 2))**2) - ((math.log(1 - x**2)) / a))) - ((2 / (math.pi * a)) + (math.log(1 - x**2)) / 2))
-        return y
+	    return y
 
     @classmethod
     def calculate_event_points(cls, event):
