2538ca440620de8ed08510e0ae902c82184a9daa
Phil Lopreiato <plnyyanks@gmail.com>
2015-05-11 12:46:19 -0400

    Revert "Remove AuthType.EVENT_DATA and renumber"
    This reverts commit 38248941eb47a04b82fe52e1adca7387dafcf7f3.

--- consts/auth_type.py
+++ consts/auth_type.py
@@ -2,14 +2,16 @@ class AuthType(object):
     """
     An auth type defines what write privileges an authenticated agent has.
     """
-    MATCH_VIDEO = 0
-    EVENT_TEAMS = 1
-    EVENT_MATCHES = 2
-    EVENT_RANKINGS = 3
-    EVENT_ALLIANCES = 4
-    EVENT_AWARDS = 5
+    EVENT_DATA = 0
+    MATCH_VIDEO = 1
+    EVENT_TEAMS = 2
+    EVENT_MATCHES = 3
+    EVENT_RANKINGS = 4
+    EVENT_ALLIANCES = 5
+    EVENT_AWARDS = 6
 
     type_names = {
+        EVENT_DATA: "event data",
         MATCH_VIDEO: "match video",
         EVENT_TEAMS: "event teams",
         EVENT_MATCHES: "event matches",
