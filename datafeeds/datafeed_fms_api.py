6846adfc8ca0d8a18e5443775e325cb71077ea10
Eugene Fang <fang.eugene@gmail.com>
2015-07-14 11:55:28 -0400

    parse and save match details from FRC API

--- datafeeds/datafeed_fms_api.py
+++ datafeeds/datafeed_fms_api.py
@@ -13,7 +13,7 @@ from models.sitevar import Sitevar
 from parsers.fms_api.fms_api_awards_parser import FMSAPIAwardsParser
 from parsers.fms_api.fms_api_event_alliances_parser import FMSAPIEventAlliancesParser
 from parsers.fms_api.fms_api_event_rankings_parser import FMSAPIEventRankingsParser
-from parsers.fms_api.fms_api_hybrid_schedule_parser import FMSAPIHybridScheduleParser
+from parsers.fms_api.fms_api_match_parser import FMSAPIHybridScheduleParser, FMSAPIMatchDetailsParser
 from parsers.fms_api.fms_api_team_details_parser import FMSAPITeamDetailsParser
 
 
@@ -62,6 +62,8 @@ class DatafeedFMSAPI(object):
             self.FMS_API_AWARDS_URL_PATTERN = FMS_API_URL_BASE + '/%s/awards/%s'  # (year, event_short)
             self.FMS_API_HYBRID_SCHEDULE_QUAL_URL_PATTERN = FMS_API_URL_BASE + '/%s/schedule/%s/qual/hybrid'  # (year, event_short)
             self.FMS_API_HYBRID_SCHEDULE_PLAYOFF_URL_PATTERN = FMS_API_URL_BASE + '/%s/schedule/%s/playoff/hybrid'  # (year, event_short)
+            self.FMS_API_MATCH_DETAILS_QUAL_URL_PATTERN = FMS_API_URL_BASE + '/%s/scores/%s/qual'  # (year, event_short)
+            self.FMS_API_MATCH_DETAILS_PLAYOFF_URL_PATTERN = FMS_API_URL_BASE + '/%s/scores/%s/playoff'  # (year, event_short)
             self.FMS_API_EVENT_RANKINGS_URL_PATTERN = FMS_API_URL_BASE + '/%s/rankings/%s'  # (year, event_short)
             self.FMS_API_EVENT_ALLIANCES_URL_PATTERN = FMS_API_URL_BASE + '/%s/alliances/%s'  # (year, event_short)
             self.FMS_API_TEAM_DETAILS_URL_PATTERN = FMS_API_URL_BASE + '/%s/teams/?teamNumber=%s'  # (year, teamNumber)
@@ -112,17 +114,26 @@ class DatafeedFMSAPI(object):
         year = int(event_key[:4])
         event_short = event_key[4:]
 
-        parser = FMSAPIHybridScheduleParser(year, event_short)
-        qual_matches = self._parse(self.FMS_API_HYBRID_SCHEDULE_QUAL_URL_PATTERN % (year, self._get_event_short(event_short)), parser)
-        playoff_matches = self._parse(self.FMS_API_HYBRID_SCHEDULE_PLAYOFF_URL_PATTERN % (year, self._get_event_short(event_short)), parser)
+        hs_parser = FMSAPIHybridScheduleParser(year, event_short)
+        detail_parser = FMSAPIMatchDetailsParser(year, event_short)
+        qual_matches = self._parse(self.FMS_API_HYBRID_SCHEDULE_QUAL_URL_PATTERN % (year, self._get_event_short(event_short)), hs_parser)
+        playoff_matches = self._parse(self.FMS_API_HYBRID_SCHEDULE_PLAYOFF_URL_PATTERN % (year, self._get_event_short(event_short)), hs_parser)
+        qual_details = self._parse(self.FMS_API_MATCH_DETAILS_QUAL_URL_PATTERN % (year, self._get_event_short(event_short)), detail_parser)
+        playoff_details = self._parse(self.FMS_API_MATCH_DETAILS_QUAL_URL_PATTERN % (year, self._get_event_short(event_short)), detail_parser)
 
-        matches = []
+        matches_by_key = {}
         if qual_matches is not None:
-            matches += qual_matches
+            for match in qual_matches:
+                matches_by_key[match.key.id()] = match
         if playoff_matches is not None:
-            matches += playoff_matches
+            for match in playoff_matches:
+                matches_by_key[match.key.id()] = match
 
-        return matches
+        for match_key, match_details in qual_details.items() + playoff_details.items():
+            if match_key in matches_by_key:
+                matches_by_key[match_key].score_breakdown_json = json.dumps(match_details)
+
+        return matches_by_key.values()
 
     def getEventRankings(self, event_key):
         year = int(event_key[:4])
