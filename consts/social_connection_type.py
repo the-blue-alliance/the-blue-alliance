6cdd3f16ba661ba2b7c6bb88839433bb24a96216
Phil Lopreiato <plnyyanks@gmail.com>
2015-08-17 11:51:39 -0400

    Add constants for social connection types

---
+++ consts/social_connection_type.py
@@ -0,0 +1,16 @@
+class SocialConnectionType(object):
+    """
+    Constants for social connections
+    """
+
+    FACEBOOK = 0
+    TWITTER = 1
+    YOUTUBE = 2
+    GITHUB = 3
+
+    type_names = {
+        FACEBOOK: 'Facebook',
+        TWITTER: 'Twitter',
+        YOUTUBE: 'YouTube',
+        GITHUB: 'GitHub',
+    }
