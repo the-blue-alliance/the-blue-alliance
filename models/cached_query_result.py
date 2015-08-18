c74a88c16c8e65f2f6bd64ad8f3eb0133b0c2467
Eugene Fang <fang.eugene@gmail.com>
2015-08-18 17:32:14 -0400

    compress cached queries

--- models/cached_query_result.py
+++ models/cached_query_result.py
@@ -5,7 +5,7 @@ class CachedQueryResult(ndb.Model):
     """
     A CachedQueryResult stores the result of an NDB query
     """
-    result = ndb.PickleProperty(required=True)
+    result = ndb.PickleProperty(required=True, compressed=True)
 
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
