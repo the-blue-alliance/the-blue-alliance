825f672171c8951a1a7dbac3b1abc7cad4046260
Eugene Fang <fang.eugene@gmail.com>
2015-09-10 17:24:03 -0400

    enable more profiling when not in prod

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,9 @@
-# appstats_CALC_RPC_COSTS = True
-appstats_RECORD_FRACTION = 0.1
+import tba_config
+
+if tba_config.CONFIG['env'] == 'prod':
+    appstats_RECORD_FRACTION = 0.1
+else:
+    appstats_CALC_RPC_COSTS = True
 
 
 def webapp_add_wsgi_middleware(app):
