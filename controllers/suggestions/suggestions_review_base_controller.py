5712d19a6dbc28bbbd98ce7b579b35f2bab2f416
Greg Marra <greg.marra@gmail.com>
2015-11-22 16:54:38 -0800

    Moves suggestion reviews to be possible by anyone with the permission to MUTATE_DATA.

---
+++ controllers/suggestions/suggestions_review_base_controller.py
@@ -0,0 +1,11 @@
+from consts.account_permissions import AccountPermissions
+
+from controllers.base_controller import LoggedInHandler
+
+class SuggestionsReviewBaseController(LoggedInHandler):
+    """
+    Base controller for reviewing suggestions.
+    """
+    def __init__(self, *args, **kw):
+        super(SuggestionsReviewBaseController, self).__init__(*args, **kw)
+        self._require_permission(AccountPermissions.MUTATE_DATA)
