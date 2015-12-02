38960a8ec75b29f3e6a80fa35b960c4630e6a8a3
Phil Lopreiato <plnyyanks@gmail.com>
2015-12-02 15:22:55 -0500

    Add Status API Controller

---
+++ controllers/api/api_status_controller.py
@@ -0,0 +1,40 @@
+import json
+
+from controllers.api.api_base_controller import ApiBaseController
+
+from models.sitevar import Sitevar
+
+
+class ApiStatusController(ApiBaseController):
+    CACHE_KEY_FORMAT = "apiv2_status_controller"  # year
+    CACHE_VERSION = 0
+    CACHE_HEADER_LENGTH = 61
+
+    def __init__(self, *args, **kw):
+        super(ApiStatusController, self).__init__(*args, **kw)
+        self._partial_cache_key = self.CACHE_KEY_FORMAT
+
+    @property
+    def _validators(self):
+        '''
+        No validators for this endpoint
+        '''
+        return []
+
+    def _track_call(self):
+        self._track_call_defer('status', 'status')
+
+    def _render(self):
+        status_sitevar = Sitevar.get_by_id('apistatus')
+        down_events_sitevar = Sitevar.get_by_id('apistatus.down_events')
+
+        # Error out of no sitevar found
+        if not status_sitevar:
+            self._errors = json.dumps({"404": "API Status Not Found"})
+            self.abort(404)
+
+        status_dict = status_sitevar.contents
+        down_events_list = down_events_sitevar.contents
+
+        status_dict['down_events'] = down_events_list if down_events_list is not None else []
+        return json.dumps(status_dict, ensure_ascii=True)
