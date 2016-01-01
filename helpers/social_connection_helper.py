c07bb56ff65ca959959303f0b6fa0f1c707e33df
Phil Lopreiato <plnyyanks@gmail.com>
2016-01-01 17:04:12 -0500

    Clean up profile url parsing

--- helpers/social_connection_helper.py
+++ helpers/social_connection_helper.py
@@ -5,6 +5,7 @@ from consts.social_connection_type import SocialConnectionType
 
 
 class SocialConnectionHelper(object):
+
     @classmethod
     def group_by_type(cls, connections):
         conns_by_type = {}
@@ -18,33 +19,39 @@ class SocialConnectionHelper(object):
 
 
 class SocialConnectionParser(object):
-    FACEBOOK_URL_PATTERNS = ['facebook.com/']
-    TWITTER_URL_PATTERNS = ['twitter.com/']
-    YOUTUBE_URL_PATTERNS = ['youtube.com/user/']
-    GITHUB_URL_PATTERNS = ['github.com/']
+    # Foreign key patterns
+    # Dict that maps SocialConnectionType -> tuple of regex pattern and group # of foreign key
+    FOREIGN_KEY_PATTERNS = {
+        SocialConnectionType.FACEBOOK: (r".*facebook.com\/(.*)(\/(.*))?", 1),
+        SocialConnectionType.TWITTER: (r".*twitter.com\/(.*)(\/(.*))?", 1),
+        SocialConnectionType.YOUTUBE: (r".*youtube.com\/user\/(.*)(\/(.*))?", 1),
+        SocialConnectionType.GITHUB: (r".*github.com\/(.*)(\/(.*))?", 1),
+    }
+
+    # Social profile URL patterns that map a URL -> Profile type
+    URL_PATTERNS = {
+        'facebook.com/': SocialConnectionType.FACEBOOK,
+        'twitter.com/': SocialConnectionType.TWITTER,
+        'youtube.com/user': SocialConnectionType.YOUTUBE,
+        'github.com/': SocialConnectionType.GITHUB
+    }
 
     @classmethod
     def partial_social_dict_from_url(cls, url):
         """
-        Takes a url, and turns it into a partial Media object dict
+        Takes a url, and turns it into a partial social connection object dict
         """
-        if any(s in url for s in cls.FACEBOOK_URL_PATTERNS):
-            return cls._partial_social_dict_from_facebook(url)
-        elif any(s in url for s in cls.TWITTER_URL_PATTERNS):
-            return cls._partial_social_dict_from_twitter(url)
-        elif any(s in url for s in cls.YOUTUBE_URL_PATTERNS):
-            return cls._partial_social_dict_from_youtube(url)
-        elif any(s in url for s in cls.GITHUB_URL_PATTERNS):
-            return cls._partial_social_dict_from_github(url)
-        else:
-            logging.warning("Failed to determine media type from url: {}".format(url))
-            return None
+        for s, type in cls.URL_PATTERNS.iteritems():
+            if s in url:
+                return cls._partial_social_dict(type, url)
+        logging.warning("Failed to determine media type from url: {}".format(url))
+        return None
 
     @classmethod
-    def _partial_social_dict_from_facebook(cls, url):
+    def _partial_social_dict(cls, social_type, url):
         social_dict = {}
-        social_dict['social_type_enum'] = SocialConnectionType.FACEBOOK
-        foreign_key = cls._parse_facebook_foreign_key(url)
+        social_dict['social_type_enum'] = social_type
+        foreign_key = cls._parse_foreign_key(social_type, url)
         if foreign_key is None:
             logging.warning("Failed to determine foreign_key from url: {}".format(url))
             return None
@@ -53,85 +60,13 @@ class SocialConnectionParser(object):
         return social_dict
 
     @classmethod
-    def _partial_social_dict_from_twitter(cls, url):
-        social_dict = {}
-        social_dict['social_type_enum'] = SocialConnectionType.TWITTER
-        foreign_key = cls._parse_twitter_foreign_key(url)
+    def _parse_foreign_key(cls, social_type, url):
+        foreign_key = None
+        regex = re.match(cls.FOREIGN_KEY_PATTERNS[social_type][0], url)
+        if regex is not None:
+            foreign_key = regex.group(cls.FOREIGN_KEY_PATTERNS[social_type][1])
+
         if foreign_key is None:
-            logging.warning("Failed to determine foreign_key from url: {}".format(url))
-            return None
-        social_dict['foreign_key'] = foreign_key
-
-        return social_dict
-
-    @classmethod
-    def _partial_social_dict_from_youtube(cls, url):
-        social_dict = {}
-        social_dict['social_type_enum'] = SocialConnectionType.YOUTUBE
-        foreign_key = cls._parse_youtube_foreign_key(url)
-        if foreign_key is None:
-            logging.warning("Failed to determine foreign_key from url: {}".format(url))
-            return None
-        social_dict['foreign_key'] = foreign_key
-
-        return social_dict
-
-    @classmethod
-    def _partial_social_dict_from_github(cls, url):
-        social_dict = {}
-        social_dict['social_type_enum'] = SocialConnectionType.GITHUB
-        foreign_key = cls._parse_github_foreign_key(url)
-        if foreign_key is None:
-            logging.warning("Failed to determine foreign_key from url: {}".format(url))
-            return None
-        social_dict['foreign_key'] = foreign_key
-
-        return social_dict
-
-    @classmethod
-    def _parse_facebook_foreign_key(cls, url):
-        facebook_profile = None
-        regex = re.match(r".*facebook.com\/(.*)(\/(.*))?", url)
-        if regex is not None:
-            facebook_profile = regex.group(1)
-
-        if facebook_profile is None:
             return None
         else:
-            return facebook_profile
-
-    @classmethod
-    def _parse_twitter_foreign_key(cls, url):
-        twitter_profile = None
-        regex = re.match(r".*twitter.com\/(.*)(\/(.*))?", url)
-        if regex is not None:
-            twitter_profile = regex.group(1)
-
-        if twitter_profile is None:
-            return None
-        else:
-            return twitter_profile
-
-    @classmethod
-    def _parse_youtube_foreign_key(cls, url):
-        youtube_profile = None
-        regex = re.match(r".*youtube.com\/user\/(.*)(\/(.*))?", url)
-        if regex is not None:
-            youtube_profile = regex.group(1)
-
-        if youtube_profile is None:
-            return None
-        else:
-            return youtube_profile
-
-    @classmethod
-    def _parse_github_foreign_key(cls, url):
-        github_profile = None
-        regex = re.match(r".*github.com\/(.*)(\/(.*))?", url)
-        if regex is not None:
-            github_profile = regex.group(1)
-
-        if github_profile is None:
-            return None
-        else:
-            return github_profile
+            return foreign_key
