6841f60f2881e45eed4ece0afb658fca68accc88
Phil Lopreiato <plnyyanks@gmail.com>
2016-03-04 14:15:58 -0500

    Add social media type definitions

--- consts/media_type.py
+++ consts/media_type.py
@@ -3,14 +3,36 @@ class MediaType(object):
     YOUTUBE_VIDEO = 0
     CD_PHOTO_THREAD = 1
     IMGUR = 2
+    FACEBOOK_PROFILE = 3
+    TWITTER_PROFILE = 4
+    YOUTUBE_CHANNEL = 5
+    GITHUB_PROFILE = 6
 
     type_names = {
         YOUTUBE_VIDEO: 'YouTube Video',
         CD_PHOTO_THREAD: 'Chief Delphi Photo Thread',
-        IMGUR: 'Imgur Image'
+        IMGUR: 'Imgur Image',
+        FACEBOOK_PROFILE: 'Facebook Profile',
+        TWITTER_PROFILE: 'Twitter Profile',
+        YOUTUBE_CHANNEL: 'YouTube Channel',
+        GITHUB_PROFILE: 'GitHub Profile',
     }
 
     image_types = [
         CD_PHOTO_THREAD,
         IMGUR,
     ]
+
+    social_types = [
+        FACEBOOK_PROFILE,
+        TWITTER_PROFILE,
+        YOUTUBE_CHANNEL,
+        GITHUB_PROFILE,
+    ]
+
+    profile_urls = {  # Format with foreign_key
+        FACEBOOK_PROFILE: 'https://www.facebook.com/{}',
+        TWITTER_PROFILE: 'https://twitter.com/{}',
+        YOUTUBE_CHANNEL: 'https://www.youtube.com/user/{}',
+        GITHUB_PROFILE: 'https://github.com/{}',
+    }
