cc15d7b08b3e554e637fee9373da338eb0721e59
Phil Lopreiato <plnyyanks@gmail.com>
2016-03-06 18:22:51 -0500

    Create event date test objects in setUp

--- tests/test_event.py
+++ tests/test_event.py
@@ -12,6 +12,7 @@ from helpers.event_team.event_team_test_creator import EventTeamTestCreator
 from helpers.match.match_test_creator import MatchTestCreator
 from models.event import Event
 
+
 class TestEventManipulator(unittest2.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
@@ -23,8 +24,30 @@ class TestEventManipulator(unittest2.TestCase):
         self.future_event = EventTestCreator.createFutureEvent(only_event=True)
         self.present_event = EventTestCreator.createPresentEvent(only_event=True)
         self.past_event = EventTestCreator.createPastEvent(only_event=True)
-        self.event_starts_today = self.createEventStartsToday(only_event=True)
-        self.event_ends_today = self.createEventEndsToday(only_event=True)
+        self.event_starts_today = Event(
+            id="{}teststartstoday".format(datetime.datetime.now().year),
+            end_date=datetime.datetime.today() + datetime.timedelta(days=2),
+            event_short="teststartstoday",
+            event_type_enum=EventType.REGIONAL,
+            first_eid="5561",
+            name="Test Event (Starts Today)",
+            start_date=datetime.datetime.today(),
+            year=datetime.datetime.now().year,
+            venue_address="123 Fake Street, California, USA",
+            website="http://www.google.com"
+        )
+        self.event_ends_today = Event(
+            id="{}testendstoday".format(datetime.datetime.now().year),
+            end_date=datetime.datetime.today(),
+            event_short="testendstoday",
+            event_type_enum=EventType.REGIONAL,
+            first_eid="5561",
+            name="Test Event (Ends Today)",
+            start_date=datetime.datetime.today() - datetime.timedelta(days=2),
+            year=datetime.datetime.now().year,
+            venue_address="123 Fake Street, California, USA",
+            website="http://www.google.com"
+        )
 
     def tearDown(self):
         self.future_event.key.delete()
@@ -60,49 +83,3 @@ class TestEventManipulator(unittest2.TestCase):
     def test_datesEndsToday(self):
         self.assertFalse(self.event_ends_today.starts_today)
         self.assertTrue(self.event_ends_today.ends_today)
-
-    @classmethod
-    def createEventStartsToday(self, only_event=False):
-        id_string = "{}teststartstoday".format(datetime.datetime.now().year)
-        event = Event(
-            id=id_string,
-            end_date=datetime.datetime.today() + datetime.timedelta(days=2),
-            event_short="teststartstoday",
-            event_type_enum=EventType.REGIONAL,
-            first_eid="5561",
-            name="Test Event (Starts Today)",
-            start_date=datetime.datetime.today(),
-            year=datetime.datetime.now().year,
-            venue_address="123 Fake Street, California, USA",
-            website="http://www.google.com"
-        )
-        event = EventManipulator.createOrUpdate(event)
-        if not only_event:
-            EventTeamTestCreator.createEventTeams(event)
-            mtc = MatchTestCreator(event)
-            mtc.createCompleteQuals()
-            mtc.createIncompleteQuals()
-        return event
-
-    @classmethod
-    def createEventEndsToday(self, only_event=False):
-        id_string = "{}testendstoday".format(datetime.datetime.now().year)
-        event = Event(
-            id=id_string,
-            end_date=datetime.datetime.today(),
-            event_short="testendstoday",
-            event_type_enum=EventType.REGIONAL,
-            first_eid="5561",
-            name="Test Event (Ends Today)",
-            start_date=datetime.datetime.today() - datetime.timedelta(days=2),
-            year=datetime.datetime.now().year,
-            venue_address="123 Fake Street, California, USA",
-            website="http://www.google.com"
-        )
-        event = EventManipulator.createOrUpdate(event)
-        if not only_event:
-            EventTeamTestCreator.createEventTeams(event)
-            mtc = MatchTestCreator(event)
-            mtc.createCompleteQuals()
-            mtc.createIncompleteQuals()
-        return event
