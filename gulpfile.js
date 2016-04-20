8e85e84a7b5b2e975a278228031e874832ad0553
Nathan Walters <nwalters512@gmail.com>
2016-04-20 13:19:27 -0500

    Use babel for transpiling

--- gulpfile.js
+++ gulpfile.js
@@ -5,52 +5,53 @@ var watchify = require('watchify');
 var reactify = require('reactify');
 var gutil = require('gulp-util');
 var debug = require('gulp-debug');
+var babelify = require('babelify');
 
 var errorHandler = function(err) {
   gutil.log(err);
   this.emit('end');
 };
 
-gulp.task('build', function(){
-  browserify({
-    entries: ['./react/gameday2/gameday2.js'],
-    transform: [reactify]
-  })
-  .bundle()
-  .on('error', errorHandler)
-  .pipe(source('gameday2.js'))
-  .pipe(debug())
-  .pipe(gulp.dest('./static/javascript/gameday2/'));
-});
-
-gulp.task('browserify', function() {
+function compile(watch) {
   var bundler = browserify({
     entries: ['./react/gameday2/gameday2.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // We want to convert JSX to normal javascript
     debug: true, // Gives us sourcemapping
     cache: {},
     packageCache: {},
     fullPaths: true // Requirement of watchify
+  }).transform('babelify', {
+    presets: ['es2015', 'react']
   });
 
   var watcher = watchify(bundler);
 
-  watcher
-    .on('update', function() { // When any files update
-      var updateStart = Date.now();
-      console.log('Updating!');
-      watcher.bundle() // Create new bundle that uses the cache for high performance
-        .on('error', errorHandler)
-        .pipe(source('gameday2.js'))
-        .pipe(debug())
-        .pipe(gulp.dest('./static/javascript/gameday2/'));
-      console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .on('error', errorHandler)
-    .pipe(source('gameday2.js'))
-    .pipe(debug())
-    .pipe(gulp.dest('./static/javascript/gameday2/'));
+  function rebundle() {
+    bundler.bundle()
+      .on('error', errorHandler)
+      .pipe(source('gameday2.js'))
+      .pipe(debug())
+      .pipe(gulp.dest('./static/javascript/gameday2/'));
+  }
+
+  if (watch) {
+    watcher.on('update', function() {
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+}
+
+gulp.task('build', function() {
+  return compile();
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('watch', function() {
+  return watch();
+});
+
+gulp.task('default', ['watch']);
