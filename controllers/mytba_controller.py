04b440c8807d8fb801459060fdcaf4efff7ccea6
Phil Lopreiato <plnyyanks@gmail.com>
2016-04-21 20:54:50 -0400

    Try and improve db queries

--- controllers/mytba_controller.py
+++ controllers/mytba_controller.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import os
 
@@ -6,7 +7,7 @@ from google.appengine.ext.webapp import template
 
 from consts.model_type import ModelType
 from controllers.base_controller import LoggedInHandler
-from database.event_query import EventListQuery
+from database.event_query import EventListQuery, TeamYearEventsQuery
 from database.team_query import EventTeamsQuery
 from helpers.event_helper import EventHelper
 from helpers.event_team_status_helper import EventTeamStatusHelper
@@ -25,14 +26,8 @@ class MyTBALiveController(LoggedInHandler):
         team_favorites_future = Favorite.query(Favorite.model_type == ModelType.TEAM, ancestor=user).fetch_async()
 
         live_events = EventHelper.getWeekEvents()
-        all_events_this_year_future = EventListQuery(now.year).fetch_async()
         favorite_team_keys = map(lambda f: ndb.Key(Team, f.model_key), team_favorites_future.get_result())
         favorite_teams_future = ndb.get_multi_async(favorite_team_keys)
-
-        future_events = filter(lambda e: e.start_date > now, all_events_this_year_future.get_result())
-        future_eventteams_futures = []
-        for event in future_events:
-            future_eventteams_futures.append(EventTeamsQuery(event.key_name).fetch_async())
 
         live_eventteams_futures = []
         for event in live_events:
@@ -40,15 +35,27 @@ class MyTBALiveController(LoggedInHandler):
 
         favorite_teams = [team_future.get_result() for team_future in favorite_teams_future]
 
-        future_events_with_teams = []
-        for event, teams_future in zip(future_events, future_eventteams_futures):
-            favorite_eventteams = filter(lambda t: t in favorite_teams, teams_future.get_result())
-            live_teams_at_event = TeamHelper.sortTeams(favorite_eventteams)
-
-            if live_teams_at_event:
-                future_events_with_teams.append((event, live_teams_at_event))
+        favorite_teams_events_futures = []
+        for team in favorite_teams:
+            favorite_teams_events_futures.append(TeamYearEventsQuery(team.key_name, now.year).fetch_async())
 
         live_events_with_teams = EventTeamStatusHelper.buildEventTeamStatus(live_events, live_eventteams_futures, favorite_teams)
+
+        future_events_by_event = {}
+        for team, events_future in zip(favorite_teams, favorite_teams_events_futures):
+            events = events_future.get_result()
+            if not events:
+                continue
+            EventHelper.sort_events(events)
+            next_event = next((e for e in events if e.start_date > now), None)
+            if next_event:
+                if not next_event.key_name in future_events_by_event:
+                    future_events_by_event[next_event.key_name] = (next_event, [])
+                future_events_by_event[next_event.key_name][1].append(team)
+
+        future_events_with_teams = []
+        for event_key, data in future_events_by_event.iteritems():
+            future_events_with_teams.append((data[0], TeamHelper.sortTeams(data[1])))
 
         self.template_values.update({
             'live_events_with_teams': live_events_with_teams,
