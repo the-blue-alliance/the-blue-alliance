a41f72b80ef37477891e4e24740eec847c65ce57
Nathan Walters <nwalters512@gmail.com>
2016-04-28 11:31:39 -0500

    Use simpler Twitch embed

--- react/gameday2/components/EmbedTwitch.js
+++ react/gameday2/components/EmbedTwitch.js
@@ -1,34 +1,16 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react'
 
 var EmbedTwitch = React.createClass({
   propTypes: {
     webcast: PropTypes.object.isRequired
   },
   render: function() {
-    var channel = this.props.webcast.channel;
-    var src = `http://www.twitch.tv/widgets/live_embed_player.swf?channel=${channel}`;
-    var flashvars = `hostname=www.twitch.tv&channel=${channel}&auto_play=true&start_volume=25&enable_javascript=true`;
+    var channel = this.props.webcast.channel
+    let iframeSrc = `https://player.twitch.tv/?channel=${channel}&html5`
     return (
-      <object
-        type='application/x-shockwave-flash'
-        height='100%'
-        width='100%'
-        id='live_embed_player_flash'
-        data={src}
-        bgcolor='#000000'>
-        <param name='allowFullScreen' value='true' />
-        <param name='allowScriptAccess' value='always' />
-        <param name='allowNetworking' value='all' />
-        <param
-          name='movie'
-          value='http://www.twitch.tv/widgets/live_embed_player.swf' />
-        <param
-          name='flashvars'
-          value={flashvars} />
-        <param name='wmode' value='transparent' />
-      </object>
-    );
+      <iframe src={iframeSrc} frameborder="0" scrolling="no" height="100%" width="100%"></iframe>
+    )
   }
-});
+})
 
-export default EmbedTwitch;
+export default EmbedTwitch
