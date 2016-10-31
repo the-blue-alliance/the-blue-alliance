d8c52afa38d4e86737b4172364280f5bfea13954
Eugene Fang <fang.eugene@gmail.com>
2016-10-31 00:43:04 -0400

    get team location info WIP

--- helpers/location_helper.py
+++ helpers/location_helper.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import re
+import tba_config
 import urllib
 
 from google.appengine.api import memcache, urlfetch
@@ -9,98 +10,262 @@ from google.appengine.ext import ndb
 from models.sitevar import Sitevar
 
 
+GOOGLE_SECRETS = Sitevar.get_by_id("google.secrets")
+GOOGLE_API_KEY = None
+if GOOGLE_SECRETS is None:
+    logging.warning("Missing sitevar: google.api_key. API calls rate limited by IP and may be over rate limit.")
+else:
+    GOOGLE_API_KEY = GOOGLE_SECRETS.contents['api_key']
+
+
 class LocationHelper(object):
-    @classmethod
-    def get_event_lat_lon(cls, event):
-        """
-        Try different combinations of venue, address, and location to
-        get latitude and longitude for an event
-        """
-        lat_lon, _ = cls.get_lat_lon(event.venue_address_safe)
-        if not lat_lon:
-            lat_lon, _ = cls.get_lat_lon(u'{} {}'.format(event.venue, event.location))
-        if not lat_lon:
-            lat_lon, _ = cls.get_lat_lon(event.location)
-        if not lat_lon:
-            lat_lon, _ = cls.get_lat_lon(u'{} {}'.format(event.city, event.country))
-        if not lat_lon:
-            logging.warning("Finding Lat/Lon for event {} failed!".format(event.key_name))
-        return lat_lon
+    # @classmethod
+    # def get_event_lat_lon(cls, event):
+    #     """
+    #     Try different combinations of venue, address, and location to
+    #     get latitude and longitude for an event
+    #     """
+    #     lat_lon, _ = cls.get_lat_lon(event.venue_address_safe)
+    #     if not lat_lon:
+    #         lat_lon, _ = cls.get_lat_lon(u'{} {}'.format(event.venue, event.location))
+    #     if not lat_lon:
+    #         lat_lon, _ = cls.get_lat_lon(event.location)
+    #     if not lat_lon:
+    #         lat_lon, _ = cls.get_lat_lon(u'{} {}'.format(event.city, event.country))
+    #     if not lat_lon:
+    #         logging.warning("Finding Lat/Lon for event {} failed!".format(event.key_name))
+    #     return lat_lon
+
+    # @classmethod
+    # def get_team_lat_lon(cls, team):
+    #     """
+    #     Try different combinations of team name (which should include high
+    #     school or main sponsor) and team location to get latitude and longitude
+    #     for a team
+    #     """
+    #     if team.name:
+    #         split_name = re.split('/|&', team.name)  # Guessing sponsors/school by splitting name by '/' or '&'
+    #     else:
+    #         split_name = None
+
+    #     best_lat_lon = None
+    #     least_num_results = float('inf')
+    #     if split_name:
+    #         futures = []
+    #         for possible_location in [split_name[-1], split_name[0]]:
+    #             # Try to locate team. Usually first or last entries are best candidates, with last being the priority
+    #             futures.append(cls.get_lat_lon_async(u'{} {}'.format(possible_location, team.location)))
+
+    #         for future in futures:
+    #             lat_lon, num_results = future.get_result()
+    #             if lat_lon and num_results < least_num_results:  # Fewer results is less ambiguous and more likely to be correct
+    #                 best_lat_lon = lat_lon
+    #                 least_num_results = num_results
+    #     if not best_lat_lon:
+    #         best_lat_lon, _ = cls.get_lat_lon(team.location)
+    #     if not best_lat_lon:
+    #         logging.warning("Finding Lat/Lon for team {} failed!".format(team.key.id()))
+    #     return best_lat_lon
 
     @classmethod
-    def get_team_lat_lon(cls, team):
+    def get_team_location_info(cls, team):
         """
-        Try different combinations of team name (which should include high
-        school or main sponsor) and team location to get latitude and longitude
-        for a team
+        Search for different combinations of team name (which should include
+        high school or title sponsor) with city, state_prov, postalcode, and country
+        in attempt to find the correct place associated with the team.
         """
+        if not team.location:
+            return {}
+
+        # Find possible schools/title sponsors
+        possible_names = []
+        MAX_SPLIT = 3  # Filters out long names that are unlikely
         if team.name:
-            split_name = re.split('/|&', team.name)  # Guessing sponsors/school by splitting name by '/' or '&'
-        else:
-            split_name = None
+            # Guessing sponsors/school by splitting name by '/' or '&'
+            split1 = re.split('&', team.name)
+            split2 = re.split('/', team.name)
+            if split1 and \
+                    split1[-1].count('&') < MAX_SPLIT and split1[-1].count('/') < MAX_SPLIT:
+                possible_names.append(split1[-1])
+            if split2 and split2[-1] not in possible_names and \
+                     split2[-1].count('&') < MAX_SPLIT and split2[-1].count('/') < MAX_SPLIT:
+                possible_names.append(split2[-1])
+            if split1 and split1[0] not in possible_names and \
+                     split1[0].count('&') < MAX_SPLIT and split1[0].count('/') < MAX_SPLIT:
+                possible_names.append(split1[0])
+            if split2 and split2[0] not in possible_names and \
+                     split2[0].count('&') < MAX_SPLIT and split2[0].count('/') < MAX_SPLIT:
+                possible_names.append(split2[0])
 
-        best_lat_lon = None
-        least_num_results = float('inf')
-        if split_name:
-            futures = []
-            for possible_location in [split_name[-1], split_name[0]]:
-                # Try to locate team. Usually first or last entries are best candidates, with last being the priority
-                futures.append(cls.get_lat_lon_async(u'{} {}'.format(possible_location, team.location)))
+        # Construct possible queries based on name and location. Order matters.
+        possible_queries = []
+        for possible_name in possible_names:
+            for query in [possible_name, u'{} {}'.format(possible_name, team.location)]:
+                possible_queries.append(query)
 
-            for future in futures:
-                lat_lon, num_results = future.get_result()
-                if lat_lon and num_results < least_num_results:  # Fewer results is less ambiguous and more likely to be correct
-                    best_lat_lon = lat_lon
-                    least_num_results = num_results
-        if not best_lat_lon:
-            best_lat_lon, _ = cls.get_lat_lon(team.location)
-        if not best_lat_lon:
-            logging.warning("Finding Lat/Lon for team {} failed!".format(team.key.id()))
-        return best_lat_lon
+        # Try to find place based on possible queries
+        best_score = 0
+        best_location_info = {}
+        textsearch_results_candidates = []  # More trustworthy candidates are added first
+        for query in possible_queries:
+            textsearch_results = cls.google_maps_textsearch_async(query).get_result()
+            if textsearch_results:
+                if len(textsearch_results) == 1:
+                    location_info = cls.construct_location_info_async(textsearch_results[0]).get_result()
+                    score = cls.compute_team_location_score(team, location_info)
+                    if score == 1:
+                        # Very likely to be correct if only 1 result and as a perfect score
+                        return location_info
+                    elif score > best_score:
+                        # Only 1 result but score is imperfect
+                        best_score = score
+                        best_location_info = location_info
+                else:
+                    # Save queries with multiple results for later evaluation
+                    textsearch_results_candidates.append(textsearch_results)
+
+        # Check if we have found anything reasonable
+        if best_location_info and best_score > 0.5:
+            return best_location_info
+
+        # Try to find place using only location
+        if not textsearch_results_candidates:
+            textsearch_results = cls.google_maps_textsearch_async(team.location).get_result()
+            if textsearch_results:
+                textsearch_results_candidates.append(textsearch_results)
+
+        # Try to find place using only city, country
+        if not textsearch_results_candidates and team.city and team.country:
+            textsearch_results = cls.google_maps_textsearch_async(u'{} {}'.format(team.city, team.country)).get_result()
+            if textsearch_results:
+                textsearch_results_candidates.append(textsearch_results)
+
+        if not textsearch_results_candidates:
+            logging.warning("Finding textsearch results for team {} failed!".format(team.key.id()))
+            return {}
+
+        # Consider all candidates and find best one
+        for textsearch_results in textsearch_results_candidates:
+            for textsearch_result in textsearch_results:
+                location_info = cls.construct_location_info_async(textsearch_result).get_result()
+                score = cls.compute_team_location_score(team, location_info)
+                if score == 1:
+                    return location_info
+                elif score > best_score:
+                    best_score = score
+                    best_location_info = location_info
+
+        return best_location_info
 
     @classmethod
-    def get_lat_lon(cls, location):
-        return cls.get_lat_lon_async(location).get_result()
+    def compute_team_location_score(cls, team, location_info):
+        """
+        Score for correctness. 1.0 is perfect.
+        Not checking for absolute equality in case of existing data errors.
+        Check with both long and short names
+        """
+        max_score = 4.0
+        score = 0.0
+        if team.country and location_info.get('country', None) and \
+                (team.country.lower() in location_info['country'].lower() or
+                location_info['country'].lower() in team.country.lower() or
+                team.country.lower() in location_info['country_short'].lower() or
+                location_info['country_short'].lower() in team.country.lower()):
+            score += 1
+            print 1
+        if team.state_prov and location_info.get('state_prov', None) and \
+                (team.state_prov.lower() in location_info['state_prov'].lower() or
+                location_info['state_prov'].lower() in team.state_prov.lower() or
+                team.state_prov.lower() in location_info['state_prov_short'].lower() or
+                location_info['state_prov_short'].lower() in team.state_prov.lower()):
+            score += 1
+            print 2
+        if team.city and location_info.get('city', None) and \
+                (team.city.lower() in location_info['city'].lower() or
+                location_info['city'].lower() in team.city.lower()):
+            score += 1
+            print 3
+        if team.postalcode and location_info.get('postal_code', None) and \
+                (team.postalcode.lower() in location_info['postal_code'].lower() or
+                location_info['postal_code'].lower() in team.postalcode.lower()):
+            # If postal code is right and anything else is right, the confidence is very high
+            score += 3
+            print 4
+        print score
+        return min(1.0, score / max_score)
 
     @classmethod
     @ndb.tasklet
-    def get_lat_lon_async(cls, location):
-        cache_key = u'get_lat_lon_{}'.format(location)
-        result = memcache.get(cache_key)
-        if not result:
-            lat_lon = None
-            num_results = 0
+    def construct_location_info_async(cls, textsearch_result):
+        """
+        Gets location info given a textsearch result
+        """
+        location_info = {
+            'formatted_address': textsearch_result['formatted_address'],
+            'lat': textsearch_result['geometry']['location']['lat'],
+            'lng': textsearch_result['geometry']['location']['lng'],
+            'name': textsearch_result['name'],
+        }
+        geocode_results = yield cls.google_maps_geocode_async(textsearch_result['formatted_address'])
+        if geocode_results:
+            for component in geocode_results[0]['address_components']:
+                if 'street_number' in component['types']:
+                    location_info['street_number'] = component['long_name']
+                elif 'route' in component['types']:
+                    location_info['street'] = component['long_name']
+                elif 'locality' in component['types']:
+                    location_info['city'] = component['long_name']
+                elif 'administrative_area_level_1' in component['types']:
+                    location_info['state_prov'] = component['long_name']
+                    location_info['state_prov_short'] = component['short_name']
+                elif 'country' in component['types']:
+                    location_info['country'] = component['long_name']
+                    location_info['country_short'] = component['short_name']
+                elif 'postal_code' in component['types']:
+                    location_info['postal_code'] = component['long_name']
 
-            if not location:
-                raise ndb.Return(lat_lon, num_results)
+            location_info['formatted_address'] = geocode_results[0]['formatted_address']
+            location_info['lat'] = geocode_results[0]['geometry']['location']['lat']
+            location_info['lng'] = geocode_results[0]['geometry']['location']['lng']
+            location_info['location_type'] = geocode_results[0]['geometry']['location_type']
 
-            location = location.encode('utf-8')
+        raise ndb.Return(location_info)
 
-            google_secrets = Sitevar.get_by_id("google.secrets")
-            google_api_key = None
-            if google_secrets is None:
-                logging.warning("Missing sitevar: google.api_key. API calls rate limited by IP and may be over rate limit.")
-            else:
-                google_api_key = google_secrets.contents['api_key']
+    @classmethod
+    @ndb.tasklet
+    def google_maps_textsearch_async(cls, query):
+        """
+        https://developers.google.com/places/web-service/search#TextSearchRequests
+        """
+        print "TEXTSEARCH " + query.encode('ascii', 'ignore')
+        if not GOOGLE_API_KEY:
+            logging.warning("Must have sitevar google.api_key to use Google Maps Textsearch")
+            raise ndb.Return(None)
 
-            # textsearch request
-            if google_api_key:
+        results = None
+        if query:
+            cache_key = u'google_maps_textsearch:{}'.format(query.encode('ascii', 'ignore'))
+            query = query.encode('utf-8')
+            results = memcache.get(cache_key)
+            if not results:
                 textsearch_params = {
-                    'query': location,
-                    'key': google_api_key,
+                    'query': query,
+                    'key': GOOGLE_API_KEY,
                 }
                 textsearch_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?%s' % urllib.urlencode(textsearch_params)
                 try:
+                    # Make async urlfetch call
                     rpc = urlfetch.create_rpc()
                     urlfetch.make_fetch_call(rpc, textsearch_url)
                     textsearch_result = yield rpc
+
+                    # Parse urlfetch result
                     if textsearch_result.status_code == 200:
                         textsearch_dict = json.loads(textsearch_result.content)
                         if textsearch_dict['status'] == 'ZERO_RESULTS':
-                            logging.info('No textsearch results for location: {}'.format(location))
+                            logging.info('No textsearch results for query: {}'.format(query))
                         elif textsearch_dict['status'] == 'OK':
-                            lat_lon = textsearch_dict['results'][0]['geometry']['location']['lat'], textsearch_dict['results'][0]['geometry']['location']['lng']
-                            num_results = len(textsearch_dict['results'])
+                            results = textsearch_dict['results']
                         else:
                             logging.warning('Textsearch failed!')
                             logging.warning(textsearch_dict)
@@ -110,24 +275,44 @@ class LocationHelper(object):
                 except Exception, e:
                     logging.warning('urlfetch for textsearch request failed with url {}.'.format(textsearch_url))
                     logging.warning(e)
-            else:
-                # Fallback to geocode request
+
+                if tba_config.CONFIG['memcache']:
+                    memcache.set(cache_key, result)
+        raise ndb.Return(results)
+
+    @classmethod
+    @ndb.tasklet
+    def google_maps_geocode_async(cls, address):
+        """
+        https://developers.google.com/maps/documentation/geocoding/start
+        """
+        print "GEOCODE " + address.encode('ascii', 'ignore')
+        results = None
+        if address:
+            cache_key = u'google_maps_geocode:{}'.format(address.encode('ascii', 'ignore'))
+            address = address.encode('utf-8')
+            results = memcache.get(cache_key)
+            if not results:
                 geocode_params = {
-                    'address': location,
+                    'address': address,
                     'sensor': 'false',
                 }
+                if GOOGLE_API_KEY:
+                    geocode_params['key'] = GOOGLE_API_KEY
                 geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?%s' % urllib.urlencode(geocode_params)
                 try:
+                    # Make async urlfetch call
                     rpc = urlfetch.create_rpc()
                     urlfetch.make_fetch_call(rpc, geocode_url)
                     geocode_result = yield rpc
+
+                    # Parse urlfetch call
                     if geocode_result.status_code == 200:
                         geocode_dict = json.loads(geocode_result.content)
                         if geocode_dict['status'] == 'ZERO_RESULTS':
-                            logging.info('No geocode results for location: {}'.format(location))
+                            logging.info('No geocode results for address: {}'.format(address))
                         elif geocode_dict['status'] == 'OK':
-                            lat_lon = geocode_dict['results'][0]['geometry']['location']['lat'], geocode_dict['results'][0]['geometry']['location']['lng']
-                            num_results = len(geocode_dict['results'])
+                            results = geocode_dict['results']
                         else:
                             logging.warning('Geocoding failed!')
                             logging.warning(geocode_dict)
@@ -137,10 +322,9 @@ class LocationHelper(object):
                     logging.warning('urlfetch for geocode request failed with url {}.'.format(geocode_url))
                     logging.warning(e)
 
-            result = lat_lon, num_results
-            memcache.set(cache_key, result)
-
-        raise ndb.Return(result)
+                if tba_config.CONFIG['memcache']:
+                    memcache.set(cache_key, result)
+        raise ndb.Return(results)
 
     @classmethod
     def get_timezone_id(cls, location, lat_lon=None):
