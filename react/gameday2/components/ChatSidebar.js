450dc8c0a7f9fa6435242189c9c8033b4129d0fc
Nathan Walters <nwalters512@gmail.com>
2016-12-02 10:49:01 -0600

    Defer loading of chat sidebar until it is opened for the first time

--- react/gameday2/components/ChatSidebar.js
+++ react/gameday2/components/ChatSidebar.js
@@ -6,22 +6,31 @@ const ChatSidebar = (props) => {
     hidden: !props.enabled,
     'chat-sidebar': true,
   })
-  return (
-    <div className={classes}>
-      <iframe
-        frameBorder="0"
-        scrolling="no"
-        id="chat_embed"
-        src="http://twitch.tv/chat/embed?channel=tbagameday&amp;popout_chat=true"
-        height="100%"
-        width="100%"
-      />
-    </div>
-  )
+
+  let content
+  if (props.hasBeenVisible) {
+    content = (
+      <div className={classes}>
+        <iframe
+          frameBorder="0"
+          scrolling="no"
+          id="chat_embed"
+          src="http://twitch.tv/chat/embed?channel=tbagameday&amp;popout_chat=true"
+          height="100%"
+          width="100%"
+        />
+      </div>
+    )
+  } else {
+    content = (<div />)
+  }
+
+  return content
 }
 
 ChatSidebar.propTypes = {
   enabled: PropTypes.bool,
+  hasBeenVisible: PropTypes.bool,
 }
 
 export default ChatSidebar
