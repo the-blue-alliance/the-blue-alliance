1fc9c60dd9c0694d835a53039333a3f4ddc43984
Nathan Walters <nwalters512@gmail.com>
2016-12-18 17:40:14 -0600

    Converted GamedayFrame to stateless functional component

--- react/gameday2/components/GamedayFrame.js
+++ react/gameday2/components/GamedayFrame.js
@@ -4,30 +4,13 @@ import MainContentContainer from '../containers/MainContentContainer'
 import ChatSidebarContainer from '../containers/ChatSidebarContainer'
 import HashtagSidebarContainer from '../containers/HashtagSidebarContainer'
 
-const GamedayFrame = React.createClass({
-  getInitialState() {
-    return {
-      followingTeams: [177, 230],
-    }
-  },
-  handleFollowTeam(team) {
-    const newFollowingTeams = this.state.followingTeams.concat([team])
-    this.setState({ followingTeams: newFollowingTeams })
-  },
-  handleUnfollowTeam(team) {
-    const newFollowingTeams = this.state.followingTeams.filter((a) => a !== team)
-    this.setState({ followingTeams: newFollowingTeams })
-  },
-  render() {
-    return (
-      <div className="gameday container-full">
-        <GamedayNavbarContainer />
-        <HashtagSidebarContainer />
-        <ChatSidebarContainer />
-        <MainContentContainer />
-      </div>
-    )
-  },
-})
+const GamedayFrame = () => (
+  <div className="gameday container-full">
+    <GamedayNavbarContainer />
+    <HashtagSidebarContainer />
+    <ChatSidebarContainer />
+    <MainContentContainer />
+  </div>
+)
 
 export default GamedayFrame
