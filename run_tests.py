8ce1ada02b1c48fc1b92ee37a1372cd12313aaf0
Phil Lopreiato <plnyyanks@gmail.com>
2016-12-18 16:00:29 -0500

    Tests for webcast review.
    Requires setting some environment variables to make django work...

--- run_tests.py
+++ run_tests.py
@@ -3,9 +3,11 @@ import multiprocessing
 import optparse
 import StringIO
 import os
+import random
+import string
 import sys
 import time
-import warnings
+import django.conf.global_settings
 
 # Install the Python unittest2 package before you run this script.
 import unittest2
@@ -28,6 +30,10 @@ def start_suite(suite, queue):
 
 def main(sdk_path, test_pattern):
     start_time = time.time()
+
+    # Fix django template loaders being messed up
+    django.conf.global_settings.SECRET_KEY = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django.conf.global_settings')
 
     sys.path.insert(0, sdk_path)
     import dev_appserver
