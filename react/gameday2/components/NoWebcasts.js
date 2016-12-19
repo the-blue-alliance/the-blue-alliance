6c92d657c2083ae92cdf668f1dcb687fe6db25ec
Nathan Walters <nwalters512@gmail.com>
2016-12-18 18:31:43 -0600

    Removed Bootstrap button from the 'no webcasts' landing page

--- react/gameday2/components/NoWebcasts.js
+++ react/gameday2/components/NoWebcasts.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default () => (
   <div className="no-webcasts-container">
     <h1>No webcasts found</h1>
     <p>Looks like there aren&apos;t any events with webcasts this week. Check on The Blue Alliance for upcoming events!</p>
-    <a className="btn btn-default" href="https://thebluealliance.com">Go to The Blue Alliance</a>
+    <RaisedButton
+      href="https://thebluealliance.com"
+      label="Go to The Blue Alliance"
+    />
   </div>
 )
