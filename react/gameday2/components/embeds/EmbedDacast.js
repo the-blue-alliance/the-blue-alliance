2728262e4d1f735e85307aa0e530229c5f942650
Nathan Walters <nwalters512@gmail.com>
2016-12-21 22:25:48 -0600

    Fixed Dacast embed

--- react/gameday2/components/embeds/EmbedDacast.js
+++ react/gameday2/components/embeds/EmbedDacast.js
@@ -4,13 +4,13 @@ import { webcastPropType } from '../../utils/webcastUtils'
 const EmbedDacast = (props) => {
   const channel = props.webcast.channel
   const file = props.webcast.file
-  const iframeSrc = `https://static.viewer.dacast.com/b/${channel}/c/{file}`
+  const iframeSrc = `https://static.viewer.dacast.com/b/${channel}/c/${file}`
   return (
     <iframe
       src={iframeSrc}
       width="100%"
       height="100%"
-      frameborder="0"
+      frameBorder="0"
       scrolling="no"
     />
   )
