00a2da010e72ebe9e0d26f40e8b27b38127e34dc
Nathan Walters <nwalters512@gmail.com>
2017-01-01 20:04:06 -0600

    Added animations for the chat selection panel

--- react/gameday2/components/ChatSelector.js
+++ react/gameday2/components/ChatSelector.js
@@ -1,8 +1,61 @@
 import React, { PropTypes } from 'react'
+import ReactTransitionGroup from 'react-addons-transition-group'
 import { List, ListItem } from 'material-ui/List'
+import Paper from 'material-ui/Paper'
 import CheckmarkIcon from 'material-ui/svg-icons/navigation/check'
 import { fullWhite } from 'material-ui/styles/colors'
 import { chatPropType } from '../utils/PropTypes'
+
+class AnimatableContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      style: props.beginStyle,
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.enterTimeout)
+    clearTimeout(this.leaveTimeout)
+  }
+
+  componentWillEnter(callback) {
+    this.componentWillAppear(callback)
+  }
+
+  componentWillAppear(callback) {
+    // Timeout needed so that the component can render with the original styles
+    // before we apply the ones to transition to
+    setTimeout(() => {this.setState({
+      style: this.props.endStyle
+    })}, 0)
+
+    this.enterTimeout = setTimeout(callback, 300)
+  }
+
+  componentWillLeave(callback) {
+    this.setState({
+      style: this.props.beginStyle,
+    })
+
+    this.leaveTimeout = setTimeout(callback, 300)
+  }
+
+  render() {
+    const {
+      style,
+      children,
+      ...other
+    } = this.props
+
+    return (
+      <div {...other} style={Object.assign({}, style, this.state.style)}>
+        {children}
+      </div>
+    )
+  }
+}
 
 export default class ChatSelector extends React.Component {
   static propTypes = {
@@ -40,34 +93,66 @@ export default class ChatSelector extends React.Component {
       )
     })
 
-    const containerStyle = {
+    const overlayStyle = {
       width: '100%',
       height: '100%',
       background: 'rgba(0,0,0,0.7)',
       position: 'absolute',
+      transition: 'all 150ms ease-in',
+      willChange: 'opacity'
     }
 
     const listStyle = {
       width: '100%',
       position: 'absolute',
       bottom: 0,
-      background: fullWhite,
+      transition: 'all 150ms ease-in',
+      willChange: 'transform opacity',
     }
 
-    if (this.props.open) {
-      return (
-        <div
-          style={containerStyle}
-          onTouchTap={() => this.props.onRequestClose()}>
-          <List
+    return (
+      <ReactTransitionGroup
+        component="div"
+        transitionAppear={true}
+        transitionAppearTimeout={150}
+        transitionEnter={true}
+        transitionEnterTimeout={150}
+      >
+        <div />
+        {this.props.open &&
+          <AnimatableContainer
+            key="overlay"
+            style={overlayStyle}
+            onTouchTap={() => this.props.onRequestClose()}
+            beginStyle={{
+              opacity: 0,
+            }}
+            endStyle={{
+              opacity: 1,
+            }}
+          />
+        }
+        {this.props.open &&
+          <AnimatableContainer
+            key="selector"
             style={listStyle}
-            onTouchTap={(e) => e.stopPropagation()}>
-            {chatItems}
-          </List>
-        </div>
-      )
-    }
-
-    return (<div></div>)
+            beginStyle={{
+              opacity: 0,
+              transform: 'translate(0, 50%)'
+            }}
+            endStyle={{
+              opacity: 1,
+              transform: 'translate(0, 0)'
+            }}
+          >
+            <Paper zDepth={4}>
+              <List onTouchTap={(e) => e.stopPropagation()}>
+                {chatItems}
+              </List>
+            </Paper>
+          </AnimatableContainer>
+        }
+      </ReactTransitionGroup>
+    )
   }
 }
