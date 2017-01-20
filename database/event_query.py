8c7eeafca54c29165424570b9fccfa9ae6507a40
Phil Lopreiato <plnyyanks@gmail.com>
2017-01-20 17:41:51 -0500

    Update queries to use Districts

--- database/event_query.py
+++ database/event_query.py
@@ -3,6 +3,7 @@ from google.appengine.ext import ndb
 from consts.district_type import DistrictType
 from database.database_query import DatabaseQuery
 from database.dict_converters.event_converter import EventConverter
+from models.district import District
 from models.event import Event
 from models.event_team import EventTeam
 from models.team import Team
@@ -33,19 +34,15 @@ class EventListQuery(DatabaseQuery):
 
 
 class DistrictEventsQuery(DatabaseQuery):
-    CACHE_VERSION = 1
+    CACHE_VERSION = 2
     CACHE_KEY_FORMAT = 'district_events_{}'  # (district_key)
     DICT_CONVERTER = EventConverter
 
     @ndb.tasklet
     def _query_async(self):
         district_key = self._query_args[0]
-        year = int(district_key[:4])
-        district_abbrev = district_key[4:]
-        district_type = DistrictType.abbrevs.get(district_abbrev, None)
         events = yield Event.query(
-            Event.event_district_enum == district_type,
-            Event.year == year).fetch_async()
+            Event.district_key == ndb.Key(District, district_key)).fetch_async()
         raise ndb.Return(events)
 
 
