ccac42d46abcfeddd62ff6c615ec07391c7cbf37
Eugene Fang <fang.eugene@gmail.com>
2017-01-25 19:25:18 -0500

    Surrogates (#1755)
    * save surrogates from FRC API
    
    * Show surrogate teams in matches
    
    * Update tests to support surrogates
    
    * Match helper to add surrogate info to matches, tests
    
    * Fix tests
    
    * Add surrogates if not present

---
+++ tests/test_add_surrogates.py
@@ -0,0 +1,42 @@
+import json
+
+import unittest2
+from appengine_fixture_loader.loader import load_fixture
+from google.appengine.ext import testbed
+from google.appengine.ext import ndb
+
+from helpers.match_helper import MatchHelper
+from models.event import Event
+from models.match import Match
+
+
+class TestAddSurrogates(unittest2.TestCase):
+    def event_key_adder(self, obj):
+        obj.event = ndb.Key(Event, '2016cama')
+
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_taskqueue_stub(root_path=".")
+        self.testbed.init_memcache_stub()
+        ndb.get_context().clear_cache()  # Prevent data from leaking between tests
+
+        load_fixture('test_data/2016cama_no_surrogate.json',
+                      kind={'Event': Event, 'Match': Match},
+                      post_processor=self.event_key_adder)
+        self.event = Event.get_by_id('2016cama')
+        self.assertIsNotNone(self.event)
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def testEventWinner(self):
+        MatchHelper.add_surrogates(self.event)
+        for match in self.event.matches:
+            if match.comp_level != 'qm' or match.match_number != 18:
+                for alliance_color in ['red', 'blue']:
+                    self.assertEqual(match.alliances[alliance_color]['surrogates'], [])
+            else:
+                self.assertEqual(match.alliances['red']['surrogates'], ['frc5496'])
+                self.assertEqual(match.alliances['blue']['surrogates'], ['frc1323'])
