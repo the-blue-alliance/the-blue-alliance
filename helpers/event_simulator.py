0f6fdcff5f794ad2e4eb325c9f61ecc537997c4d
Eugene Fang <fang.eugene@gmail.com>
2017-02-10 12:34:15 -0500

    More event simulator testing WIP

--- helpers/event_simulator.py
+++ helpers/event_simulator.py
@@ -1,7 +1,11 @@
+import copy
+import datetime
 import json
+
 from appengine_fixture_loader.loader import load_fixture
 from google.appengine.ext import ndb
 
+from helpers.match_helper import MatchHelper
 from models.event import Event
 from models.event_details import EventDetails
 from models.match import Match
@@ -10,57 +14,132 @@ from models.match import Match
 class EventSimulator(object):
     """
     Steps through an event in time. At step = 0, only the Event exists:
-    (1) Add all unplayed qual matches
-    (n) Add results of each of the n qual matches +
+    (step 0) Add all unplayed qual matches
+    (step 1, substep n) Add results of each of the n qual matches +
         rankings being updated (if has_event_details)
-    (1) Add alliance selections (if has_event_details)
-    (1) Add unplayed QF matches
-    (n) Add results of each of the n QF matches +
+    (step 2) Add alliance selections (if has_event_details)
+    (step 3) Add unplayed QF matches
+    (step 4, substep n) Add results of each of the n QF matches +
         update SF matches with advancing alliances (if not batch_advance) +
         update alliance selection backups (if has_event_details)
-    (1) Add unplayed SF matches (if batch_advance)
-    (n) Add results of each of the n SF matches +
+    (step 5) Add unplayed SF matches (if batch_advance)
+    (step 6, substep n) Add results of each of the n SF matches +
         update F matches with advancing alliances (if not batch_advance) +
         update alliance selection backups (if has_event_details)
-    (1) Add unplayed F matches (if batch_advance)
-    (n) Add results of each of the n F matches +
+    (step 7) Add unplayed F matches (if batch_advance)
+    (step 8, substep n) Add results of each of the n F matches +
         update alliance selection backups (if has_event_details)
     """
     def __init__(self, has_event_details=True, batch_advance=True):
         self._step = 0
+        self._substep = 0
         # whether to update rankings and alliance selections
         self._has_event_details = has_event_details
         # whether to update next playoff level all at once, or as winners are determined
         self._batch_advance = batch_advance
-        self.step()
+        # self.step()
 
-    def _reload_all(self):
+        # Load and save complete data
         load_fixture('test_data/fixtures/2016nytr_event_team_status.json',
               kind={'EventDetails': EventDetails, 'Event': Event, 'Match': Match},
               post_processor=self._event_key_adder)
+        event = Event.get_by_id('2016nytr')
+
+        # Add 3rd matches that never got played
+        Match(
+            id='2016nytr_qf1m3',
+            year=2016,
+            event=event.key,
+            comp_level='qf',
+            set_number=1,
+            match_number=3,
+            alliances_json=json.dumps({
+                'red': {
+                    'teams': [],
+                    'score': -1,
+                },
+                'blue': {
+                    'teams': [],
+                    'score': -1,
+                }
+            }),
+            time=datetime.datetime(2016, 3, 19, 18, 34),
+        ).put()
+        Match(
+            id='2016nytr_qf3m3',
+            year=2016,
+            event=event.key,
+            comp_level='qf',
+            set_number=3,
+            match_number=3,
+            alliances_json=json.dumps({
+                'red': {
+                    'teams': [],
+                    'score': -1,
+                },
+                'blue': {
+                    'teams': [],
+                    'score': -1,
+                }
+            }),
+            time=datetime.datetime(2016, 3, 19, 18, 48),
+        ).put()
+
+        self._event_detials = event.details
+        self._played_matches = MatchHelper.organizeMatches(event.matches)
+
+        # Delete data
+        event.details.key.delete()
+        ndb.delete_multi([match.key for match in event.matches])
+        ndb.get_context().clear_cache()
+
+    # def _reload_all(self):
+    #     load_fixture('test_data/fixtures/2016nytr_event_team_status.json',
+    #           kind={'EventDetails': EventDetails, 'Event': Event, 'Match': Match},
+    #           post_processor=self._event_key_adder)
 
     def _event_key_adder(self, obj):
         obj.event = ndb.Key(Event, '2016nytr')
 
     def step(self):
-        self._reload_all()
+        # self._reload_all()
         event = Event.get_by_id('2016nytr')
 
-        if self._step == 0:  # Before anything has happened
-            event.details.key.delete()
-            ndb.delete_multi([match.key for match in event.matches])
-        elif self._step == 1:  # Qual match schedule added
-            event.details.key.delete()
-            for match in event.matches:
-                if match.comp_level == 'qm':
-                    for alliance in ['red', 'blue']:
-                        match.alliances[alliance]['score'] = -1
-                    match.alliances_json = json.dumps(match.alliances)
-                    match.score_breakdown_json = None
-                    match.actual_time = None
-                    match.put()
-                else:
-                    match.key.delete()
+        if self._step == 0:  # Qual match schedule added
+            for match in copy.deepcopy(self._played_matches['qm']):
+                for alliance in ['red', 'blue']:
+                    match.alliances[alliance]['score'] = -1
+                match.alliances_json = json.dumps(match.alliances)
+                match.score_breakdown_json = None
+                match.actual_time = None
+                match.put()
+
+            self._step += 1
+        elif self._step == 1:  # After each qual match
+            self._played_matches['qm'][self._substep].put()
+            if self._substep < len(self._played_matches['qm']) - 1:
+                self._substep += 1
+            else:
+                self._step += 1
+                self._substep = 0
+        elif self._step == 2:  # After alliance selections
+            self._step += 1
+        elif self._step == 3:  # QF schedule added
+            for match in copy.deepcopy(self._played_matches['qf']):
+                for alliance in ['red', 'blue']:
+                    match.alliances[alliance]['score'] = -1
+                match.alliances_json = json.dumps(match.alliances)
+                match.score_breakdown_json = None
+                match.actual_time = None
+                match.put()
+
+            self._step += 1
+        elif self._step == 4:  # After each qf match
+            MatchHelper.play_order_sort_matches(self._played_matches['qf'])[self._substep].put()
+            if self._substep < len(self._played_matches['qf']) - 1:
+                self._substep += 1
+            else:
+                self._step += 1
+                self._substep = 0
 
         ndb.get_context().clear_cache()
-        self._step += 1
--- tests/test_event_simulator.py
+++ tests/test_event_simulator.py
@@ -5,12 +5,13 @@ from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
 from helpers.event_simulator import EventSimulator
+from helpers.match_helper import MatchHelper
 from models.event import Event
 from models.event_details import EventDetails
 from models.match import Match
 
 
-class TestAwardManipulator(unittest2.TestCase):
+class TestEventSimulator(unittest2.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
@@ -25,7 +26,7 @@ class TestAwardManipulator(unittest2.TestCase):
     def tearDown(self):
         self.testbed.deactivate()
 
-    def test_stuff(self):
+    def test_event_smulator(self):
         # Before anything has happened
         event = Event.get_by_id('2016nytr')
         self.assertNotEqual(event, None)
@@ -37,9 +38,67 @@ class TestAwardManipulator(unittest2.TestCase):
         event = Event.get_by_id('2016nytr')
         self.assertNotEqual(event, None)
         self.assertEqual(event.details, None)
-        self.assertNotEqual(event.matches, [])
+        self.assertEqual(len(event.matches), 72)
         for match in event.matches:
             self.assertEqual(match.comp_level, 'qm')
             self.assertFalse(match.has_been_played)
             self.assertEqual(match.score_breakdown, None)
             self.assertEqual(match.actual_time, None)
+
+        # After each qual match
+        for i in xrange(72):
+            self.es.step()
+            event = Event.get_by_id('2016nytr')
+            self.assertNotEqual(event, None)
+            # self.assertNotEqual(event.details, None)
+            # self.assertEqual(event.details.alliance_selections, None)
+            self.assertEqual(len(event.matches), 72)
+
+            matches = MatchHelper.play_order_sort_matches(event.matches)
+            for j, match in enumerate(matches):
+                if j <= i:
+                    self.assertTrue(match.has_been_played)
+                else:
+                    self.assertFalse(match.has_been_played)
+
+        # After alliance selections
+        self.es.step()
+        event = Event.get_by_id('2016nytr')
+        self.assertNotEqual(event, None)
+        # self.assertNotEqual(event.details, None)
+        # self.assertNotEqual(event.details.alliance_selections, None)
+        self.assertEqual(len(event.matches), 72)
+
+        # QF schedule added
+        self.es.step()
+        event = Event.get_by_id('2016nytr')
+        self.assertNotEqual(event, None)
+        self.assertEqual(event.details, None)
+        self.assertEqual(len(event.matches), 84)
+        for match in event.matches:
+            if match.comp_level == 'qm':
+                self.assertTrue(match.has_been_played)
+            else:
+                self.assertEqual(match.comp_level, 'qf')
+                self.assertFalse(match.has_been_played)
+                self.assertEqual(match.score_breakdown, None)
+                self.assertEqual(match.actual_time, None)
+
+        # After each QF match
+        for i in xrange(72, 84):
+            self.es.step()
+            event = Event.get_by_id('2016nytr')
+            self.assertNotEqual(event, None)
+            # self.assertNotEqual(event.details, None)
+            # self.assertEqual(event.details.alliance_selections, None)
+            self.assertEqual(len(event.matches), 84)
+
+            matches = MatchHelper.play_order_sort_matches(event.matches)
+            for j, match in enumerate(matches):
+                if match.key.id() in {'2016nytr_qf1m3', '2016nytr_qf3m3'}:
+                    # Unneeded tiebreak matches
+                    self.assertFalse(match.has_been_played)
+                elif j <= i:
+                    self.assertTrue(match.has_been_played)
+                else:
+                    self.assertFalse(match.has_been_played)
