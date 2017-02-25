ae62e351e32f9e1dcc957c682dd62184dad0655b
Phil Lopreiato <plnyyanks@gmail.com>
2017-02-24 21:21:17 -0500

    Calculate match hotness

--- helpers/bluezone_helper.py
+++ helpers/bluezone_helper.py
@@ -17,7 +17,15 @@ class BlueZoneHelper(object):
         return matches
 
     @classmethod
-    def should_add_match(cls, matches, candidate_match):
+    def get_upcoming_match_predictions(cls, live_events, n=5):
+        predictions = {}
+        for event in live_events:
+            if event.details and event.details.predictions:
+                predictions.update(event.details.predictions['match_predictions'])
+        return predictions
+
+    @classmethod
+    def should_add_match(cls, matches, candidate_match, predictions):
         # Can we put this match in the beginning of the list?
         if matches and candidate_match.predicted_time <= matches[0].predicted_time - cls.BUFFER_AFTER:
             return 0
@@ -26,10 +34,37 @@ class BlueZoneHelper(object):
             # Can we insert this match in between these two
             last_match = matches[i - 1]
             next_match = matches[i]
-            if candidate_match.predicted_time >= last_match.predicted_time + cls.BUFFER_AFTER and candidate_match.predicted_time + cls.BUFFER_AFTER <= next_match.predicted_time:
-                return i
+            if candidate_match.predicted_time >= last_match.predicted_time + cls.BUFFER_AFTER:
+                if candidate_match.predicted_time + cls.BUFFER_AFTER <= next_match.predicted_time:
+                    if candidate_match.key_name in predictions:
+                        return i
 
         return None
+
+    @classmethod
+    def calculate_match_hotness(cls, matches, predictions):
+        max_hotness = 0
+        min_hotness = float('inf')
+        for match in matches:
+            if not match.has_been_played and match.key.id() in predictions:
+                prediction = predictions[match.key.id()]
+                red_score = prediction['red']['score']
+                blue_score = prediction['blue']['score']
+                if red_score > blue_score:
+                    winner_score = red_score
+                    loser_score = blue_score
+                else:
+                    winner_score = blue_score
+                    loser_score = red_score
+
+                hotness = winner_score + 2.0*loser_score  # Favor close high scoring matches
+
+                max_hotness = max(max_hotness, hotness)
+                min_hotness = min(min_hotness, hotness)
+                match.hotness = hotness
+
+        for match in matches:
+            match.hotness = 100 * (match.hotness - min_hotness) / (max_hotness - min_hotness)
 
     @classmethod
     def update_bluezone(cls, live_events):
@@ -41,16 +76,19 @@ class BlueZoneHelper(object):
         4.
         """
         upcoming_matches = cls.get_upcoming_matches(live_events)
-        # TODO calculate hotness and sort
+        upcoming_predictions = cls.get_upcoming_match_predictions(live_events)
+
+        cls.calculate_match_hotness(upcoming_matches, upcoming_predictions)
+        upcoming_matches.sort(lambda x: -match.hotness)
 
         bluezone_matches = []
-        while len(bluezone_matches)< 5:
+        while len(bluezone_matches) < 5:
             match = upcoming_matches.pop(0)
             now = datetime.datetime.now()
             if match.predicted_time < now:
                 # No use switching to this match
                 continue
 
-            possible_index = cls.should_add_match(bluezone_matches, match)
-            if possible_index:
+            possible_index = cls.should_add_match(bluezone_matches, match, upcoming_predictions)
+            if possible_index is not None:
                 bluezone_matches.insert(possible_index, match)
