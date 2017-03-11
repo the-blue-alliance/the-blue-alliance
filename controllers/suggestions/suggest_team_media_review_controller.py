5f4686649d483b8e5e645390f850f53657f66ed6
Phil Lopreiato <plnyyanks@gmail.com>
2017-03-10 23:23:22 -0500

    Fix team media suggestions not marked accepted
    Introduced in 09ed4725258c8f556339e5b64e8e54606b669308

--- controllers/suggestions/suggest_team_media_review_controller.py
+++ controllers/suggestions/suggest_team_media_review_controller.py
@@ -90,7 +90,8 @@ class SuggestTeamMediaReviewController(SuggestionsReviewBaseController):
         if to_replace_id:
             to_replace = Media.get_by_id(to_replace_id)
             if team_reference not in to_replace.preferred_references:
-                return  # Preferred reference must have been edited earlier. Skip this Suggestion for now.
+                # Preferred reference must have been edited earlier. Skip this Suggestion for now.
+                return
             to_replace.preferred_references.remove(team_reference)
 
         # Add preferred reference to current Media (images only) if explicitly listed in preferred_keys or if to_replace_id exists
@@ -101,15 +102,10 @@ class SuggestTeamMediaReviewController(SuggestionsReviewBaseController):
 
         media = MediaCreator.create_media_model(suggestion, team_reference, preferred_references)
 
-        # Mark Suggestion as accepted
-        suggestion.review_state = Suggestion.REVIEW_ACCEPTED
-        suggestion.reviewer = self.user_bundle.account.key
-        suggestion.reviewed_at = datetime.datetime.now()
-
         # Do all DB writes
         if to_replace:
             MediaManipulator.createOrUpdate(to_replace, auto_union=False)
-        MediaManipulator.createOrUpdate(media)
+        return MediaManipulator.createOrUpdate(media)
 
     def post(self):
         self.preferred_keys = self.request.POST.getall("preferred_keys[]")
