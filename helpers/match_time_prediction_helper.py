01714a368f566ca4cb67c7a6e8e34a56af9e0db3
Phil Lopreiato <plnyyanks@gmail.com>
2017-03-31 10:33:45 -0400

    No longer restrict time predictions to schedule

--- helpers/match_time_prediction_helper.py
+++ helpers/match_time_prediction_helper.py
@@ -105,12 +105,10 @@ class MatchTimePredictionHelper(object):
             else:
                 predicted = match.time
 
-            # Never predict a match to happen more than 2 minutes ahead of schedule or in the past
-            # However, if the event is not live (we're running the job manually for a single event),
+            # If the event is not live (we're running the job manually for a single event),
             # then allow predicted times to be in the past.
             now = datetime.datetime.now(timezone) + cls.MAX_IN_PAST if is_live else cls.as_local(cls.EPOCH, timezone)
-            earliest_possible = cls.as_local(match.time + datetime.timedelta(minutes=-2), timezone)
-            match.predicted_time = max(cls.as_utc(predicted), cls.as_utc(earliest_possible), cls.as_utc(now))
+            match.predicted_time = max(cls.as_utc(predicted), cls.as_utc(now))
             last = match
 
         MatchManipulator.createOrUpdate(unplayed_matches)
