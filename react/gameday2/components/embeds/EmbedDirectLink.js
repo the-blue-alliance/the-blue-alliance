e94656a5541e7978248a8f463962972a22e6e767
Eugene Fang <fang.eugene@gmail.com>
2017-04-02 23:58:16 -0400

    Lint fixes

--- react/gameday2/components/embeds/EmbedDirectLink.js
+++ react/gameday2/components/embeds/EmbedDirectLink.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import { webcastPropType } from '../../utils/webcastUtils'
 
 const EmbedDirectLink = (props) => {
   const directLink = props.webcast.channel
   const style = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translateX(-50%) translateY(-50%)',
-    }
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translateX(-50%) translateY(-50%)',
+  }
 
   return (
     <Card style={style}>
