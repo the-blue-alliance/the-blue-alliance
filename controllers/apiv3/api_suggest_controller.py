0a9772c3d2b576ef3a5707feffca501dea997fb5
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-04-05 08:14:22 -0700

    Abort on failed participation query

--- controllers/apiv3/api_suggest_controller.py
+++ controllers/apiv3/api_suggest_controller.py
@@ -12,32 +12,29 @@ class ApiSuggestTeamMediaController(ApiSuggestBaseController):
         self._track_call_defer('team/suggest/media', team_key)
 
     def _render(self, team_key, year):
-        if year in TeamParticipationQuery(team_key).fetch():
-            if 'media_url' in self.request.POST:
-                status, suggestion = SuggestionCreator.createTeamMediaSuggestion(
-                    author_account_key=self.auth_owner_key,
-                    media_url=self.request.POST["media_url"],
-                    team_key=team_key,
-                    year_str=year)
+        if year not in TeamParticipationQuery(team_key).fetch():
+            self.abort(400)
 
-                if status == 'success':
-                    message = {
-                        "success": True
-                    }
-                else:
-                    message ={
-                        "success": False,
-                        "message": status
-                    }
-            else:
+        if 'media_url' in self.request.POST:
+            status, suggestion = SuggestionCreator.createTeamMediaSuggestion(
+                author_account_key=self.auth_owner_key,
+                media_url=self.request.POST["media_url"],
+                team_key=team_key,
+                year_str=year)
+
+            if status == 'success':
                 message = {
+                    "success": True
+                }
+            else:
+                message ={
                     "success": False,
-                    "message": "missing media_url"
+                    "message": status
                 }
         else:
             message = {
                 "success": False,
-                "message": "invalid year"
+                "message": "missing media_url"
             }
 
         return json.dumps(message, ensure_ascii=True, indent=2, sort_keys=True)
