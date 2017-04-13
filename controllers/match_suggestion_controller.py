a17b1fdddbf76a51e6ac677fea0e37ff097d709e
Eugene Fang <fang.eugene@gmail.com>
2017-04-13 18:34:50 -0400

    ranks

--- controllers/match_suggestion_controller.py
+++ controllers/match_suggestion_controller.py
@@ -19,6 +19,7 @@ class MatchSuggestionHandler(LoggedInHandler):
 
         current_matches = []
         upcoming_matches = []
+        ranks = {}
         for event in current_events:
             for i, match in enumerate(MatchHelper.upcomingMatches(event.matches, num=3)):
                 match.prediction = event.details.predictions['match_predictions']['qual' if match.comp_level == 'qm' else 'playoff'][match.key.id()]
@@ -26,12 +27,15 @@ class MatchSuggestionHandler(LoggedInHandler):
                     current_matches.append(match)
                 else:
                     upcoming_matches.append(match)
+            for rank in event.details.rankings2:
+                ranks[rank['team_key']] = rank['rank']
         current_matches = sorted(current_matches, key=lambda m: m.predicted_time if m.predicted_time else m.time)
         upcoming_matches = sorted(upcoming_matches, key=lambda m: m.predicted_time if m.predicted_time else m.time)
 
         self.template_values.update({
             'current_matches': current_matches,
             'upcoming_matches': upcoming_matches,
+            'ranks': ranks,
         })
 
         self.response.out.write(jinja2_engine.render('match_suggestion.html', self.template_values))
