282b66dbd40ce5370801a6cd58ed46165819e79f
Eugene Fang <fang.eugene@gmail.com>
2017-04-13 14:25:04 -0400

    Basic timeline working

--- controllers/admin/bluezone_controller.py
+++ controllers/admin/bluezone_controller.py
@@ -1,11 +1,19 @@
+import json
+
 from controllers.base_controller import LoggedInHandler
 from template_engine import jinja2_engine
+
+from helpers.event_helper import EventHelper
 
 
 class BlueZoneDebugHandler(LoggedInHandler):
     def get(self):
         self._require_admin()
 
-        self.template_values.update({})
+        current_events = filter(lambda e: e.now, EventHelper.getEventsWithinADay())
+
+        self.template_values.update({
+            'event_keys_json': json.dumps([e.key.id() for e in current_events]),
+        })
 
         self.response.out.write(jinja2_engine.render('bluezone_debug.html', self.template_values))
