d31af457ab828cad05003e0d3dd079dff4394726
Phil Lopreiato <plnyyanks@gmail.com>
2017-04-22 16:23:02 -0400

    Show alliance name/record in round robin advancement

--- helpers/match_helper.py
+++ helpers/match_helper.py
@@ -271,6 +271,7 @@ class MatchHelper(object):
     @classmethod
     def generatePlayoffAdvancementRoundRobin(cls, matches, alliance_selections=None):
         complete_alliances = []
+        alliance_names = []
         advancement = defaultdict(list)  # key: comp level; value: list of [complete_alliance, [champ_points], sum_champ_points, [match_points], sum_match_points
         for comp_level in ['sf']:  # In case this needs to scale to more levels
             any_unplayed = False
@@ -279,26 +280,33 @@ class MatchHelper(object):
                     any_unplayed = True
                 for color in ['red', 'blue']:
                     alliance = cls.getOrderedAlliance(match.alliances[color]['teams'], alliance_selections)
+                    alliance_name = cls.getAllianceName(match.alliances[color]['teams'], alliance_selections)
                     for i, complete_alliance in enumerate(complete_alliances):  # search for alliance. could be more efficient
                         if len(set(alliance).intersection(set(complete_alliance))) >= 2:  # if >= 2 teams are the same, then the alliance is the same
                             backups = list(set(alliance).difference(set(complete_alliance)))
                             complete_alliances[i] += backups  # ensures that backup robots are listed last
+                            alliance_names[i] = alliance_name
                             break
                     else:
                         i = None
                         complete_alliances.append(alliance)
+                        alliance_names.append(alliance_name)
 
                     is_new = False
                     if i is not None:
-                        for j, (complete_alliance, champ_points, _, match_points, _) in enumerate(advancement[comp_level]):  # search for alliance. could be more efficient
+                        for j, (complete_alliance, champ_points, _, match_points, _, _, record) in enumerate(advancement[comp_level]):  # search for alliance. could be more efficient
                             if len(set(complete_alliances[i]).intersection(set(complete_alliance))) >= 2:  # if >= 2 teams are the same, then the alliance is the same
-                                complete_alliance = complete_alliances[i]
-                                if match.winning_alliance == color:
+                                if not match.has_been_played:
+                                    cp = 0
+                                elif match.winning_alliance == color:
                                     cp = 2
+                                    record['wins'] += 1
                                 elif match.winning_alliance == '':
                                     cp = 1
+                                    record['losses'] += 1
                                 else:
                                     cp = 0
+                                    record['ties'] += 1
                                 if match.has_been_played:
                                     champ_points.append(cp)
                                     match_points.append(match.alliances[color]['score'])
@@ -309,18 +317,22 @@ class MatchHelper(object):
                             is_new = True
 
                     score = match.alliances[color]['score'] if match.has_been_played else 0
+                    record = {'wins': 0, 'losses': 0, 'ties': 0}
                     if not match.has_been_played:
                         cp = 0
                     elif match.winning_alliance == color:
                         cp = 2
+                        record['wins'] += 1
                     elif match.winning_alliance == '':
                         cp = 1
+                        record['losses'] += 1
                     else:
                         cp = 0
+                        record['ties'] += 1
                     if i is None:
-                        advancement[comp_level].append([alliance, [cp], cp, [score], score])
+                        advancement[comp_level].append([alliance, [cp], cp, [score], score, alliance_name, record])
                     elif is_new:
-                        advancement[comp_level].append([complete_alliances[i], [cp], cp, [score], score])
+                        advancement[comp_level].append([complete_alliances[i], [cp], cp, [score], score, alliance_names[i], record])
 
             advancement[comp_level] = sorted(advancement[comp_level], key=lambda x: -x[4])  # sort by match points
             advancement[comp_level] = sorted(advancement[comp_level], key=lambda x: -x[2])  # sort by championship points
@@ -343,6 +355,16 @@ class MatchHelper(object):
             # Strip the "frc" prefix
             team_nums.append(team[3:])
         return team_nums
+
+    @classmethod
+    def getAllianceName(cls, team_keys, alliance_selections):
+        if alliance_selections:
+            for alliance_selection in alliance_selections:  # search for alliance. could be more efficient
+                picks = alliance_selection['picks']
+                if len(set(picks).intersection(set(team_keys))) >= 2:  # if >= 2 teams are the same, then the alliance is the same
+                    return alliance_selection.get('name')
+
+        return ''
 
     """
     Valid breakdowns are those used for seeding. Varies by year.
