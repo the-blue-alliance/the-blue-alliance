03a655d8efff469e697e29fa9671c7f939d5114e
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-07-07 13:13:13 -0700

    Add string stripping tests

--- tests/test_website_helper.py
+++ tests/test_website_helper.py
@@ -7,6 +7,7 @@ class TestWebsiteHelper(unittest2.TestCase):
     def test_format_url(self):
         # No scheme
         self.assertEqual(WebsiteHelper.format_url('website.com'), 'http://website.com')
+        self.assertEqual(WebsiteHelper.format_url(' website.com '), 'http://website.com')
         self.assertEqual(WebsiteHelper.format_url('website.com/path'), 'http://website.com/path')
         self.assertEqual(WebsiteHelper.format_url('website.com/path/subpath'), 'http://website.com/path/subpath')
         self.assertEqual(WebsiteHelper.format_url('subdomain.website.com'), 'http://subdomain.website.com')
@@ -14,6 +15,7 @@ class TestWebsiteHelper(unittest2.TestCase):
 
         # Provided HTTP
         self.assertEqual(WebsiteHelper.format_url('http://website.com'), 'http://website.com')
+        self.assertEqual(WebsiteHelper.format_url(' http://website.com '), 'http://website.com')
         self.assertEqual(WebsiteHelper.format_url('http://website.com/path'), 'http://website.com/path')
         self.assertEqual(WebsiteHelper.format_url('http://website.com/path/subpath'), 'http://website.com/path/subpath')
         self.assertEqual(WebsiteHelper.format_url('http://subdomain.website.com'), 'http://subdomain.website.com')
@@ -21,6 +23,7 @@ class TestWebsiteHelper(unittest2.TestCase):
 
         # Provided HTTPS
         self.assertEqual(WebsiteHelper.format_url('https://website.com'), 'https://website.com')
+        self.assertEqual(WebsiteHelper.format_url(' https://website.com '), 'https://website.com')
         self.assertEqual(WebsiteHelper.format_url('https://website.com/path'), 'https://website.com/path')
         self.assertEqual(WebsiteHelper.format_url('https://website.com/path/subpath'), 'https://website.com/path/subpath')
         self.assertEqual(WebsiteHelper.format_url('https://subdomain.website.com'), 'https://subdomain.website.com')
