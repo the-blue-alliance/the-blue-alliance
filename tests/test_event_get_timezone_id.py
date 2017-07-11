d52fae77ace0089b66780753d318223a4de0cc1b
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-07-11 16:39:50 -0700

    Fix iffy location tests (#1953)

--- tests/test_event_get_timezone_id.py
+++ tests/test_event_get_timezone_id.py
@@ -19,6 +19,7 @@ class TestEventGetTimezoneId(unittest2.TestCase):
     def test_2012ct_no_location(self):
         with open('test_data/usfirst_html/usfirst_event_details_2012ct.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
+
         event['location'] = None
         self.assertEqual(LocationHelper.get_lat_lng(event['location']), None)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), None)
@@ -26,6 +27,7 @@ class TestEventGetTimezoneId(unittest2.TestCase):
     def test_2012ct_bad_location(self):
         with open('test_data/usfirst_html/usfirst_event_details_2012ct.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
+
         event['location'] = "somewhere on mars"
         self.assertEqual(LocationHelper.get_lat_lng(event['location']), None)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), None)
@@ -34,54 +36,70 @@ class TestEventGetTimezoneId(unittest2.TestCase):
         with open('test_data/usfirst_html/usfirst_event_details_2012ct.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (41.76371109999999, -72.6850932))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 41.76371109999999, places=6)
+        self.assertAlmostEqual(location[1], -72.6850932, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/New_York')
 
     def test_2013flbr(self):
         with open('test_data/usfirst_html/usfirst_event_details_2013flbr.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (26.1224386, -80.13731740000001))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 26.1224386, places=6)
+        self.assertAlmostEqual(location[1], -80.13731740000001, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/New_York')
 
     def test_2013casj(self):
         with open('test_data/usfirst_html/usfirst_event_details_2013casj.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (37.3382082, -121.8863286))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 37.3382082, places=6)
+        self.assertAlmostEqual(location[1], -121.8863286, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/Los_Angeles')
 
     def test_2001sj(self):
         with open('test_data/usfirst_html/usfirst_event_details_2001ca2.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (37.3382082, -121.8863286))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 37.3382082, places=6)
+        self.assertAlmostEqual(location[1], -121.8863286, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/Los_Angeles')
 
     def test_2005is(self):
         with open('test_data/usfirst_html/usfirst_event_details_2005is.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (32.7940463, 34.989571))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 32.7940463, places=6)
+        self.assertAlmostEqual(location[1], 34.989571, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'Asia/Jerusalem')
 
     def test_2005or(self):
         with open('test_data/usfirst_html/usfirst_event_details_2005or.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (45.5230622, -122.6764816))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 45.5230622, places=6)
+        self.assertAlmostEqual(location[1], -122.6764816, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/Los_Angeles')
 
     def test_1997il(self):
         with open('test_data/usfirst_html/usfirst_event_details_1997il.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (41.8781136, -87.6297982))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 41.8781136, places=6)
+        self.assertAlmostEqual(location[1], -87.6297982, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/Chicago')
 
     def test_2002sj(self):
         with open('test_data/usfirst_html/usfirst_event_details_2002sj.html', 'r') as f:
             event, _ = UsfirstEventDetailsParser.parse(f.read())
 
-        self.assertEqual(LocationHelper.get_lat_lng(event['location']), (37.3382082, -121.8863286))
+        location = LocationHelper.get_lat_lng(event['location'])
+        self.assertAlmostEqual(location[0], 37.3382082, places=6)
+        self.assertAlmostEqual(location[1], -121.8863286, places=6)
         self.assertEqual(LocationHelper.get_timezone_id(event['location']), 'America/Los_Angeles')
