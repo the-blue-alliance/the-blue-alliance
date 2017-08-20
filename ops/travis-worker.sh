dd101766a96dba530b93dae706a2a08fd5b80ac0
Phil Lopreiato <plnyyanks@gmail.com>
2017-08-19 21:46:55 -0400

    Split travis build into multiple jobs

---
+++ ops/travis-worker.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+case "$1" in
+
+    "PYUNIT")
+        echo "Running python unit tests"
+        paver test
+        ;;
+    "PYLINT")
+        echo "TODO: Run flake8"
+        ;;
+    "JSUNIT")
+        echo "Running javascript tests"
+        npm test
+        ;;
+    "JSLINT")
+        echo "Running javascript lint"
+        npm run lint -s
+        ;;
+    *)
+        echo "Unknown job type $JOB"
+        exit -1
+        ;;
+esac
