9b38a9d393888da43712e2e53acf5f33a8d58530
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-08-23 20:03:02 -0700

    Remove caching stuff

--- controllers/short_controller.py
+++ controllers/short_controller.py
@@ -2,17 +2,9 @@ from base_controller import CacheableHandler
 
 
 class ShortTeamHandler(CacheableHandler):
-    LONG_CACHE_EXPIRATION = 60 * 60 * 24 * 30
-    CACHE_VERSION = 0
-    CACHE_KEY_FORMAT = "short_team_canonical_{}"  # (team_number)
-
     def get(self, team_number):
         return self.redirect("/team/%s" % int(team_number))
 
 class ShortEventHandler(CacheableHandler):
-    LONG_CACHE_EXPIRATION = 60 * 60 * 24 * 30
-    CACHE_VERSION = 0
-    CACHE_KEY_FORMAT = "short_event_detail_{}"  # (event_key)
-
     def get(self, event_key):
         return self.redirect("/event/%s" % event_key)
