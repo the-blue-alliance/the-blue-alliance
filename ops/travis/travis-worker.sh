ac79a3d31996f24770b7c97cca8112f77b9bbb37
Phil Lopreiato <plnyyanks@gmail.com>
2017-08-26 18:46:05 -0400

    Remove unnecessary condition

--- ops/travis/travis-worker.sh
+++ ops/travis/travis-worker.sh
@@ -8,11 +8,6 @@ case "$1" in
         paver test
         ;;
     "LINT")
-        if test "$TRAVIS" == "true" && "$TRAVIS_EVENT_TYPE" != "pull_request" ; then
-            echo "Lint should only run on pull request builds"
-            exit 0
-        fi
-
         if test "$TRAVIS_BRANCH" != "" ; then
             echo "Linting all changes between HEAD and $TRAVIS_BRANCH"
             paver lint --base $TRAVIS_BRANCH
