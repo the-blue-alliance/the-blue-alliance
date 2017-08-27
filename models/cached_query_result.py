30a6bd9ecc376016a0a8a9327744d6d1e8108e6e
Eugene Fang <fang.eugene@gmail.com>
2017-08-27 17:32:30 -0400

    Revert DBQuery Serialization (#1985)
    * Revert "Fix inheritance issues"
    
    This reverts commit 1a6568cd1ebfadb734b34a2101fc81681adba1b3.
    
    * Revert "Disable writing to DB from deserialized models"
    
    This reverts commit def0c8f45a732d8b81e8de3b76124fd41bf574f2.
    
    * Revert "Merge remote-tracking branch 'upstream/master'"
    
    This reverts commit e00f058b199e150e96d5f19e72bf7c22fc7c968b, reversing
    changes made to 8eccc907abb6ea2f23d165829e0c103cc9e3e2f1.
    
    * Revert "Lazy load from serialized model"
    
    This reverts commit 8eccc907abb6ea2f23d165829e0c103cc9e3e2f1.
    
    * Revert "Compress property"
    
    This reverts commit b1eb723e2f9993279682ccb2a16ce04be348e699.
    
    * Revert "Fix deprecated properties causing serializer to fail"
    
    This reverts commit 21f8222c76dcd78f4b97badf9d29bb89d28587e7.
    
    * Revert "DBQuery rework (#1983)"
    
    This reverts commit a8cbaecd1d1194169b36a29cc4900c42a47f67e0.
    
    * Reverting DBQuery
    
    * fix bash magic

--- models/cached_query_result.py
+++ models/cached_query_result.py
@@ -6,8 +6,8 @@ class CachedQueryResult(ndb.Model):
     A CachedQueryResult stores the result of an NDB query
     """
     # Only one of result or result_dict should ever be populated for one model
-    result_dict_full = ndb.JsonProperty(compressed=True)  # Entire serialized result
-    result_dict = ndb.JsonProperty()  # Dict version of models (for API)
+    result = ndb.PickleProperty(compressed=True)  # Raw models
+    result_dict = ndb.JsonProperty()  # Dict version of models
 
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
