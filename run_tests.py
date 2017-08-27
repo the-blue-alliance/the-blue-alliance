43513ec74739438e3770ddf369f30315332004e5
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2017-08-26 17:31:05 -0700

    Bring Back Parallel Tests (#1981)
    * Re-enable parallel tests
    
    * unused import
    
    [nodeploy]

--- run_tests.py
+++ run_tests.py
@@ -21,13 +21,28 @@ SDK_PATH    Path to the SDK installation"""
 
 
 sys.path.insert(1, 'lib')
-MULTITHREAD = False
+MULTITHREAD = True
+MAX_JOBS = 4
 
 
-def start_suite(suite, queue):
+def proc_init(l, fail, total):
+    global lock
+    global fail_count
+    global total_run
+    lock = l
+    fail_count = fail
+    total_run = total
+
+
+def run_suite(suite):
     sio = StringIO.StringIO()
     testresult = unittest2.TextTestRunner(sio, verbosity=2).run(suite)
-    queue.put((sio.getvalue(), testresult.testsRun, testresult.wasSuccessful()))
+    output = sio.getvalue()
+    with lock:
+        total_run.value += testresult.testsRun
+        if not testresult.wasSuccessful():
+            fail_count.value += 1
+        print output.encode('utf-8')
 
 
 def main(sdk_path, test_pattern):
@@ -51,36 +66,35 @@ def main(sdk_path, test_pattern):
 
     suites = unittest2.loader.TestLoader().discover("tests", test_pattern)
 
-    processes = []
-    result_queue = multiprocessing.Queue()
-    for suite in suites:
-        if MULTITHREAD:
-            process = multiprocessing.Process(target=start_suite, args=[suite, result_queue])
-            process.start()
-            processes.append(process)
-        else:
-            sio = StringIO.StringIO()
-            testresult = unittest2.TextTestRunner(sio, verbosity=2).run(suite)
-            result_queue.put((sio.getvalue(), testresult.testsRun, testresult.wasSuccessful()))
-            if not MULTITHREAD:
-                print '-----------------------'
-                print sio.getvalue().encode('utf-8')
-
-    for process in processes:
-        process.join()
-
-    os.unsetenv('IS_TBA_TEST')
     fail = False
     total_tests_run = 0
-    while not result_queue.empty():
-        test_output, tests_run, was_successful = result_queue.get()
-        total_tests_run += tests_run
-        if MULTITHREAD:
-            print '-----------------------'
-            print test_output.encode('utf-8')
-        if not was_successful:
-            fail = True
+    if MULTITHREAD:
+        proc_lock = multiprocessing.Lock()
+        fail_count = multiprocessing.Value('i', 0)
+        total_run = multiprocessing.Value('i', 0)
+        pool = multiprocessing.Pool(MAX_JOBS, initializer=proc_init, initargs=(proc_lock, fail_count, total_run,))
+        pool.map(run_suite, suites)
+        pool.close()
+        pool.join()
 
+        fail = fail_count.value > 0
+        total_tests_run = total_run.value
+    else:
+        result_queue = multiprocessing.Queue()
+        for suite in suites:
+            sio = StringIO.StringIO()
+            testresult = unittest2.TextTestRunner(sio, verbosity=2).run(suite)
+            result_queue.put((testresult.testsRun, testresult.wasSuccessful()))
+            print '-----------------------'
+            print sio.getvalue().encode('utf-8')
+
+        while not result_queue.empty():
+            tests_run, was_successful = result_queue.get()
+            total_tests_run += tests_run
+            if not was_successful:
+                fail = True
+
+    os.unsetenv('IS_TBA_TEST')
     print "================================"
     print "Completed {} tests in: {} seconds".format(total_tests_run, time.time() - start_time)
     if fail:
