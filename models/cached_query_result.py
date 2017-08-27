b1eb723e2f9993279682ccb2a16ce04be348e699
Eugene Fang <fang.eugene@gmail.com>
2017-08-27 02:34:20 -0400

    Compress property

--- models/cached_query_result.py
+++ models/cached_query_result.py
@@ -6,7 +6,7 @@ class CachedQueryResult(ndb.Model):
     A CachedQueryResult stores the result of an NDB query
     """
     # Only one of result or result_dict should ever be populated for one model
-    result_dict_full = ndb.JsonProperty()  # Entire serialized result
+    result_dict_full = ndb.JsonProperty(compressed=True)  # Entire serialized result
     result_dict = ndb.JsonProperty()  # Dict version of models (for API)
 
     created = ndb.DateTimeProperty(auto_now_add=True)
