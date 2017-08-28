ef86e51e6a5ec7e5666978cc6237d2aa107c7df8
Phil Lopreiato <plnyyanks@gmail.com>
2017-08-28 19:25:41 -0400

    Add Admin APIv3 call to set build info

---
+++ controllers/apiv3/api_admin_controller.py
@@ -0,0 +1,40 @@
+from controllers.apiv3.api_base_controller import ApiBaseController
+from models.sitevar import Sitevar
+
+
+class ApiAdminController(ApiBaseController):
+    """
+    Allow special admin-only endpoints for APIv3
+    """
+    REQUIRE_ADMIN_AUTH = True
+
+    def get(self, *args, **kw):
+        # We want these to be POST-only
+        self.abort(405)
+
+
+class ApiAdminSetBuildInfo(ApiAdminController):
+    """
+    A controller that allows us to set data about the currently deployed version
+    """
+
+    def _render(self):
+        current_commit_sha = ''
+        commit_time = ''
+        build_time = ''
+        deploy_time = ''
+        travis_job = ''
+
+        web_info = {
+            'current_commit': current_commit_sha,
+            'commit_time': commit_time,
+            'build_time': build_time,
+            'deploy_time': deploy_time,
+            'travis_job': travis_job,
+        }
+
+        status_sitevar = Sitevar.get_or_insert('apistatus', values_json='{}')
+        contents = status_sitevar.contents
+        contents['web'] = web_info
+        status_sitevar.contents = contents
+        status_sitevar.put()
