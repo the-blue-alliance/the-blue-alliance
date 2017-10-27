15b0071ed491c1902ea0b93b6225baeea0b4a05f
Phil Lopreiato <plnyyanks@gmail.com>
2017-10-27 18:35:48 -0400

    Set up gulp in devserver, as well
    Might require inotify limit to be increased (see #2008)

--- ops/dev/start-devserver.sh
+++ ops/dev/start-devserver.sh
@@ -3,4 +3,11 @@ set -e
 
 source ops/dev/vars.sh
 echo "Starting devserver in tmux session..."
-tmux new-session -d "paver devserver; read"
+tmux start-server
+tmux new-session -d -s tba
+tmux new-window -t "tba:1" -n gae "paver devserver; read"
+tmux new-window -t "tba:2" -n gulp "gulp; read"
+tmux select-window -t "tba:0"
+
+tmux list-sessions
+tmux list-windows
