054c2099c7c628acae4dfc1d33363f719cb20349
Phil Lopreiato <plnyyanks@gmail.com>
2017-10-26 20:28:33 -0400

    Test new endpoint

--- tests/test_api_trusted.py
+++ tests/test_api_trusted.py
@@ -12,6 +12,7 @@ from google.appengine.ext import testbed
 from consts.auth_type import AuthType
 from consts.event_type import EventType
 from consts.media_type import MediaType
+from consts.playoff_type import PlayoffType
 from models.account import Account
 from models.api_auth_access import ApiAuthAccess
 from models.award import Award
@@ -94,6 +95,12 @@ class TestApiTrustedController(unittest2.TestCase):
                                                 auth_types_enum=[AuthType.EVENT_MATCHES],
                                                 owner=ndb.Key(Account, "42"),
                                                 expiration=datetime.datetime(year=1970, month=1, day=1))
+
+        self.event_info_auth = ApiAuthAccess(id='tEsT_id_9',
+                                             secret='321tEsTsEcReT',
+                                             description='test',
+                                             event_list=[ndb.Key(Event, '2014casj')],
+                                             auth_types_enum=[AuthType.EVENT_INFO])
 
         self.event = Event(
             id='2014casj',
@@ -604,3 +611,29 @@ class TestApiTrustedController(unittest2.TestCase):
         self.assertEqual(media.media_type_enum, MediaType.YOUTUBE_VIDEO)
         self.assertEqual(media.foreign_key, 'aFZy8iibMD0')
         self.assertIn(ndb.Key(Event, '2014casj'), media.references)
+
+    def test_update_event_info(self):
+        self.event_info_auth.put()
+
+        request = {
+            'first_code': 'abc123',
+            'official': True,
+            'playoff_type': PlayoffType.ROUND_ROBIN_6_TEAM,
+            'webcasts': ['https://youtu.be/abc123'],
+        }
+        request_body = json.dumps(request)
+        request_path = '/api/trusted/v1/event/2014casj/info/update'
+        sig = md5.new('{}{}{}'.format('321tEsTsEcReT', request_path, request_body)).hexdigest()
+        response = self.testapp.post(request_path, request_body, headers={'X-TBA-Auth-Id': 'tEsT_id_9', 'X-TBA-Auth-Sig': sig})
+
+        self.assertEqual(response.status_code, 200)
+
+        event = Event.get_by_id('2014casj')
+        self.assertEqual(event.first_code, 'abc123')
+        self.assertEqual(event.official, True)
+        self.assertEqual(event.playoff_type, PlayoffType.ROUND_ROBIN_6_TEAM)
+        webcasts = event.webcast
+        self.assertEqual(len(webcasts), 1)
+        webcast = webcasts[0]
+        self.assertEqual(webcast['type'], 'youtube')
+        self.assertEqual(webcast['channel'], 'abc123')
