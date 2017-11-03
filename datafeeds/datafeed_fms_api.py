the-blue-alliance/the-blue-alliance

commit 2296cadfa2848b88768ae81abdb346e1ab1c0e18

Add cronjob to fetch sync-enabled offseason events
Doesn't actually do anything with them yet

datafeeds/datafeed_fms_api.py
@@ -318,11 +318,24 @@ class DatafeedFMSAPI(object):
 
     # Returns a tuple: (list(Event), list(District))
     def getEventList(self, year):
-        result = self._parse(self.FMS_API_EVENT_LIST_URL_PATTERN % (year), FMSAPIEventListParser(year))
+        result = self._parse(self.FMS_API_EVENT_LIST_URL_PATTERN % (year))
         if result:
             return result
         else:
             return [], []
+
+    # Returns a list of sync-enabled offseason events
+    def getSyncEnabledOffseasonEvents(self, year):
+        result = self._parse(
+            self.FMS_API_EVENT_LIST_URL_PATTERN % (year),
+            FMSAPIEventListParser(
+                year,
+                event_types_override={
+                    'offseasonwithazuresync': EventType.OFFSEASON,
+                },
+            )
+        )
+        return result
 
     # Returns a tuple: (list(Event), list(District))
     def getEventDetails(self, event_key):

backend_main.py
@@ -9,7 +9,7 @@ from controllers.admin.admin_cron_controller import AdminPostEventTasksDo, Admin
 from controllers.backup_controller import DatastoreBackupFull, BigQueryImportEnqueue, \
     BigQueryImportEntity, MainBackupsEnqueue, DatastoreBackupArchive, DatastoreBackupArchiveFile
 from controllers.datafeed_controller import EventListEnqueue, EventDetailsEnqueue
-from controllers.datafeed_controller import EventListGet, EventDetailsGet, TeamDetailsGet
+from controllers.datafeed_controller import EventListGet, EventDetailsGet, TeamDetailsGet, OffseasonEventListGet
 
 
 app = webapp2.WSGIApplication([('/backend-tasks/enqueue/event_list/([0-9]*)', EventListEnqueue),
@@ -17,6 +17,7 @@ app = webapp2.WSGIApplication([('/backend-tasks/enqueue/event_list/([0-9]*)', Ev
                                ('/backend-tasks/get/event_list/([0-9]*)', EventListGet),
                                ('/backend-tasks/get/event_details/(.*)', EventDetailsGet),
                                ('/backend-tasks/get/team_details/(.*)', TeamDetailsGet),
+                               ('/backend-tasks/get/fms-sync-offseasons/(.*)', OffseasonEventListGet),
                                ('/backend-tasks/do/post_event_tasks/(.*)', AdminPostEventTasksDo),
                                ('/backend-tasks/enqueue/rebuild_district_teams/([0-9]+)', AdminCreateDistrictTeamsEnqueue),
                                ('/backend-tasks/do/rebuild_district_teams/([0-9]+)', AdminCreateDistrictTeamsDo),

controllers/datafeed_controller.py
@@ -30,6 +30,7 @@ from helpers.media_manipulator import MediaManipulator
 from helpers.team_manipulator import TeamManipulator
 from helpers.district_team_manipulator import DistrictTeamManipulator
 from helpers.robot_manipulator import RobotManipulator
+from helpers.event.offseason_event_helper import OffseasonEventHelper
 
 from models.district_team import DistrictTeam
 from models.event import Event
@@ -432,6 +433,19 @@ class EventListGet(webapp.RequestHandler):
         if 'X-Appengine-Taskname' not in self.request.headers:  # Only write out if not in taskqueue
             path = os.path.join(os.path.dirname(__file__), '../templates/datafeeds/fms_event_list_get.html')
             self.response.out.write(template.render(path, template_values))
+
+
+class OffseasonEventListGet(webapp.RequestHandler):
+    """
+    Fetch one year's sync-enabled offseason events
+    """
+    def get(self, year):
+        df = DatafeedFMSAPI('v2.0')
+        first_events, _ = df.getSyncEnabledOffseasonEvents(year)
+        linked_events, maybed_linked_events, new_events = \
+            OffseasonEventHelper.categorize_offseasons(int(year), first_events)
+
+        logging.info("linked: {}, maybe: {}, new: {}".format(len(linked_events), len(maybed_linked_events), len(new_events)))
 
 
 class EventDetailsEnqueue(webapp.RequestHandler):

cron.yaml
@@ -4,6 +4,11 @@ cron:
   schedule: every day 02:00
   timezone: America/Los_Angeles
 
+- description: FIRST official offseason scraping
+  url: /backend-tasks/get/fms-sync-offseasons/2017
+  schedule: every tuesday,thursday 02:30
+  timezone: America/Los_Angeles
+
 # - description: FIRST event detail scraping
 #   url: /tasks/enqueue/usfirst_event_details/2014
 #   schedule: every thursday 02:00

datafeeds/parsers/fms_api/fms_api_event_list_parser.py
@@ -51,9 +51,11 @@ class FMSAPIEventListParser(object):
     EINSTEIN_NAME_DEFAULT = 'Einstein Field'
     EINSTEIN_CODES = {'cmp', 'cmpmi', 'cmpmo', 'cmptx'}
 
-    def __init__(self, season, short=None):
+    def __init__(self, season, short=None, event_types_override=None):
         self.season = int(season)
         self.event_short = short
+        if event_types_override:
+            self.EVENT_TYPES = event_types_override
 
     def parse(self, response):
         events = []

helpers/event/offseason_event_helper.py
@@ -1,0 +1,50 @@
+import logging
+
+from consts.event_type import EventType
+from database.event_query import EventListQuery
+
+
+class OffseasonEventHelper(object):
+
+    @classmethod
+    def is_direct_match(cls, tba_event, first_event):
+        return tba_event.key_name == first_event.key_name or \
+               (tba_event.first_code and tba_event.first_code == first_event.key_name)
+
+    @classmethod
+    def is_maybe_match(cls, tba_event, first_event):
+        return tba_event.start_date == first_event.start_date \
+               and tba_event.end_date == first_event.end_date \
+               and tba_event.city == first_event.city \
+               and tba_event.state_prov == first_event.state_prov
+
+    @classmethod
+    def categorize_offseasons(cls, year, first_events):
+        """
+        Takes a list of sync-enabled offseasons from FIRST and sorts them
+        Returns a tuple of events (linked to TBA, candidates to link, new)
+        """
+        year_events_future = EventListQuery(year).fetch_async()
+        year_events = year_events_future.get_result()
+        year_offseasons = [e for e in year_events if e.event_type_enum == EventType.OFFSEASON]
+
+        matched_events = []
+        likely_matched_events = []
+        new_events = []
+
+        logging.info("Categorizing {} events from FIRST: {}".format(len(first_events), first_events))
+        for event in first_events:
+            logging.info("Event: {}".format(event.key_name))
+            matching_tba_event_by_key = next((e for e in year_offseasons if cls.is_direct_match(e, event)), None)
+            if matching_tba_event_by_key:
+                matched_events.append(event)
+                continue
+
+            likely_match = next((e for e in year_offseasons if cls.is_maybe_match(e, event)), None)
+            if likely_match:
+                likely_matched_events.append(event)
+                continue
+
+            new_events.append(event)
+
+        return (matched_events, likely_matched_events, new_events)

commit 6e11ee79cb1a12d3618f48c877dd1e77ae3fcdef

Fix FRC API getEventList datafeed
Bug introduced in https://github.com/the-blue-alliance/the-blue-alliance/commit/2296cadfa2848b88768ae81abdb346e1ab1c0e18

datafeeds/datafeed_fms_api.py
@@ -318,7 +318,7 @@ class DatafeedFMSAPI(object):
 
     # Returns a tuple: (list(Event), list(District))
     def getEventList(self, year):
-        result = self._parse(self.FMS_API_EVENT_LIST_URL_PATTERN % (year))
+        result = self._parse(self.FMS_API_EVENT_LIST_URL_PATTERN % (year), FMSAPIEventListParser(year))
         if result:
             return result
         else: