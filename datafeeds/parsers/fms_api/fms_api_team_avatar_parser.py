cd0eff0aabbfa7373f63a1defa0fb1470822c07b
Eugene Fang <fang.eugene@gmail.com>
2018-02-11 15:01:51 -0500

    Delete avatars if FRC API returns null. Closes #2085 (#2086)

--- datafeeds/parsers/fms_api/fms_api_team_avatar_parser.py
+++ datafeeds/parsers/fms_api/fms_api_team_avatar_parser.py
@@ -23,20 +23,21 @@ class FMSAPITeamAvatarParser(object):
         total_pages = response['pageTotal']
         teams = response['teams']
         avatars = []
+        keys_to_delete = set()
 
         for teamData in teams:
+            team_number = teamData['teamNumber']
+            foreign_key = "avatar_{}_frc{}".format(self.year, team_number)
+            media_key = ndb.Key('Media', Media.render_key_name(MediaType.AVATAR, foreign_key))
+
             if not teamData['encodedAvatar']:
+                keys_to_delete.add(media_key)
                 continue
             else:
                 encoded_avatar = teamData['encodedAvatar']
 
-            team_number = teamData['teamNumber']
-
-            foreign_key = "avatar_{}_frc{}".format(self.year, team_number)
-            media_key = Media.render_key_name(MediaType.AVATAR, foreign_key)
-
             avatar = Media(
-                key=ndb.Key('Media', media_key),
+                key=media_key,
                 details_json=json.dumps({'base64Image': encoded_avatar}),
                 foreign_key=foreign_key,
                 media_type_enum=MediaType.AVATAR,
@@ -46,4 +47,4 @@ class FMSAPITeamAvatarParser(object):
 
             avatars.append(avatar)
 
-        return (avatars, (current_page < total_pages))
+        return (avatars, keys_to_delete, (current_page < total_pages))
