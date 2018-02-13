45bdb2a949f547e9eef7ec6782d8754a2b78ec74
Eugene Fang <fang.eugene@gmail.com>
2018-02-12 20:26:07 -0500

    Change responsive breakpoints

--- react/liveevent/components/LiveEventPanel.js
+++ react/liveevent/components/LiveEventPanel.js
@@ -7,15 +7,15 @@ class LiveEventPanel extends PureComponent {
   render() {
     return (
       <div>
-        <div className='col-md-3 text-center'>
+        <div className='col-lg-3 text-center'>
           <h4>Last Matches</h4>
           <LastMatchesTable />
         </div>
-        <div className='col-md-6 text-center'>
+        <div className='col-lg-6 text-center'>
           <h4>Current Match: Quals 4</h4>
           <CurrentMatchDisplay />
         </div>
-        <div className='col-md-3 text-center'>
+        <div className='col-lg-3 text-center'>
           <h4>Upcoming Matches</h4>
           <UpcomingMatchesTable />
         </div>
