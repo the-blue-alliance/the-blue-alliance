6ed8c6cb0ff00f5877934842d5973493a1e55647
Eugene Fang <fang.eugene@gmail.com>
2018-02-24 11:58:27 -0500

    Increase shards to 5

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -62,7 +62,7 @@ class Avatars2018Handler(CacheableHandler):
 
     def _render(self, *args, **kw):
         avatars = []
-        shards = memcache.get_multi(['2018avatars_{}'.format(i) for i in xrange(3)])
+        shards = memcache.get_multi(['2018avatars_{}'.format(i) for i in xrange(5)])
         for _, shard in sorted(shards.items(), key=lambda kv: kv[0]):
             if shard is not None:
                 avatars += shard
@@ -76,8 +76,8 @@ class Avatars2018Handler(CacheableHandler):
             avatars = sorted(avatars_future.get_result(), key=lambda a: int(a.references[0].id()[3:]))
 
             shards = {}
-            size = len(avatars) / 3 + 1
-            for i in xrange(3):
+            size = len(avatars) / 5 + 1
+            for i in xrange(5):
                 start = i * size
                 end = start + size
                 shards['2018avatars_{}'.format(i)] = avatars[start:end]
