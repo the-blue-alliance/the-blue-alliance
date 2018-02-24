27e51298ac60ca455822bd9761c351286b286347
Eugene Fang <fang.eugene@gmail.com>
2018-02-24 12:31:30 -0500

    Sort matches by play order not time

--- react/gameday2/selectors/index.js
+++ react/gameday2/selectors/index.js
@@ -77,9 +77,9 @@ export const getTickerMatches = createSelector(
       if (match.alliances.red.score !== -1 && match.alliances.blue.score !== -1) {
         time = 0
       }
-      if (match.predicted_time) {
-        time = match.predicted_time
-      }
+      // if (match.predicted_time) {
+      //   time = match.predicted_time
+      // }
       return (time * 10000000) + (compLevelsPlayOrder[match.comp_level] * 100000) + (match.match_number * 100) + match.set_number
     }
 
