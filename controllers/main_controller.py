edb68f8e0b856522f8cfa7a15363f5f5cadb95d8
Eugene Fang <fang.eugene@gmail.com>
2018-02-27 01:32:55 -0500

    Add popular teams competing to homepage (#2126)
    * Add popular teams to competition season landing
    
    * More efficient caching of queries
    
    * Cleanup, edit verbiage
    
    * Show week number in title
    
    * Link to team's event

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -21,6 +21,8 @@ from helpers.event_helper import EventHelper
 from helpers.firebase.firebase_pusher import FirebasePusher
 from models.award import Award
 from models.event import Event
+from models.event_team import EventTeam
+from models.favorite import Favorite
 from models.insight import Insight
 from models.media import Media
 from models.team import Team
@@ -200,11 +202,63 @@ class MainCompetitionseasonHandler(CacheableHandler):
     def _render(self, *args, **kw):
         week_events = EventHelper.getWeekEvents()
         special_webcasts = FirebasePusher.get_special_webcasts()
+        events = {}
+        for event in week_events:
+            events[event.key.id()] = event
+
+        # Calculate popular teams
+        # Get cached team keys
+        event_team_keys = memcache.get_multi(events.keys(), namespace='event-team-keys')
+
+        # Get uncached team keys
+        to_query = set(events.keys()).difference(event_team_keys.keys())
+        event_teams_futures = [(
+            event_key,
+            EventTeam.query(EventTeam.event == ndb.Key(Event, event_key)).fetch_async(projection=[EventTeam.team])
+        ) for event_key in to_query]
+
+        # Merge cached and uncached team keys
+        for event_key, event_teams in event_teams_futures:
+            event_team_keys[event_key] = [et.team.id() for et in event_teams.get_result()]
+        memcache.set_multi(event_team_keys, 60*60*24, namespace='event-team-keys')
+
+        team_keys = []
+        team_events = {}
+        for event_key, event_team_keys in event_team_keys.items():
+            team_keys += event_team_keys
+            for team_key in event_team_keys:
+                team_events[team_key] = events[event_key]
+
+        # Get cached counts
+        team_favorite_counts = memcache.get_multi(team_keys, namespace='team-favorite-counts')
+
+        # Get uncached counts
+        to_count = set(team_keys).difference(team_favorite_counts.keys())
+        count_futures = [(
+            team_key,
+            Favorite.query(Favorite.model_key == team_key).count_async()
+        ) for team_key in to_count]
+
+        # Merge cached and uncached counts
+        for team_key, count_future in count_futures:
+            team_favorite_counts[team_key] = count_future.get_result()
+        memcache.set_multi(team_favorite_counts, 60*60*24, namespace='team-favorite-counts')
+
+        # Sort to get top popular teams
+        popular_team_keys = []
+        for team_key, _ in sorted(team_favorite_counts.items(), key=lambda tc: -tc[1])[:25]:
+            popular_team_keys.append(ndb.Key(Team, team_key))
+        popular_teams =  sorted(ndb.get_multi(popular_team_keys), key=lambda team: team.team_number)
+
+        popular_teams_events = []
+        for team in popular_teams:
+            popular_teams_events.append((team, team_events[team.key.id()]))
 
         self.template_values.update({
             "events": week_events,
             "any_webcast_online": any(w.get('status') == 'online' for w in special_webcasts),
             "special_webcasts": special_webcasts,
+            "popular_teams_events": popular_teams_events,
         })
 
         path = os.path.join(os.path.dirname(__file__), '../templates/index_competitionseason.html')
