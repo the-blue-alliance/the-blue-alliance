39361ee9eaa0e44fb3ac1331fbf27a6f42b0d1ae
Eugene Fang <fang.eugene@gmail.com>
2018-04-19 08:14:53 -0400

    Fix lint

--- react/liveevent/liveevent.js
+++ react/liveevent/liveevent.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import LiveEventPanel from './components/LiveEventPanel'
 
 const els = document.getElementsByClassName('liveevent-content')
-for (const el of els) {
+for (let i = 0; i < els.length; i++) {
+  const el = els[i]
   ReactDOM.render(
     <LiveEventPanel
       eventKey={el.getAttribute('data-eventkey')}
