87ce7ee9fac1991184d5881f2f4d85d502c65415
Eugene Fang <fang.eugene@gmail.com>
2018-05-29 14:43:13 -0400

    API auth testing, set scopes [clowntown]

--- mobile_main.py
+++ mobile_main.py
@@ -60,7 +60,7 @@ if tba_config.DEBUG:
 @endpoints.api(name='tbaMobile', version='v9', description="API for TBA Mobile clients",
                allowed_client_ids=client_ids,
                # audiences=[ANDROID_AUDIENCE],
-               # scopes=[endpoints.EMAIL_SCOPE],
+               scopes=[endpoints.EMAIL_SCOPE],
                issuers={
                'firebase': endpoints.Issuer(
                   'https://securetoken.google.com/tbatv-prod-hrd',
