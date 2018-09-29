6d47e95dd40c23b071f7c83eb2fad27862f0601f
dracco1993 <dracco1993@gmail.com>
2018-09-29 15:27:12 -0400

    Fixed URL generation

--- models/media.py
+++ models/media.py
@@ -145,15 +145,18 @@ class Media(ndb.Model):
 
     @property
     def instagram_direct_url(self):
-        return self.details['thumbnail_url']
+        return self.instagram_url_helper("l")
 
     @property
     def instagram_direct_url_med(self):
-        return self.instagram_direct_url
+        return self.instagram_url_helper("m")
 
     @property
     def instagram_direct_url_sm(self):
-        return self.instagram_direct_url
+        return self.instagram_url_helper("t")
+
+    def instagram_url_helper(self, size):
+        return "{}/media/?size={}".format(self.instagram_url, size)
 
     @property
     def view_image_url(self):
