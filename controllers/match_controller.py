0a1545e2175b7b0cb50413b49cef38d1b74ac8f6
Eugene Fang <fang.eugene@gmail.com>
2019-02-15 13:04:50 -0500

    Match breakdowns for 2019

--- controllers/match_controller.py
+++ controllers/match_controller.py
@@ -16,7 +16,7 @@ class MatchDetail(CacheableHandler):
     SHORT_CACHE_EXPIRATION = 61
     CACHE_VERSION = 4
     CACHE_KEY_FORMAT = "match_detail_{}"  # (match_key)
-    VALID_BREAKDOWN_YEARS = set([2015, 2016, 2017, 2018])
+    VALID_BREAKDOWN_YEARS = set([2015, 2016, 2017, 2018, 2019])
 
     def __init__(self, *args, **kw):
         super(MatchDetail, self).__init__(*args, **kw)
