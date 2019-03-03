5cc73e138d845a18d5058ade2aea418f6ea5036f
Eugene Fang <fang.eugene@gmail.com>
2019-03-02 22:04:15 -0500

    Track webcast view time in GameDay

---
+++ react/gameday2/components/VideoCellAnalyticsTracker.js
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react'
+import ReactGA from 'react-ga'
+import { webcastPropType } from '../utils/webcastUtils'
+
+ReactGA.initialize('UA-1090782-9')
+
+export default class VideoCellAnalyticsTracker extends React.Component {
+  static propTypes = {
+    webcast: webcastPropType,
+  }
+
+  elapsedTime = 0 // In minutes
+
+  sendTracking() {
+    const { key, type, channel, file } = this.props.webcast
+    let action = `${key}::${type}::${channel}`
+    if (file) {
+      action += `::${file}`
+    }
+
+    ReactGA.event({
+      category: 'Webcast View Time',
+      action: action.toLowerCase(),
+      label: this.elapsedTime.toString(),
+      value: this.elapsedTime === 0 ? 0 : 1,
+    })
+    this.elapsedTime++
+  }
+
+  componentDidMount() {
+    this.sendTracking()
+    this.interval = setInterval(this.sendTracking.bind(this), 60000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return null
+  }
+}
