46eda5d980f3cf285e713b4fd459d8f58cbcb814
Zachary Orr <ZachOrr@users.noreply.github.com>
2019-03-07 13:56:47 -0500

    Update PYTHONPATH to look for protorpc library

--- ops/dev/vars.sh
+++ ops/dev/vars.sh
@@ -5,7 +5,6 @@ NODE_VERSION=8.0.0
 NODE_DIR=${NVM_DIR}/versions/node/v${NODE_VERSION}
 
 # First, configure environment
-export PYTHONPATH="${PYTHONPATH}:${GAE_DIR}"
+export PYTHONPATH="${PYTHONPATH}:${GAE_DIR}:${GAE_DIR}/lib/protorpc-1.0"
 export NODE_PATH="${NODE_DIR}/lib/node_modules"
 export PATH="${PATH}:${GAE_DIR}:${NODE_DIR}/bin"
-
