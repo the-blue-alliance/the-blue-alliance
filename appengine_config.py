7f46311bac1f1d5e4262e7eba4df8a713cbc27f4
Eugene Fang <fang.eugene@gmail.com>
2019-03-14 15:12:57 -0400

    Disable appstats
    We have stackdriver now. [clowntown]

--- appengine_config.py
+++ appengine_config.py
@@ -1,16 +1,4 @@
-import tba_config
 from google.appengine.ext import vendor
 
 
 vendor.add('lib')
-
-
-if tba_config.CONFIG['env'] == 'prod':
-    appstats_RECORD_FRACTION = tba_config.RECORD_FRACTION
-    appstats_CALC_RPC_COSTS = True
-
-
-def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
-    return app
