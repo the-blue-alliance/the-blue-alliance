c0c098b82480959a3fdcff5f9159dc73af730eb7
ZachOrr <ZachOrr@users.noreply.github.com>
2019-03-21 17:46:01 -0400

    Remove empty verification for webhook secrets

--- tests/tbans_tests/models/requests/notifications/test_webhook_request.py
+++ tests/tbans_tests/models/requests/notifications/test_webhook_request.py
@@ -52,8 +52,8 @@ class TestWebhookRequest(unittest2.TestCase):
             WebhookRequest(MockNotification(), 'https://www.thebluealliance.com/', 200)
 
     def test_secret_empty(self):
-        with self.assertRaises(ValueError):
-            WebhookRequest(MockNotification(), 'https://www.thebluealliance.com/', '')
+        # TODO: Migrate existing users, make this throw again
+        WebhookRequest(MockNotification(), 'https://www.thebluealliance.com/', '')
 
     def test_str(self):
         message_str = WebhookRequest(MockNotification(), 'https://www.thebluealliance.com/', 'secret')
