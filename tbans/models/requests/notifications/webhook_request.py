52744f6e93b8a68a3ab2d9060e43673caf238eb6
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2019-03-21 17:56:29 -0400

    Remove empty verification for webhook secrets (#2473)

--- tbans/models/requests/notifications/webhook_request.py
+++ tbans/models/requests/notifications/webhook_request.py
@@ -29,7 +29,7 @@ class WebhookRequest(NotificationRequest):
         self.url = url
 
         # Check that we have a secret
-        validate_is_string(secret=secret)
+        validate_is_type(basestring, not_empty=False, secret=secret)
         self.secret = secret
 
     def __str__(self):
