1fed481d9e8534d8371638473360a7411219a4c2
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2019-04-06 11:38:47 -0400

    Return None for Team motto (#2410)
    * Return None for Team motto
    
    * Update Swagger docs
    
    * Update api_v3.json

--- helpers/model_to_dict.py
+++ helpers/model_to_dict.py
@@ -16,7 +16,7 @@ class ModelToDict(object):
         team_dict["website"] = team.website
         team_dict["location"] = team.location
         team_dict["rookie_year"] = team.rookie_year
-        team_dict["motto"] = team.motto
+        team_dict["motto"] = None
 
         try:
             team_dict["location"] = team.location
