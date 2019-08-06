4e5f4c9c49688e6a44b75a37c206e51a3063a514
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2019-08-06 19:06:39 -0400

    WIP: Fix build breaking. (#2563)
    * Remove duplicate packages
    
    * Remove package-lock.js for testing
    
    * Add package-lock.json
    
    * Update node version
    
    * Update package-lock.json
    
    * Pin node version
    
    * Update node version
    
    * Update node packages
    
    * Update to use gulp 4
    
    * Update package-lock.json
    
    * Revert jest version

--- ops/dev/vars.sh
+++ ops/dev/vars.sh
@@ -1,7 +1,7 @@
 # Constants for env config
 GAE_DIR=/google_appengine
 NVM_DIR=/nvm
-NODE_VERSION=8.0.0
+NODE_VERSION=8.16.0
 NODE_DIR=${NVM_DIR}/versions/node/v${NODE_VERSION}
 
 # First, configure environment
