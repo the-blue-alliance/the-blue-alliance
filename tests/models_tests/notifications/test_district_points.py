afc6be6f1c5dd22f303fe482d215a15a380f4d97
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-01-21 13:17:35 -0500

    Change all user-facing notifications to be high priority (#2664)
    * Change all user-facing notifications to be high priority
    
    * Update test_broadcast.py
    
    * Update test_district_points.py

--- tests/models_tests/notifications/test_district_points.py
+++ tests/models_tests/notifications/test_district_points.py
@@ -1,8 +1,9 @@
 import unittest2
 
+from consts.fcm.platform_priority import PlatformPriority
 from consts.notification_type import NotificationType
-from models.district import District
 
+from models.district import District
 from models.notifications.district_points import DistrictPointsNotification
 
 
@@ -19,6 +20,9 @@ class TestDistrictPointsNotification(unittest2.TestCase):
         self.assertIsNotNone(self.notification.fcm_notification)
         self.assertEqual(self.notification.fcm_notification.title, 'FIM District Points Updated')
         self.assertEqual(self.notification.fcm_notification.body, 'FIRST In Michigan district point calculations have been updated.')
+
+    def test_platform_config(self):
+        self.assertEqual(self.notification.platform_config.priority, PlatformPriority.HIGH)
 
     def test_data_payload(self):
         self.assertEqual(self.notification.data_payload, {'district_key': '2015fim'})
