69e92ee264e8f413407597ba09ea2fdc5e7673ba
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-02-10 20:35:12 -0500

    Only npm install before JS tests (#2693)
    [nodeploy]

--- ops/travis/travis-before.sh
+++ ops/travis/travis-before.sh
@@ -8,10 +8,15 @@ fi
 
 case "$1" in
 
-    "PYUNIT" | "JSUNIT" | "PYBUILD" )
+    "JSUNIT" )
         echo "Setting up for unit tests..."
-        cp static/javascript/tba_js/tba_keys_template.js static/javascript/tba_js/tba_keys.js
-        paver make
+        npm i
+        ;;
+    "PYUNIT")
+        echo "Setting up pyunit. Nothing to do..."
+        ;;
+    "PYBUILD")
+        echo "Setting up pybuild. Nothing to do..."
         ;;
     "LINT")
         echo "Setting up to lint. Nothing to do..."
