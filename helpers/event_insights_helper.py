27da927f60319150862ac9fa536f1a5f9cbc72b5
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2020-02-15 22:42:56 -0500

    2020 event insights (#2708)
    * 2020 event insights

--- helpers/event_insights_helper.py
+++ helpers/event_insights_helper.py
@@ -13,12 +13,171 @@ class EventInsightsHelper(object):
             2017: cls.calculate_event_insights_2017,
             2018: cls.calculate_event_insights_2018,
             2019: cls.calculate_event_insights_2019,
+            2020: cls.calculate_event_insights_2020,
         }
 
         if year in INSIGHTS_MAP:
             return INSIGHTS_MAP[year](matches)
         else:
             return None
+
+    @classmethod
+    def calculate_event_insights_2020(cls, matches):
+        qual_matches = []
+        playoff_matches = []
+        for match in matches:
+            if match.comp_level == 'qm':
+                qual_matches.append(match)
+            else:
+                playoff_matches.append(match)
+
+        qual_insights = cls._calculate_event_insights_2020_helper(qual_matches)
+        playoff_insights = cls._calculate_event_insights_2020_helper(playoff_matches)
+
+        return {
+            'qual': qual_insights,
+            'playoff': playoff_insights,
+        }
+
+    @classmethod
+    def _calculate_event_insights_2020_helper(cls, matches):
+        # Auto
+        init_line_auto = 0
+        cell_points_auto = 0
+        points_auto = 0
+
+        # Teleop
+        climb_park_teleop = 0
+        robots_hanging = 0
+        cell_points_teleop = 0
+        control_panel_points = 0
+        points_teleop = 0
+
+        # Overall
+        exit_init_line_count = 0
+        achieve_stage1_count = 0
+        achieve_stage2_count = 0
+        achieve_stage3_count = 0
+        park_count = 0
+        hang_count = 0
+        generator_level_count = 0
+        generator_operational_count = 0
+        generator_energized_count = 0
+
+        unicorn_matches = 0
+        winning_scores = 0
+        win_margins = 0
+        total_scores = 0
+        total_cell = 0
+        foul_scores = 0
+        high_score = [0, "", ""]
+
+        finished_matches = 0
+        has_insights = False
+        for match in matches:
+            if not match.has_been_played:
+                continue
+
+            red_score = match.alliances['red']['score']
+            blue_score = match.alliances['blue']['score']
+            win_score = max(red_score, blue_score)
+
+            winning_scores += win_score
+            win_margins += (win_score - min(red_score, blue_score))
+            total_scores += red_score + blue_score
+
+            if win_score > high_score[0]:
+                high_score = [win_score, match.key_name, match.short_name]
+
+            for alliance_color in ['red', 'blue']:
+                isRed = alliance_color == 'red'
+                try:
+                    alliance_breakdown  = match.score_breakdown[alliance_color]
+
+                    # Auto
+                    init_line_auto += alliance_breakdown['autoInitLinePoints']
+                    cell_points_auto += alliance_breakdown['autoCellPoints']
+                    points_auto += alliance_breakdown['autoPoints']
+
+                    # Teleop
+                    climb_park_teleop += alliance_breakdown['endgamePoints']
+                    robots_hanging += alliance_breakdown['tba_numRobotsHanging']
+                    cell_points_teleop += alliance_breakdown['teleopCellPoints']
+                    control_panel_points += alliance_breakdown['controlPanelPoints']
+                    points_teleop += alliance_breakdown['teleopPoints']
+
+                    # Overall
+                    for i in xrange(3):
+                        init_line = 'initLineRobot{}'.format(i+1)
+                        if alliance_breakdown[init_line] == 'Exited':
+                            exit_init_line_count += 1
+
+                        endgame = 'endgameRobot{}'.format(i+1)
+                        if alliance_breakdown[endgame] == 'Park':
+                            park_count += 1
+                        elif alliance_breakdown[endgame] == 'Hang':
+                            hang_count += 1
+
+                    achieve_stage1_count += 1 if alliance_breakdown['stage1Activated'] else 0
+                    achieve_stage2_count += 1 if alliance_breakdown['stage2Activated'] else 0
+                    achieve_stage3_count += 1 if alliance_breakdown['stage3Activated'] else 0
+                    generator_level_count += 1 if alliance_breakdown['endgameRungIsLevel'] == 'IsLevel' and alliance_breakdown['tba_numRobotsHanging'] > 0 else 0
+                    generator_operational_count += 1 if alliance_breakdown['shieldOperationalRankingPoint'] else 0
+                    generator_energized_count += 1 if alliance_breakdown['shieldEnergizedRankingPoint'] else 0
+                    total_cell += alliance_breakdown['autoCellPoints'] + alliance_breakdown['teleopCellPoints']
+
+                    alliance_win = alliance_color == match.winning_alliance
+                    unicorn_matches += (1 if alliance_win and alliance_breakdown['shieldOperationalRankingPoint'] and alliance_breakdown['shieldEnergizedRankingPoint'] else 0)
+                    foul_scores += alliance_breakdown['foulPoints']
+                    has_insights = True
+                except Exception, e:
+                    msg = "Event insights failed for {}: {}".format(match.key.id(), e.message)
+                    # event.get() below should be cheap since it's backed by context cache
+                    if match.event.get().event_type_enum in EventType.SEASON_EVENT_TYPES:
+                        logging.warning(msg)
+                        logging.warning(traceback.format_exc())
+                    else:
+                        logging.info(msg)
+            finished_matches += 1
+
+        if not has_insights:
+            return None
+
+        if finished_matches == 0:
+            return {}
+
+        opportunities_1x = 2 * finished_matches
+        opportunities_3x = 6 * finished_matches
+        event_insights = {
+            # Auto
+            'average_init_line_points_auto': float(init_line_auto) / opportunities_1x,
+            'average_cell_points_auto': float(cell_points_auto) / opportunities_1x,
+            'average_points_auto': float(points_auto) / opportunities_1x,
+            # Teleop
+            'average_endgame_points': float(climb_park_teleop) / opportunities_1x,
+            'average_num_robots_hanging': float(robots_hanging) / opportunities_1x,
+            'average_cell_points_teleop': float(cell_points_teleop) / opportunities_1x,
+            'average_control_panel_points': float(control_panel_points) / opportunities_1x,
+            'average_points_teleop': float(points_teleop) / opportunities_1x,
+            # Overall
+            'exit_init_line_count': [exit_init_line_count, opportunities_3x, 100.0 * float(exit_init_line_count) / opportunities_3x],
+            'achieve_stage1_count': [achieve_stage1_count, opportunities_1x, 100.0 * float(achieve_stage1_count) / opportunities_1x],
+            'achieve_stage2_count': [achieve_stage2_count, opportunities_1x, 100.0 * float(achieve_stage2_count) / opportunities_1x],
+            'achieve_stage3_count': [achieve_stage3_count, opportunities_1x, 100.0 * float(achieve_stage3_count) / opportunities_1x],
+            'park_count': [park_count, opportunities_3x, 100.0 * float(park_count) / opportunities_3x],
+            'hang_count': [hang_count, opportunities_3x, 100.0 * float(hang_count) / opportunities_3x],
+            'generator_level_count': [generator_level_count, opportunities_1x, 100.0 * float(generator_level_count) / opportunities_1x],
+            'generator_operational_rp_achieved': [generator_operational_count, opportunities_1x, 100.0 * float(generator_operational_count) / opportunities_1x],
+            'generator_energized_rp_achieved': [generator_energized_count, opportunities_1x, 100.0 * float(generator_energized_count) / opportunities_1x],
+            'unicorn_matches': [unicorn_matches, opportunities_1x, 100.0 * float(unicorn_matches) / opportunities_1x],
+            'average_win_score': float(winning_scores) / finished_matches,
+            'average_win_margin': float(win_margins) / finished_matches,
+            'average_score': float(total_scores) / opportunities_1x,
+            'average_cell_score': float(total_cell) / opportunities_1x,
+            'average_foul_score': float(foul_scores) / opportunities_1x,
+            'high_score': high_score,
+        }
+        return event_insights
 
     @classmethod
     def calculate_event_insights_2019(cls, matches):
