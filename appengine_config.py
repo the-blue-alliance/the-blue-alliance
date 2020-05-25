f76251efca0ba3bbbc5f07452b9edb70230f0bad
Phil Lopreiato <plnyyanks@gmail.com>
2020-05-25 14:56:29 -0400

    Take grpcio dependency from built-in

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,7 @@
 from google.appengine.ext import vendor
 
+import six
+# https://github.com/googleapis/python-ndb/issues/249#issuecomment-560957294
+six.moves.reload_module(six)
 
 vendor.add('lib')
