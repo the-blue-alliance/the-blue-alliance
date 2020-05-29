2db78c909191562508579b7b91f1c956dbea51b0
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-05-28 23:47:53 -0700

    Build CSS/JS

--- ops/do_compress.py
+++ ops/do_compress.py
@@ -7,14 +7,14 @@ SCRIPTS_MAIN = ['src/web/static/jwplayer/jwplayer.js',
                 'src/web/static/xcharts/xcharts.min.js',
                 'src/web/static/javascript/utils/client_detection.js',
                 'src/web/static/javascript/tba_js/tablesorter.js',
-                'src/web/static/javascript/tba_js/tba_keys.js',
+                # 'src/web/static/javascript/tba_js/tba_keys.js',
                 'src/web/static/javascript/tba_js/tba.js',
                 'src/web/static/javascript/tba_js/tba_charts.js',
                 'src/web/static/javascript/tba_js/tba_countdown.js',
                 'src/web/static/javascript/tba_js/tba_sidebar.js',
                 'src/web/static/javascript/tba_js/tba_typeahead.js',
                 'src/web/static/javascript/tba_js/tba_favorites.js',
-                'src/web/static/javascript/tba_js/tba_fcm.js',
+                # 'src/web/static/javascript/tba_js/tba_fcm.js',
                 'src/web/static/javascript/tba_js/ReView0.65b.js',
                 ]
 
@@ -51,13 +51,13 @@ STYLESHEETS_GAMEDAY_OUT = 'src/web/static/compiled/css/tba_combined_style.gameda
 
 
 def compress_css(in_files, out_file, verbose=False, temp_file='.temp'):
-    os.system('uglifycss {} --output {}'.format(' '.join(in_files), out_file))
+    os.system('npx uglifycss {} --output {}'.format(' '.join(in_files), out_file))
     print('=> %s' % out_file)
     print('')
 
 
 def compress_js(in_files, out_file):
-    os.system('uglifyjs {} --compress -o {}'.format(' '.join(in_files), out_file))
+    os.system('npx uglify-js {} --compress -o {}'.format(' '.join(in_files), out_file))
     print('=> %s' % out_file)
     print('')
 
@@ -71,21 +71,21 @@ def main(kind=None):
         print('Compressing Main JavaScript...')
         compress_js(SCRIPTS_MAIN, SCRIPTS_MAIN_OUT)
 
-        print('Compressing Firebase Messaging Serviceworker JavaScript...')
-        compress_js(SCRIPTS_FIREBASE_SERVICEWORKER, SCRIPTS_FIREBASE_SERVICEWORKER_OUT)
-
-        print('Compressing GameDay JavaScript...')
-        compress_js(SCRIPTS_GAMEDAY, SCRIPTS_GAMEDAY_OUT)
-
-        print('Compressing EventWizard JavaScript...')
-        compress_js(SCRIPTS_EVENTWIZARD, SCRIPTS_EVENTWIZARD_OUT)
+        # print('Compressing Firebase Messaging Serviceworker JavaScript...')
+        # compress_js(SCRIPTS_FIREBASE_SERVICEWORKER, SCRIPTS_FIREBASE_SERVICEWORKER_OUT)
+        #
+        # print('Compressing GameDay JavaScript...')
+        # compress_js(SCRIPTS_GAMEDAY, SCRIPTS_GAMEDAY_OUT)
+        #
+        # print('Compressing EventWizard JavaScript...')
+        # compress_js(SCRIPTS_EVENTWIZARD, SCRIPTS_EVENTWIZARD_OUT)
 
     if kind == 'css' or kind is None:
         print('Compressing Main CSS...')
         compress_css(STYLESHEETS_MAIN, STYLESHEETS_MAIN_OUT)
 
-        print('Compressing GameDay CSS...')
-        compress_css(STYLESHEETS_GAMEDAY, STYLESHEETS_GAMEDAY_OUT)
+        # print('Compressing GameDay CSS...')
+        # compress_css(STYLESHEETS_GAMEDAY, STYLESHEETS_GAMEDAY_OUT)
 
 
 if __name__ == '__main__':
