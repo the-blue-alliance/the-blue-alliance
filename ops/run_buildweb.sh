3a626304d69bf8eacee514fcb1ab40117a51411c
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-05-29 03:07:03 -0400

    [py3-web] HTML templating, CSS, JS (#2803)
    * Copy old templates and filters over
    
    * Comment out unused filters for now
    
    * Lint fixes
    
    * Move over static files
    
    * Working on getting css/js to build
    
    * Build CSS/JS
    
    * Build on PR
    
    * Build on deploy

---
+++ ops/run_buildweb.sh
@@ -0,0 +1,2 @@
+npx -p less lessc src/web/static/css/less_css/tba_style.main.less src/web/static/css/less_css/tba_style.main.css
+python ./ops/do_compress.py
--- src/web/main.py
+++ src/web/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from web.handlers import RootHandler
+from web.handlers import HomeHandler
+from web.jinja2_filters import register_template_filters
 
 
 app = Flask(__name__)
-app.add_url_rule("/", view_func=RootHandler)
+app.add_url_rule("/", view_func=HomeHandler)
+register_template_filters(app)
