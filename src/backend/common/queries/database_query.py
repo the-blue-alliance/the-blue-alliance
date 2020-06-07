264afb6bd9fcb125c95b3139599381f6860c9fef
Phil Lopreiato <plnyyanks@gmail.com>
2020-06-07 16:09:15 -0400

    [py3] Introduce Typed DatabaseQuery abstraction

---
+++ src/backend/common/queries/database_query.py
@@ -0,0 +1,29 @@
+import abc
+from backend.common.typed_future import TypedFuture
+from google.cloud import ndb
+from typing import TypeVar, Generic
+
+
+QueryReturn = TypeVar("QueryReturn")
+
+
+class DatabaseQuery(abc.ABC, Generic[QueryReturn]):
+
+    _query_args: int
+
+    def __init__(self, *args, **kwargs):
+        self._query_args = kwargs
+
+    @abc.abstractmethod
+    def _query_async(self) -> TypedFuture[QueryReturn]:
+        ...
+
+    def fetch(self) -> QueryReturn:
+        return self.fetch_async().get_result()
+
+    @ndb.tasklet
+    def fetch_async(self) -> TypedFuture[QueryReturn]:
+        query_result = yield self._query_async(**self._query_args)
+        # Type-hinting the tasklet decorator is hard, but it consumes
+        # the generator and returns the overall future
+        return query_result  # pyre-ignore
---
+++ src/backend/common/queries/tests/database_query_test.py
@@ -0,0 +1,98 @@
+import pytest
+from backend.common.typed_future import TypedFuture
+from backend.common.queries.database_query import DatabaseQuery
+from google.cloud import ndb
+from typing import List
+
+
+@pytest.fixture(autouse=True)
+def auto_add_ndb_context(ndb_context):
+    pass
+
+
+class DummyModel(ndb.Model):
+    int_prop = ndb.IntegerProperty()
+
+
+class DummyModelPointQuery(DatabaseQuery[DummyModel]):
+    @ndb.tasklet
+    def _query_async(self, model_key: str) -> TypedFuture[DummyModel]:
+        model = yield DummyModel.get_by_id_async(model_key)
+        return model
+
+
+class DummyModelRangeQuery(DatabaseQuery[List[DummyModel]]):
+    @ndb.tasklet
+    def _query_async(self, min: int, max: int) -> TypedFuture[List[DummyModel]]:
+        models = yield DummyModel.query(
+            DummyModel.int_prop >= min, DummyModel.int_prop <= max
+        ).fetch_async()
+        return models
+
+
+def test_point_query_exists_sync() -> None:
+    m = DummyModel(id="test")
+    m.put()
+
+    query = DummyModelPointQuery(model_key="test")
+    result = query.fetch()
+    assert result == m
+
+
+def test_point_query_not_exists_sync() -> None:
+    query = DummyModelPointQuery(model_key="test")
+    result = query.fetch()
+    assert result is None
+
+
+def test_point_query_exists_async() -> None:
+    m = DummyModel(id="test")
+    m.put()
+
+    query = DummyModelPointQuery(model_key="test")
+    result_future = query.fetch_async()
+
+    result = result_future.result()
+    assert result == m
+
+
+def test_point_query_not_exists_async() -> None:
+    query = DummyModelPointQuery(model_key="test")
+    result_future = query.fetch_async()
+
+    result = result_future.result()
+    assert result is None
+
+
+def test_range_query_empty_sync() -> None:
+    query = DummyModelRangeQuery(min=0, max=10)
+    result = query.fetch()
+
+    assert result == []
+
+
+def test_range_query_empty_async() -> None:
+    query = DummyModelRangeQuery(min=0, max=10)
+    result_future = query.fetch_async()
+    result = result_future.result()
+
+    assert result == []
+
+
+def test_range_query_with_data_sync() -> None:
+    keys = ndb.put_multi([DummyModel(id=f"{i}", int_prop=i) for i in range(0, 5)])
+    assert len(keys) == 5
+
+    query = DummyModelRangeQuery(min=0, max=2)
+    result = query.fetch()
+    assert len(result) == 3
+
+
+def test_range_query_with_data_async() -> None:
+    keys = ndb.put_multi([DummyModel(id=f"{i}", int_prop=i) for i in range(0, 5)])
+    assert len(keys) == 5
+
+    query = DummyModelRangeQuery(min=0, max=2)
+    result_future = query.fetch_async()
+    result = result_future.result()
+    assert len(result) == 3
