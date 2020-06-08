1bf5d0c1b0be4f6ce094f81bc55b7ef047cf0dcc
Zachary Orr <ZachOrr@users.noreply.github.com>
2020-06-08 12:14:05 -0400

    Add Google Analytics wrapper class

---
+++ src/backend/common/google_analytics.py
@@ -0,0 +1,55 @@
+class GoogleAnalytics:
+    """
+    Class that manages sending information to Google Analytics
+
+    For more information about GAnalytics Protocol Parameters, visit
+    https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+    """
+
+    @classmethod
+    def track_event(
+        cls,
+        client_id: str,
+        event_category: str,
+        event_action: str,
+        event_value: int = None,
+    ) -> None:
+        from backend.common.sitevars.google_analytics_id import GoogleAnalyticsID
+
+        google_analytics_id = GoogleAnalyticsID.google_analytics_id()
+
+        if not google_analytics_id:
+            import logging
+
+            logging.warning(
+                "Missing sitevar: google_analytics.id. Can't track API usage."
+            )
+            return
+
+        import uuid
+
+        cid = uuid.uuid3(uuid.NAMESPACE_X500, client_id)
+
+        params = {
+            "v": 1,
+            "tid": google_analytics_id,
+            "cid": cid,
+            "t": "event",
+            "ec": event_category,
+            "ea": event_action,
+            "ni": 1,
+            "sc": "end",  # forces tracking session to end
+        }
+        if event_value:
+            params["ev"] = event_value
+
+        from urllib.parse import urlencode
+
+        params = urlencode(params)
+
+        import urlfetch
+
+        analytics_url = "https://www.google-analytics.com/collect?%s" % params
+        urlfetch.fetch(
+            url=analytics_url, method=urlfetch.GET, deadline=10,
+        )
---
+++ src/backend/common/tests/test_google_analytics.py
@@ -0,0 +1,64 @@
+from google.cloud import ndb
+import logging
+import pytest
+from unittest.mock import patch
+from urllib.parse import urlparse, parse_qsl
+
+from backend.common.google_analytics import GoogleAnalytics
+
+
+@pytest.fixture(autouse=True)
+def auto_add_ndb_context(ndb_context: ndb.Context) -> None:
+    pass
+
+
+def test_GoogleAnalytics_track_event_missing_sitevar() -> None:
+    with patch("logging.warning") as mock_warning:
+        GoogleAnalytics.track_event("testbed", "test", "test")
+        mock_warning.assert_called_with(
+            "Missing sitevar: google_analytics.id. Can't track API usage."
+        )
+
+
+@pytest.mark.parametrize("ev", [(None), (123)])
+def test_GoogleAnalytics_track_event(ev) -> None:
+    from backend.common.sitevars.google_analytics_id import GoogleAnalyticsID
+
+    sitevar = GoogleAnalyticsID._default_sitevar()
+    sitevar.contents["GOOGLE_ANALYTICS_ID"] = "abc"
+
+    with patch("urlfetch.fetch") as mock_fetch:
+        GoogleAnalytics.track_event("testbed", "test", "test", ev)
+
+    mock_fetch.assert_called()
+    _, kwargs = mock_fetch.call_args
+
+    assert len(kwargs) == 3
+    assert kwargs["deadline"] == 10
+    assert kwargs["method"] == "GET"
+
+    url = kwargs["url"]
+    assert url
+
+    url_components = urlparse(url)
+    assert url_components.scheme == "https"
+    assert url_components.netloc == "www.google-analytics.com"
+    assert url_components.path == "/collect"
+
+    query_components = parse_qsl(url_components.query)
+    assert len(query_components) == 8 + (1 if ev else 0)
+
+    query_components_expected = {
+        ("v", "1"),
+        ("tid", "abc"),
+        ("cid", "6dcf939a-da96-33c4-acd1-51208db9ceaa"),
+        ("t", "event"),
+        ("ec", "test"),
+        ("ea", "test"),
+        ("ni", "1"),
+        ("sc", "end"),
+    }
+    if ev:
+        query_components_expected.add(("ev", str(ev)))
+
+    assert set(query_components) == query_components_expected
