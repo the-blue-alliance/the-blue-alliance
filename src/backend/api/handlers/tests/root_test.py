19a0abb20606f237f5086c0551e08599e8e3fe92
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-06-10 12:15:57 -0700

    Add api 404 error handler

--- src/backend/api/handlers/tests/root_test.py
+++ src/backend/api/handlers/tests/root_test.py
@@ -2,5 +2,5 @@ from werkzeug.test import Client
 
 
 def test_root(api_client: Client) -> None:
-    resp = api_client.get("/api/v3")
+    resp = api_client.get("/api/v3/test")
     assert resp.status_code == 200
--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from backend.api.handlers.error import handle_404
 from backend.api.handlers.root import root
 from backend.common.middleware import install_middleware
 
 
 app = Flask(__name__)
 install_middleware(app)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
-app.add_url_rule("/api/<path:path>", view_func=root)
+app.add_url_rule("/api/v3/<path:path>", view_func=root)
+app.register_error_handler(404, handle_404)
