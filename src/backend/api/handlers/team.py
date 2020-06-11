eed70ad4ee85e55c9e4fba93df6bbd57989cad46
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-06-11 09:31:44 -0700

    Fix view return type

--- src/backend/api/handlers/team.py
+++ src/backend/api/handlers/team.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, Response
 
 from backend.api.handlers.decorators import api_authenticated
 from backend.common.decorators import cached_public
@@ -7,11 +7,11 @@ from backend.common.queries.team_query import TeamQuery, TeamListQuery
 
 @api_authenticated
 @cached_public
-def team(team_key: str) -> dict:
+def team(team_key: str) -> Response:
     return jsonify(TeamQuery(team_key=team_key).fetch_dict(3))
 
 
 @api_authenticated
 @cached_public
-def team_list(page_num: int) -> list:
+def team_list(page_num: int) -> Response:
     return jsonify(TeamListQuery(page=page_num).fetch_dict(3))
