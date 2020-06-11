cbd1d46042aadb2410292f6512953c085e25dfee
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-06-11 09:26:03 -0700

    Populate db for tests

--- src/backend/api/handlers/tests/decorators_test.py
+++ src/backend/api/handlers/tests/decorators_test.py
@@ -1,16 +1,21 @@
 from werkzeug.test import Client
 
+from backend.common.models.team import Team
+
 
 def test_not_authenticated(api_client: Client) -> None:
+    Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254")
     assert resp.status_code == 401
 
 
 def test_authenticated_header(api_client: Client) -> None:
+    Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
 
 
 def test_authenticated_urlparam(api_client: Client) -> None:
+    Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254?X-TBA-Auth-Key=test")
     assert resp.status_code == 200
--- src/backend/api/handlers/tests/team_test.py
+++ src/backend/api/handlers/tests/team_test.py
@@ -1,11 +1,15 @@
 from werkzeug.test import Client
 
+from backend.common.models.team import Team
+
 
 def test_team(api_client: Client) -> None:
+    Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
 
 
 def test_team_list(api_client: Client) -> None:
+    Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/teams/0", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
