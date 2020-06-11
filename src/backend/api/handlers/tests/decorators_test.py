4d01b3f12098f17b4317b8cd75e09cd98ea8c718
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-06-11 11:04:41 -0700

    Fix ndb context issue in tests

--- src/backend/api/handlers/tests/decorators_test.py
+++ src/backend/api/handlers/tests/decorators_test.py
@@ -1,21 +1,25 @@
+from google.cloud import ndb
 from werkzeug.test import Client
 
 from backend.common.models.team import Team
 
 
-def test_not_authenticated(api_client: Client) -> None:
-    Team(id="frc254", team_number=254).put()
+def test_not_authenticated(ndb_client: ndb.Client, api_client: Client) -> None:
+    with ndb_client.context():
+        Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254")
     assert resp.status_code == 401
 
 
-def test_authenticated_header(api_client: Client) -> None:
-    Team(id="frc254", team_number=254).put()
+def test_authenticated_header(ndb_client: ndb.Client, api_client: Client) -> None:
+    with ndb_client.context():
+        Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
 
 
-def test_authenticated_urlparam(api_client: Client) -> None:
-    Team(id="frc254", team_number=254).put()
+def test_authenticated_urlparam(ndb_client: ndb.Client, api_client: Client) -> None:
+    with ndb_client.context():
+        Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254?X-TBA-Auth-Key=test")
     assert resp.status_code == 200
--- src/backend/api/handlers/tests/team_test.py
+++ src/backend/api/handlers/tests/team_test.py
@@ -1,15 +1,18 @@
+from google.cloud import ndb
 from werkzeug.test import Client
 
 from backend.common.models.team import Team
 
 
-def test_team(api_client: Client) -> None:
-    Team(id="frc254", team_number=254).put()
+def test_team(ndb_client: ndb.Client, api_client: Client) -> None:
+    with ndb_client.context():
+        Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/team/frc254", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
 
 
-def test_team_list(api_client: Client) -> None:
-    Team(id="frc254", team_number=254).put()
+def test_team_list(ndb_client: ndb.Client, api_client: Client) -> None:
+    with ndb_client.context():
+        Team(id="frc254", team_number=254).put()
     resp = api_client.get("/api/v3/teams/0", headers={"X-TBA-Auth-Key": "test"})
     assert resp.status_code == 200
