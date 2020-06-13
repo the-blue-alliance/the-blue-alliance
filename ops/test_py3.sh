a3c3a21290274f4915fcad5789560db49bc925dd
Phil Lopreiato <plnyyanks@gmail.com>
2020-06-13 10:27:49 -0400

    fix off-by-one
    [nodeploy]

--- ops/test_py3.sh
+++ ops/test_py3.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "--relevant" ] ; then
   pytest --testmon --suppress-no-test-exit-code src
-elif [ "$#" -ge 0 ] ; then
+elif [ "$#" -ge 1 ] ; then
   pytest src -k "$@"
 else
   pytest src --cov-report=xml --cov=src
