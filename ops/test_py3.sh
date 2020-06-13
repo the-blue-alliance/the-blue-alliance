4fbfdedf21cde38cb2fbb34e6f0df0ffdba716e4
Phil Lopreiato <plnyyanks@gmail.com>
2020-06-13 10:24:53 -0400

    support selecting single tests to run in script
    [nodeploy]

--- ops/test_py3.sh
+++ ops/test_py3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ "$1" == "--relevant" ]
-then
+if [ "$1" == "--relevant" ] ; then
   pytest --testmon --suppress-no-test-exit-code src
+elif [ "$#" -ge 0 ] ; then
+  pytest src -k "$@"
 else
   pytest src --cov-report=xml --cov=src
 fi
