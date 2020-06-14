fea5d256e3a20e287ca040c5721e13180086a9eb
Phil Lopreiato <plnyyanks@gmail.com>
2020-06-14 15:48:48 -0400

    Implement team detail/canonical delegation

--- src/backend/web/handlers/tests/team_detail_test.py
+++ src/backend/web/handlers/tests/team_detail_test.py
@@ -1,9 +1,14 @@
+from datetime import datetime
 from typing import NamedTuple, Optional
 
 import bs4
+from freezegun import freeze_time
 from google.cloud import ndb
 from werkzeug.test import Client
 
+from backend.common.consts.event_type import EventType
+from backend.common.models.event import Event
+from backend.common.models.event_team import EventTeam
 from backend.common.models.team import Team
 
 
@@ -33,6 +38,26 @@ def preseed_team(ndb_client: ndb.Client, team_number: int) -> None:
         ).put()
 
 
+def preseed_event_for_team(
+    ndb_client: ndb.Client, team_number: int, event_key: str
+) -> None:
+    with ndb_client.context():
+        Event(
+            id=event_key,
+            event_short=event_key[4:],
+            year=int(event_key[:4]),
+            event_type_enum=EventType.REGIONAL,
+            start_date=datetime(2020, 3, 1),
+            end_date=datetime(2020, 3, 5),
+        ).put()
+        EventTeam(
+            id=f"{event_key}_frc{team_number}",
+            event=ndb.Key(Event, event_key),
+            team=ndb.Key(Team, f"frc{team_number}"),
+            year=int(event_key[:4]),
+        ).put()
+
+
 def get_team_info(resp_data: str) -> TeamInfo:
     soup = bs4.BeautifulSoup(resp_data, "html.parser")
     header = soup.find("h2")
@@ -56,25 +81,61 @@ def get_team_info(resp_data: str) -> TeamInfo:
     )
 
 
-def test_get_bad_team_num(web_client: Client) -> None:
+def test_canonical_get_bad_team_num(web_client: Client) -> None:
     resp = web_client.get("/team/0")
     assert resp.status_code == 404
 
 
-def test_team_not_found(web_client: Client) -> None:
+def test_detail_get_bad_team_num(web_client: Client) -> None:
+    resp = web_client.get("/team/0/2020")
+    assert resp.status_code == 404
+
+
+def test_canonical_team_not_found(web_client: Client) -> None:
     resp = web_client.get("/team/254")
     assert resp.status_code == 404
 
 
-def test_team_found(web_client: Client, ndb_client: ndb.Client) -> None:
+def test_detail_team_not_found(web_client: Client) -> None:
+    resp = web_client.get("/team/254/2020")
+    assert resp.status_code == 404
+
+
+def test_canonical_team_found_no_events(
+    web_client: Client, ndb_client: ndb.Client
+) -> None:
     preseed_team(ndb_client, 254)
     resp = web_client.get("/team/254")
+    # This should render team history, but it's not implemented yet
+    assert resp.status_code == 501
+
+
+def test_detail_team_found_no_events(
+    web_client: Client, ndb_client: ndb.Client
+) -> None:
+    preseed_team(ndb_client, 254)
+    resp = web_client.get("/team/254/2020")
     assert resp.status_code == 200
 
 
-def test_team_info(web_client: Client, ndb_client: ndb.Client) -> None:
+@freeze_time("2020-02-01")
+def test_canonical_team_info(web_client: Client, ndb_client: ndb.Client) -> None:
     preseed_team(ndb_client, 254)
+    # We need an event to not render the history page
+    preseed_event_for_team(ndb_client, 254, "2020test")
     resp = web_client.get("/team/254")
+    assert resp.status_code == 200
+
+    team_info = get_team_info(resp.data)
+    assert team_info.header == "Team 254 - The 254 Team"
+    assert team_info.location == "New York, NY, USA"
+    assert team_info.full_name == "The Blue Alliance / Some High School"
+    assert team_info.rookie_year == "Rookie Year: 2008"
+
+
+def test_detail_team_info(web_client: Client, ndb_client: ndb.Client) -> None:
+    preseed_team(ndb_client, 254)
+    resp = web_client.get("/team/254/2020")
     assert resp.status_code == 200
 
     team_info = get_team_info(resp.data)
