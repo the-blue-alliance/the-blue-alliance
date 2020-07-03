e35fdb6eb53bf70bd72e9ae64500204595922281
Phil Lopreiato <plnyyanks@gmail.com>
2020-07-03 15:34:49 -0400

    add db queries

---
+++ src/backend/common/queries/dict_converters/robot_converter.py
@@ -0,0 +1,31 @@
+from typing import Dict, List
+
+from backend.common.consts.api_version import ApiMajorVersion
+from backend.common.models.robot import Robot
+from backend.common.queries.dict_converters.converter_base import ConverterBase
+
+
+class RobotConverter(ConverterBase):
+    # SUBVERSIONS = {  # Increment every time a change to the dict is made
+    #     3: 3,
+    # }
+    # TODO use for cache clearing
+
+    def _convert_list(
+        self, model_list: List[Robot], version: ApiMajorVersion
+    ) -> List[Dict]:
+        ROBOT_CONVERTERS = {
+            ApiMajorVersion.API_V3: self.robotsConverter_v3,
+        }
+        return ROBOT_CONVERTERS[version](model_list)
+
+    def robotsConverter_v3(self, robots: List[Robot]) -> List[Dict]:
+        return list(map(self.robotConverter_v3, robots))
+
+    def robotConverter_v3(self, robot: Robot) -> Dict:
+        return {
+            "key": robot.key_name,
+            "team_key": robot.team.id(),
+            "year": robot.year,
+            "robot_name": robot.robot_name,
+        }
