33500c1b70442890f148d0f98236a030a413d868
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-07-13 11:21:09 -0700

    Pin less version
    3.12.0 seems to have broken our workflow

--- ops/build/run_buildweb.sh
+++ ops/build/run_buildweb.sh
@@ -1,5 +1,5 @@
 set -e
 
-npx -p less lessc src/backend/web/static/css/less_css/tba_style.main.less src/build/temp/tba_style.main.css
+npx -p less@3.11.3 lessc src/backend/web/static/css/less_css/tba_style.main.less src/build/temp/tba_style.main.css
 python ./ops/build/do_compress.py
 npm run build
