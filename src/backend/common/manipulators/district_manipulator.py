38f3e5bf76d7a0ff9453e7f07f9b741c27c801e6
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2020-08-09 15:46:58 -0400

    port district manipulator (#2933)

--- src/backend/common/manipulators/district_manipulator.py
+++ src/backend/common/manipulators/district_manipulator.py
@@ -1,9 +1,5 @@
-import logging
-
-from database.district_query import DistrictHistoryQuery
-from helpers.cache_clearer import CacheClearer
-from helpers.manipulator_base import ManipulatorBase
-from models.district import District
+from backend.common.manipulators.manipulator_base import ManipulatorBase
+from backend.common.models.district import District
 
 
 class DistrictManipulator(ManipulatorBase):
@@ -11,12 +7,13 @@ class DistrictManipulator(ManipulatorBase):
     Handles District database writes
     """
 
+    """
     @classmethod
     def postUpdateHook(cls, districts, updated_attr_list, is_new_list):
-        """
+        '''
         To run after a district has been updated.
         For new districts, tries to guess the names based on other year's data
-        """
+        '''
         for (district, is_new, updated_attrs) in zip(districts, is_new_list, updated_attr_list):
             if is_new and (not district.display_name or not district.elasticsearch_name):
                 last_year_key = District.renderKeyName(district.year - 1, district.abbreviation)
@@ -42,34 +39,17 @@ class DistrictManipulator(ManipulatorBase):
                         other_district.elasticsearch_name = district.elasticsearch_name
                         to_put.append(other_district)
                 cls.createOrUpdate(to_put, run_post_update_hook=False)
+    """
 
+    """
     @classmethod
     def getCacheKeysAndControllers(cls, affected_refs):
         return CacheClearer.get_district_cache_keys_and_controllers(affected_refs)
+    """
 
     @classmethod
-    def updateMerge(self, new_district, old_district, auto_union=True):
-        """
-        Update and return Districts
-        """
-        immutable_attrs = [
-            "year",
-            "abbreviation",
-        ]  # These build key_name, and cannot be changed without deleting the model.
-
-        attrs = [
-            "display_name",
-            "elasticsearch_name",
-            "rankings",
-            "advancement",
-        ]
-
-        old_district._updated_attrs = []
-        for attr in attrs:
-            if getattr(new_district, attr) is not None:
-                if getattr(new_district, attr) != getattr(old_district, attr):
-                    setattr(old_district, attr, getattr(new_district, attr))
-                    old_district._updated_attrs.append(attr)
-                    old_district.dirty = True
-
+    def updateMerge(
+        cls, new_district: District, old_district: District, auto_union: bool = True
+    ) -> District:
+        cls._update_attrs(new_district, old_district, auto_union)
         return old_district
---
+++ src/backend/common/manipulators/tests/test_district_manipulator.py
@@ -0,0 +1,38 @@
+import unittest
+
+import pytest
+
+from backend.common.manipulators.district_manipulator import DistrictManipulator
+from backend.common.models.district import District
+
+
+@pytest.mark.usefixtures("ndb_context")
+class TestDistrictManipulator(unittest.TestCase):
+    def setUp(self):
+        self.old_district = District(id="2014ne", year=2014, display_name="")
+
+        self.new_district = District(
+            id="2014ne", year=2014, display_name="New England",
+        )
+
+    def assertMergedDistrict(self, district: District) -> None:
+        self.assertOldDistrict(district)
+        self.assertEqual(district.display_name, "New England")
+
+    def assertOldDistrict(self, district: District) -> None:
+        self.assertEqual(district.year, 2014)
+
+    def test_createOrUpdate(self) -> None:
+        DistrictManipulator.createOrUpdate(self.old_district)
+        self.assertOldDistrict(District.get_by_id("2014ne"))
+        DistrictManipulator.createOrUpdate(self.new_district)
+        self.assertMergedDistrict(District.get_by_id("2014ne"))
+
+    def test_findOrSpawn(self) -> None:
+        self.old_district.put()
+        self.assertMergedDistrict(DistrictManipulator.findOrSpawn(self.new_district))
+
+    def test_updateMerge(self) -> None:
+        self.assertMergedDistrict(
+            DistrictManipulator.updateMerge(self.new_district, self.old_district)
+        )
