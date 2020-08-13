539b800b2e14a70ccf76ad1b95d1be35244c4cd2
Justin Tervay <justin@tervay.com>
2020-08-12 23:11:06 -0400

    Remove old testing code

--- src/backend/common/helpers/tests/matchstat_helper_test.py
+++ src/backend/common/helpers/tests/matchstat_helper_test.py
@@ -1,11 +1,8 @@
 import json
 import os
-from typing import Dict
 
 import pytest
 
-from backend.common.models.keys import TeamKey
-from backend.common.models.event_matchstats import EventMatchStats
 from backend.common.helpers.matchstats_helper import MatchstatsHelper
 
 
@@ -33,38 +30,18 @@ def test_compute_matchstats_skip_coprs(test_data_importer):
         assert expected_stats[stat] == pytest.approx(stats[stat])
 
 
+def test_compute_matchstats_with_b_teams_skip_coprs(test_data_importer):
+    matches = test_data_importer.parse_match_list(
+        __file__, "data/2019mttd_matches.json"
+    )
+    with open(
+        os.path.join(os.path.dirname(__file__), "data/2019mttd_stats.json"), "r"
+    ) as f:
+        expected_stats = json.load(f)
+
+    stats = MatchstatsHelper.calculate_matchstats(matches)
+    for stat in expected_stats.keys():
+        assert expected_stats[stat] == pytest.approx(stats[stat])
+
+
 #
-#
-# def assert_stats_equal(stats: EventMatchStats, expected_stats: EventMatchStats) -> None:
-#     assert stats.keys() == expected_stats.keys()
-#     for stat, team_stats in stats.items():
-#         assert team_stats.keys() == expected_stats[stat].keys()
-#
-#
-#
-# def test_compute_matchstats(test_data_importer) -> None:
-#     matches = test_data_importer.parse_match_list(
-#         __file__, "data/2019nyny_matches.json"
-#     )
-#     with open(
-#         os.path.join(os.path.dirname(__file__), "data/2019nyny_stats.json"), "r"
-#     ) as f:
-#         expected_stats = json.load(f)
-#
-#     stats = MatchstatsHelper.calculate_matchstats(matches, 2019)
-#     expected_stats = api_data_to_matchstats(expected_stats)
-#     assert_stats_equal(stats, expected_stats)
-#
-#
-# def test_compute_matchstats_with_b_teams(test_data_importer) -> None:
-#     matches = test_data_importer.parse_match_list(
-#         __file__, "data/2019mttd_matches.json"
-#     )
-#     with open(
-#         os.path.join(os.path.dirname(__file__), "data/2019mttd_stats.json"), "r"
-#     ) as f:
-#         expected_stats = json.load(f)
-#
-#     stats = MatchstatsHelper.calculate_matchstats(matches, 2019)
-#     expected_stats = api_data_to_matchstats(expected_stats)
-#     assert_stats_equal(stats, expected_stats)
