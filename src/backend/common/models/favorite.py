2e29b3f93bdee86f962891a6d0be6ebe537be2ba
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-08-18 10:43:13 -0400

    Add better typing to Favorite

--- src/backend/common/models/favorite.py
+++ src/backend/common/models/favorite.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from google.cloud import ndb
+from pyre_extensions import safe_cast
 
 from backend.common.consts.model_type import ModelType
 
@@ -9,12 +12,14 @@ class Favorite(ndb.Model):
     should be created with a parent that is the associated Account key.
     """
 
-    user_id = ndb.StringProperty(required=True)
-    model_key = ndb.StringProperty(required=True)
-    model_type = ndb.IntegerProperty(required=True, choices=list(ModelType))
+    user_id: str = ndb.StringProperty(required=True)
+    model_key: str = ndb.StringProperty(required=True)
+    model_type: ModelType = safe_cast(
+        ModelType, ndb.IntegerProperty(required=True, choices=list(ModelType))
+    )
 
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    updated = ndb.DateTimeProperty(auto_now=True)
+    created: datetime = ndb.DateTimeProperty(auto_now_add=True)
+    updated: datetime = ndb.DateTimeProperty(auto_now=True)
 
     def __init__(self, *args, **kw):
         super(Favorite, self).__init__(*args, **kw)
