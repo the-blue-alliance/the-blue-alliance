d6f973614b9bfd4f9a221816a6eb5d97b02d1890
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-08-18 11:21:40 -0400

    Add User model to py3 (#2972)

---
+++ src/backend/web/models/tests/user_test.py
@@ -0,0 +1,472 @@
+from typing import Callable
+
+import pytest
+from google.cloud import ndb
+from pyre_extensions import none_throws
+
+from backend.common.consts.auth_type import AuthType
+from backend.common.consts.client_type import ClientType
+from backend.common.consts.model_type import ModelType
+from backend.common.consts.suggestion_state import SuggestionState
+from backend.common.consts.suggestion_type import SuggestionType
+from backend.common.models.account import Account
+from backend.common.models.api_auth_access import ApiAuthAccess
+from backend.common.models.favorite import Favorite
+from backend.common.models.mobile_client import MobileClient
+from backend.common.models.subscription import Subscription
+from backend.common.models.suggestion import Suggestion
+from backend.common.queries.account_query import AccountQuery
+from backend.web.models.user import User
+
+
+def test_init_no_email() -> None:
+    user = User(session_claims={"email": ""})
+    assert user._account is None
+
+
+def test_init_account() -> None:
+    email = "zach@zach.com"
+    account = Account(email=email)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+
+
+def test_init_account_uid_required() -> None:
+    email = "zach@zachorr.com"
+    with pytest.raises(KeyError, match="uid"):
+        User(session_claims={"email": email})
+
+
+def test_init_new_account() -> None:
+    id = "abc"
+    nickname = "zach"
+    email = "{}@zachorr.com".format(nickname)
+
+    user = User(session_claims={"uid": id, "email": email})
+
+    assert user._account is not None
+    user_account = none_throws(user._account)
+
+    assert user_account.key.id() == id
+    assert user_account.email == email
+    assert not user_account.registered
+    assert user_account.nickname == nickname
+    assert user_account.display_name is None
+
+    # Test that the model has been inserted in to our DB
+    account = AccountQuery(email=email).fetch()
+    assert account == user_account
+
+
+def test_init_new_account_name() -> None:
+    id = "abc"
+    email = "zach@zachorr.com"
+    name = "Zach"
+
+    user = User(session_claims={"uid": id, "email": email, "name": name})
+    assert none_throws(user._account).display_name == name
+
+
+def test_email_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.email is None
+
+
+def test_email() -> None:
+    email = "zach@zach.com"
+    account = Account(email=email)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user.email == email
+
+
+def test_display_name_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.display_name is None
+
+
+def test_display_name() -> None:
+    email = "zach@zach.com"
+    name = "Zach"
+    account = Account(email=email, display_name=name)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user.display_name == name
+
+
+def test_nickname_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.nickname is None
+
+
+def test_nickname() -> None:
+    email = "zach@zach.com"
+    nickname = "zach"
+    account = Account(email=email, nickname=nickname)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user.nickname == nickname
+
+
+def test_uid_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.uid is None
+
+
+def test_uid() -> None:
+    email = "zach@zach.com"
+    account = Account(email=email)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user.uid == account.key.id()
+
+
+def test_is_registered_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert not user.is_registered
+
+
+def test_is_registered_false() -> None:
+    email = "zach@zach.com"
+    account = Account(email=email)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    assert not user.is_registered
+
+
+def test_is_registered() -> None:
+    email = "zach@zach.com"
+    account = Account(email=email, registered=True)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user.is_registered
+
+
+def test_mobile_clients_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.mobile_clients == []
+
+
+def test_mobile_clients() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=True)
+    account.put()
+
+    m1 = MobileClient(
+        parent=account.key,
+        user_id=account.key.id(),
+        messaging_id="abc",
+        client_type=ClientType.OS_IOS,
+        verified=True,
+    )
+    m1.put()
+
+    m2 = MobileClient(
+        parent=account.key,
+        user_id=account.key.id(),
+        messaging_id="cde",
+        client_type=ClientType.OS_IOS,
+        verified=False,
+    )
+    m2.put()
+
+    user = User(session_claims={"email": email})
+    assert user.mobile_clients == [m1, m2]
+
+
+def test_favorites_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.favorites == []
+
+
+def test_favorites() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=True)
+    account.put()
+
+    f = Favorite(
+        parent=account.key,
+        user_id=account.key.id(),
+        model_key="frc7332",
+        model_type=ModelType.TEAM,
+    )
+    f.put()
+
+    user = User(session_claims={"email": email})
+    assert user.favorites == [f]
+
+
+def test_favorites_count_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.favorites_count == 0
+
+
+def test_favorites_count() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=True)
+    account.put()
+
+    f = Favorite(
+        parent=account.key,
+        user_id=account.key.id(),
+        model_key="frc7332",
+        model_type=ModelType.TEAM,
+    )
+    f.put()
+
+    user = User(session_claims={"email": email})
+    assert user.favorites_count == 1
+
+
+def test_subscriptions_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.subscriptions == []
+
+
+def test_subscriptions() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=True)
+    account.put()
+
+    s = Subscription(
+        parent=account.key,
+        user_id=account.key.id(),
+        model_key="frc7332",
+        model_type=ModelType.TEAM,
+    )
+    s.put()
+
+    user = User(session_claims={"email": email})
+    assert user.subscriptions == [s]
+
+
+def test_subscriptions_count_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.subscriptions_count == 0
+
+
+def test_subscriptions_count() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=True)
+    account.put()
+
+    s = Subscription(
+        parent=account.key,
+        user_id=account.key.id(),
+        model_key="frc7332",
+        model_type=ModelType.TEAM,
+    )
+    s.put()
+
+    user = User(session_claims={"email": email})
+    assert user.subscriptions_count == 1
+
+
+def test_is_admin_none() -> None:
+    user = User(session_claims={})
+    assert not user.is_admin
+
+
+def test_is_admin_false() -> None:
+    user = User(session_claims={"admin": False})
+    assert not user.is_admin
+
+
+def test_is_admin() -> None:
+    user = User(session_claims={"admin": True})
+    assert user.is_admin
+
+
+def test_register_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    user.register("Zach")
+    assert user._account is None
+
+
+def test_register() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=False)
+    account.put()
+
+    user = User(session_claims={"email": email})
+
+    assert user._account is not None
+    user_account = none_throws(user._account)
+
+    assert not user_account.registered
+    assert user_account.display_name is None
+
+    name = "Zach"
+    user.register(name)
+    assert user_account.registered
+    assert user_account.display_name == name
+
+
+def test_update_display_name_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    user.update_display_name("Zach")
+    assert user._account is None
+
+
+def test_update_display_name() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, registered=False)
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    user_account = none_throws(user._account)
+
+    assert not user_account.registered
+    assert user_account.display_name is None
+
+    name = "Zach"
+    user.update_display_name(name)
+    assert not user_account.registered
+    assert user_account.display_name == name
+
+
+submission_methods = (User.submissions_pending_count, User.submissions_accepted_count)
+
+
+@pytest.mark.parametrize("method", submission_methods)
+def test_submissions_method_count_none(method) -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert method.__get__(user) == 0
+
+
+@pytest.mark.parametrize("method, count", zip(submission_methods, [1, 1]))
+def test_submissions_method_count(method, count) -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email)
+    account.put()
+
+    for state in list(SuggestionState):
+        s = Suggestion(
+            review_state=state, author=account.key, target_model=SuggestionType.ROBOT
+        )
+        s.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    # A very gross way to indirectly call this property method
+    assert method.__get__(user) == count
+
+
+def test_submissions_reviewed_count_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.submissions_reviewed_count == 0
+
+
+def test_submissions_reviewed_count() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email)
+    account.put()
+
+    for state in list(SuggestionState):
+        s = Suggestion(
+            review_state=state,
+            author=account.key,
+            reviewer=account.key,
+            target_model=SuggestionType.ROBOT,
+        )
+        s.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    assert user.submissions_reviewed_count == 2
+
+
+def test_has_review_permissions_none() -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert user.has_review_permissions is False
+
+
+def test_has_review_permissions_empty() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, permissions=[])
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    assert user.has_review_permissions is False
+
+
+def test_has_review_permissions() -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email, permissions=[1])
+    account.put()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    assert user.has_review_permissions is True
+
+
+api_keys_method = (User.api_keys, User.api_read_keys, User.api_write_keys)
+
+
+@pytest.mark.parametrize("method", api_keys_method)
+def test_api_keys_method_none(method) -> None:
+    user = User(session_claims={})
+    assert user._account is None
+    assert method.__get__(user) == []
+
+
+def _api_key(auth_type: AuthType) -> Callable[[], ApiAuthAccess]:
+    def create_key():
+        k = ndb.Key(Account, "account")
+        key = ApiAuthAccess(parent=k, owner=k, auth_types_enum=[auth_type])
+        key.put()
+        return key
+
+    return create_key
+
+
+@pytest.mark.parametrize(
+    "method, keys",
+    zip(
+        api_keys_method,
+        [
+            [_api_key(AuthType.ZEBRA_MOTIONWORKS), _api_key(AuthType.READ_API)],
+            [_api_key(AuthType.READ_API)],
+            [_api_key(AuthType.ZEBRA_MOTIONWORKS)],
+        ],
+    ),
+)
+def test_api_keys_method(method, keys) -> None:
+    email = "zach@zach.com"
+    account = Account(id="account", email=email)
+    account.put()
+
+    expected_keys = [k() for k in keys]
+    auth_types = {k.auth_types_enum[0] for k in expected_keys}
+    for auth_type in {AuthType.ZEBRA_MOTIONWORKS, AuthType.READ_API} - auth_types:
+        _api_key(auth_type)()
+
+    user = User(session_claims={"email": email})
+    assert user._account is not None
+    assert method.__get__(user) == expected_keys
---
+++ src/backend/web/models/user.py
@@ -0,0 +1,192 @@
+from typing import Any, Dict, List, Optional
+
+from pyre_extensions import none_throws
+
+from backend.common.consts.suggestion_state import SuggestionState
+from backend.common.models.account import Account
+from backend.common.models.api_auth_access import ApiAuthAccess
+from backend.common.models.favorite import Favorite
+from backend.common.models.mobile_client import MobileClient
+from backend.common.models.subscription import Subscription
+from backend.common.queries.account_query import AccountQuery
+from backend.common.queries.api_auth_access_query import ApiAuthAccessQuery
+from backend.common.queries.favorite_query import FavoriteQuery
+from backend.common.queries.mobile_client_query import MobileClientQuery
+from backend.common.queries.subscription_query import SubscriptionQuery
+from backend.common.queries.suggestion_query import SuggestionQuery
+
+
+class User:
+    def __init__(self, session_claims: Dict[str, Any]) -> None:
+        self._session_claims = session_claims
+        self._account: Optional[Account] = None
+
+        email = self._session_claims.get("email")
+        if not email:
+            return
+
+        self._account = AccountQuery(email=email).fetch()
+        if self._account:
+            return
+
+        name = self._session_claims.get("name")
+        # Get nickname from email
+        nickname = email.split("@")[0]
+        self._account = Account(
+            id=self._session_claims["uid"],
+            email=email,
+            registered=False,
+            nickname=nickname,
+            display_name=name,
+        )
+        none_throws(self._account).put()
+
+    @property
+    def email(self) -> Optional[str]:
+        if self._account is None:
+            return None
+        return none_throws(self._account).email
+
+    @property
+    def display_name(self) -> Optional[str]:
+        if self._account is None:
+            return None
+        return none_throws(self._account).display_name
+
+    @property
+    def nickname(self) -> Optional[str]:
+        if self._account is None:
+            return None
+        return none_throws(self._account).nickname
+
+    @property
+    def uid(self) -> Optional[str]:
+        if self._account is None:
+            return None
+        return none_throws(self._account).key.id()
+
+    @property
+    def is_registered(self) -> bool:
+        if self._account is None:
+            return False
+        return none_throws(self._account).registered
+
+    @property
+    def mobile_clients(self) -> List[MobileClient]:
+        if self._account is None:
+            return []
+        return MobileClientQuery(
+            user_ids=[none_throws(self._account).key.id()], only_verified=False
+        ).fetch()
+
+    @property
+    def favorites(self) -> List[Favorite]:
+        if self._account is None:
+            return []
+        return FavoriteQuery(account=none_throws(self._account)).fetch()
+
+    @property
+    def favorites_count(self) -> int:
+        if self._account is None:
+            return 0
+        return len(
+            FavoriteQuery(account=none_throws(self._account), keys_only=True).fetch()
+        )
+
+    @property
+    def subscriptions(self) -> List[Subscription]:
+        if self._account is None:
+            return []
+        return SubscriptionQuery(account=none_throws(self._account)).fetch()
+
+    @property
+    def subscriptions_count(self) -> int:
+        if self._account is None:
+            return 0
+        return len(
+            SubscriptionQuery(
+                account=none_throws(self._account), keys_only=True
+            ).fetch()
+        )
+
+    @property
+    def is_admin(self):
+        return self._session_claims.get("admin", False)
+
+    def register(self, display_name: str):
+        if self._account is None:
+            return
+        none_throws(self._account).display_name = display_name
+        none_throws(self._account).registered = True
+        none_throws(self._account).put()
+
+    def update_display_name(self, display_name: str):
+        if self._account is None:
+            return
+        none_throws(self._account).display_name = display_name
+        none_throws(self._account).put()
+
+    @property
+    def submissions_pending_count(self) -> int:
+        if self._account is None:
+            return 0
+        return len(
+            SuggestionQuery(
+                review_state=SuggestionState.REVIEW_PENDING,
+                author=none_throws(self._account),
+                keys_only=True,
+            ).fetch()
+        )
+
+    @property
+    def submissions_accepted_count(self) -> int:
+        if self._account is None:
+            return 0
+        return len(
+            SuggestionQuery(
+                review_state=SuggestionState.REVIEW_ACCEPTED,
+                author=none_throws(self._account),
+                keys_only=True,
+            ).fetch()
+        )
+
+    @property
+    def submissions_reviewed_count(self) -> int:
+        if self._account is None:
+            return 0
+        review_states = [
+            SuggestionState.REVIEW_ACCEPTED,
+            SuggestionState.REVIEW_REJECTED,
+        ]
+        return sum(
+            [
+                len(
+                    SuggestionQuery(
+                        review_state=state,
+                        reviewer=none_throws(self._account),
+                        keys_only=True,
+                    ).fetch()
+                )
+                for state in review_states
+            ]
+        )
+
+    @property
+    def has_review_permissions(self) -> bool:
+        if self._account is None:
+            return False
+        return True if none_throws(self._account).permissions else False
+
+    @property
+    def api_keys(self) -> List[ApiAuthAccess]:
+        if self._account is None:
+            return []
+        return ApiAuthAccessQuery(owner=none_throws(self._account)).fetch()
+
+    @property
+    def api_read_keys(self) -> List[ApiAuthAccess]:
+        return list(filter(lambda key: key.is_read_key, self.api_keys))
+
+    @property
+    def api_write_keys(self) -> List[ApiAuthAccess]:
+        return list(filter(lambda key: key.is_write_key, self.api_keys))
