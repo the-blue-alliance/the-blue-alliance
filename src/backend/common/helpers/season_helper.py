e48f498e6284213e918ee5f554a9accbbe33973c
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-10-07 20:46:00 -0700

    Update homepage for 2021 kickoff

--- src/backend/common/helpers/season_helper.py
+++ src/backend/common/helpers/season_helper.py
@@ -1,7 +1,12 @@
+from datetime import datetime, timedelta
 from typing import Iterable
+
+from pytz import timezone, UTC
 
 from backend.common.models.keys import Year
 from backend.common.sitevars.apistatus import ApiStatus
+
+EST = timezone("EST")
 
 
 class SeasonHelper(object):
@@ -22,3 +27,20 @@ class SeasonHelper(object):
     def get_valid_years(cls) -> Iterable[Year]:
         max_year = cls.get_max_year()
         return range(cls.MIN_YEAR, max_year + 1)
+
+    @staticmethod
+    def kickoff_datetime_est(year: int = datetime.now().year) -> datetime:
+        """ Computes the date of Kickoff for a given year. Kickoff is always the first Saturday in January after Jan 2nd. """
+        jan_2nd = datetime(
+            year, 1, 2, 10, 30, 00, tzinfo=EST
+        )  # Start Kickoff at 10:30am EST
+        days_ahead = 5 - jan_2nd.weekday()  # Saturday is 5
+        # Kickoff won't occur *on* Jan 2nd if it's a Saturday - it'll be the next Saturday
+        if days_ahead <= 0:
+            days_ahead += 7
+        return jan_2nd + timedelta(days=days_ahead)
+
+    @staticmethod
+    def kickoff_datetime_utc(year: int = datetime.now().year) -> datetime:
+        """ Converts kickoff_date to a UTC datetime """
+        return SeasonHelper.kickoff_datetime_est(year).astimezone(UTC)
--- src/backend/web/handlers/index.py
+++ src/backend/web/handlers/index.py
@@ -1,8 +1,12 @@
-from flask import render_template
-
 from backend.common.decorators import cached_public
+from backend.common.helpers.season_helper import SeasonHelper
+from backend.web.profiled_render import render_template
 
 
 @cached_public
 def index() -> str:
-    return render_template("base.html")
+    template_values = {
+        "kickoff_datetime_est": SeasonHelper.kickoff_datetime_est(2021),
+        "kickoff_datetime_utc": SeasonHelper.kickoff_datetime_utc(2021),
+    }
+    return render_template("index/index_kickoff.html", template_values)
