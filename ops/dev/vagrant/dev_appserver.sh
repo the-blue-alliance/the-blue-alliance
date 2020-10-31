373afc03f3f34b2b8ea8f8c630aed5b57b751c39
Phil Lopreiato <plnyyanks@gmail.com>
2020-10-30 20:55:41 -0400

    wip redis

--- ops/dev/vagrant/dev_appserver.sh
+++ ops/dev/vagrant/dev_appserver.sh
@@ -20,6 +20,7 @@ log_level=$(get_config_prop log_level)
 tba_log_level=$(get_config_prop tba_log_level)
 ndb_log_level=$(get_config_prop ndb_log_level)
 datastore_mode=$(get_config_prop datastore_mode)
+redis_cache_url=$(get_config_prop redis_cache_url)
 tasks_mode=$(get_config_prop tasks_mode)
 tasks_remote_config_ngrok_url=$(get_config_prop tasks_remote_config.ngrok_url)
 datastore_args=""
@@ -64,6 +65,14 @@ elif [ "$datastore_mode" == "remote" ]; then
 else
     echo "Unknown datastore mode $datastore_mode! Must be one of [local, remote]"
     exit -1
+fi
+
+# Set up Cloud Datastore global redis cache
+if [ -z "$redis_cache_url" ]; then
+    echo "Running without redis cache"
+else
+    echo "Starting with redis cache at $redis_cache_url"
+    env="$env --env_var REDIS_CACHE_URL $redis_cache_url"
 fi
 
 # Setup Google Cloud Tasks local/remote
