6989e63fa781f31dca27ce38f845e6f010cac366
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-11-21 23:07:59 -0500

    Add Environment.service() for getting GAE service name

--- src/backend/common/tests/environment_test.py
+++ src/backend/common/tests/environment_test.py
@@ -24,6 +24,16 @@ def set_tasks_remote(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setenv("TASKS_MODE", "remote")
 
 
+@pytest.fixture
+def set_project(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setenv("GOOGLE_CLOUD_PROJECT", "tbatv-prod-hrd")
+
+
+@pytest.fixture
+def set_service(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setenv("GAE_SERVICE", "default")
+
+
 def test_dev_env(set_dev) -> None:
     assert Environment.is_dev() is True
     assert Environment.is_prod() is False
@@ -32,6 +42,22 @@ def test_dev_env(set_dev) -> None:
 def test_prod_env(set_prod) -> None:
     assert Environment.is_dev() is False
     assert Environment.is_prod() is True
+
+
+def test_project_none() -> None:
+    assert Environment.project() is None
+
+
+def test_project(set_project) -> None:
+    assert Environment.project() == "tbatv-prod-hrd"
+
+
+def test_service_none() -> None:
+    assert Environment.service() is None
+
+
+def test_service(set_service) -> None:
+    assert Environment.service() == "default"
 
 
 def test_tasks_mode_prod(set_prod) -> None:
