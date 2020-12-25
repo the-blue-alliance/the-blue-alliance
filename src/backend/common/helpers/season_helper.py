d274fd59caf0a7497e666d9f489c14fa154ccf33
Ofek Ashery <ofek.ashery@gmail.com>
2020-12-25 06:25:00 +0200

    Update 2021 Kickoff time (#3215)
    * Update 2021 Kickoff time
    
    * [py3] Add season helper tests
    
    * Fix lint

--- src/backend/common/helpers/season_helper.py
+++ src/backend/common/helpers/season_helper.py
@@ -31,9 +31,10 @@ class SeasonHelper(object):
     @staticmethod
     def kickoff_datetime_est(year: int = datetime.now().year) -> datetime:
         """ Computes the date of Kickoff for a given year. Kickoff is always the first Saturday in January after Jan 2nd. """
-        jan_2nd = datetime(
-            year, 1, 2, 10, 30, 00, tzinfo=EST
-        )  # Start Kickoff at 10:30am EST
+        jan_2nd = datetime(year, 1, 2, 10, 30, 00, tzinfo=EST)
+        # Since 2021, Kickoff starts at 12:00am EST
+        if year >= 2021:
+            jan_2nd = jan_2nd.replace(hour=12, minute=00)
         days_ahead = 5 - jan_2nd.weekday()  # Saturday is 5
         # Kickoff won't occur *on* Jan 2nd if it's a Saturday - it'll be the next Saturday
         if days_ahead <= 0:
---
+++ src/backend/common/helpers/tests/season_helper_test.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pytz import timezone, UTC
+
+from backend.common.helpers.season_helper import SeasonHelper
+
+
+EST = timezone("EST")
+
+
+def test_kickoff_datetime() -> None:
+    # 2021 - Saturday the 9th, 12:00am EST (https://www.firstinspires.org/robotics/frc/blog/2021-kickoff)
+    kickoff_2021 = datetime(2021, 1, 9, 12, 00, 00, tzinfo=EST)
+    kickoff_2021_utc = kickoff_2021.astimezone(UTC)
+    assert SeasonHelper.kickoff_datetime_est(year=2021) == kickoff_2021
+    assert SeasonHelper.kickoff_datetime_utc(year=2021) == kickoff_2021_utc
+
+    # 2020 - Saturday the 4th, 10:30am EST (https://www.firstinspires.org/robotics/frc/blog/2020-kickoff-downloads)
+    kickoff_2020 = datetime(2020, 1, 4, 10, 30, 00, tzinfo=EST)
+    kickoff_2020_utc = kickoff_2020.astimezone(UTC)
+    assert SeasonHelper.kickoff_datetime_est(year=2020) == kickoff_2020
+    assert SeasonHelper.kickoff_datetime_utc(year=2020) == kickoff_2020_utc
+
+    # 2019 - Saturday the 5th, 10:30am EST (https://en.wikipedia.org/wiki/Logo_Motion)
+    kickoff_2019 = datetime(2019, 1, 5, 10, 30, 00, tzinfo=EST)
+    kickoff_2019_utc = kickoff_2019.astimezone(UTC)
+    assert SeasonHelper.kickoff_datetime_est(year=2019) == kickoff_2019
+    assert SeasonHelper.kickoff_datetime_utc(year=2019) == kickoff_2019_utc
+
+    # 2010 - Saturday the 9th, 10:30am EST (https://en.wikipedia.org/wiki/Breakaway_(FIRST))
+    kickoff_2010 = datetime(2010, 1, 9, 10, 30, 00, tzinfo=EST)
+    kickoff_2010_utc = kickoff_2010.astimezone(UTC)
+    assert SeasonHelper.kickoff_datetime_est(year=2010) == kickoff_2010
+    assert SeasonHelper.kickoff_datetime_utc(year=2010) == kickoff_2010_utc
