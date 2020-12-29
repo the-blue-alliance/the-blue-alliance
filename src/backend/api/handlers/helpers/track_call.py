2c8956a678d1c1801dba75627151604851edf330
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-12-29 10:47:54 -0800

    Use request context to store auth

--- src/backend/api/handlers/helpers/track_call.py
+++ src/backend/api/handlers/helpers/track_call.py
@@ -1,16 +1,14 @@
-from flask import request
+from flask import g
 
 from backend.common.google_analytics import GoogleAnalytics
-from backend.common.models.api_auth_access import ApiAuthAccess
 from backend.common.run_after_response import run_after_response
 
 
 def track_call_after_response(api_action: str, api_label: str) -> None:
-    auth_key = request.headers.get("X-TBA-Auth-Key", request.args.get("X-TBA-Auth-Key"))
+    auth_owner_id = g.api_auth_access.owner.id() if g.api_auth_access.owner else None
 
     @run_after_response
     def track_call():
-        auth_owner_id = ApiAuthAccess.get_by_id(auth_key).owner.id()
         GoogleAnalytics.track_event(
             auth_owner_id, "api-v03", api_action, event_label=api_label
         )
