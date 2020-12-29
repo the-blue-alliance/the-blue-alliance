1b77146faaab1c56cdd1b0325f8a93d4ee872996
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-12-29 10:34:13 -0800

    Fetch auth owner in callback

--- src/backend/api/handlers/helpers/track_call.py
+++ src/backend/api/handlers/helpers/track_call.py
@@ -7,10 +7,10 @@ from backend.common.run_after_response import run_after_response
 
 def track_call_after_response(api_action: str, api_label: str) -> None:
     auth_key = request.headers.get("X-TBA-Auth-Key", request.args.get("X-TBA-Auth-Key"))
-    auth_owner_id = ApiAuthAccess.get_by_id(auth_key).owner.id()
 
     @run_after_response
     def track_call():
+        auth_owner_id = ApiAuthAccess.get_by_id(auth_key).owner.id()
         GoogleAnalytics.track_event(
             auth_owner_id, "api-v03", api_action, event_label=api_label
         )
