3c1af3ff1caafc1276f751714fa42eecb5f5b257
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-12-30 12:29:36 -0800

    Fix lint

--- src/backend/api/handlers/helpers/track_call.py
+++ src/backend/api/handlers/helpers/track_call.py
@@ -1,5 +1,6 @@
-from flask import g
 from typing import Optional
+
+from flask import g
 
 from backend.common.google_analytics import GoogleAnalytics
 from backend.common.run_after_response import run_after_response
