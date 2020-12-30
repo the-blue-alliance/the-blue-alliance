df77ab71c9adab0ae609942e27ae4f34ef64f80c
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2020-12-30 12:25:06 -0800

    Add team list endpoints, model filtering

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from backend.api.handlers.error import handle_404
 from backend.api.handlers.event import event
 from backend.api.handlers.status import status
-from backend.api.handlers.team import team, team_list
+from backend.api.handlers.team import team, team_list, team_list_all, team_list_year
 from backend.common.flask_cache import configure_flask_cache
 from backend.common.logging import configure_logging
 from backend.common.middleware import install_middleware
@@ -17,9 +17,23 @@ configure_flask_cache(app)
 
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
+MODEL_TYPE = "<any('simple','keys'):model_type>"
+
+# Overall Status
 app.add_url_rule("/api/v3/status", view_func=status)
+# Event
 app.add_url_rule("/api/v3/event/<string:event_key>", view_func=event)
+# Team
 app.add_url_rule("/api/v3/team/<string:team_key>", view_func=team)
+app.add_url_rule(f"/api/v3/team/<string:team_key>/{MODEL_TYPE}", view_func=team)
+# Team List
+app.add_url_rule("/api/v3/teams/all", view_func=team_list_all)
+app.add_url_rule(f"/api/v3/teams/all/{MODEL_TYPE}", view_func=team_list_all)
 app.add_url_rule("/api/v3/teams/<int:page_num>", view_func=team_list)
+app.add_url_rule(f"/api/v3/teams/<int:page_num>/{MODEL_TYPE}", view_func=team_list)
+app.add_url_rule("/api/v3/teams/<int:year>/<int:page_num>", view_func=team_list_year)
+app.add_url_rule(
+    f"/api/v3/teams/<int:year>/<int:page_num>/{MODEL_TYPE}", view_func=team_list_year
+)
 
 app.register_error_handler(404, handle_404)
