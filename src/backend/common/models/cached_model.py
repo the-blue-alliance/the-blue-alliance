f0a54da4e4c27950b892947a1672aab01668388c
Phil Lopreiato <plnyyanks@gmail.com>
2020-12-31 13:59:15 -0500

    it works

--- src/backend/common/models/cached_model.py
+++ src/backend/common/models/cached_model.py
@@ -1,9 +1,20 @@
+import datetime
 from typing import Any, Dict, Optional, Set
 
 from google.cloud import ndb
+from google.cloud.ndb._datastore_types import BlobKey
+from google.cloud.ndb._legacy_entity_pb import (
+    EntityProto,
+    Property as ProtoProperty,
+    PropertyValue as ProtoPropertyValue,
+)
+from google.cloud.ndb.model import _CompressedValue
 
 
 TAffectedReferences = Dict[str, Set[ndb.Key]]
+
+_EPOCH = datetime.datetime.utcfromtimestamp(0)
+_MEANING_URI_COMPRESSED = "ZLIB"
 
 
 class CachedModel(ndb.Model):
@@ -62,3 +73,65 @@ class CachedModel(ndb.Model):
 
         See https://github.com/googleapis/python-ndb/issues/587 about fixing upstream
         """
+        pb = EntityProto()
+        pb.MergePartialFromString(state)
+
+        self.__init__()
+        self._set_state_from_pb(pb)
+
+    def _set_state_from_pb(self, pb: EntityProto) -> None:
+        deserialized_props = {}
+        for pb_prop in pb.property_list():
+            prop_value = self._get_prop_value(pb_prop.value(), pb_prop)
+            if pb_prop.multiple() and not isinstance(prop_value, list):
+                prop_value = [prop_value]
+            deserialized_props[pb_prop.name().decode()] = prop_value
+        self._set_attributes(deserialized_props)
+
+    @staticmethod
+    def _get_prop_value(v: ProtoPropertyValue, p: ProtoProperty) -> Any:
+        # rougly based on https://github.com/GoogleCloudPlatform/datastore-ndb-python/blob/cf4cab3f1f69cd04e1a9229871be466b53729f3f/ndb/model.py#L2647
+        if v.has_stringvalue():
+            sval = v.stringvalue()
+            meaning = p.meaning()
+
+            if meaning == ProtoProperty.BLOBKEY:
+                sval = BlobKey(sval)
+            elif meaning == ProtoProperty.BLOB:
+                if p.meaning_uri() == _MEANING_URI_COMPRESSED:
+                    sval = _CompressedValue(sval)
+            elif meaning == ProtoProperty.ENTITY_PROTO:
+                raise Exception("ENTITY_PROTO meaning implementation")
+            elif meaning != ProtoProperty.BYTESTRING:
+                try:
+                    sval = sval.decode("ascii")
+                    # If this passes, don't return unicode.
+                except UnicodeDecodeError:
+                    try:
+                        sval = str(sval.decode("utf-8"))
+                    except UnicodeDecodeError:
+                        pass
+            return sval
+        elif v.has_int64value():
+            ival = v.int64value()
+            if p.meaning() == ProtoProperty.GD_WHEN:
+                return _EPOCH + datetime.timedelta(microseconds=ival)
+            return ival
+        elif v.has_booleanvalue():
+            return bool(v.booleanvalue())
+        elif v.has_doublevalue():
+            return v.doublevalue()
+        elif v.has_referencevalue():
+            rv = v.referencevalue()
+            app = rv.app().decode()
+            namespace = rv.name_space()
+            pairs = [
+                (elem.type().decode(), elem.id() or elem.name().decode())
+                for elem in rv.pathelement_list()
+            ]
+            return ndb.Key(pairs=pairs, app=app, namespace=namespace)
+        elif v.has_pointvalue():
+            raise Exception("missing datastore_types.GeoPt")
+        else:
+            # A missing value implies null.
+            return None
--- src/backend/common/queries/tests/database_query_test.py
+++ src/backend/common/queries/tests/database_query_test.py
@@ -1,4 +1,3 @@
-import base64
 from typing import List
 
 from google.cloud import ndb
@@ -138,48 +137,3 @@ def test_cached_query() -> None:
     query = CachedDummyModelRangeQuery(min=0, max=2)
     result = query.fetch()
     assert len(result) == 3
-
-
-def test_py2_compatibility() -> None:
-    """
-    The purpose of this test is to make sure that py3 can deserialize cached query results
-    pickled by the py2 version of the site.
-
-    We do a bit of ndb hackery to test it. We start with a base64 encoded cached query, pulled
-    from the datastore admin tool on prod. We then construct a dummy model (as the value of the
-    property has already been pickled+compressed for storage) and then round-trip it to a datastore
-    Entity, to force the ndb library to interpret the raw data as a different model type. This will
-    let us prove we can deserialize it.
-
-    If we do nothing, this fails because the import path of models has changed between py2/py3.
-    It would fail with a message like:
-
-    E       ModuleNotFoundError: No module named 'models'
-    """
-
-    class RawCachedQueryResult(ndb.Model):
-        import pickletools
-
-        result = ndb.Property()
-
-        created = ndb.DateTimeProperty(auto_now_add=True)
-        updated = ndb.DateTimeProperty(auto_now=True)
-
-        @classmethod
-        def _get_kind(cls):
-            return "CachedQueryResult"
-
-    raw_model = RawCachedQueryResult(
-        id="py2_data",
-        # This is a py2 serialized value, pulled from the datastore admin tool
-        result=base64.b64decode(
-            "eJyVkLFKxEAURZO4kphdUMKCMtUiKriwK4iI1tZ228owm3mJEzOT5M1kISlE7fQL/AELCz/JT7C1NjuITSqbV9x7OFzeg3dTubEsOOR6LoELFl7bW3nHj9XGgrqOk11lO/reLJlZzUos+OwWOZ4Mo01L7h8IpQ1Lkcl1m4gcqBGGKVFLI1JAfXZxejnCQzJMCgSRKnoHzcSZjknU53CXbNsZ1DQlUFC17FAv8PGchAgJIKgY9MSdHsX9VbodLIDJF9dPMF7rDMeQDBpg2Fmcdi/wiR8jMAO8C7aC1+/n94/PL6+NyIiDYSLXNNOFsjQh4xLFqoNpr7OmuuR/prdf05P7388s2fwH/A+AHg=="
-        ),
-    )
-    raw_entity = ndb.model._entity_to_ds_entity(raw_model)  # pyre-ignore
-
-    query_result = ndb.model._entity_from_ds_entity(  # pyre-ignore
-        raw_entity, model_class=CachedQueryResult
-    )
-
-    assert query_result.result is None
