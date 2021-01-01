10c7a6113f62cdacbef8d57e6ceb22d53f90f4ae
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2020-12-31 21:01:24 -0500

    Support Unpickling CachedQueryResults Created By Py2 (#3234)
    This is a rabbit hole, as pickling across python versions is a terrible idea.
    
    However, we can hack around a few of the major issues that come up. Start by adding a repro unit test that tries to load+depickle a value I got from the datastore admin tool for something written by the py2 app.
    
    Next, we need to fix:
     - we changed the import path of models, so we'll need to rewrite those on the fly. We can do this with a custom `Unpickler`. See [this answer](https://stackoverflow.com/a/40916570).
     - bytestrings are handled totally differently between python 2 and 3, so we need to make sure that the py3 reader will unpickle using `encoding="bytes"`. We can do this in our custom unpickler too. Plus there's this whole thing about unpickling `datetimes`. See [this answer](https://stackoverflow.com/a/28218598).
     - Finally, there's a ndb incompatibility. The legacy app uses a [custom pickling format](https://github.com/GoogleCloudPlatform/datastore-ndb-python/blob/master/ndb/model.py#L2964-L2970) (a serialized protobuf), which it does by defining `__getstate__` and `__setstate` on the base `Model` class. Since the py3 compatible ndb library does not do this, unpickling them will fail. I filed https://github.com/googleapis/python-ndb/issues/587 in hope of working with upstream to fix this, but if not, we can potentially hack around it ourselves by manually deserializing the protobuf and setting the field ourselves.
    
    
    **WARNING**: After this, we should be able to read data written by the legacy app, but we can not yet write data that the legacy app can read. So after this, when we re-enable `CachedQueryResult`, we'll have to make sure that we don't write anything to the datastore.

--- src/backend/common/models/cached_model.py
+++ src/backend/common/models/cached_model.py
@@ -1,9 +1,21 @@
-from typing import Dict, Optional, Set
+import datetime
+from typing import Any, Dict, Optional, Set
 
 from google.cloud import ndb
+from google.cloud.datastore.helpers import GeoPoint
+from google.cloud.ndb._datastore_types import BlobKey
+from google.cloud.ndb._legacy_entity_pb import (
+    EntityProto,
+    Property as ProtoProperty,
+    PropertyValue as ProtoPropertyValue,
+)
+from google.cloud.ndb.model import _CompressedValue
 
 
 TAffectedReferences = Dict[str, Set[ndb.Key]]
+
+_EPOCH = datetime.datetime.utcfromtimestamp(0)
+_MEANING_URI_COMPRESSED = "ZLIB"
 
 
 class CachedModel(ndb.Model):
@@ -43,3 +55,115 @@ class CachedModel(ndb.Model):
         # The initialization path is different for models vs those created via
         # constructors, so make sure we have a common set of properties defined
         self._fix_up_properties()
+
+    """
+    From the legacy NDB model implementation:
+    ```
+    def __getstate__(self):
+        return self._to_pb().Encode()
+
+    def __setstate__(self, serialized_pb):
+        pb = entity_pb.EntityProto(serialized_pb)
+        self.__init__()
+        self.__class__._from_pb(pb, set_key=False, ent=self)
+    ```
+
+    See https://github.com/googleapis/python-ndb/issues/587 about fixing upstream
+    """
+
+    def __setstate__(self, state: Any) -> None:
+        pb = EntityProto()
+        pb.MergePartialFromString(state)
+
+        self.__init__()
+        self._set_state_from_pb(pb)
+
+    def _set_state_from_pb(self, pb: EntityProto) -> None:
+        deserialized_props = {}
+        if len(pb.key().path().element_list()):
+            key_ref = pb.key()
+            app = key_ref.app().decode()
+            namespace = key_ref.name_space()
+            pairs = [
+                (elem.type().decode(), elem.id() or elem.name().decode())
+                for elem in key_ref.path().element_list()
+            ]
+            deserialized_props["key"] = ndb.Key(
+                pairs=pairs, app=app, namespace=namespace
+            )
+
+        for pb_prop in pb.property_list():
+            prop_name = pb_prop.name().decode()
+
+            # There are some fields we did not port to py3, skip them
+            # StructuredProperty uses "." in field names, so skip those
+            if not hasattr(self, prop_name) and "." not in prop_name:
+                continue
+
+            prop_value = self._get_prop_value(pb_prop.value(), pb_prop)
+            if not hasattr(self, prop_name) and "." in prop_name:
+                supername, subname = prop_name.split(".", 1)
+                structprop = getattr(self.__class__, supername, None)
+                prop_type = structprop._model_class
+                if getattr(self, supername) is None:
+                    self._set_attributes({supername: prop_type()})
+                getattr(self, supername)._set_attributes({subname: prop_value})
+
+                if pb_prop.multiple():
+                    raise Exception("TODO multiple structured property")
+
+                continue
+
+            if pb_prop.multiple() and not isinstance(prop_value, list):
+                prop_value = [prop_value]
+            deserialized_props[prop_name] = prop_value
+        self._set_attributes(deserialized_props)
+
+    @staticmethod
+    def _get_prop_value(v: ProtoPropertyValue, p: ProtoProperty) -> Any:
+        # rougly based on https://github.com/GoogleCloudPlatform/datastore-ndb-python/blob/cf4cab3f1f69cd04e1a9229871be466b53729f3f/ndb/model.py#L2647
+        if v.has_stringvalue():
+            sval = v.stringvalue()
+            meaning = p.meaning()
+
+            if meaning == ProtoProperty.BLOBKEY:
+                sval = BlobKey(sval)
+            elif meaning == ProtoProperty.BLOB:
+                if p.meaning_uri() == _MEANING_URI_COMPRESSED:
+                    sval = _CompressedValue(sval)
+            elif meaning == ProtoProperty.ENTITY_PROTO:
+                raise Exception("ENTITY_PROTO meaning implementation")
+            elif meaning != ProtoProperty.BYTESTRING:
+                try:
+                    sval = sval.decode("ascii")
+                    # If this passes, don't return unicode.
+                except UnicodeDecodeError:
+                    try:
+                        sval = str(sval.decode("utf-8"))
+                    except UnicodeDecodeError:
+                        pass
+            return sval
+        elif v.has_int64value():
+            ival = v.int64value()
+            if p.meaning() == ProtoProperty.GD_WHEN:
+                return _EPOCH + datetime.timedelta(microseconds=ival)
+            return ival
+        elif v.has_booleanvalue():
+            return bool(v.booleanvalue())
+        elif v.has_doublevalue():
+            return v.doublevalue()
+        elif v.has_referencevalue():
+            rv = v.referencevalue()
+            app = rv.app().decode()
+            namespace = rv.name_space()
+            pairs = [
+                (elem.type().decode(), elem.id() or elem.name().decode())
+                for elem in rv.pathelement_list()
+            ]
+            return ndb.Key(pairs=pairs, app=app, namespace=namespace)
+        elif v.has_pointvalue():
+            pv = v.pointvalue()
+            return GeoPoint(pv.x(), pv.y())
+        else:
+            # A missing value implies null.
+            return None
--- src/backend/common/models/cached_query_result.py
+++ src/backend/common/models/cached_query_result.py
@@ -1,4 +1,48 @@
+import io
+import pickle
+from typing import Any
+
 from google.cloud import ndb
+
+
+class ImportFixingPickler(pickle.Pickler):
+    pass
+
+
+class ImportFixingUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        renamed_module = module
+        if module.startswith("models."):
+            renamed_module = "backend.common." + module
+
+        return super().find_class(renamed_module, name)
+
+
+class ImportFixingPickleProperty(ndb.BlobProperty):
+    def _to_base_type(self, value: Any) -> bytes:
+        """Convert a value to the "base" value type for this property.
+        Args:
+            value (Any): The value to be converted.
+        Returns:
+            bytes: The pickled ``value``.
+        """
+
+        file_obj = io.BytesIO()
+        ImportFixingPickler(file_obj, protocol=2, fix_imports=True).dump(value)
+        return file_obj.getvalue()
+
+    def _from_base_type(self, value: bytes) -> Any:
+        """Convert a value from the "base" value type for this property.
+        Args:
+            value (bytes): The value to be converted.
+        Returns:
+            Any: The unpickled ``value``.
+        """
+
+        file_obj = io.BytesIO(value)
+        return ImportFixingUnpickler(
+            file_obj, encoding="bytes", fix_imports=True
+        ).load()
 
 
 class CachedQueryResult(ndb.Model):
@@ -7,7 +51,7 @@ class CachedQueryResult(ndb.Model):
     """
 
     # Only one of result or result_dict should ever be populated for one model
-    result = ndb.PickleProperty(compressed=True)  # Raw models
+    result = ImportFixingPickleProperty(compressed=True)  # Raw models
     result_dict = ndb.JsonProperty()  # Dict version of models
 
     created = ndb.DateTimeProperty(auto_now_add=True)
