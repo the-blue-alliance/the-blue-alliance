f515b8e0bfc94c23fbce1ba7db056a3bfb8f1334
Phil Lopreiato <plnyyanks@gmail.com>
2021-01-03 20:22:12 -0500

    Consolidate deploy commnands to reduce workflow duplication

--- ops/deploy/deploy_module.sh
+++ ops/deploy/deploy_module.sh
@@ -3,7 +3,15 @@ set -e
 
 SERVICE=$1
 
+# Install dependencies
+python -m pip install --upgrade pip
+pip install -r deploy_requirements.txt
+
+# We need gcloud beta to set up the VPC connector
+gcloud components install beta
+
 # Post-Process YAML to set up redis connections
 python3 ops/deploy/postprocess_yaml.py "$SERVICE"
 
+# Actualy run the deploy
 gcloud beta app deploy "$SERVICE" --version 1 --quiet
