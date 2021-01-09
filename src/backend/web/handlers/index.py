b5e150c67097092e469821302fe8aa42eea75c09
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-01-09 13:47:26 -0500

    Port homepage to build season homepage

--- src/backend/web/handlers/index.py
+++ src/backend/web/handlers/index.py
@@ -5,6 +5,21 @@ from backend.web.profiled_render import render_template
 
 @cached_public
 def index() -> str:
+    effective_season_year = SeasonHelper.effective_season_year()
+    # special_webcasts = FirebasePusher.get_special_webcasts()
+    template_values = {
+        "seasonstart_datetime_utc": SeasonHelper.first_event_datetime_utc(effective_season_year),
+        # "events": EventHelper.getWeekEvents(),
+        # "any_webcast_online": any(w.get('status') == 'online' for w in special_webcasts),
+        # "special_webcasts": special_webcasts,
+        "manual_password": "!+Gam3^time#2021",  # TODO: Pull from Sitevar
+        "game_name": "INFINITE RECHARGE at Home",
+        "game_animation_youtube_id": "gmiYWTmFRVE",
+    }
+    return render_template("index/index_buildseason.html", template_values)
+
+
+def index_kickoff() -> str:
     # "any_webcast_online": any(w.get('status') == 'online' for w in special_webcasts),
     # special_webcasts = FirebasePusher.get_special_webcasts()
     effective_season_year = SeasonHelper.effective_season_year()
