2c7d070a72e3f84a008208a2fc6e25219219c0b3
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-01-08 22:39:20 -0500

    Add SeasonHelper methods to support Kickoff template is_kickoff (#3272)
    [clowntown]

--- src/backend/common/helpers/season_helper.py
+++ src/backend/common/helpers/season_helper.py
@@ -1,15 +1,17 @@
 from datetime import datetime, timedelta
-from typing import Sequence
+from typing import Optional, Sequence
 
 from pytz import timezone, UTC
 
 from backend.common.models.keys import Year
+from backend.common.queries.event_query import EventListQuery, LastSeasonEventQuery
 from backend.common.sitevars.apistatus import ApiStatus
 
 EST = timezone("EST")
 
 
 class SeasonHelper(object):
+    """ General season-information helper methods """
 
     MIN_YEAR: Year = 1992
 
@@ -29,7 +31,41 @@ class SeasonHelper(object):
         return range(cls.MIN_YEAR, max_year + 1)
 
     @staticmethod
-    def kickoff_datetime_est(year: int = datetime.now().year) -> datetime:
+    def effective_season_year(date=datetime.now()) -> Year:
+        """
+        Given a date, find the "effective season" year for the date. If all official events have been played, it's
+        effectively next season.
+        """
+        effective_season_year = date.year
+        last_event_end_date = None
+
+        last_event = LastSeasonEventQuery(year=effective_season_year).fetch()
+        if last_event:
+            last_event_end_date = last_event.end_date
+
+        if last_event_end_date is None:
+            # No events for year - assume current year is effective season year
+            return effective_season_year
+
+        if date > last_event_end_date:
+            # All events for current season have been played - effective season is next year
+            return effective_season_year + 1
+        return effective_season_year
+
+    @staticmethod
+    def is_kickoff_at_least_one_day_away(
+        date=datetime.now(UTC), year: Year = datetime.now().year
+    ) -> bool:
+        """
+        Returns True if Kickoff for a given year is at least one day away from the current date.
+        This will always be True if Kickoff for a given year happened before the current date.
+        Ex: SeasonHelper.is_kickoff_at_least_one_day_away(1992) == True
+        """
+        kickoff_date = SeasonHelper.kickoff_datetime_utc(year)
+        return date >= (kickoff_date - timedelta(days=1))
+
+    @staticmethod
+    def kickoff_datetime_est(year: Year = datetime.now().year) -> datetime:
         """ Computes the date of Kickoff for a given year. Kickoff is always the first Saturday in January after Jan 2nd. """
         jan_2nd = datetime(year, 1, 2, 10, 30, 00, tzinfo=EST)
         # Since 2021, Kickoff starts at 12:00am EST
@@ -42,6 +78,20 @@ class SeasonHelper(object):
         return jan_2nd + timedelta(days=days_ahead)
 
     @staticmethod
-    def kickoff_datetime_utc(year: int = datetime.now().year) -> datetime:
+    def kickoff_datetime_utc(year: Year = datetime.now().year) -> datetime:
         """ Converts kickoff_date to a UTC datetime """
         return SeasonHelper.kickoff_datetime_est(year).astimezone(UTC)
+
+    @staticmethod
+    def first_event_datetime_utc(
+        year: Year = datetime.now().year,
+    ) -> Optional[datetime]:
+        """ Computes day the first in-season event begins """
+        events = EventListQuery(year).fetch()
+        earliest_start = None
+        for event in events:
+            if event.is_season_event and (
+                earliest_start is None or event.start_date < earliest_start
+            ):
+                earliest_start = event.start_date
+        return earliest_start
