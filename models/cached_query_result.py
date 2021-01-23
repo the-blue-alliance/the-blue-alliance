18df29443f9d18ca8eb3a05936dfa6059b7baf9f
Phil Lopreiato <plnyyanks@gmail.com>
2021-01-23 11:37:52 -0500

    Support unpickling CachedQueryResults created by py3

--- models/cached_query_result.py
+++ models/cached_query_result.py
@@ -1,4 +1,48 @@
+import io
+import pickle
+
 from google.appengine.ext import ndb
+
+
+class ImportFixingUnpickler(pickle.Unpickler):
+    """
+    In the case where we're reading a CachedQueryResult written by
+    the py3 version of TBA, we'll need to fix the imports to be
+    compatible with this one.
+    """
+
+    def find_class(self, module, name):
+        renamed_module = module
+        prefix = "backend.common."
+        if module.startswith(prefix):
+            renamed_module = module[len(prefix):]
+
+        return pickle.Unpickler.find_class(self, renamed_module, name)
+
+
+class ImportFixingPickleProperty(ndb.BlobProperty):
+    def _to_base_type(self, value):
+        """Convert a value to the "base" value type for this property.
+        Args:
+            value (Any): The value to be converted.
+        Returns:
+            bytes: The pickled ``value``.
+        """
+
+        file_obj = io.BytesIO()
+        pickle.Pickler(file_obj, protocol=2).dump(value)
+        return file_obj.getvalue()
+
+    def _from_base_type(self, value):
+        """Convert a value from the "base" value type for this property.
+        Args:
+            value (bytes): The value to be converted.
+        Returns:
+            Any: The unpickled ``value``.
+        """
+
+        file_obj = io.BytesIO(value)
+        return ImportFixingUnpickler(file_obj).load()
 
 
 class CachedQueryResult(ndb.Model):
@@ -6,7 +50,7 @@ class CachedQueryResult(ndb.Model):
     A CachedQueryResult stores the result of an NDB query
     """
     # Only one of result or result_dict should ever be populated for one model
-    result = ndb.PickleProperty(compressed=True)  # Raw models
+    result = ImportFixingPickleProperty(compressed=True)  # Raw models
     result_dict = ndb.JsonProperty()  # Dict version of models
 
     created = ndb.DateTimeProperty(auto_now_add=True)
