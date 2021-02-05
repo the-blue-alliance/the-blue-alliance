80adc891c3681984aa1c6344a6c80c2926205da1
Phil Lopreiato <plnyyanks@gmail.com>
2021-02-05 17:49:57 -0500

    Add pre-commit config for bash lints

--- ops/lint_bash.sh
+++ ops/lint_bash.sh
@@ -6,5 +6,9 @@ find ops -type f -iname "*.sh" -exec shellcheck --format=tty {} +
 if ! type "shfmt" >/dev/null; then
     echo "shfmt not found, install from https://github.com/mvdan/sh to use"
 else
-    shfmt -i 4 -l -d ops/
+    if [ "$1" == "--fix" ]; then
+        shfmt -i 4 -w ops/
+    else
+        shfmt -i 4 -l -d ops/
+    fi
 fi
