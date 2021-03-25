344e4bf8b3b09bf06a0b9844f7cc5a01037c68e0
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-03-24 21:07:57 -0400

    Add FMS API District List + Event Alliance parsers (#3432)

--- src/backend/common/datafeeds/parsers/parser_base.py
+++ src/backend/common/datafeeds/parsers/parser_base.py
@@ -1,68 +1,15 @@
 import abc
-
-# import re
-from typing import Generic, TypeVar
+from typing import Any, Generic, TypeVar
 
 
-TModel = TypeVar("TModel")
+TParsedResponse = TypeVar("TParsedResponse")
 
 
 class ParserInputException(Exception):
     pass
 
 
-class ParserBase(abc.ABC, Generic[TModel]):
-    """
-    Provides a basic structure for parsing pages.
-    Parsers are not allowed to return Model objects, only dictionaries.
-    """
-
-    @classmethod
+class ParserBase(abc.ABC, Generic[TParsedResponse]):
     @abc.abstractmethod
-    def parse(cls, html: bytes) -> TModel:
-        """
-        Given a chunk of HTML, return a (result dictionary, more_pages) tuple
-        """
+    def parse(self, response: Any) -> TParsedResponse:
         ...
-
-    # @classmethod
-    # def _recurseUntilString(cls, node):
-    #     """
-    #     Digs through HTML that Word made worse.
-    #     Written to deal with http://www2.usfirst.org/2011comp/Events/cmp/matchresults.html
-    #     """
-    #     from bs4 import NavigableString
-    #     if node.string is not None:
-    #         return re.sub('\s+', ' ', node.string.replace(u'\xa0', ' ')).strip()  # remove multiple whitespaces
-    #     if isinstance(node, NavigableString):
-    #         return node
-    #     if hasattr(node, 'contents'):
-    #         results = []
-    #         for content in node.contents:
-    #             result = self._recurseUntilString(content)
-    #             if result is not None:
-    #                 result = result.strip().replace('\r', '').replace('\n', '').replace('  ', ' ')
-    #             if result is not None and result != "":
-    #                 results.append(result)
-    #         if results != []:
-    #             return ' '.join(results)
-    #     return None
-    #
-    # @classmethod
-    # def _html_unescape(cls, html):
-    #     import HTMLParser
-    #     h = HTMLParser.HTMLParser()
-    #     return h.unescape(html)
-    #
-    # @classmethod
-    # def _html_unescape_items(cls, d):
-    #     """
-    #     Unescapes HTML in a dict
-    #     """
-    #     import HTMLParser
-    #     h = HTMLParser.HTMLParser()
-    #     for key, value in d.items():
-    #         try:
-    #             d[key] = h.unescape(value)
-    #         except TypeError:
-    #             continue
