34f113c2f4f7c5cc52722768a3d378ed8baba0b9
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-03-27 14:01:18 -0400

    Add FMS API team avatar parser to py3

---
+++ src/backend/common/datafeeds/parsers/fms_api/fms_api_team_avatar_parser.py
@@ -0,0 +1,56 @@
+import json
+
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+from google.cloud import ndb
+
+from backend.common.consts.media_type import MediaType
+from backend.common.datafeeds.parsers.json.parser_paginated_json import (
+    ParserPaginatedJSON,
+)
+from backend.common.models.media import Media
+from backend.common.models.team import Team
+
+
+class FMSAPITeamAvatarParser(ParserPaginatedJSON[Tuple[List[Media], Set[ndb.Key]]]):
+    def __init__(self, year: int):
+        self.year = year
+
+    def parse(
+        self, response: Dict[str, Any]
+    ) -> Tuple[Optional[Tuple[List[Media], Set[ndb.Key]]], bool]:
+        current_page = response["pageCurrent"]
+        total_pages = response["pageTotal"]
+
+        avatars: List[Media] = []
+        media_keys_to_delete: Set[ndb.Key] = set()
+
+        for teamData in response["teams"]:
+            team_number = teamData["teamNumber"]
+            foreign_key = "avatar_{}_frc{}".format(self.year, team_number)
+            media_key = ndb.Key(
+                Media, Media.render_key_name(MediaType.AVATAR, foreign_key)
+            )
+
+            encoded_avatar = teamData["encodedAvatar"]
+            if not encoded_avatar:
+                media_keys_to_delete.add(media_key)
+                continue
+
+            avatars.append(
+                Media(
+                    key=media_key,
+                    details_json=json.dumps({"base64Image": encoded_avatar}),
+                    foreign_key=foreign_key,
+                    media_type_enum=MediaType.AVATAR,
+                    references=[ndb.Key(Team, "frc{}".format(team_number))],
+                    year=self.year,
+                )
+            )
+
+        return (
+            (avatars, media_keys_to_delete)
+            if avatars or media_keys_to_delete
+            else None,
+            (current_page < total_pages),
+        )
