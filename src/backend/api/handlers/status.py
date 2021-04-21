6ad6a6644ba5197733011363a352b1febaaa30f0
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-04-20 20:08:42 -0400

    Add apistatus.fmsapi_down Sitevar (#3519)
    [nodeploy]

--- src/backend/api/handlers/status.py
+++ src/backend/api/handlers/status.py
@@ -5,6 +5,7 @@ from backend.api.handlers.helpers.track_call import track_call_after_response
 from backend.common.decorators import cached_public
 from backend.common.models.sitevar import Sitevar
 from backend.common.sitevars.apistatus import ApiStatus
+from backend.common.sitevars.apistatus_fmsapi_down import ApiStatusFMSApiDown
 
 
 @api_authenticated
@@ -13,7 +14,6 @@ def status() -> Response:
     track_call_after_response("status", "status")
 
     # TODO: Remove Sitevar usage for Sitevar classes
-    fmsapi_sitevar_future = Sitevar.get_by_id_async("apistatus.fmsapi_down")
     down_events_sitevar_future = Sitevar.get_by_id_async("apistatus.down_events")
 
     status = dict()
@@ -22,10 +22,7 @@ def status() -> Response:
     down_events_sitevar = down_events_sitevar_future.get_result()
     down_events_list = down_events_sitevar.contents if down_events_sitevar else None
 
-    fmsapi_sitevar = fmsapi_sitevar_future.get_result()
-    status["is_datafeed_down"] = (
-        True if fmsapi_sitevar and fmsapi_sitevar.contents is True else False
-    )
+    status["is_datafeed_down"] = ApiStatusFMSApiDown.get()
     status["down_events"] = down_events_list if down_events_list is not None else []
 
     return jsonify(status)
--- src/backend/api/handlers/tests/status_test.py
+++ src/backend/api/handlers/tests/status_test.py
@@ -1,3 +1,4 @@
+import pytest
 from google.cloud import ndb
 from werkzeug.test import Client
 
@@ -5,9 +6,11 @@ from backend.common.consts.auth_type import AuthType
 from backend.common.models.api_auth_access import ApiAuthAccess
 from backend.common.sitevars import apistatus
 from backend.common.sitevars.apistatus import ApiStatus
+from backend.common.sitevars.apistatus_fmsapi_down import ApiStatusFMSApiDown
 
 
-def test_status(ndb_client: ndb.Client, api_client: Client) -> None:
+@pytest.mark.parametrize("fmsapi_down", [True, False])
+def test_status(fmsapi_down, ndb_client: ndb.Client, api_client: Client) -> None:
     status = apistatus.ContentType(
         current_season=2019, max_season=2020, web=None, android=None, ios=None
     )
@@ -19,6 +22,7 @@ def test_status(ndb_client: ndb.Client, api_client: Client) -> None:
         ).put()
 
         ApiStatus.put(status)
+        ApiStatusFMSApiDown.put(fmsapi_down)
 
     resp = api_client.get("/api/v3/status", headers={"X-TBA-Auth-Key": "test_auth_key"})
     assert resp.status_code == 200
@@ -26,6 +30,6 @@ def test_status(ndb_client: ndb.Client, api_client: Client) -> None:
     expected_status = dict()
     expected_status.update(status)
     expected_status["down_events"] = []
-    expected_status["is_datafeed_down"] = False
+    expected_status["is_datafeed_down"] = fmsapi_down
 
     assert resp.json == expected_status
