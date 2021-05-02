6e9a63d0ac70e4be799255da2578ea0458efc4d2
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-05-01 19:09:40 -0700

    Test basic rendering

---
+++ src/frontend/eventwizard/eventwizard.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import EventWizardFrame from "./components/EventWizardFrame";
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("renders without crashing", () => {
+    act(() => {
+        render(<EventWizardFrame />, container);
+    });
+});
