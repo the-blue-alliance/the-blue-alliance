145da535f0ebfdde269b85e7e0824a4e1b1101a7
Phil Lopreiato <plnyyanks@gmail.com>
2021-05-02 11:11:30 -0400

    Port event teams trusted API

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from json import JSONDecodeError
+
+from flask import Blueprint, Flask, jsonify, make_response, Response
+from flask_cors import CORS
 
 from backend.api.handlers.error import handle_404
 from backend.api.handlers.event import event
 from backend.api.handlers.status import status
 from backend.api.handlers.team import team, team_list, team_list_all, team_list_year
+from backend.api.handlers.trusted import update_teams
+from backend.common.datafeed_parsers.exceptions import ParserInputException
 from backend.common.flask_cache import configure_flask_cache
 from backend.common.logging import configure_logging
 from backend.common.middleware import install_middleware
@@ -22,22 +27,49 @@ app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 SIMPLE_MODEL_TYPE = "<any('simple'):model_type>"
 ANY_MODEL_TYPE = "<any('simple','keys'):model_type>"
 
+api_v3 = Blueprint("apiv3", __name__, url_prefix="/api/v3")
+CORS(api_v3, origins="*", methods=["OPTIONS", "GET"], allow_headers=["X-TBA-Auth-Key"])
+
 # Overall Status
-app.add_url_rule("/api/v3/status", view_func=status)
+api_v3.add_url_rule("/status", view_func=status)
 # Event
-app.add_url_rule("/api/v3/event/<string:event_key>", view_func=event)
+api_v3.add_url_rule("/event/<string:event_key>", view_func=event)
 # Team
-app.add_url_rule("/api/v3/team/<string:team_key>", view_func=team)
-app.add_url_rule(f"/api/v3/team/<string:team_key>/{SIMPLE_MODEL_TYPE}", view_func=team)
+api_v3.add_url_rule("/team/<string:team_key>", view_func=team)
+api_v3.add_url_rule(f"/team/<string:team_key>/{SIMPLE_MODEL_TYPE}", view_func=team)
 # Team List
-app.add_url_rule("/api/v3/teams/all", view_func=team_list_all)
-app.add_url_rule(f"/api/v3/teams/all/{ANY_MODEL_TYPE}", view_func=team_list_all)
-app.add_url_rule("/api/v3/teams/<int:page_num>", view_func=team_list)
-app.add_url_rule(f"/api/v3/teams/<int:page_num>/{ANY_MODEL_TYPE}", view_func=team_list)
-app.add_url_rule("/api/v3/teams/<int:year>/<int:page_num>", view_func=team_list_year)
-app.add_url_rule(
-    f"/api/v3/teams/<int:year>/<int:page_num>/{ANY_MODEL_TYPE}",
+api_v3.add_url_rule("/teams/all", view_func=team_list_all)
+api_v3.add_url_rule(f"/teams/all/{ANY_MODEL_TYPE}", view_func=team_list_all)
+api_v3.add_url_rule("/teams/<int:page_num>", view_func=team_list)
+api_v3.add_url_rule(f"/teams/<int:page_num>/{ANY_MODEL_TYPE}", view_func=team_list)
+api_v3.add_url_rule("/teams/<int:year>/<int:page_num>", view_func=team_list_year)
+api_v3.add_url_rule(
+    f"/teams/<int:year>/<int:page_num>/{ANY_MODEL_TYPE}",
     view_func=team_list_year,
 )
 
+# Trusted API
+trusted_api = Blueprint("trusted_api", __name__, url_prefix="/api/trusted/v1")
+CORS(
+    trusted_api,
+    origins="*",
+    methods=["OPTIONS", "POST"],
+    allow_headers=["Content-Type", "X-TBA-Auth-Id", "X-TBA-Auth-Sig"],
+)
+
+trusted_api.add_url_rule(
+    "/event/<string:event_key>/team_list/update",
+    methods=["POST"],
+    view_func=update_teams,
+)
+
+
+@trusted_api.errorhandler(JSONDecodeError)
+@trusted_api.errorhandler(ParserInputException)
+def handle_bad_input(e: Exception) -> Response:
+    return make_response(jsonify({"Error": f"{e}"}), 400)
+
+
+app.register_blueprint(api_v3)
+app.register_blueprint(trusted_api)
 app.register_error_handler(404, handle_404)
