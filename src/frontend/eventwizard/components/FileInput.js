6b2ffbd5017d338c1cf3575fe7677613bef5a068
Phil Lopreiato <plnyyanks@gmail.com>
2021-05-09 10:45:22 -0400

    lint

--- src/frontend/eventwizard/components/FileInput.js
+++ src/frontend/eventwizard/components/FileInput.js
@@ -1,64 +1,64 @@
-import React from 'react';
+import React from "react";
 
 class FileInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            styles: {
-                parent: {
-                    position: 'relative'
-                },
-                file: {
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    opacity: 0,
-                    width: '100%',
-                    zIndex: 1
-                },
-                text: {
-                    position: 'relative',
-                    zIndex: -1
-                }
-            }
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      styles: {
+        parent: {
+          position: "relative",
+        },
+        file: {
+          position: "absolute",
+          top: 0,
+          left: 0,
+          opacity: 0,
+          width: "100%",
+          zIndex: 1,
+        },
+        text: {
+          position: "relative",
+          zIndex: -1,
+        },
+      },
+    };
+  }
 
-    handleChange(e) {
-        this.setState({
-            value: e.target.value.split(/(\\|\/)/g).pop()
-        });
-        if (this.props.onChange) {
-            this.props.onChange(e);
-        }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value.split(/(\\|\/)/g).pop(),
+    });
+    if (this.props.onChange) {
+      this.props.onChange(e);
     }
+  }
 
-    render() {
-        return (
-            <div style={this.state.styles.parent}>
-                <input
-                    type="file"
-                    name={this.props.name}
-                    className={this.props.className}
-                    onChange={this.handleChange}
-                    disabled={this.props.disabled}
-                    accept={this.props.accept}
-                    style={this.state.styles.file}
-                />
-                <input
-                    type="text"
-                    tabIndex={-1}
-                    name={this.props.name + "_filename"}
-                    value={this.state.value}
-                    className={this.props.className}
-                    placeholder={this.props.placeholder}
-                    disabled={this.props.disabled}
-                    style={this.state.styles.text}
-                />
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div style={this.state.styles.parent}>
+        <input
+          type="file"
+          name={this.props.name}
+          className={this.props.className}
+          onChange={this.handleChange}
+          disabled={this.props.disabled}
+          accept={this.props.accept}
+          style={this.state.styles.file}
+        />
+        <input
+          type="text"
+          tabIndex={-1}
+          name={this.props.name + "_filename"}
+          value={this.state.value}
+          className={this.props.className}
+          placeholder={this.props.placeholder}
+          disabled={this.props.disabled}
+          style={this.state.styles.text}
+        />
+      </div>
+    );
+  }
+}
 
 export default FileInput;
--- src/frontend/eventwizard/components/teamsTab/AddTeamsFMSReport.js
+++ src/frontend/eventwizard/components/teamsTab/AddTeamsFMSReport.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Dialog from "react-bootstrap-dialog";
 import TeamList from "./TeamList";
-import FileInput from "../FileInput"
+import FileInput from "../FileInput";
 
 class AddTeamsFMSReport extends Component {
   constructor(props) {
