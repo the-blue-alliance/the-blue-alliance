9ceeb3eb45ee740c333e45c0ed9284115479e317
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-10-03 13:43:06 -0400

    Include 2021 in years where total rankings points are calculated

--- src/backend/common/models/event_details.py
+++ src/backend/common/models/event_details.py
@@ -127,7 +127,7 @@ class EventDetails(CachedModel):
                 if game_year == 2021:
                     # 2021 did not have matches played for rankings
                     continue
-                elif game_year in {2017, 2018, 2019, 2020}:
+                elif game_year in {2017, 2018, 2019, 2020, 2021}:
                     rank["extra_stats"] = [
                         int(round(rank["sort_orders"][0] * rank["matches_played"])),
                     ]
@@ -147,7 +147,7 @@ class EventDetails(CachedModel):
             if game_year == 2021:
                 # 2021 did not have matches played for rankings
                 pass
-            elif game_year in {2017, 2018, 2019, 2020}:
+            elif game_year in {2017, 2018, 2019, 2020, 2021}:
                 extra_stats_info = [{"name": "Total Ranking Points", "precision": 0}]
             elif sort_order_info is not None:
                 extra_stats_info = [
