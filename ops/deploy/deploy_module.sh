39a7f61406f50a55e5fad9c62715f123b7e1231b
Phil Lopreiato <plnyyanks@gmail.com>
2021-11-26 17:53:33 -0500

    remove some more redis references

--- ops/deploy/deploy_module.sh
+++ ops/deploy/deploy_module.sh
@@ -10,8 +10,5 @@ pip install -r deploy_requirements.txt
 # We need gcloud beta to set up the VPC connector
 gcloud components install beta
 
-# Post-Process YAML to set up redis connections
-python3 ops/deploy/postprocess_yaml.py "$SERVICE"
-
 # Actualy run the deploy
 gcloud beta app deploy "$SERVICE" --version 1 --quiet
