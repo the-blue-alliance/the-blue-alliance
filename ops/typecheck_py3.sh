3368664ec60ad6bb3516c4c9ccb9382360f5687d
Phil Lopreiato <plnyyanks@gmail.com>
2021-12-28 18:49:28 -0500

    don't use no-arg pyre

--- ops/typecheck_py3.sh
+++ ops/typecheck_py3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-# We simply use `pyre` over `pyre check` because this will
-# allow incremental checking if watchman can be found
-pyre
+if ! command -v watchman &> /dev/null
+then
+    pyre check
+else
+    pyre incremental
+fi
