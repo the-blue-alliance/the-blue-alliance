db5853c109e5b3ccf275d3729c54089b5ec6e069
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-12-28 10:16:05 -0800

    Configure special webcasts and default chat in gameday

--- src/backend/web/handlers/gameday.py
+++ src/backend/web/handlers/gameday.py
@@ -1,10 +1,20 @@
+import json
 from datetime import timedelta
 
 from flask import render_template
 
 from backend.common.decorators import cached_public
+from backend.common.sitevars.gameday_special_webcasts import GamedaySpecialWebcasts
 
 
 @cached_public(ttl=timedelta(seconds=61))
 def gameday() -> str:
-    return render_template("gameday2.html", webcasts_json={}, default_chat=None)
+    webcasts_json = json.dumps(
+        {
+            "special_webcasts": GamedaySpecialWebcasts.webcasts(),
+        }
+    )
+    default_chat = GamedaySpecialWebcasts.default_chat()
+    return render_template(
+        "gameday2.html", webcasts_json=webcasts_json, default_chat=default_chat
+    )
