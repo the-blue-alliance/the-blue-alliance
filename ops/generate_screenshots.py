8757b80102b205ddd059a9b2ce0f811b27ba671d
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-12-29 11:18:38 -0800

    Github upload

--- ops/generate_screenshots.py
+++ ops/generate_screenshots.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import base64
 import os
+import requests
 import shutil
 import subprocess
+import sys
+import time
 from typing import List, Tuple
 
 
@@ -10,21 +14,17 @@ CAPTURE_URLS = [
     ("Homepage", "https://www.thebluealliance.com"),
     ("GameDay", "https://www.thebluealliance.com/gameday"),
 ]  # (name, url)
-SAVE_DIR = "ci_screenshots"
-MESSAGE_FILENAME = "message.md"
+MESSAGE_FILENAME = "ci_screenshots_message.md"
+GITHUB_REF = os.environ.get("GITHUB_REF", "")
+GITHUB_PULL_REQUEST_NUMBER = (
+    int(GITHUB_REF.split("/")[2]) if "refs/pull/" in GITHUB_REF else None
+)
 
 
-def reset_directory() -> None:
-    if os.path.isdir(SAVE_DIR):
-        shutil.rmtree(SAVE_DIR)
-    os.mkdir(SAVE_DIR)
-
-
-def capture_screenshots(urls: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
-    screenshots = []
-    for i, (name, url) in enumerate(urls):
+def capture_screenshots(urls: List[Tuple[str, str, str]]) -> List[Tuple[str, str]]:
+    screenshots = []  # (name, filename, base64encode image)
+    for name, url in urls:
         print(f"Screenshotting {name}: {url}")
-        file_name = f"{SAVE_DIR}/out_{i}.png"
         try:
             cmd = [
                 "capture-website",
@@ -33,18 +33,25 @@ def capture_screenshots(urls: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
                 "1920",
                 "--height",
                 "1080",
-                "--output",
-                file_name,
             ]
-            subprocess.check_output(cmd)
-            screenshots.append((name, file_name))
+            image_data = subprocess.check_output(cmd)
+            image = base64.b64encode(image_data).decode("utf-8")
+            filename = (
+                f"pr-{GITHUB_PULL_REQUEST_NUMBER}-{url}-{int(time.time())}.png".replace(
+                    "/", "-"
+                ).replace(" ", "")
+            )
+            screenshots.append((name, filename, image))
         except subprocess.CalledProcessError as e:
             print(f"Error: {e}")
     return screenshots
 
 
-def upload_screenshots(screenshots: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+def upload_screenshots(
+    screenshots: List[Tuple[str, str, str]], GITHUB_TOKEN: str
+) -> List[Tuple[str, str]]:
     GITHUB_API_URL = "https://api.github.com"
+    GITHUB_REPOSITORY = os.environ.get("GITHUB_REPOSITORY")
     BRANCH_NAME = "ci-screenshots"
     AUTHOR_NAME = "github-actions[bot]"
     AUTHOR_EMAIL = "github-actions[bot]@users.noreply.github.com"
@@ -57,25 +64,65 @@ def upload_screenshots(screenshots: List[Tuple[str, str]]) -> List[Tuple[str, st
     remote_branches = subprocess.check_output(
         ["git", "branch", "-r"],
     )
+
+    # Create branch if it does not yet exist
     if BRANCH_NAME not in str(remote_branches):
         subprocess.run(["git", "checkout", "--orphan", BRANCH_NAME])
+        subprocess.run(
+            ["git", "commit", "--allow-emtpy", "-m", "Initial commit on empty branch"]
+        )
         subprocess.run(["git", "push", "-u", "origin", BRANCH_NAME])
     else:
         print(f'Branch "{BRANCH_NAME}" Already Exists')
 
+    def upload_single_screenshot(filename: str, image: str) -> str:
+        url = f"{GITHUB_API_URL}/repos/{GITHUB_REPOSITORY}/contents/{filename}"
+        data = {
+            "message": f"[CI] Added Screenshots for PR #{GITHUB_PULL_REQUEST_NUMBER}",
+            "content": image,
+            "branch": BRANCH_NAME,
+            "author": {"name": AUTHOR_NAME, "email": AUTHOR_EMAIL},
+            "committer": {"name": AUTHOR_NAME, "email": AUTHOR_EMAIL},
+        }
+        headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "authorization": f"Bearer {GITHUB_TOKEN}",
+        }
+        response = requests.put(url, headers=headers, json=data)
 
-def generate_message(screenshots: List[Tuple[str, str]]):
+        if response.status_code in {200, 201}:
+            link = (
+                f"https://github.com/{GITHUB_REPOSITORY}/raw/{BRANCH_NAME}/{filename}"
+            )
+            print(f'Image "{filename}" Uploaded to "{link}"')
+            return link
+        else:
+            print(f'Error uploading "{filename}"')
+            return None
+
+    image_urls = []  # (name, image_url)
+    for name, filename, image in screenshots:
+        image_url = upload_single_screenshot(filename, image)
+        image_urls.append((name, image_url))
+    return image_urls
+
+
+def generate_message(image_urls: List[Tuple[str, str]]):
     print("Generating message")
     message = "## Screenshots"
-    for name, filename in screenshots:
+    for name, image_url in image_urls:
+        if image_url is None:
+            continue
         message += f"\n\n### {name}"
-    with open(f"{SAVE_DIR}/{MESSAGE_FILENAME}", "w") as file:
+        message += f"\n![{name}]({image_url})"
+    with open(MESSAGE_FILENAME, "w") as file:
         file.write(message)
 
 
 if __name__ == "__main__":
-    reset_directory()
-    screenshots = capture_screenshots(CAPTURE_URLS)
     if os.environ.get("CI"):
-        image_urls = upload_screenshots(screenshots)
-        # generate_message(image_urls)
+        screenshots = capture_screenshots(CAPTURE_URLS)
+        image_urls = upload_screenshots(screenshots, sys.argv[1])
+        generate_message(image_urls)
+    else:
+        print("Only runnable in CI.")
