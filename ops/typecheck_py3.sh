d01a779f02a07ed1d94b6c0a17ff2a3ed97c684d
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-12-30 17:27:16 -0500

    [CI] Show lint/typecheck issues in PR (#4097) [nodeploy]
    * Intentionally add lint/typecheck issues
    
    * More intentional mistakes
    
    * Add problem matchers
    
    * Revert "Intentionally add lint/typecheck issues"
    
    This reverts commit a2af1861640525177029c7a73fe8938f9e8da054.
    
    * Revert "More intentional mistakes"
    
    This reverts commit 3cf1cbc12958d568fa41c374aa8305d8e9d3754e.
    
    * Ony add matchers when running on CI

--- ops/typecheck_py3.sh
+++ ops/typecheck_py3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -n "$CI" ]; then
+    echo "::add-matcher::./ops/problem_matchers/pyre_error.json"
+fi
+
 if ! command -v watchman &>/dev/null; then
     pyre check
 else
