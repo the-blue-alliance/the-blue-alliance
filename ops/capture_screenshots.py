ad08859edb20fae397260ce79187b91ffa97a757
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-12-30 09:38:04 -0800

    Save screenshots to pickle

---
+++ ops/capture_screenshots.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import base64
+import os
+import pickle
+import subprocess
+import time
+from typing import List, Tuple
+
+
+CAPTURE_URLS = [
+    ("Homepage", "https://www.thebluealliance.com"),
+    ("GameDay", "https://www.thebluealliance.com/gameday"),
+]  # (name, url)
+GITHUB_REF = os.environ.get("GITHUB_REF", "")
+GITHUB_PULL_REQUEST_NUMBER = (
+    int(GITHUB_REF.split("/")[2]) if "refs/pull/" in GITHUB_REF else None
+)
+OUTPUT_FILE = "ci_screenshots.pickle"
+
+
+def capture_screenshots(urls: List[Tuple[str, str]]) -> List[Tuple[str, str, str]]:
+    screenshots = []  # (name, filename, base64encode image)
+    for name, url in urls:
+        print(f"Screenshotting {name}: {url}")
+        try:
+            cmd = [
+                "capture-website",
+                url,
+                "--width",
+                "1920",
+                "--height",
+                "1080",
+            ]
+            image_data = subprocess.check_output(cmd)
+            image = base64.b64encode(image_data).decode("utf-8")
+            filename = (
+                f"pr-{GITHUB_PULL_REQUEST_NUMBER}-{url}-{int(time.time())}.png".replace(
+                    "/", "-"
+                ).replace(" ", "")
+            )
+            screenshots.append((name, filename, image))
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+    return screenshots
+
+
+if __name__ == "__main__":
+    if os.environ.get("CI"):
+        screenshots = capture_screenshots(CAPTURE_URLS)
+        pickle.dump(screenshots, open(OUTPUT_FILE, "wb"))
+    else:
+        print("Only runnable in CI.")
