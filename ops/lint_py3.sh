ac4400f8cd1d43505f2a41788e97df43f90739e4
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2021-12-30 13:58:03 -0800

    Add problem matchers

--- ops/lint_py3.sh
+++ ops/lint_py3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "::add-matcher::./ops/problem_matchers/flake8_error.json"
+echo "::add-matcher::./ops/problem_matchers/flake8_warning.json"
 if [ "$1" == "--fix" ]; then
     black ./ --exclude '/(old_py2|stubs|subtrees|venv)/'
 else
--- ops/typecheck_py3.sh
+++ ops/typecheck_py3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "::add-matcher::./ops/problem_matchers/pyre_error.json"
 if ! command -v watchman &>/dev/null; then
     pyre check
 else
