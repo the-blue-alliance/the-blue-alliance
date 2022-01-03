797f21e3b40401a5e70808cbd56c4b91207a14f7
Phil Lopreiato <plnyyanks@gmail.com>
2022-01-03 11:48:54 -0500

    Port suggestion review slack nag

--- old_py2/controllers/cron_controller.py
+++ old_py2/controllers/cron_controller.py
@@ -765,35 +765,6 @@ class BlueZoneUpdateDo(webapp.RequestHandler):
             logging.exception(e)
 
 
-class SuggestionQueueDailyNag(webapp.RequestHandler):
-    """
-    Daily job to nag a slack channel about pending suggestions
-    """
-    def get(self):
-        hook_sitevars = Sitevar.get_by_id('slack.hookurls')
-        if not hook_sitevars:
-            return
-        channel_url = hook_sitevars.contents.get('suggestion-nag')
-        if not channel_url:
-            return
-        counts = map(lambda t: SuggestionFetcher.count(Suggestion.REVIEW_PENDING, t),
-                     Suggestion.MODELS)
-
-        nag_text = "There are pending suggestions!\n"
-        suggestions_to_nag = False
-        for count, name in zip(counts, Suggestion.MODELS):
-            if count > 0:
-                suggestions_to_nag = True
-                nag_text += "*{0}*: {1} pending suggestions\n".format(
-                    Suggestion.MODEL_NAMES.get(name),
-                    count
-                )
-
-        if suggestions_to_nag:
-            nag_text += "_Review them on <https://www.thebluealliance.com/suggest/review|TBA>_"
-            OutgoingNotificationHelper.send_slack_alert(channel_url, nag_text)
-
-
 class RemapTeamsDo(webapp.RequestHandler):
     """
     Remaps teams within an Event. Useful for offseason events.
--- src/backend/tasks_io/main.py
+++ src/backend/tasks_io/main.py
@@ -4,6 +4,7 @@ from google.appengine.api import wrap_wsgi_app
 from backend.common.deferred import install_defer_routes
 from backend.common.logging import configure_logging
 from backend.common.middleware import install_middleware
+from backend.tasks_io.handlers.cron_misc import blueprint as cron_misc_blueprint
 from backend.tasks_io.handlers.frc_api import blueprint as frc_api_blueprint
 
 
@@ -14,4 +15,5 @@ app.wsgi_app = wrap_wsgi_app(app.wsgi_app, use_deferred=True)
 install_middleware(app)
 install_defer_routes(app)
 
+app.register_blueprint(cron_misc_blueprint)
 app.register_blueprint(frc_api_blueprint)
