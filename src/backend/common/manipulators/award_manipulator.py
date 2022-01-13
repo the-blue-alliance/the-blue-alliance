a4f18581750ebe561d72277a2d7c661ff5ebee80
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2022-01-13 17:31:55 -0500

    Enqueue district points calculation in update hooks (#4164)

--- src/backend/common/manipulators/award_manipulator.py
+++ src/backend/common/manipulators/award_manipulator.py
@@ -1,8 +1,12 @@
 import json
-from typing import List
+from typing import List, Set
+
+from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
+from pyre_extensions import none_throws
 
 from backend.common.cache_clearing import get_affected_queries
-from backend.common.manipulators.manipulator_base import ManipulatorBase
+from backend.common.manipulators.manipulator_base import ManipulatorBase, TUpdatedModel
 from backend.common.models.award import Award
 from backend.common.models.cached_model import TAffectedReferences
 
@@ -17,37 +21,6 @@ class AwardManipulator(ManipulatorBase[Award]):
         cls, affected_refs: TAffectedReferences
     ) -> List[get_affected_queries.TCacheKeyAndQuery]:
         return get_affected_queries.award_updated(affected_refs)
-
-    """
-    @classmethod
-    def postUpdateHook(cls, awards, updated_attr_list, is_new_list):
-        # Note, updated_attr_list will always be empty, for now
-        # Still needs to be implemented in updateMerge
-        # See helpers.EventManipulator
-        events = []
-        for (award, updated_attrs) in zip(awards, updated_attr_list):
-            event = award.event
-            if event not in events:
-                events.append(event)
-
-        for event in events:
-            if event.get().within_a_day:
-                try:
-                    NotificationHelper.send_award_update(event.get())
-                except Exception:
-                    logging.error("Error sending award update for {}".format(event.id()))
-                try:
-                    TBANSHelper.awards(event.get())
-                except Exception, exception:
-                    logging.error("Error sending {} award updates: {}".format(event.id(), exception))
-                    logging.error(traceback.format_exc())
-
-        # Enqueue task to calculate district points
-        for event in events:
-            taskqueue.add(
-                url='/tasks/math/do/district_points_calc/{}'.format(event.id()),
-                method='GET')
-    """
 
     @classmethod
     def updateMerge(
@@ -88,3 +61,45 @@ class AwardManipulator(ManipulatorBase[Award]):
             setattr(old_model, attr, merged_list)
 
         return old_model
+
+
+@AwardManipulator.register_post_update_hook
+def award_post_update_hook(updated_models: List[TUpdatedModel[Award]]) -> None:
+    event_keys: Set[ndb.Key] = set()
+    for updated_award in updated_models:
+        event_keys.add(none_throws(updated_award.model.event))
+
+    for event_key in event_keys:
+        # Enqueue task to calculate district points
+        taskqueue.add(
+            url=f"/tasks/math/do/district_points_calc/{event_key.string_id()}",
+            method="GET",
+            target="py3-tasks-io",
+            queue_name="default",
+        )
+
+
+"""
+    @classmethod
+    def postUpdateHook(cls, awards, updated_attr_list, is_new_list):
+        # Note, updated_attr_list will always be empty, for now
+        # Still needs to be implemented in updateMerge
+        # See helpers.EventManipulator
+        events = []
+        for (award, updated_attrs) in zip(awards, updated_attr_list):
+            event = award.event
+            if event not in events:
+                events.append(event)
+
+        for event in events:
+            if event.get().within_a_day:
+                try:
+                    NotificationHelper.send_award_update(event.get())
+                except Exception:
+                    logging.error("Error sending award update for {}".format(event.id()))
+                try:
+                    TBANSHelper.awards(event.get())
+                except Exception, exception:
+                    logging.error("Error sending {} award updates: {}".format(event.id(), exception))
+                    logging.error(traceback.format_exc())
+"""
