415a2405fae19934215ea44317746cca35ff199b
Greg Stanton Marra <greg.marra@gmail.com>
2022-02-19 20:59:17 -0500

    linting

--- src/backend/web/handlers/eventwizard.py
+++ src/backend/web/handlers/eventwizard.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 from backend.common.decorators import cached_public
 
+
 @cached_public(ttl=timedelta(seconds=61))
 def eventwizard() -> str:
     return render_template("eventwizard.html")
