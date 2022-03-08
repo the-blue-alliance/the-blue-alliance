190a463aed169ac952a729f41f66bc1a281caa53
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-03-08 16:42:05 -0500

    Move TBANS code to common (#4298)

---
+++ src/backend/common/models/notifications/match_score.py
@@ -0,0 +1,81 @@
+from backend.common.models.notifications.notification import Notification
+
+
+class MatchScoreNotification(Notification):
+    def __init__(self, match, team=None):
+        self.match = match
+        self.event = match.event.get()
+        self.team = team
+
+    @classmethod
+    def _type(cls):
+        from backend.common.consts.notification_type import NotificationType
+
+        return NotificationType.MATCH_SCORE
+
+    @property
+    def fcm_notification(self):
+        winning_alliance = self.match.winning_alliance
+        losing_alliance = self.match.losing_alliance
+
+        alliance_one = (
+            self.match.alliances[winning_alliance]
+            if winning_alliance
+            else self.match.alliances["red"]
+        )
+        alliance_two = (
+            self.match.alliances[losing_alliance]
+            if losing_alliance
+            else self.match.alliances["blue"]
+        )
+
+        # [3:] to remove 'frc' prefix
+        alliance_one_teams = ", ".join([team[3:] for team in alliance_one["teams"]])
+        alliance_two_teams = ", ".join([team[3:] for team in alliance_two["teams"]])
+
+        alliance_one_score = alliance_one["score"]
+        alliance_two_score = alliance_two["score"]
+        if alliance_one_score == alliance_two_score:
+            action = "tied with"
+        else:
+            action = "beat"
+
+        title = self.event.event_short.upper()
+        if self.team:
+            title = "Team {} {}".format(self.team.team_number, title)
+
+        from firebase_admin import messaging
+
+        return messaging.Notification(
+            title="{} {} Results".format(title, self.match.short_name),
+            body="{} {} {} scoring {}-{}.".format(
+                alliance_one_teams,
+                action,
+                alliance_two_teams,
+                alliance_one_score,
+                alliance_two_score,
+            ),
+        )
+
+    @property
+    def data_payload(self):
+        payload = {"event_key": self.event.key_name, "match_key": self.match.key_name}
+
+        if self.team:
+            payload["team_key"] = self.team.key_name
+
+        return payload
+
+    @property
+    def webhook_message_data(self):
+        from backend.common.queries.dict_converters.match_converter import (
+            MatchConverter,
+        )
+
+        payload = self.data_payload
+        # Remove the FCM-only keys
+        del payload["match_key"]
+
+        payload["event_name"] = self.event.name
+        payload["match"] = MatchConverter.matchConverter_v3(self.match)
+        return payload
---
+++ src/backend/common/models/notifications/tests/notification_test.py
@@ -0,0 +1,48 @@
+import pytest
+from firebase_admin import messaging
+
+from backend.common.consts.fcm.platform_priority import PlatformPriority
+from backend.common.models.fcm.platform_config import PlatformConfig
+from backend.common.models.notifications.notification import Notification
+from backend.common.models.notifications.tests.mocks.notifications.mock_notification import (
+    MockNotification,
+)
+
+
+def test_str():
+    fcm_notification = messaging.Notification(title="Title Here", body="Body here")
+    data_payload = {"data": "payload"}
+    platform_config = PlatformConfig(
+        priority=PlatformPriority.HIGH, collapse_key="general_collapse_key"
+    )
+    apns_config = messaging.APNSConfig(headers={"apns-collapse-id": "ios_collapse_key"})
+
+    notification = MockNotification(
+        fcm_notification=fcm_notification,
+        data_payload=data_payload,
+        platform_config=platform_config,
+        apns_config=apns_config,
+    )
+    assert (
+        str(notification)
+        == "MockNotification(data_payload={'data': 'payload'} fcm_notification.title=\"Title Here\" fcm_notification.body=\"Body here\" platform_config=PlatformConfig(collapse_key=\"general_collapse_key\" priority=1) apns_config={'apns-collapse-id': 'ios_collapse_key'} webhook_message_data={'data': 'payload'})"
+    )
+
+
+def test_type_raises():
+    with pytest.raises(NotImplementedError):
+        Notification._type()
+
+
+def test_webhook_message_data_same_data_payload():
+    data_payload = {"data": "here"}
+    notification = MockNotification(data_payload=data_payload)
+    assert notification.data_payload == data_payload
+    assert notification.webhook_message_data == data_payload
+
+
+def test_webhook_message_data():
+    webhook_message_data = {"data": "here"}
+    notification = MockNotification(webhook_message_data=webhook_message_data)
+    assert notification.data_payload is None
+    assert notification.webhook_message_data == webhook_message_data
