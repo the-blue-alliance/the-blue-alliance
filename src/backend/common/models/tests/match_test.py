220248cf7d590e97cee9e951d685af4b4dceb834
Greg Marra <greg.marra@gmail.com>
2022-03-09 20:50:43 -0500

    Simple regex change to fix match pages (#4307)

--- src/backend/common/models/tests/match_test.py
+++ src/backend/common/models/tests/match_test.py
@@ -50,12 +50,12 @@ def get_base_qual_match(**kwargs) -> Match:
     )
 
 
-@pytest.mark.parametrize("key", ["2019nyny_qm1", "2010ct_sf1m3"])
+@pytest.mark.parametrize("key", ["2019nyny_qm1", "2010ct_sf1m3", "2022on306_qm15"])
 def test_valid_key_names(key: str) -> None:
     assert Match.validate_key_name(key) is True
 
 
-@pytest.mark.parametrize("key", ["frc177", "2010ct_qm1m1", "2010ctf1m1", "2010ct_f1"])
+@pytest.mark.parametrize("key", ["frc177", "2010ct_qm1m1", "2010ctf1m1", "2010ct_f1", "2022on_306_qm15"])
 def test_invalid_key_names(key: str) -> None:
     assert Match.validate_key_name(key) is False
 
