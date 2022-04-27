148535ad11866828826ee061ecd121d73dd7b79e
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2022-04-26 21:47:11 -0700

    Fix lint

--- src/backend/common/models/insight.py
+++ src/backend/common/models/insight.py
@@ -1,4 +1,5 @@
 import json
+
 from google.appengine.ext import ndb
 
 
@@ -34,34 +35,39 @@ class Insight(ndb.Model):
     YEAR_SPECIFIC = 1000
 
     # Used for datastore keys! Don't change unless you know what you're doing.
-    INSIGHT_NAMES = {MATCH_HIGHSCORE: 'match_highscore',
-                     MATCH_HIGHSCORE_BY_WEEK: 'match_highscore_by_week',
-                     MATCH_AVERAGES_BY_WEEK: 'match_averages_by_week',
-                     ELIM_MATCH_AVERAGES_BY_WEEK: 'elim_match_averages_by_week',
-                     SCORE_DISTRIBUTION: 'score_distribution',
-                     ELIM_SCORE_DISTRIBUTION: 'elim_score_distribution',
-                     NUM_MATCHES: 'num_matches',
-                     BLUE_BANNERS: 'blue_banners',
-                     CA_WINNER: 'ca_winner',
-                     RCA_WINNERS: 'rca_winners',
-                     WORLD_CHAMPIONS: 'world_champions',
-                     WORLD_FINALISTS: 'world_finalists',
-                     DIVISION_WINNERS: 'division_winners',
-                     DIVISION_FINALISTS: 'division_finalists',
-                     REGIONAL_DISTRICT_WINNERS: 'regional_district_winners',
-                     SUCCESSFUL_ELIM_TEAMUPS: 'successful_elim_teamups',
-                     MATCH_PREDICTIONS: 'match_predictions',
-                     MATCH_AVERAGE_MARGINS_BY_WEEK: 'match_average_margins_by_week',
-                     ELIM_MATCH_AVERAGE_MARGINS_BY_WEEK: 'elim_match_average_margins_by_week',
-                     WINNING_MARGIN_DISTRIBUTION: 'winning_margin_distribution',
-                     ELIM_WINNING_MARGIN_DISTRIBUTION: 'elim_winning_margin_distribution',
-                     YEAR_SPECIFIC_BY_WEEK: 'year_specific_by_week',
-                     YEAR_SPECIFIC: 'year_specific',
-                     }
+    INSIGHT_NAMES = {
+        MATCH_HIGHSCORE: "match_highscore",
+        MATCH_HIGHSCORE_BY_WEEK: "match_highscore_by_week",
+        MATCH_AVERAGES_BY_WEEK: "match_averages_by_week",
+        ELIM_MATCH_AVERAGES_BY_WEEK: "elim_match_averages_by_week",
+        SCORE_DISTRIBUTION: "score_distribution",
+        ELIM_SCORE_DISTRIBUTION: "elim_score_distribution",
+        NUM_MATCHES: "num_matches",
+        BLUE_BANNERS: "blue_banners",
+        CA_WINNER: "ca_winner",
+        RCA_WINNERS: "rca_winners",
+        WORLD_CHAMPIONS: "world_champions",
+        WORLD_FINALISTS: "world_finalists",
+        DIVISION_WINNERS: "division_winners",
+        DIVISION_FINALISTS: "division_finalists",
+        REGIONAL_DISTRICT_WINNERS: "regional_district_winners",
+        SUCCESSFUL_ELIM_TEAMUPS: "successful_elim_teamups",
+        MATCH_PREDICTIONS: "match_predictions",
+        MATCH_AVERAGE_MARGINS_BY_WEEK: "match_average_margins_by_week",
+        ELIM_MATCH_AVERAGE_MARGINS_BY_WEEK: "elim_match_average_margins_by_week",
+        WINNING_MARGIN_DISTRIBUTION: "winning_margin_distribution",
+        ELIM_WINNING_MARGIN_DISTRIBUTION: "elim_winning_margin_distribution",
+        YEAR_SPECIFIC_BY_WEEK: "year_specific_by_week",
+        YEAR_SPECIFIC: "year_specific",
+    }
 
     name = ndb.StringProperty(required=True)  # general name used for sorting
-    year = ndb.IntegerProperty(required=True)  # year this insight pertains to. year = 0 for overall insights
-    data_json = ndb.TextProperty(required=True, indexed=False)  # JSON dictionary of the data of the insight
+    year = ndb.IntegerProperty(
+        required=True
+    )  # year this insight pertains to. year = 0 for overall insights
+    data_json = ndb.TextProperty(
+        required=True, indexed=False
+    )  # JSON dictionary of the data of the insight
 
     created = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
     updated = ndb.DateTimeProperty(auto_now=True, indexed=False)
@@ -89,6 +95,6 @@ class Insight(ndb.Model):
     @classmethod
     def renderKeyName(cls, year, name):
         if year == 0:
-            return 'insights' + '_' + str(name)
+            return "insights" + "_" + str(name)
         else:
-            return str(year) + 'insights' + '_' + str(name)
+            return str(year) + "insights" + "_" + str(name)
