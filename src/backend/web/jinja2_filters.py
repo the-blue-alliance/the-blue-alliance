935057c9539966c4dd44d49b0eeeacc7cf5adfc3
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-05-18 22:04:27 -0400

    Cleanup Jinja2 filters from py2. Add type hints to py3 Jinja2 filters

--- src/backend/web/jinja2_filters.py
+++ src/backend/web/jinja2_filters.py
@@ -2,13 +2,13 @@
 import email
 import re
 import time
+from datetime import datetime, timezone
+from typing import Any, Optional, Union
 
 from backend.common.helpers.youtube_video_helper import YouTubeVideoHelper
+from backend.common.models.keys import MatchKey
 from backend.common.models.match import Match
 
-# import urllib
-#
-# from models.match import Match
 
 defense_render_names_2016 = {
     "A_ChevalDeFrise": "Cheval De Frise",
@@ -22,17 +22,13 @@ defense_render_names_2016 = {
 }
 
 
-# def ceil(value):
-#     return int(math.ceil(value))
-
-
-def defense_name(value):
+def defense_name(value: str) -> str:
     if value in defense_render_names_2016:
         return defense_render_names_2016[value]
     return value
 
 
-def digits(s):
+def digits(s: Optional[Union[int, str]]) -> Union[int, str]:
     if not s:
         return ""
     if type(s) is int:
@@ -40,41 +36,35 @@ def digits(s):
     return re.sub("[^0-9]", "", s)
 
 
-def floatformat(num, num_decimals):
+def floatformat(num: float, num_decimals: int) -> str:
     return "%.{}f".format(num_decimals) % num
 
 
-def isoformat(datetime):
-    return datetime.isoformat()
+def isoformat(dt: datetime) -> datetime:
+    return dt.isoformat()
 
 
-def utc_timezone_datetime(datetime):
+def utc_timezone_datetime(dt: datetime) -> datetime:
     """
     Used with Python 3 for <time datetime= tag where the datetime does not have a timezone
     already associated with it. Adds a UTC timezone to the given datetime
     """
-    from datetime import timezone
-
-    return datetime.astimezone(timezone.utc)
+    return dt.astimezone(timezone.utc)  # pyre-ignore[16]
 
 
-# def union(one, two):
-#     return set(one) | set(two)
-#
-#
-def limit_prob(prob):
+def limit_prob(prob: float) -> int:
     prob *= 100
     prob = min(95, max(prob, 5))
     return int(round(prob))
 
 
-def strftime(datetime, formatstr):
+def strftime(dt: datetime, formatstr: str) -> str:
     """
     Uses Python's strftime with some tweaks
     """
 
     # https://github.com/django/django/blob/54ea290e5bbd19d87bd8dba807738eeeaf01a362/django/utils/dateformat.py#L289
-    def t(day):
+    def t(day: int) -> str:
         "English ordinal suffix for the day of the month, 2 characters; i.e. 'st', 'nd', 'rd' or 'th'"
         if day in (11, 12, 13):  # Special case
             return "th"
@@ -87,27 +77,23 @@ def strftime(datetime, formatstr):
             return "rd"
         return "th"
 
-    formatstr = formatstr.replace("%t", t(datetime.day))
-    return datetime.strftime(formatstr).lstrip("0").replace(" 0", " ")
+    formatstr = formatstr.replace("%t", t(dt.day))
+    return dt.strftime(formatstr).lstrip("0").replace(" 0", " ")
 
 
-def strip_frc(s):
+def strip_frc(s: Optional[str]) -> str:
     if not s:
         return ""
     return s[3:]
 
 
-# def urlencode(s):
-#     return urllib.quote(s.encode('utf8'))
-
-
-def rfc2822(datetime):
-    tt = datetime.timetuple()
+def rfc2822(dt: datetime) -> str:
+    tt = dt.timetuple()
     timestamp = time.mktime(tt)
     return email.utils.formatdate(timestamp)
 
 
-def slugify(value):
+def slugify(value: Any) -> str:
     """
     Based on django's slugify template
     https://github.com/django/django/blob/a6b3938afc0204093b5356ade2be30b461a698c5/django/utils/text.py#L394
@@ -125,7 +111,7 @@ def slugify(value):
     return re.sub(r"[-\s]+", "-", value)
 
 
-def yt_start(value):
+def yt_start(value: str) -> str:
     if "?t=" in value:  # Treat ?t= the same as #t=
         value = value.replace("?t=", "#t=")
     if "#t=" in value:
@@ -138,7 +124,7 @@ def yt_start(value):
     return value
 
 
-def match_short(match_key):
+def match_short(match_key: MatchKey) -> str:
     if not Match.validate_key_name(match_key):
         return ""
     match_id = match_key.split("_")[1]
