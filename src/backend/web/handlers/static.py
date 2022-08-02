7b75121f080ced7043a9bcd0f39ad691cff0b2fd
Jordan Miller <dracco1993@gmail.com>
2022-08-02 18:15:06 -0400

    Add brand url and handler (#4661)
    * Add add-data url and handler
    
    * Add brand url and handler
    
    * A bit of refactoring for static pages

---
+++ src/backend/web/handlers/static.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+
+from flask import redirect, render_template
+from werkzeug.wrappers.response import Response
+
+from backend.common.decorators import cached_public
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def add_data() -> str:
+    return render_template("add_data.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def brand() -> str:
+    return render_template("brand.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def contact() -> str:
+    return render_template("contact.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def bigquery() -> Response:
+    return redirect(
+        "https://console.cloud.google.com/bigquery?project=tbatv-prod-hrd&p=tbatv-prod-hrd&d=the_blue_alliance&page=dataset"
+    )
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def opr() -> str:
+    return render_template("opr.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def privacy() -> str:
+    return render_template("privacy.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
+def swag() -> Response:
+    return redirect(
+        "https://www.amazon.com/s/ref=w_bl_sl_s_ap_web_7141123011?ie=UTF8&node=7141123011&field-brandtextbin=The+Blue+Alliance"
+    )
