5a405a57a0fc31d74754e0ec5755e8e064d28a54
Jordan Miller <dracco1993@gmail.com>
2022-08-02 13:57:52 -0400

    Add bigquery url and handler (#4649)
    * Add bigquery url and handler
    
    * Correct the returned response type
    
    * Correct the returned response type... again
    
    * Fix spacing

---
+++ src/backend/web/handlers/bigquery.py
@@ -0,0 +1,13 @@
+from datetime import timedelta
+
+from flask import redirect
+from werkzeug.wrappers.response import Response
+
+from backend.common.decorators import cached_public
+
+
+@cached_public(ttl=timedelta(seconds=61))
+def bigquery() -> Response:
+    return redirect(
+        "https://console.cloud.google.com/bigquery?project=tbatv-prod-hrd&p=tbatv-prod-hrd&d=the_blue_alliance&page=dataset"
+    )
