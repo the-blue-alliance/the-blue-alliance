ececbc26fd903dcd62fdefca0f25391fc91c33a0
dracco1993 <dracco1993@gmail.com>
2022-08-02 16:42:56 -0400

    Add privacy url and handler

--- src/backend/web/main.py
+++ src/backend/web/main.py
@@ -21,6 +21,7 @@ from backend.web.handlers.index import about, avatar_list, index
 from backend.web.handlers.insights import insights_detail, insights_overview
 from backend.web.handlers.match import match_detail
 from backend.web.handlers.opr import opr
+from backend.web.handlers.privacy import privacy
 from backend.web.handlers.suggestions.suggestion_review import (
     blueprint as suggestion_review_blueprint,
 )
@@ -87,6 +88,8 @@ app.add_url_rule("/insights", view_func=insights_overview)
 app.add_url_rule("/insights/<int:year>", view_func=insights_detail)
 
 app.add_url_rule("/opr", view_func=opr)
+
+app.add_url_rule("/privacy", view_func=privacy)
 
 app.add_url_rule("/bigquery", view_func=bigquery)
 
