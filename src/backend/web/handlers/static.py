935fa2cd173cb298c501d7c071c9c664256e8627
Andrew Ke <andrewke@berkeley.edu>
2022-10-08 21:55:14 -0700

    port /thanks to py3

--- src/backend/web/handlers/static.py
+++ src/backend/web/handlers/static.py
@@ -39,6 +39,11 @@ def privacy() -> str:
 
 
 @cached_public(ttl=timedelta(weeks=1))
+def thanks() -> str:
+    return render_template("thanks.html")
+
+
+@cached_public(ttl=timedelta(weeks=1))
 def swag() -> Response:
     return redirect(
         "https://www.amazon.com/s/ref=w_bl_sl_s_ap_web_7141123011?ie=UTF8&node=7141123011&field-brandtextbin=The+Blue+Alliance"
