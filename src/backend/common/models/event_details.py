4006d7a21702ee1c1f5e575d82f7199f7119263e
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2023-02-18 21:13:00 -0800

    Update ranking sort order

--- src/backend/common/models/event_details.py
+++ src/backend/common/models/event_details.py
@@ -118,7 +118,7 @@ class EventDetails(CachedModel):
                 if game_year == 2021:
                     # 2021 did not have matches played for rankings
                     continue
-                elif game_year in {2017, 2018, 2019, 2020, 2021, 2022}:
+                elif game_year in {2017, 2018, 2019, 2020, 2021, 2022, 2023}:
                     rank["extra_stats"] = [
                         int(round(rank["sort_orders"][0] * rank["matches_played"])),
                     ]
@@ -138,7 +138,7 @@ class EventDetails(CachedModel):
             if game_year == 2021:
                 # 2021 did not have matches played for rankings
                 pass
-            elif game_year in {2017, 2018, 2019, 2020, 2021, 2022}:
+            elif game_year in {2017, 2018, 2019, 2020, 2021, 2022, 2023}:
                 extra_stats_info = [{"name": "Total Ranking Points", "precision": 0}]
             elif sort_order_info is not None:
                 extra_stats_info = [
