52c800828b6e1756d87179f623ba3441ffb3ede5
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2023-02-18 22:58:15 -0800

    Handle inverse mapping

--- src/backend/common/consts/playoff_type.py
+++ src/backend/common/consts/playoff_type.py
@@ -223,3 +223,5 @@ DOUBLE_ELIM_MAPPING: Dict[int, Tuple[CompLevel, int, int]] = {
     16: (CompLevel.F, 2, 3),
     17: (CompLevel.F, 2, 4),  # Overtime
 }
+
+DOUBLE_ELIM_MAPPING_INVERSE: Dict[Tuple[CompLevel, int, int], int] = {v: k for k, v in DOUBLE_ELIM_MAPPING.items()}
