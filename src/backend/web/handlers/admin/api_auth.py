138eb3f5236214c79882eb2c645403e191ac88e1
Phil Lopreiato <plnyyanks@gmail.com>
2023-02-19 10:32:37 -0500

    Admin panel interface to set all_official_events on trustedapi auth

--- src/backend/web/handlers/admin/api_auth.py
+++ src/backend/web/handlers/admin/api_auth.py
@@ -106,6 +106,10 @@ def api_auth_edit_post(auth_id: str) -> Response:
     else:
         event_list = []
 
+    all_official_events = False
+    if request.form.get("all_official_events"):
+        all_official_events = True
+
     if not auth:
         auth = ApiAuthAccess(
             id=auth_id,
@@ -120,6 +124,7 @@ def api_auth_edit_post(auth_id: str) -> Response:
                 for _ in range(64)
             ),
             event_list=event_list,
+            all_official_events=all_official_events,
             auth_types_enum=auth_types_enum,
         )
     else:
