f1e2e5b2baf8c716e3e267c16a84181a9a02d0c7
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2023-03-01 17:03:00 -0800

    Relax zebra data teams check (#5047)

--- src/backend/common/models/tests/match_test.py
+++ src/backend/common/models/tests/match_test.py
@@ -50,13 +50,23 @@ def get_base_qual_match(**kwargs) -> Match:
     )
 
 
-@pytest.mark.parametrize("key", ["2019nyny_qm1", "2010ct_sf1m3", "2022on306_qm15", "2023week0_sf13m1"])
+@pytest.mark.parametrize(
+    "key", ["2019nyny_qm1", "2010ct_sf1m3", "2022on306_qm15", "2023week0_sf13m1"]
+)
 def test_valid_key_names(key: str) -> None:
     assert Match.validate_key_name(key) is True
 
 
 @pytest.mark.parametrize(
-    "key", ["frc177", "2010ct_qm1m1", "2010ctf1m1", "2010ct_f1", "2022on_306_qm15", "2023week0_sf130m1"]
+    "key",
+    [
+        "frc177",
+        "2010ct_qm1m1",
+        "2010ctf1m1",
+        "2010ct_f1",
+        "2022on_306_qm15",
+        "2023week0_sf130m1",
+    ],
 )
 def test_invalid_key_names(key: str) -> None:
     assert Match.validate_key_name(key) is False
