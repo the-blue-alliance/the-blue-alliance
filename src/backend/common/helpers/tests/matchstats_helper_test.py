adcf929f62c7a7ab621bde20c796de5348055043
dracco1993 <dracco1993@gmail.com>
2023-04-12 17:02:09 -0400

    pyre-ignore pytest's lacking type annotations

--- src/backend/common/helpers/tests/matchstats_helper_test.py
+++ src/backend/common/helpers/tests/matchstats_helper_test.py
@@ -49,7 +49,7 @@ def assert_coprs_values_equal(
     for component, oprs in coprs.items():
         assert oprs.keys() == expected_coprs[component].keys()
         for team_key, opr in oprs.items():
-            assert opr == approx(expected_coprs[component][team_key])
+            assert opr == approx(expected_coprs[component][team_key])  # pyre-ignore[16]
 
 
 def test_compute_matchstats_no_matches() -> None:
