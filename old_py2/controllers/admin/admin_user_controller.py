09cb3209b192b1f788c0a579c51ad84dfb138e92
Phil Lopreiato <plnyyanks@gmail.com>
2023-04-30 10:56:20 -0500

    Port admin user pages to py3

--- old_py2/controllers/admin/admin_user_controller.py
+++ old_py2/controllers/admin/admin_user_controller.py
@@ -13,58 +13,6 @@ from models.account import Account
 import tba_config
 
 
-class AdminUserList(LoggedInHandler):
-    """
-    List all Users.
-    """
-    PAGE_SIZE = 1000
-
-    def get(self, page_num=0):
-        self._require_admin()
-        page_num = int(page_num)
-
-        num_users = Account.query().count()
-        max_page = num_users / self.PAGE_SIZE
-
-        if page_num <= max_page:
-            offset=self.PAGE_SIZE*page_num
-        else:
-            page_num = 0
-            offset = 0
-        users = Account.query().order(Account.created).fetch(self.PAGE_SIZE, offset=offset)
-
-        self.template_values.update({
-            "num_users": num_users,
-            "users": users,
-            "page_num": page_num,
-            "page_labels": [p+1 for p in xrange(max_page+1)],
-        })
-
-        path = os.path.join(os.path.dirname(__file__), '../../templates/admin/user_list.html')
-        self.response.out.write(template.render(path, self.template_values))
-
-
-class AdminUserLookup(LoggedInHandler):
-    """
-    Lookup a single user by email
-    """
-    def get(self):
-        self._require_admin()
-        path = os.path.join(os.path.dirname(__file__), '../../templates/admin/user_lookup.html')
-        self.response.out.write(template.render(path, self.template_values))
-
-    def post(self):
-        self._require_admin()
-        user_email = self.request.get('email')
-        if not user_email:
-            self.abort(404)
-        users = Account.query(Account.email == user_email).fetch()
-        if not users:
-            self.abort(404)
-        user = users[0]
-        self.redirect('/admin/user/edit/{}'.format(user.key.id()))
-
-
 class AdminUserPermissionsList(LoggedInHandler):
     """
     List all Users with Permissions.
@@ -80,54 +28,6 @@ class AdminUserPermissionsList(LoggedInHandler):
 
         path = os.path.join(os.path.dirname(__file__), '../../templates/admin/user_permissions_list.html')
         self.response.out.write(template.render(path, self.template_values))
-
-
-class AdminUserDetail(LoggedInHandler):
-    """
-    Show a User.
-    """
-    def get(self, user_id):
-        self._require_admin()
-        user = Account.get_by_id(user_id)
-
-        self.template_values.update({
-            "user": user,
-            "permissions": AccountPermissions.permissions
-        })
-
-        path = os.path.join(os.path.dirname(__file__), '../../templates/admin/user_details.html')
-        self.response.out.write(template.render(path, self.template_values))
-
-
-class AdminUserEdit(LoggedInHandler):
-    """
-    Edit a User.
-    """
-    def get(self, user_id):
-        self._require_admin()
-        user = Account.get_by_id(user_id)
-        self.template_values.update({
-            "user": user,
-            "permissions": AccountPermissions.permissions
-        })
-
-        path = os.path.join(os.path.dirname(__file__), '../../templates/admin/user_edit.html')
-        self.response.out.write(template.render(path, self.template_values))
-
-    def post(self, user_id):
-        self._require_admin()
-        user = Account.get_by_id(user_id)
-
-        user.display_name = self.request.get("display_name")
-        user.shadow_banned = True if self.request.get("shadow_banned") else False
-        user.permissions = []
-        for enum in AccountPermissions.permissions:
-            permcheck = self.request.get("perm-" + str(enum))
-            if permcheck:
-                user.permissions.append(enum)
-        user.put()
-
-        self.redirect("/admin/user/" + user_id)
 
 
 class AdminUserTestSetup(LoggedInHandler):
