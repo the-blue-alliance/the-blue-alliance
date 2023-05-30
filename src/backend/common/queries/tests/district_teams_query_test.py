e4d012c9f27d07dca8a93c94486bfbf0f3caa9b2
Caleb Denio <cjdenio44@gmail.com>
2023-05-30 12:53:33 -0400

    Attempt to fix the test!

--- src/backend/common/queries/tests/district_teams_query_test.py
+++ src/backend/common/queries/tests/district_teams_query_test.py
@@ -1,10 +1,13 @@
 from typing import List, Optional
+from backend.common.models.event import Event
+from backend.common.models.event_team import EventTeam
+from backend.web.handlers.tests import helpers
 
 from google.appengine.ext import ndb
 
 from backend.common.models.district import District
 from backend.common.models.district_team import DistrictTeam
-from backend.common.models.keys import DistrictKey
+from backend.common.models.keys import DistrictKey, EventKey
 from backend.common.models.team import Team
 from backend.common.queries.team_query import DistrictTeamsQuery
 
@@ -36,6 +39,18 @@ def preseed_district_teams(team_keys: List[ndb.Key], district_key: DistrictKey) 
     ]
     ndb.put_multi(district_teams)
 
+def preseed_event_teams(team_keys: List[ndb.Key], event_key: EventKey) -> None:
+    event_teams = [
+        EventTeam(
+            id=f"{event_key}_{t.id()}",
+            team=t,
+            event=ndb.Key(Event, event_key),
+            year=int(event_key[:4]),
+        )
+        for t in team_keys
+    ]
+    ndb.put_multi(event_teams)
+
 
 def test_no_teams() -> None:
     teams = DistrictTeamsQuery(district_key="2019ne").fetch()
@@ -45,6 +60,8 @@ def test_no_teams() -> None:
 def test_get_teams() -> None:
     stored_teams = preseed_teams(1, 10)
     preseed_district_teams(stored_teams, "2019ne")
+    helpers.preseed_event("2019nhgrs")
+    preseed_event_teams(stored_teams[:-1], "2019nhgrs")
 
     teams = DistrictTeamsQuery(district_key="2019ne").fetch()
-    assert len(teams) == len(stored_teams)
+    assert len(teams) == len(stored_teams) - 1
