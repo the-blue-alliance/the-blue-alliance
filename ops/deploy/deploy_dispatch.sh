9e729d5aaa6a098d1927aa79a29cf97f511e5083
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2023-07-15 14:14:19 -0400

    standardize on one dispatch.yaml (#5393)
    Closes https://github.com/the-blue-alliance/the-blue-alliance/issues/5388
    
    This will remove routing for the `py2` subdomain (since we can't delete services as long as there are dispatch rules for them). And we can also finally converge on a single `dispatch.yaml` living in this repo.

--- ops/deploy/deploy_dispatch.sh
+++ ops/deploy/deploy_dispatch.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
 set -e
 
-# This needs to exist for as long as we stil need to support routing for the py2 instance
-# once that's gone, we can remove this, and simply deploy based on src/dispatch.yaml
-# Until then, this is the same command + version from the old py2 deploy step
-# https://github.com/the-blue-alliance/the-blue-alliance/blob/3dafb6697bd9d5511afaf7240eab733bd11b26a6/.github/workflows/push_py2.yml#L196C14-L196C70
-
-mv ops/prod_dispatch.yaml src/dispatch.yaml
 gcloud app deploy src/dispatch.yaml --version prod-2 --quiet
