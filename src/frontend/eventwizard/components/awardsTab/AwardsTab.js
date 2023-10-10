b163f1e9ee0698f03470d4d5662d21db8ca2c0f6
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2023-10-09 22:06:45 -0700

    WIP

--- src/frontend/eventwizard/components/awardsTab/AwardsTab.js
+++ src/frontend/eventwizard/components/awardsTab/AwardsTab.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
-
+const getAwardKey = (type_enum, team_key, awardee) => {
+  return `${type_enum}_${team_key}_${awardee}`;
+}
 
 function AwardsTab({selectedEvent}) {
   selectedEvent = "2023casj"
   const [updating, setUpdating] = useState(false);
   const [awards, setAwards] = useState([]);
+  const [awardsFetched, setAwardsFetched] = useState(false);
+  const [awardsToDelete, setAwardsToDelete] = useState(new Set());
 
   const [newAwardType, setNewAwardType] = useState("");
   const [newAwardName, setNewAwardName] = useState("");
@@ -34,6 +38,7 @@ function AwardsTab({selectedEvent}) {
 
     setAwards(awards);
     setUpdating(false);
+    setAwardsFetched(true);
   }
 
   const addAward = () => {
@@ -46,6 +51,22 @@ function AwardsTab({selectedEvent}) {
     setAwards(prevAwards => [...prevAwards, newAward]);
   }
 
+  const toggleDeletion = (awardKey) => {
+    setAwardsToDelete(prevAwardsToDelete => {
+      const newAwardsToDelete = new Set(prevAwardsToDelete);
+      if (newAwardsToDelete.has(awardKey)) {
+        newAwardsToDelete.delete(awardKey);
+      } else {
+        newAwardsToDelete.add(awardKey);
+      }
+      return newAwardsToDelete;
+    })
+  }
+  
+  const saveEdits = async () => {
+
+  }
+
   const sortedAwards = awards.sort((a, b) => a.type_enum - b.type_enum);
 
   return <div className="tab-pane" id="awards">
@@ -56,6 +77,9 @@ function AwardsTab({selectedEvent}) {
     <hr />
 
     <h4>Add Award</h4>
+    <p>If an award type is not specified, we will automatically try to determine it from the award name. Note that this is not guaranteed to work, especially for offseason events.</p>
+    <p>For a list of award type enums, see <a href="https://github.com/the-blue-alliance/the-blue-alliance/blob/py3/src/backend/common/consts/award_type.py" target="_blank">here</a>.</p>
+
     <div className="form-group">
       <label htmlFor="award_type" className="col-sm-2 control-label">
         Award Type
@@ -67,11 +91,12 @@ function AwardsTab({selectedEvent}) {
           id="award_type"
           value={newAwardType}
           onChange={e => setNewAwardType(e.target.value)}
+          placeholder="3"
         />
       </div>
 
       <label htmlFor="award_name" className="col-sm-2 control-label">
-        Name
+        Award Name (Required)
       </label>
       <div className="col-sm-10">
         <input
@@ -80,6 +105,7 @@ function AwardsTab({selectedEvent}) {
           id="award_name"
           value={newAwardName}
           onChange={e => setNewAwardName(e.target.value)}
+          placeholder="Woodie Flowers Finalist Award"
         />
       </div>
 
@@ -93,6 +119,7 @@ function AwardsTab({selectedEvent}) {
           id="award_team"
           value={newAwardTeamNumber}
           onChange={e => setNewAwardTeamNumber(e.target.value)}
+          placeholder="604"
         />
       </div>
 
@@ -106,10 +133,11 @@ function AwardsTab({selectedEvent}) {
           id="award_awardee"
           value={newAwardAwardee}
           onChange={e => setNewAwardAwardee(e.target.value)}
+          placeholder="Helen Arrington"
         />
       </div>
     </div>
-    <button className="btn btn-info" onClick={addAward}>Add Award</button>
+    <button className="btn btn-info" onClick={addAward} disabled={updating}>Add Award</button>
 
     <hr />
 
@@ -126,18 +154,37 @@ function AwardsTab({selectedEvent}) {
       </thead>
       <tbody>
         {sortedAwards.map(({type_enum, name_str, team_key, awardee}) => {
+          const awardKey = getAwardKey(type_enum, team_key, awardee);
+          const toDelete = awardsToDelete.has(awardKey);
           return (
-            <tr key={`${type_enum}_${team_key}_${awardee}`}>
+            <tr key={awardKey}>
               <td>{type_enum}</td>
               <td>{name_str}</td>
               <td>{team_key}</td>
               <td>{awardee}</td>
-              <td><button className="btn">Keep</button></td>
+              <td>
+                <button className={toDelete ? "btn btn-danger" : "btn"} onClick={() => toggleDeletion(awardKey)}>
+                  {toDelete ? "Will be deleted": "Keep"}
+                </button>
+              </td>
             </tr>
           );
         })}
       </tbody>
     </table>
+
+    <hr />
+
+    <h4>Save Edits</h4>
+    <p>All existing awards will be overwritten with the awards above!</p>
+    <p>Please ensure that you have fetched existing awards before submitting!</p>
+    <button
+      className="btn btn-danger"
+      onClick={saveEdits}
+      disabled={updating || !awardsFetched}
+    >
+      Save Edits
+    </button>
   </div>
 }
 
