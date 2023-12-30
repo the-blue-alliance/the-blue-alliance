750a7b1e58ebdc89e10fdd8382837c09d0c10c71
Phil Lopreiato <plnyyanks@gmail.com>
2023-12-30 10:09:40 -0500

    Update to python 3.12

--- ops/dev/vagrant/bootstrap-dev-container.sh
+++ ops/dev/vagrant/bootstrap-dev-container.sh
@@ -8,7 +8,8 @@ mkdir -p /datastore
 apt-get update && apt-get upgrade -y
 
 # The datastore emulator requires grpcio
-python -m pip install --upgrade pip
+python -m ensurepip --upgrade
+pip install --upgrade setuptools
 pip install --ignore-installed -r requirements.txt
 pip install --ignore-installed -r src/requirements.txt
 
