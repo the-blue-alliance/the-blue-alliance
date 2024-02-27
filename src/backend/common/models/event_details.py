74d8d29c15db8854351fc88c6046c52148786f3a
Alan <lethosor@users.noreply.github.com>
2024-02-26 20:05:43 -0500

    Show "total ranking points" column in 2024 rankings
    Also apply to future years, assuming this is unlikely to change

--- src/backend/common/models/event_details.py
+++ src/backend/common/models/event_details.py
@@ -114,7 +114,7 @@ class EventDetails(CachedModel):
                 if game_year == 2021:
                     # 2021 did not have matches played for rankings
                     continue
-                elif game_year in {2017, 2018, 2019, 2020, 2021, 2022, 2023}:
+                elif game_year >= 2017:
                     rank["extra_stats"] = [
                         int(round(rank["sort_orders"][0] * rank["matches_played"])),
                     ]
