f05b6191cf6d447696db8d3371aba07265d7eb0f
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-04-16 23:21:17 -0700

    2024 match suggestions (#5888)
    * Update match suggestions for 2024
    
    * team details
    
    * Reduce margin
    
    * Lint
    
    * Lint
    
    * Fix types

--- src/backend/web/handlers/match_suggestion.py
+++ src/backend/web/handlers/match_suggestion.py
@@ -1,10 +1,21 @@
+import datetime
 from collections import defaultdict
 
+from google.appengine.ext import ndb
 from werkzeug.wrappers import Response
 
+from backend.common.consts.alliance_color import AllianceColor
+from backend.common.consts.award_type import AwardType
+from backend.common.consts.event_type import EventType
 from backend.common.helpers.event_helper import EventHelper
 from backend.common.helpers.match_helper import MatchHelper
 from backend.common.helpers.team_helper import TeamHelper
+from backend.common.models.event import Event
+from backend.common.models.keys import TeamKey
+from backend.common.models.team import Team
+from backend.common.queries.award_query import TeamEventTypeAwardsQuery
+from backend.common.queries.event_details_query import EventDetailsQuery
+from backend.common.queries.event_query import TeamYearEventTeamsQuery
 from backend.web.decorators import require_login
 from backend.web.profiled_render import render_template
 
@@ -19,10 +30,10 @@ def get_qual_bluezone_score(prediction):
     )  # High score ~200, up to 50 BlueZone points
     skillPower = min(
         (
-            min(prediction["red"]["links"] / 9, 1)
-            + min(prediction["blue"]["links"] / 9, 1)
-            + min(prediction["red"]["charge_station_points"] / 42, 1)
-            + min(prediction["blue"]["charge_station_points"] / 42, 1)
+            min(prediction["red"]["note_scored"] / 21, 1)
+            + min(prediction["blue"]["note_scored"] / 21, 1)
+            + min(prediction["red"]["stage_points"] / 10, 1)
+            + min(prediction["blue"]["stage_points"] / 10, 1)
         )
         * 50
         / 4,
@@ -30,6 +41,62 @@ def get_qual_bluezone_score(prediction):
     )  # Up to 50 BlueZone points
 
     return scorePower + skillPower
+
+
+@ndb.tasklet
+def fetch_team_details_async(team_key: TeamKey):
+    team = yield Team.get_by_id_async(team_key)
+
+    current_year = datetime.datetime.now().year
+    event_teams = yield TeamYearEventTeamsQuery(
+        team_key=team_key, year=current_year
+    ).fetch_async()
+    division_win_awards = yield TeamEventTypeAwardsQuery(
+        team_key=team_key,
+        event_type=EventType.CMP_DIVISION,
+        award_type=AwardType.WINNER,
+    ).fetch_async()
+
+    events_details = []
+    if event_teams is not None:
+        for event_team in event_teams:
+            event_key = event_team.key.id().split("_")[0]
+            event = yield Event.get_by_id_async(event_key)
+            event_details = yield EventDetailsQuery(event_key).fetch_async()
+
+            if event is None or event_details is None:
+                continue
+
+            alliance = event_team.status["alliance"]["number"]
+            pick = event_team.status["alliance"]["pick"]
+            events_details.append(
+                {
+                    "event_short": event.event_short,
+                    "name": event.name,
+                    "alliance": f"A{alliance}P{'C' if pick == 0 else pick}",
+                    "finish": f"{event_team.status['playoff']['double_elim_round']} ({event_team.status['playoff']['status']})",
+                    "auto_note_copr": event_details.coprs.get(
+                        "Total Auto Game Pieces", {}
+                    ).get(team_key[3:]),
+                    "teleop_note_copr": event_details.coprs.get(
+                        "Total Teleop Game Pieces", {}
+                    ).get(team_key[3:]),
+                    "trap_copr": event_details.coprs.get("Total Trap", {}).get(
+                        team_key[3:]
+                    ),
+                }
+            )
+
+    past_einstein = []
+    if division_win_awards is not None:
+        for division_win_award in division_win_awards:
+            past_einstein.append(division_win_award.year)
+
+    return {
+        "team": team,
+        "past_einstein": past_einstein,
+        "events": events_details,
+    }
 
 
 @require_login
@@ -50,6 +117,7 @@ def match_suggestion() -> Response:
     upcoming_matches = []
     ranks = {}
     alliances = {}
+    team_keys = set()
     for event in current_events:
         if not event.details:
             continue
@@ -57,6 +125,12 @@ def match_suggestion() -> Response:
         for i, match in enumerate(MatchHelper.upcoming_matches(event.matches, num=3)):
             if not match.time:
                 continue
+
+            for team_key in (
+                match.alliances[AllianceColor.RED]["teams"]
+                + match.alliances[AllianceColor.BLUE]["teams"]
+            ):
+                team_keys.add(team_key)
 
             if (
                 not event.details.predictions
@@ -96,6 +170,12 @@ def match_suggestion() -> Response:
         upcoming_matches, key=lambda m: m.predicted_time if m.predicted_time else m.time
     )
 
+    team_detail_futures = [fetch_team_details_async(team_key) for team_key in team_keys]
+    team_details = {}
+    for detail_future in team_detail_futures:
+        detail = detail_future.get_result()
+        team_details[detail["team"].key.id()] = detail
+
     template_values = {
         "finished_matches": finished_matches,
         "current_matches": current_matches,
@@ -103,6 +183,7 @@ def match_suggestion() -> Response:
         "ranks": ranks,
         "alliances": alliances,
         "popular_team_keys": popular_team_keys,
+        "team_details": team_details,
     }
 
     return render_template("match_suggestion.html", template_values)
