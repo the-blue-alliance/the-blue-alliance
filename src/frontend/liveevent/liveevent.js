8a5c2ee71b16a398ab8c8d839a4b0ab6b66a37b0
dracco1993 <dracco1993@gmail.com>
2024-04-26 00:54:03 -0400

    Update liveevent

--- src/frontend/liveevent/liveevent.js
+++ src/frontend/liveevent/liveevent.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import LiveEventPanel from "./components/LiveEventPanel";
 
 const els = document.getElementsByClassName("liveevent-content");
 for (let i = 0; i < els.length; i++) {
   const el = els[i];
-  ReactDOM.render(
+  const root = createRoot(el);
+  root.render(
     <LiveEventPanel
       eventKey={el.getAttribute("data-eventkey")}
       simple={el.getAttribute("data-simple") !== null}
-    />,
-    el
+    />
   );
 }
