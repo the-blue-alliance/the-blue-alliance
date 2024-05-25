6a1bbd8ab6a4c83a3cec62d50adc0674f6a52c2c
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-05-25 09:59:09 -0700

    Testing

--- ops/dev/docker/docker-entry.sh
+++ ops/dev/docker/docker-entry.sh
@@ -3,7 +3,7 @@ set -e
 
 # TODO: Maybe figure out how to not run this every time, since it's slow
 # and we don't need to do it every time
-./ops/dev/vagrant/bootstrap-dev-container.sh
+# ./ops/dev/vagrant/bootstrap-dev-container.sh
 
 ./ops/dev/vagrant/start-devserver.sh
 
