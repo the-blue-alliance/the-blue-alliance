4933c613cc440bacee3ebd0ae5c468af75ac14f5
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-06-10 23:54:21 -0700

    Configure flask secret key in api service

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -91,7 +91,7 @@ configure_logging()
 
 app = Flask(__name__)
 app.wsgi_app = wrap_wsgi_app(app.wsgi_app)
-install_middleware(app)
+install_middleware(app, configure_secret_key=True)
 install_url_converters(app)
 configure_flask_cache(app)
 
