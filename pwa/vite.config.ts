519075772a5c956be999779688e04da96d44ff91
Justin Tervay <justin@tervay.com>
2024-07-30 17:42:14 -0400

    lint

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import Icons from 'unplugin-icons/vite'
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   plugins: [
@@ -14,8 +14,8 @@ export default defineConfig({
     }),
     tsconfigPaths(),
     Icons({
-      compiler: "jsx",
-      jsx: "react"
+      compiler: 'jsx',
+      jsx: 'react',
     }),
   ],
 });
