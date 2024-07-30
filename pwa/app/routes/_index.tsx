3bcd9da480275052f0b69d19ac6c64a3a4e94e3b
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-07-29 21:45:09 -0400

    [PWA] Blank remix app + deploy (#6089)
    * Blank create-remix project
    
    * Add pwa yaml
    
    * Deploy pwa
    
    * Update landing
    
    * Move to pwa directory
    
    * Add pwa to eslintignore
    
    * Update meta headers
    
    * Update workflows

---
+++ pwa/app/routes/_index.tsx
@@ -0,0 +1,24 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "The Blue Alliance" },
+    {
+      name: "description",
+      content:
+        "Team information and match videos and results from the FIRST Robotics Competition",
+    },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">The Blue Alliance</h1>
+      <p>
+        The Blue Alliance is the best way to scout, watch, and relive the{" "}
+        <i>FIRST</i> Robotics Competition.
+      </p>
+    </div>
+  );
+}
