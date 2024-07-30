d06d3f1086530176c7afa14a0846b7b3766f1c1e
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-07-29 22:54:19 -0700

    Add prettier

--- pwa/.eslintrc.cjs
+++ pwa/.eslintrc.cjs
@@ -22,7 +22,7 @@ module.exports = {
   ignorePatterns: ["!**/.server", "!**/.client"],
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "prettier"],
 
   overrides: [
     // React
---
+++ pwa/prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
