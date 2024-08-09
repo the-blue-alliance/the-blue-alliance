14ccd10d6f37616c1ceaf255ba33208808851f7a
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-08 23:46:15 -0400

    [PWA] Add sentry (#6145)
    [clowntown]

--- pwa/app/root.tsx
+++ pwa/app/root.tsx
@@ -5,8 +5,25 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import * as Sentry from '@sentry/react';
 import './tailwind.css';
 import * as api from '~/api/v3';
+
+Sentry.init({
+  dsn: 'https://1420d805bff3f6f12a13817725266abd@o4507688293695488.ingest.us.sentry.io/4507745278492672',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/beta\.thebluealliance\.com/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  enabled: process.env.NODE_ENV === 'production',
+});
 
 api.defaults.baseUrl = 'https://thebluealliance.com/api/v3/';
 api.defaults.headers = {
