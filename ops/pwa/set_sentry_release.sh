c8c7f6c415b006e34beddec8a6703e636a4a82f6
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-08 21:51:39 -0700

    Fix sentry-cli args [clowntown

--- ops/pwa/set_sentry_release.sh
+++ ops/pwa/set_sentry_release.sh
@@ -6,10 +6,10 @@ curl -sL https://sentry.io/get-cli/ | bash
 VERSION="sentry-cli releases propose-version"
 
 # Create a release
-sentry-cli releases new "$VERSION"
+sentry-cli releases new "$VERSION" --org="$SENTRY_ORG" --project="$SENTRY_PROJECT" --auth-token="$SENTRY_AUTH_TOKEN"
 
 # Associate commits with the release
-sentry-cli releases set-commits "$VERSION" --auto
+sentry-cli releases set-commits "$VERSION" --auto --org="$SENTRY_ORG" --project="$SENTRY_PROJECT" --auth-token="$SENTRY_AUTH_TOKEN"
 
 # Finalize
-sentry-cli releases finalize "$VERSION"
+sentry-cli releases finalize "$VERSION" --org="$SENTRY_ORG" --project="$SENTRY_PROJECT" --auth-token="$SENTRY_AUTH_TOKEN"
