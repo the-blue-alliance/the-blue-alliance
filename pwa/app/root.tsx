0c994383b50d3bdd228640af7a246fe32c03d1f2
Justin Tervay <justin@tervay.com>
2024-08-09 12:21:55 -0400

    [PWA] Wraps app in container; changes container sizing/responsiveness to match prod's bootstrap sizing/responsiveness

--- pwa/app/root.tsx
+++ pwa/app/root.tsx
@@ -36,11 +36,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <div className="container mx-auto text-sm">{children}</div>
         <ScrollRestoration />
         <Scripts />
       </body>
