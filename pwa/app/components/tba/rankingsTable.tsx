cbe69e28bbfe39e6faa722af7968a99444c7f35d
Justin Tervay <justin@tervay.com>
2024-08-09 14:56:31 -0400

    [PWA] Add event page sortable rankings tab

---
+++ pwa/app/components/tba/rankingsTable.tsx
@@ -0,0 +1,67 @@
+import { Link } from '@remix-run/react';
+import type { ColumnDef } from '@tanstack/react-table';
+import { cn } from '~/lib/utils';
+import BiTrophy from '~icons/bi/trophy';
+import InlineIcon from './inlineIcon';
+import { DataTable } from '~/components/tba/dataTable';
+import { EventRanking } from '~/api/v3';
+
+type RankingColumnType = ColumnDef<EventRanking['rankings'][number]>[];
+
+export default function RankingsTable({
+  rankings,
+  winners,
+}: {
+  rankings: EventRanking;
+  winners: string[];
+}) {
+  const standardCols: RankingColumnType = [
+    { header: 'Rank', accessorKey: 'rank' },
+    {
+      header: 'Team',
+      cell: ({ row }) => (
+        <Link
+          to={`/team/${row.original.team_key.substring(3)}`}
+          className="whitespace-nowrap"
+        >
+          {winners.includes(row.original.team_key) ? (
+            <InlineIcon className="justify-center">
+              <BiTrophy />
+              {row.original.team_key.substring(3)}
+            </InlineIcon>
+          ) : (
+            <>{row.original.team_key.substring(3)}</>
+          )}
+        </Link>
+      ),
+      accessorFn: (row) => Number(row.team_key.substring(3)),
+    },
+  ];
+
+  const sortOrderCols: RankingColumnType = rankings.sort_order_info.map(
+    (sortOrder, idx) => ({
+      header: sortOrder.name,
+      accessorFn: (row) => row.sort_orders?.[idx].toFixed(sortOrder.precision),
+    }),
+  );
+
+  const generatedCols: RankingColumnType = (
+    rankings.extra_stats_info ?? []
+  ).map((stat, idx) => ({
+    header: stat.name,
+    accessorFn: (row) => row.extra_stats?.[idx],
+  }));
+
+  return (
+    <DataTable
+      columns={standardCols.concat(sortOrderCols).concat(generatedCols)}
+      data={rankings.rankings}
+      conditionalRowStyling={(row) =>
+        cn({
+          'bg-yellow-100 shadow-inner shadow-yellow-200 font-bold':
+            winners.includes(row.original.team_key),
+        })
+      }
+    />
+  );
+}
