56052eab50dbca8fa79c02a6ad33c8986b6ce3ea
Justin <7595639+tervay@users.noreply.github.com>
2024-08-12 22:24:48 -0400

    [PWA] Add team header info; add team robot pics carousel (#6230)
    * [PWA] Add team page meta title/description
    
    * Add team header info; add team robot pics carousel

--- pwa/app/routes/team.$teamNumber.tsx
+++ pwa/app/routes/team.$teamNumber.tsx
@@ -2,10 +2,19 @@ import { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import {
   ClientLoaderFunctionArgs,
   json,
+  Link,
   Params,
   useLoaderData,
 } from '@remix-run/react';
-import { getTeam } from '~/api/v3';
+import { useMemo } from 'react';
+import { getTeam, getTeamMediaByYear } from '~/api/v3';
+import InlineIcon from '~/components/tba/inlineIcon';
+import TeamRobotPicsCarousel from '~/components/tba/teamRobotPicsCarousel';
+import BiCalendar from '~icons/bi/calendar';
+import BiGraphUp from '~icons/bi/graph-up';
+import BiInfoCircleFill from '~icons/bi/info-circle-fill';
+import BiLink from '~icons/bi/link';
+import BiPinMapFill from '~icons/bi/pin-map-fill';
 
 async function loadData(params: Params) {
   if (params.teamNumber === undefined) {
@@ -13,18 +22,23 @@ async function loadData(params: Params) {
   }
 
   const teamKey = `frc${params.teamNumber}`;
+  // todo: add year support
+  const year = 2024;
 
-  const [team] = await Promise.all([getTeam({ teamKey })]);
+  const [team, media] = await Promise.all([
+    getTeam({ teamKey }),
+    getTeamMediaByYear({ teamKey, year }),
+  ]);
 
   if (team.status === 404) {
     throw new Response(null, { status: 404 });
   }
 
-  if (team.status !== 200) {
+  if (team.status !== 200 || media.status !== 200) {
     throw new Response(null, { status: 500 });
   }
 
-  return { team: team.data };
+  return { team: team.data, media: media.data };
 }
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -50,12 +64,66 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 };
 
 export default function TeamPage(): JSX.Element {
-  const { team } = useLoaderData<typeof loader>();
+  const { team, media } = useLoaderData<typeof loader>();
+
+  const robotPics = useMemo(
+    () =>
+      media
+        .filter((m) => m.type === 'imgur')
+        .sort((a, b) => {
+          if (a.preferred) {
+            return -1;
+          }
+          if (b.preferred) {
+            return 1;
+          }
+
+          return 0;
+        }),
+    [media],
+  );
 
   return (
-    <div>
-      <div className="text-3xl font-semibold">
-        Team {team.team_number} - {team.nickname}
+    <div className="flex flex-wrap justify-center">
+      <div className="basis-1/2">
+        <div className="text-3xl font-semibold">
+          Team {team.team_number} - {team.nickname}
+        </div>
+
+        <InlineIcon>
+          <BiPinMapFill />
+          {team.city}, {team.state_prov}, {team.country}
+        </InlineIcon>
+
+        <InlineIcon displayStyle={'flexless'}>
+          <BiInfoCircleFill />
+          {team.name}
+        </InlineIcon>
+
+        <InlineIcon>
+          <BiCalendar />
+          Rookie Year: {team.rookie_year}
+        </InlineIcon>
+
+        <InlineIcon>
+          <BiLink />
+          Details on{' '}
+          <Link
+            to={`https://frc-events.firstinspires.org/team/${team.team_number}`}
+          >
+            FRC Events
+          </Link>
+        </InlineIcon>
+
+        <InlineIcon>
+          <BiGraphUp />
+          <Link to={`https://www.statbotics.io/team/${team.team_number}`}>
+            Statbotics
+          </Link>
+        </InlineIcon>
+      </div>
+      <div className="">
+        <TeamRobotPicsCarousel media={robotPics} />
       </div>
     </div>
   );
