abd999e553ef438b2e5cd1e4951d2c1d49bd7177
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-12 20:44:26 -0700

    More groups

--- pwa/app/routes/events.$year.tsx
+++ pwa/app/routes/events.$year.tsx
@@ -42,30 +42,83 @@ type EventGroup = {
   events: Event[];
 };
 
-function groupByWeek(events: Event[]): EventGroup[] {
+function groupBySections(events: Event[]): EventGroup[] {
+  // TODO: Handle 2021 remote events
   const eventsByWeek: Map<number, EventGroup> = new Map();
+  const eventsByChampionship: Map<string, EventGroup> = new Map();
+  const FOCEvents: EventGroup = {
+    groupName: 'FIRST Festival of Champions',
+    events: [],
+  };
+  const preaseasonEvents: EventGroup = { groupName: 'Preseason', events: [] };
+  const offseasonEvents: EventGroup = { groupName: 'Offseason', events: [] };
   events.forEach((event) => {
-    if (event.week == null) {
-      return;
+    // Events by week
+    if (event.week != null) {
+      const weekGroup = eventsByWeek.get(event.week);
+      if (weekGroup) {
+        weekGroup.events.push(event);
+      } else {
+        eventsByWeek.set(event.week, {
+          groupName: `Week ${event.week + 1}`,
+          events: [event],
+        });
+      }
     }
-    const weekGroup = eventsByWeek.get(event.week);
-    if (weekGroup) {
-      weekGroup.events.push(event);
-    } else {
-      eventsByWeek.set(event.week, {
-        groupName: `Week ${event.week + 1}`,
-        events: [event],
-      });
+
+    // Events by Championship
+    if (event.event_type == 3 || event.event_type == 4) {
+      const groupName =
+        event.year >= 2017 && event.year <= 2022
+          ? `FIRST Championship - ${event.city}`
+          : 'FIRST Championship';
+      const championshipGroup = eventsByChampionship.get(groupName);
+      if (championshipGroup) {
+        championshipGroup.events.push(event);
+      } else {
+        eventsByChampionship.set(groupName, {
+          groupName,
+          events: [event],
+        });
+      }
+    }
+
+    // FOC
+    if (event.event_type == 6) {
+      FOCEvents.events.push(event);
+    }
+
+    // Preaseason
+    if (event.event_type == 100) {
+      preaseasonEvents.events.push(event);
+    }
+
+    // Offseason
+    if (event.event_type == 99) {
+      offseasonEvents.events.push(event);
     }
   });
-  return Array.from(eventsByWeek.values());
+
+  const groups = Array.from(eventsByWeek.values()).concat(
+    Array.from(eventsByChampionship.values()),
+  );
+  if (FOCEvents.events.length > 0) {
+    groups.push(FOCEvents);
+  }
+  if (preaseasonEvents.events.length > 0) {
+    groups.push(preaseasonEvents);
+  }
+  if (offseasonEvents.events.length > 0) {
+    groups.push(offseasonEvents);
+  }
+  return groups;
 }
 
 export default function YearEventsPage() {
   const { year, events } = useLoaderData<typeof loader>();
 
   const sortedEvents = events.sort(sortEventsComparator);
-  const eventsByWeek = groupByWeek(sortedEvents);
+  const groupedEvents = groupBySections(sortedEvents);
 
   return (
     <div className="flex flex-wrap gap-4 lg:flex-nowrap">
@@ -74,7 +127,7 @@ export default function YearEventsPage() {
         <h1 className="mb-2.5 mt-5 text-4xl">
           {year} <i>FIRST</i> Robotics Competition Events
         </h1>
-        {eventsByWeek.map((group) => (
+        {groupedEvents.map((group) => (
           <div key={group.groupName}>
             <h2 className="text-2xl mt-5">
               {group.groupName}{' '}
