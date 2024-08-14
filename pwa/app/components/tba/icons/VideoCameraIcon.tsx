3ed858d527153e52e19d54366078f24623478974
Emile Hamwey <ehamwey@procads.net>
2024-08-13 21:10:35 -0400

    remove unneeded icons

--- pwa/app/components/tba/icons/VideoCameraIcon.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import type { SVGProps } from 'react';
-
-export function VideoCameraIcon(props: SVGProps<SVGSVGElement>) {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" {...props}><path fill="currentColor" fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2z"></path></svg>);
-}
