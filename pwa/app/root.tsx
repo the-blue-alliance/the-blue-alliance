ffbc25cc14c4b3da21f02db988a73bba58a57346
Justin <7595639+tervay@users.noreply.github.com>
2024-08-13 21:39:21 -0400

    [PWA] Test out credenza (dialog+drawer) for teams at event list (#6248)

--- pwa/app/root.tsx
+++ pwa/app/root.tsx
@@ -50,7 +50,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div className="container mx-auto text-sm">{children}</div>
+        <div className="container mx-auto text-sm">
+          <div vaul-drawer-wrapper="" className="bg-background">
+            {children}
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
