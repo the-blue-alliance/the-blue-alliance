9b9bf8236c3b593765e26391b70a5bfb1758cb24
Justin Tervay <justin@tervay.com>
2024-08-15 19:39:13 -0400

    [PWA] Add basic insights route

---
+++ pwa/app/lib/insightUtils.ts
@@ -0,0 +1,8 @@
+export const NAME_TO_DISPLAY_NAME: Record<string, string> = {
+  typed_leaderboard_blue_banners: 'Most Blue Banners',
+  typed_leaderboard_most_matches_played: 'Most Matches Played',
+  typed_leaderboard_highest_median_score_by_event: 'Highest Median Score',
+  typed_leaderboard_highest_match_clean_score: 'Highest Clean Score',
+  typed_leaderboard_highest_match_clean_combined_score:
+    'Highest Clean Combined Score',
+};
