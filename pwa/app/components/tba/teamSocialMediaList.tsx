e37e191668b42f46591815ccc48fcb6dc9f65400
Justin Tervay <justin@tervay.com>
2024-08-18 13:05:54 -0400

    [PWA] Add team socials to page, minor styling tweaks

---
+++ pwa/app/components/tba/teamSocialMediaList.tsx
@@ -0,0 +1,112 @@
+import { Link } from '@remix-run/react';
+
+import LogosFacebook from '~icons/logos/facebook';
+import LogosGithubIcon from '~icons/logos/github-icon';
+import LogosGitlab from '~icons/logos/gitlab';
+import LogosInstagramIcon from '~icons/logos/instagram-icon';
+import LogosYoutubeIcon from '~icons/logos/youtube-icon';
+import SimpleIconsX from '~icons/simple-icons/x';
+
+import { Media } from '~/api/v3';
+import InlineIcon from '~/components/tba/inlineIcon';
+import { badgeVariants } from '~/components/ui/badge';
+import { cn } from '~/lib/utils';
+
+function SingleSocialIcon({
+  media,
+  className,
+}: {
+  media: Media;
+  className: string;
+}) {
+  switch (media.type) {
+    case 'youtube':
+    case 'youtube-channel':
+      return (
+        <Link
+          to={`https://www.youtube.com/${media.foreign_key}`}
+          className={className}
+        >
+          <InlineIcon>
+            <LogosYoutubeIcon />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    case 'facebook-profile':
+      return (
+        <Link
+          to={`https://www.facebook.com/${media.foreign_key}`}
+          className={className}
+        >
+          <InlineIcon>
+            <LogosFacebook />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    case 'github-profile':
+      return (
+        <Link
+          to={`https://github.com/${media.foreign_key}`}
+          className={className}
+        >
+          <InlineIcon>
+            <LogosGithubIcon />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    case 'instagram-profile':
+      return (
+        <Link
+          to={`https://www.instagram.com/${media.foreign_key}`}
+          className={className}
+        >
+          <InlineIcon>
+            <LogosInstagramIcon />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    case 'twitter-profile':
+      return (
+        <Link to={`https://x.com/${media.foreign_key}`} className={className}>
+          <InlineIcon>
+            <SimpleIconsX />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    case 'gitlab-profile':
+      return (
+        <Link
+          to={`https://gitlab.com/${media.foreign_key}`}
+          className={className}
+        >
+          <InlineIcon>
+            <LogosGitlab />
+            {media.foreign_key}
+          </InlineIcon>
+        </Link>
+      );
+    default:
+      return null;
+  }
+}
+
+export default function TeamSocialMediaList({ socials }: { socials: Media[] }) {
+  socials.sort((a, b) => a.type.localeCompare(b.type));
+
+  return (
+    <>
+      {socials.map((m) => (
+        <SingleSocialIcon
+          media={m}
+          key={`${m.type}-${m.foreign_key}`}
+          className={cn(badgeVariants({ variant: 'secondary' }), 'm-0.5')}
+        />
+      ))}
+    </>
+  );
+}
--- pwa/app/routes/team.$teamNumber.tsx
+++ pwa/app/routes/team.$teamNumber.tsx
@@ -6,6 +6,7 @@ import {
   Params,
   json,
   useLoaderData,
+  useNavigate,
 } from '@remix-run/react';
 import { useMemo } from 'react';
 
@@ -15,10 +16,17 @@ import BiInfoCircleFill from '~icons/bi/info-circle-fill';
 import BiLink from '~icons/bi/link';
 import BiPinMapFill from '~icons/bi/pin-map-fill';
 
-import { getTeam, getTeamMediaByYear } from '~/api/v3';
+import {
+  getTeam,
+  getTeamEventsByYear,
+  getTeamMediaByYear,
+  getTeamSocialMedia,
+  getTeamYearsParticipated,
+} from '~/api/v3';
 import InlineIcon from '~/components/tba/inlineIcon';
 import TeamAvatar from '~/components/tba/teamAvatar';
 import TeamRobotPicsCarousel from '~/components/tba/teamRobotPicsCarousel';
+import TeamSocialMediaList from '~/components/tba/teamSocialMediaList';
 import {
   Accordion,
   AccordionContent,
@@ -26,6 +34,20 @@ import {
   AccordionTrigger,
 } from '~/components/ui/accordion';
 import { Badge } from '~/components/ui/badge';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { Separator } from '~/components/ui/separator';
+import {
+  TableOfContentsItem,
+  TableOfContentsLink,
+  TableOfContentsList,
+} from '~/components/ui/toc';
+import { sortEventsComparator } from '~/lib/eventUtils';
 import {
   attemptToParseSchoolNameFromOldTeamName,
   attemptToParseSponsors,
@@ -40,20 +62,35 @@ async function loadData(params: Params) {
   // todo: add year support
   const year = 2024;
 
-  const [team, media] = await Promise.all([
+  const [team, media, socials, yearsParticipated, events] = await Promise.all([
     getTeam({ teamKey }),
     getTeamMediaByYear({ teamKey, year }),
+    getTeamSocialMedia({ teamKey }),
+    getTeamYearsParticipated({ teamKey }),
+    getTeamEventsByYear({ teamKey, year }),
   ]);
 
   if (team.status === 404) {
     throw new Response(null, { status: 404 });
   }
 
-  if (team.status !== 200 || media.status !== 200) {
+  if (
+    team.status !== 200 ||
+    media.status !== 200 ||
+    socials.status !== 200 ||
+    yearsParticipated.status !== 200 ||
+    events.status !== 200
+  ) {
     throw new Response(null, { status: 500 });
   }
 
-  return { team: team.data, media: media.data };
+  return {
+    team: team.data,
+    media: media.data,
+    socials: socials.data,
+    yearsParticipated: yearsParticipated.data,
+    events: events.data,
+  };
 }
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -79,7 +116,13 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 };
 
 export default function TeamPage(): JSX.Element {
-  const { team, media } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const { team, media, socials, yearsParticipated, events } =
+    useLoaderData<typeof loader>();
+
+  events.sort(sortEventsComparator);
+
+  yearsParticipated.sort((a, b) => b - a);
 
   const robotPics = useMemo(
     () =>
@@ -108,73 +151,123 @@ export default function TeamPage(): JSX.Element {
     team.school_name ?? attemptToParseSchoolNameFromOldTeamName(team.name);
 
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="basis-1/2">
-        <h1 className="mb-2.5 mt-5 text-4xl">
-          {maybeAvatar && <TeamAvatar media={maybeAvatar} />}
-          Team {team.team_number} - {team.nickname}
-        </h1>
-
-        <InlineIcon>
-          <BiPinMapFill />
-          {team.city}, {team.state_prov}, {team.country}
-        </InlineIcon>
-
-        {sponsors.length > 0 ? (
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1" className="border-0">
-              <AccordionTrigger className="p-0 text-left font-normal">
-                <InlineIcon displayStyle={'flexless'}>
-                  <BiInfoCircleFill />
-                  {schoolName}
-                  {sponsors.length > 0 && ` with ${sponsors.length} sponsor`}
-                  {sponsors.length > 1 && 's'}
-                </InlineIcon>
-              </AccordionTrigger>
-              <AccordionContent className="pb-0">
-                {sponsors.map((sponsor, i) => (
-                  <Badge
-                    className="m-px font-normal"
-                    key={i}
-                    variant={'secondary'}
-                  >
-                    {sponsor}
-                  </Badge>
-                ))}
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        ) : (
-          <InlineIcon displayStyle={'flexless'}>
-            <BiInfoCircleFill />
-            {schoolName}
-          </InlineIcon>
-        )}
-
-        <InlineIcon>
-          <BiCalendar />
-          Rookie Year: {team.rookie_year}
-        </InlineIcon>
-
-        <InlineIcon>
-          <BiLink />
-          Details on{' '}
-          <Link
-            to={`https://frc-events.firstinspires.org/team/${team.team_number}`}
-          >
-            FRC Events
-          </Link>
-        </InlineIcon>
-
-        <InlineIcon>
-          <BiGraphUp />
-          <Link to={`https://www.statbotics.io/team/${team.team_number}`}>
-            Statbotics
-          </Link>
-        </InlineIcon>
+    <div className="flex flex-wrap sm:flex-nowrap">
+      <div className="top-0 mr-4 pt-5 sm:sticky">
+        <Select
+          onValueChange={(value) => {
+            navigate(`/team/${team.team_number}/${value}`);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={yearsParticipated[0]} />
+          </SelectTrigger>
+          <SelectContent>
+            {yearsParticipated.map((y) => (
+              <SelectItem key={y} value={`${y}`}>
+                {y}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <TableOfContentsList className="mt-5">
+          {events.map((e) => (
+            <TableOfContentsItem key={e.key}>
+              <TableOfContentsLink
+                to="#todo_implement_me"
+                replace={true}
+                // isActive={inView.has(group.slug)}
+              >
+                {e.short_name}
+              </TableOfContentsLink>
+            </TableOfContentsItem>
+          ))}
+        </TableOfContentsList>
       </div>
-      <div className="">
-        <TeamRobotPicsCarousel media={robotPics} />
+
+      <div className="mt-5 w-full">
+        <div className="flex flex-wrap justify-center sm:flex-nowrap sm:justify-between">
+          <div className="">
+            <h1 className="mb-2.5 text-4xl">
+              {maybeAvatar && <TeamAvatar media={maybeAvatar} />}
+              Team {team.team_number} - {team.nickname}
+            </h1>
+            <InlineIcon>
+              <BiPinMapFill />
+              <a
+                href={`https://maps.google.com/maps?q=${team.city}, ${team.state_prov}, ${team.country}`}
+              >
+                {team.city}, {team.state_prov}, {team.country}
+              </a>
+            </InlineIcon>
+
+            {sponsors.length > 0 ? (
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1" className="border-0">
+                  <AccordionTrigger className="justify-normal p-0 text-left font-normal">
+                    <InlineIcon displayStyle={'flexless'}>
+                      <BiInfoCircleFill />
+                      {schoolName}
+                      {sponsors.length > 0 &&
+                        ` with ${sponsors.length} sponsor`}
+                      {sponsors.length > 1 && 's'}
+                    </InlineIcon>
+                  </AccordionTrigger>
+                  <AccordionContent className="pb-0">
+                    {sponsors.map((sponsor, i) => (
+                      <Badge
+                        className="m-px font-normal"
+                        key={i}
+                        variant={'secondary'}
+                      >
+                        {sponsor}
+                      </Badge>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            ) : (
+              <InlineIcon displayStyle={'flexless'}>
+                <BiInfoCircleFill />
+                {schoolName}
+              </InlineIcon>
+            )}
+
+            <InlineIcon>
+              <BiCalendar />
+              Rookie Year: {team.rookie_year}
+            </InlineIcon>
+
+            <InlineIcon>
+              <BiLink />
+              Details on{' '}
+              <Link
+                to={`https://frc-events.firstinspires.org/team/${team.team_number}`}
+              >
+                FRC Events
+              </Link>
+            </InlineIcon>
+
+            <InlineIcon>
+              <BiGraphUp />
+              <Link to={`https://www.statbotics.io/team/${team.team_number}`}>
+                Statbotics
+              </Link>
+            </InlineIcon>
+
+            <div className="flex flex-wrap justify-center md:justify-start">
+              <TeamSocialMediaList socials={socials} />
+            </div>
+          </div>
+          <div className="flex-none">
+            <TeamRobotPicsCarousel media={robotPics} />
+          </div>
+        </div>
+
+        <Separator className="my-2" />
+
+        <div>
+          <h1 className=" text-4xl">Event Results</h1>
+        </div>
       </div>
     </div>
   );
