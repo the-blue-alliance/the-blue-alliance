ea88c2b932f8aad43631761d3da564d3de7cfe5c
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-20 22:14:09 -0700

    Use fixed

--- pwa/app/components/tba/globalLoadingProgress.tsx
+++ pwa/app/components/tba/globalLoadingProgress.tsx
@@ -47,9 +47,6 @@ export default function GlobalLoadingProgress() {
     return null;
   }
   return (
-    <Progress
-      className="absolute top-0 z-20 h-1 rounded-none"
-      value={progress}
-    />
+    <Progress className="fixed top-0 z-20 h-1 rounded-none" value={progress} />
   );
 }
