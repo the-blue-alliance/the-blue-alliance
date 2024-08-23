ed27b6dee932d7d26bb30748eca79cf51c196b7f
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-22 22:39:14 -0400

    [PWA] Base match page (#6355)

---
+++ pwa/app/lib/matchUtils.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+
+import { isValidMatchKey } from './matchUtils';
+
+describe.concurrent('isValidMatchKey', () => {
+  test.each([
+    '2019nyny_qm1',
+    '2010ct_sf1m3',
+    '2022on306_qm15',
+    '2023week0_sf13m1',
+    '2023bc_ef10m1',
+    '2023bc_qf10m1',
+  ])('valid match key', (key) => {
+    expect(isValidMatchKey(key)).toBe(true);
+  });
+
+  test.each([
+    'frc177',
+    '2010ct_qm1m1',
+    '2010ctf1m1',
+    '2010ct_f1',
+    '2022on_306_qm15',
+    '2023week0_sf130m1',
+    '2023bc_f10m1',
+    '2023bc_ef123m1',
+    '2023bc_qf123m1',
+  ])('invalid match key', (key) => {
+    expect(isValidMatchKey(key)).toBe(false);
+  });
+});
