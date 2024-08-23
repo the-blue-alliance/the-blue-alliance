f039a1f9895ddf42b55fd150ec90aeea784da171
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2024-08-22 18:56:04 -0700

    Base match page

---
+++ pwa/app/routes/match.$matchKey.tsx
@@ -0,0 +1,88 @@
+import { LoaderFunctionArgs, json } from '@remix-run/node';
+import {
+  type ClientLoaderFunctionArgs,
+  Link,
+  MetaFunction,
+  Params,
+  useLoaderData,
+} from '@remix-run/react';
+
+import { getEvent, getMatch } from '~/api/v3';
+import { isValidMatchKey, matchTitleShort } from '~/lib/matchUtils';
+
+async function loadData(params: Params) {
+  if (params.matchKey === undefined) {
+    throw new Error('Missing matchKey');
+  }
+
+  if (!isValidMatchKey(params.matchKey)) {
+    throw new Response(null, {
+      status: 404,
+    });
+  }
+
+  const eventKey = params.matchKey.split('_')[0];
+
+  const [event, match] = await Promise.all([
+    getEvent({ eventKey }),
+    getMatch({ matchKey: params.matchKey }),
+  ]);
+
+  if (event.status == 404 || match.status == 404) {
+    throw new Response(null, {
+      status: 404,
+    });
+  }
+
+  if (event.status !== 200 || match.status !== 200) {
+    throw new Response(null, {
+      status: 500,
+    });
+  }
+
+  return {
+    event: event.data,
+    match: match.data,
+  };
+}
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  return json(await loadData(params));
+}
+
+export async function clientLoader({ params }: ClientLoaderFunctionArgs) {
+  return await loadData(params);
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  const match = data?.match;
+  const event = data?.event;
+  if (!match || !event) {
+    return;
+  }
+  return [
+    {
+      title: `${matchTitleShort(match, event)} - ${event.name} (${event.year}) - The Blue Alliance`,
+    },
+    {
+      name: 'description',
+      content: `${matchTitleShort(match, event)} at the ${event.year} ${event.name} FIRST Robotics Competition in ${event.city}, ${event.state_prov}, ${event.country}`,
+    },
+  ];
+};
+
+export default function EventPage() {
+  const { event, match } = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <h1 className="mt-5 text-4xl">
+        {matchTitleShort(match, event)}{' '}
+        <small className="text-xl">
+          <Link to={`/event/${event.key}`}>
+            {event.name} {event.year}
+          </Link>
+        </small>
+      </h1>
+    </div>
+  );
+}
