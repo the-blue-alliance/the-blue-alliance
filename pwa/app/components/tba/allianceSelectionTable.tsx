7dbb09f8707ea5ef2bc9baa04eb6d631a78c1d5e
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2024-10-24 17:03:30 -0600

    Fixed #6378

--- pwa/app/components/tba/allianceSelectionTable.tsx
+++ pwa/app/components/tba/allianceSelectionTable.tsx
@@ -74,7 +74,10 @@ export default function AllianceSelectionTable(props: {
             <TableHead>Captain</TableHead>
             {[...Array(allianceSize - 1).keys()].map((i) => (
               <TableHead key={i}>Pick {i + 1}</TableHead>
+            ))}{allianceSize > 1 && [...Array(allianceSize - 1).keys()].map((i) => (
+              <TableHead key={i}>Pick {i + 1}</TableHead>
             ))}
+            
           </TableRow>
         </TableHeader>
         <TableBody>
