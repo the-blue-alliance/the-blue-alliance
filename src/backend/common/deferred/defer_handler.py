3e8d8a98f16d3648dc697733ed9825db157369f2
Phil Lopreiato <plnyyanks@gmail.com>
2025-02-17 18:19:20 -0500

    Implement deferred with b64

--- src/backend/common/deferred/defer_handler.py
+++ src/backend/common/deferred/defer_handler.py
@@ -1,3 +1,6 @@
+import base64
+import io
+
 from flask import Flask, request, Response
 from google.appengine.ext import deferred
 
@@ -7,8 +10,23 @@ from backend.common.url_converters import (
 )
 
 
+def _decode_deferred_payload(environ):
+    try:
+        request_body_size = int(environ.get("CONTENT_LENGTH", 0))
+    except ValueError:
+        request_body_size = 0
+
+    request_body = environ["wsgi.input"].read(request_body_size)
+
+    decoded_body = base64.b64decode(request_body)
+    environ["CONTENT_LENGTH"] = len(decoded_body)
+    environ["wsgi.input"] = io.BytesIO(decoded_body)
+    return environ
+
+
 def handle_defer(path: str) -> Response:
-    return deferred.application.post(request.environ)
+    updated_environ = _decode_deferred_payload(request.environ)
+    return deferred.application.post(updated_environ)
 
 
 def install_defer_routes(app: Flask) -> None:
