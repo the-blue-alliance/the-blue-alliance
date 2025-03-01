152dfb9472d367f7cf0428bbdfaeaee7437e4796
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-03-01 02:34:54 -0500

    Move gcloud deploy off beta deploy. Add app_engine_apis flag to default.yaml (#7211)

--- ops/deploy/deploy_module.sh
+++ ops/deploy/deploy_module.sh
@@ -3,8 +3,4 @@ set -e
 
 SERVICE=$1
 
-# We need gcloud beta to set up the VPC connector
-gcloud components install beta
-
-# Actualy run the deploy
-gcloud beta app deploy "$SERVICE" --version 1 --quiet
+gcloud app deploy "$SERVICE" --version 1 --quiet
