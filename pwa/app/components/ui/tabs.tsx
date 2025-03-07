cb1ce7a31200b50f78c5cf907289e05a5e2b70f7
Justin <7595639+tervay@users.noreply.github.com>
2025-03-06 20:20:45 -0500

    [pwa] Add pointer cursor to clickable tabs (#7276)
    Add pointer cursor to clickable tabs

--- pwa/app/components/ui/tabs.tsx
+++ pwa/app/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-5 py-1.5
-      text-sm font-medium ring-offset-background transition-all
+      text-sm font-medium ring-offset-background transition-all cursor-pointer
       focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring
       focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
       data-[state=active]:bg-background data-[state=active]:text-foreground
