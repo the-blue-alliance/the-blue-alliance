28aae1aa49bc5a994f4c72b620f4b51c8b6496e5
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-03-11 21:57:19 -0400

    Add secret key setup to PWA auth

--- pwa/app/firebase/firebaseConfig.tsx
+++ pwa/app/firebase/firebaseConfig.tsx
@@ -4,11 +4,7 @@ import { connectAuthEmulator, getAuth } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 let app;
