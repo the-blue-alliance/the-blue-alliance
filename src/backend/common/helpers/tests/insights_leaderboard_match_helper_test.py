10a880f0e0531d73178ea123f45f36f93ea76da7
Justin Tervay <justin@tervay.com>
2025-04-07 18:32:58 -0400

    Fix coral leaderboard to not double-count auton

--- src/backend/common/helpers/tests/insights_leaderboard_match_helper_test.py
+++ src/backend/common/helpers/tests/insights_leaderboard_match_helper_test.py
@@ -53,22 +53,23 @@ def test_highest_clean_combined_score(ndb_stub, test_data_importer):
 def test_2025_most_coral_scored(ndb_stub, test_data_importer):
     test_data_importer.import_event(__file__, "data/2025isde1.json")
     test_data_importer.import_match_list(__file__, "data/2025isde1_matches.json")
+    test_data_importer.import_event(__file__, "data/2025oncmp2.json")
+    test_data_importer.import_match_list(__file__, "data/2025oncmp2_matches.json")
 
     insight = InsightsLeaderboardMatchHelper._2025_most_coral_scored(
         LeaderboardInsightArguments(
-            events=[Event.get_by_id("2025isde1")],
+            events=[Event.get_by_id("2025isde1"), Event.get_by_id("2025oncmp2")],
             year=2025,
         )
     )
 
     assert insight is not None
-
     assert insight.data["rankings"][:5] == [
-        {"keys": ["2025isde1_sf1m1"], "value": 38},
-        {"keys": ["2025isde1_f1m1"], "value": 37},
-        {"keys": ["2025isde1_f1m2", "2025isde1_qm19"], "value": 33},
-        {"keys": ["2025isde1_qm32", "2025isde1_qm33"], "value": 31},
-        {"keys": ["2025isde1_qm36"], "value": 30},
+        {"keys": ["2025oncmp2_f1m1", "2025oncmp2_sf7m1"], "value": 48},
+        {"keys": ["2025oncmp2_qm95", "2025oncmp2_sf11m1"], "value": 47},
+        {"keys": ["2025oncmp2_sf3m1"], "value": 46},
+        {"keys": ["2025oncmp2_f1m2", "2025oncmp2_qm56"], "value": 45},
+        {"keys": ["2025oncmp2_qm74"], "value": 44},
     ]
     assert insight.data["key_type"] == "match"
 
