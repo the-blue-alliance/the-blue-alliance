9c76bb985e71f850d5f64bda90ba9348107e7e0a
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-05-18 21:11:53 -0400

    Debugging ci pyre (#7712)
    * Debugging ci pyre
    
    * yolo?
    
    * Run just pyre and have pyre figure out incremental or check

--- ops/typecheck_py3.sh
+++ ops/typecheck_py3.sh
@@ -5,8 +5,4 @@ if [ -n "$CI" ]; then
     echo "::add-matcher::./ops/problem_matchers/pyre_error.json"
 fi
 
-if ! command -v watchman &>/dev/null; then
-    pyre check
-else
-    pyre incremental
-fi
+pyre
