9395d7a99ec5425a15db9fc8b56c78534820680b
Phil Lopreiato <plnyyanks@gmail.com>
2025-05-22 23:14:07 -0400

    include cert env var in dev_appserver

--- ops/dev/vagrant/dev_appserver.sh
+++ ops/dev/vagrant/dev_appserver.sh
@@ -100,6 +100,7 @@ dev_appserver.py \
     --runtime="$runtime_version" \
     --application="$application" \
     "${env[@]}" \
+    --env_var HTTPLIB2_CA_CERTS="/usr/lib/google-cloud-sdk/platform/google_appengine/lib/httplib2/httplib2/cacerts.txt" \
     --env_var TBA_LOG_LEVEL="$tba_log_level" \
     --env_var NDB_LOG_LEVEL="$ndb_log_level" \
     --env_var STORAGE_MODE="$storage_mode" \
