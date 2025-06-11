e8a4f775fc640ef751dffdbad5bc671e3cdb7861
GrahamSH <grahamshllk@gmail.com>
2025-06-11 23:41:29 +0000

    Add file copying infrastructure

--- pwa/generate-api.sh
+++ pwa/generate-api.sh
@@ -16,3 +16,5 @@ import { AwardType } from \"~/lib/api/AwardType\";\
 import { EventType } from \"~/lib/api/EventType\";' app/api/tba/types.gen.ts"
 
 npx prettier --write app/api/tba/
+
+cp -r ../src/backend/web/static/swagger/ ./public/swagger/
