92d4bcfc57ca4e5cc34f31ed4c3a30ee27adce65
GrahamSH <grahamshllk@gmail.com>
2025-06-11 23:41:26 +0000

    do a cleanup pass on frontend

--- pwa/app/routes/apidocs.$doc.($version).tsx
+++ pwa/app/routes/apidocs.$doc.($version).tsx
@@ -1,11 +1,10 @@
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 import '@scalar/api-reference-react/style.css';
-import { useEffect, useRef } from 'react';
 import { useLoaderData } from 'react-router';
 
-import { Route } from './+types/apidocs.$doc.($version)';
+import { Route } from '.react-router/types/app/routes/+types/apidocs.$doc.($version)';
 
-async function loadData(params: Route.LoaderArgs['params']) {
+function loadData(params: Route.LoaderArgs['params']) {
   if (!params.doc) {
     throw new Response(null, {
       status: 404,
@@ -20,24 +19,20 @@ async function loadData(params: Route.LoaderArgs['params']) {
     });
   }
 }
-export async function loader({ params }: Route.LoaderArgs) {
-  return await loadData(params);
+export function loader({ params }: Route.LoaderArgs) {
+  return loadData(params);
 }
 
-export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  return await loadData(params);
+export function clientLoader({ params }: Route.ClientLoaderArgs) {
+  return loadData(params);
 }
 
 export default function ApiDocsV3(): React.JSX.Element {
   const { path } = useLoaderData<typeof loader>();
-  let ref = useRef<HTMLDivElement>(null);
-
-  //useDelegatedReactRouterLinks(ref);
 
   return (
     <>
       <div
-        ref={ref}
         style={
           {
             '--scalar-custom-header-height': '56px',
