50716bc1f0ed4c370be2f25db753fd0a9f50abb1
Brennan Bibic <brennan.bibic@gmail.com>
2025-07-24 10:15:30 -0400

    [PWA] Visual fixes for tool tips (#7996)
    * Make readable+add scrollbar for massive tooltips
    
    * visual cleanup
    
    * fix z index bug with arrow rendering above tooltip content

--- pwa/app/components/ui/tooltip.tsx
+++ pwa/app/components/ui/tooltip.tsx
@@ -44,9 +44,9 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          `z-50 w-fit origin-(--radix-tooltip-content-transform-origin)
-          animate-in rounded-md bg-primary px-3 py-1.5 text-xs text-balance
-          text-primary-foreground fade-in-0 zoom-in-95
+          `z-50 w-fit max-w-xs origin-(--radix-tooltip-content-transform-origin)
+          animate-in rounded-md border bg-popover text-xs
+          text-popover-foreground fade-in-0 zoom-in-95
           data-[side=bottom]:slide-in-from-top-2
           data-[side=left]:slide-in-from-right-2
           data-[side=right]:slide-in-from-left-2
@@ -54,12 +54,24 @@ function TooltipContent({
           data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95`,
           className,
         )}
+        style={{
+          filter:
+            'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))',
+        }}
         {...props}
       >
-        {children}
+        <div
+          className="relative z-10 max-h-60 overflow-x-hidden overflow-y-auto
+            px-3 py-1.5 text-center text-balance [&::-webkit-scrollbar]:w-2
+            [&::-webkit-scrollbar-thumb]:rounded-full
+            [&::-webkit-scrollbar-thumb]:bg-muted-foreground/30
+            [&::-webkit-scrollbar-track]:bg-transparent"
+        >
+          {children}
+        </div>
         <TooltipPrimitive.Arrow
-          className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45
-            rounded-[2px] bg-primary fill-primary"
+          className="relative -z-10 size-3 translate-y-[calc(-50%_-_2px)]
+            rotate-45 rounded-[2px] bg-popover fill-popover"
         />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
