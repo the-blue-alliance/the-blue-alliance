630d8d71de7d19736eed704c505c854a558160ec
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2025-08-08 17:23:20 -0700

    Track owner and description in apiv3 (#8065)
    [clowntown]

--- src/backend/api/handlers/helpers/track_call.py
+++ src/backend/api/handlers/helpers/track_call.py
@@ -11,13 +11,16 @@ def track_call_after_response(
     """
     Schedules a callback to Google Analytics to track an API call.
     """
-    # Save |auth_owner_id| while we stil have access to the flask request context.
+    # Save |auth_owner_id| and |auth_description| while we stil have access to the flask request context.
     auth_owner_id = g.auth_owner_id if hasattr(g, "auth_owner_id") else None
+    auth_description = g.auth_description if hasattr(g, "auth_description") else None
+
     if model_type is not None:
         api_action += f"/{model_type}"
 
     params = {
         "client_id": f"_{auth_owner_id}",  # Force this to be non-numeric so GA doesn't try to handle it as a number
+        "onwer_description": f"{auth_owner_id}:{auth_description}",
         "action": api_action,
         "label": api_label,
     }
