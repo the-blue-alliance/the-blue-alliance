2af7bdc53fee2b85fe42f52ad743ae44f5e32df5
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-12-01 14:09:08 -0500

    Use `firebase emulators:exec` for importing Firebase Auth users (#8462)
    Use firebase emulators:exec for importing Firebase Auth users

--- ops/dev/vagrant/run_firebase_emulator.sh
+++ ops/dev/vagrant/run_firebase_emulator.sh
@@ -5,11 +5,6 @@ source ops/dev/vagrant/config.sh
 
 project=$(project)
 
-firebase emulators:start --project="$project" &
+firebase emulators:exec --project="$project" --import=.firebase-data --export-on-exit=.firebase-data "python ops/dev/vagrant/create_auth_emulator_accounts.py --project='$project'"
 
-# Give the emualtor a second to boot before inserting users
-sleep 500
-
-python ops/dev/vagrant/create_auth_emulator_accounts.py --project="$project"
-
-fg
+firebase emulators:start --project="$project" --import=.firebase-data --export-on-exit=.firebase-data
