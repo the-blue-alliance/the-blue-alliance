9ca5e6bfc0b63ac91f0b78783818ef33b60b454a
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2025-12-02 18:40:25 -0500

    [eventwizard] Add FMS Alliances tab (#8500)

---
+++ src/frontend/eventwizard/containers/FmsAlliancesTabContainer.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import FmsAlliancesTab from "../components/fmsAlliancesTab/FmsAlliancesTab";
+import makeTrustedApiRequest from "../net/TrustedApiRequest";
+
+const mapStateToProps = (state) => ({
+  selectedEvent: state.auth.selectedEvent,
+  makeTrustedRequest: (requestPath, requestBody, onSuccess, onError) => {
+    makeTrustedApiRequest(
+      state.auth.authId,
+      state.auth.authSecret,
+      requestPath,
+      requestBody,
+      onSuccess,
+      onError
+    );
+  },
+});
+
+export default connect(mapStateToProps)(FmsAlliancesTab);
