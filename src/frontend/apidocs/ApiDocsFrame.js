0dad3f8b657cb7612039b4e5059390d183c4b68e
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-12-02 00:15:35 -0500

    Deweird

--- src/frontend/apidocs/ApiDocsFrame.js
+++ src/frontend/apidocs/ApiDocsFrame.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect, useCallback, useMemo, memo } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import SwaggerUi from "swagger-ui";
+import SwaggerUI from "swagger-ui";
 
-const ShowChangesPlugin = (system) => {
+const ShowChangesPlugin = function (system) {
   return {
     wrapComponents: {
-      info: (Original) => (props) => {
-        const [isOpened, setIsOpened] = useState(false);
-        const changes = props.info.get("x-changes") || new Map();
+      InfoContainer: (Original, system) => (props) => {
+        const [isOpened, setIsOpened] = React.useState(false);
+        const changes = props.specSelectors.info().get("x-changes");
         const Collapse = system.getComponent("Collapse");
-
-        const toggleOpen = useCallback(() => {
-          setIsOpened((prev) => !prev);
-        }, []);
 
         return (
           <div>
@@ -20,8 +16,8 @@ const ShowChangesPlugin = (system) => {
             <h2>
               API Changelog{" "}
               <button
-                className="swagger-ui btn"
-                onClick={toggleOpen}
+                className="btn"
+                onClick={() => setIsOpened(!isOpened)}
                 aria-expanded={isOpened}
               >
                 {isOpened ? "Hide" : "Show"}
@@ -37,48 +33,49 @@ const ShowChangesPlugin = (system) => {
   };
 };
 
-const ChangesComponent = memo(({ changes }) => {
-  return changes.entrySeq().map((item) => {
-    const version = item[0];
-    const changes = item[1];
-    return (
-      <div key={version}>
-        <h3>{version}</h3>
-        <ul>
-          {changes.map((c, i) => (
-            <li key={i}>{c}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  });
-});
-
-ChangesComponent.displayName = "ChangesComponent";
+class ChangesComponent extends Component {
+  render() {
+    return this.props.changes.entrySeq().map((item) => {
+      const version = item[0];
+      const changes = item[1];
+      return (
+        <div key={version}>
+          <h3>{version}</h3>
+          <ul>
+            {changes.map((c, i) => (
+              <li key={i}>{c}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    });
+  }
+}
 
 ChangesComponent.propTypes = {
-  // Should be an Immutable.js Map with entrySeq and map methods
-  changes: PropTypes.object.isRequired,
+  changes: PropTypes.object,
 };
 
-const ApiDocsFrame = ({
-  url = "http://localhost:8080/swagger/api_v3.json",
-}) => {
-  const plugins = useMemo(() => [ShowChangesPlugin], []);
-
-  useEffect(() => {
-    SwaggerUi({
+class ApiDocsFrame extends Component {
+  componentDidMount() {
+    SwaggerUI({
       dom_id: "#swaggerContainer",
-      url,
-      plugins,
+      url: this.props.url,
+      plugins: [ShowChangesPlugin],
     });
-  }, [url, plugins]);
+  }
 
-  return <div id="swaggerContainer" />;
-};
+  render() {
+    return <div id="swaggerContainer" />;
+  }
+}
 
 ApiDocsFrame.propTypes = {
   url: PropTypes.string,
+};
+
+ApiDocsFrame.defaultProps = {
+  url: "http://localhost:8080/swagger/api_v3.json",
 };
 
 export default ApiDocsFrame;
