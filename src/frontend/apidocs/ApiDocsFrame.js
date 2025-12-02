8e51a2320155f5636fbceb28fa24673231c18e48
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-12-01 22:39:29 -0500

    Lint, PR feedback

--- src/frontend/apidocs/ApiDocsFrame.js
+++ src/frontend/apidocs/ApiDocsFrame.js
@@ -5,46 +5,59 @@ import SwaggerUi from "swagger-ui";
 const ShowChangesPlugin = function (system) {
   return {
     wrapComponents: {
-      info: (Original, system) => class WrappedNumberDisplay extends Component {
-        constructor(props) {
-          super(props);
+      info: (Original, system) =>
+        class WrappedNumberDisplay extends Component {
+          constructor(props) {
+            super(props);
 
-          this.state = { isOpened: false };
-          this.changes = props.info.get('x-changes');
-        }
+            this.state = { isOpened: false };
+            this.changes = props.info.get("x-changes");
+          }
 
-        render() {
-          const Collapse = system.getComponent("Collapse");
+          render() {
+            const Collapse = system.getComponent("Collapse");
 
-          return (
-            <div>
-              <Original {...this.props} />
-              <h2>API Changelog <button className="swagger-ui btn" onClick={() => this.setState({ isOpened: !this.state.isOpened })}>{this.state.isOpened ? "Hide" : "Show"}</button></h2>
-              <Collapse isOpened={this.state.isOpened}>
-                <ChangesComponent changes={this.changes} />
-              </Collapse>
-            </div>
-          );
-        }
-      }
-    }
-  }
-}
+            return (
+              <div>
+                <Original {...this.props} />
+                <h2>
+                  API Changelog{" "}
+                  <button
+                    className="swagger-ui btn"
+                    onClick={() =>
+                      this.setState({ isOpened: !this.state.isOpened })
+                    }
+                  >
+                    {this.state.isOpened ? "Hide" : "Show"}
+                  </button>
+                </h2>
+                <Collapse isOpened={this.state.isOpened}>
+                  <ChangesComponent changes={this.changes} />
+                </Collapse>
+              </div>
+            );
+          }
+        },
+    },
+  };
+};
 
 class ChangesComponent extends Component {
   render() {
-    return this.props.changes.map((changes, version) => {
+    return this.props.changes.entrySeq().map((item) => {
+      const version = item[0];
+      const changes = item[1];
       return (
-        <div>
+        <div key={version}>
           <h3>{version}</h3>
           <ul>
-            {changes.map((c) => {
-              return <li><md>{c}</md></li>
-            })}
+            {changes.map((c, i) => (
+              <li key={i}>{c}</li>
+            ))}
           </ul>
         </div>
       );
-    })
+    });
   }
 }
 
@@ -53,9 +66,7 @@ class ApiDocsFrame extends Component {
     SwaggerUi({
       dom_id: "#swaggerContainer",
       url: this.props.url,
-      plugins: [
-        ShowChangesPlugin
-      ]
+      plugins: [ShowChangesPlugin],
     });
   }
 
@@ -69,7 +80,7 @@ ApiDocsFrame.propTypes = {
 };
 
 ApiDocsFrame.defaultProps = {
-  url: "http://localhost:8080/swagger/api_v3.json"
+  url: "http://localhost:8080/swagger/api_v3.json",
 };
 
 export default ApiDocsFrame;
