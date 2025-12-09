5adc9066f880283ac7be7dfc612cdc6b4cfc5b50
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2025-12-09 15:13:57 -0800

    [pwa] Cleanup search data fetching and filtering (#8572)
    Cleanup search data fetching and filtering

--- pwa/app/lib/search/api.ts
+++
@@ -1,9 +0,0 @@
-import { SearchIndex } from '~/api/tba/read';
-
-export interface SearchDataProvider {
-  provide(): Promise<SearchIndex>;
-}
-
-export interface SearchDataFilterer {
-  filter(data: SearchIndex, query: string): SearchIndex;
-}
