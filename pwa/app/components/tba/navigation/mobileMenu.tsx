6b65f5f310f64375545ecfd82ce056e78abc3219
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2025-12-14 22:21:09 -0800

    Mobile menu fixes (#8661)

--- pwa/app/components/tba/navigation/mobileMenu.tsx
+++ pwa/app/components/tba/navigation/mobileMenu.tsx
@@ -9,27 +9,6 @@ import {
   NavigationMenuTrigger,
 } from '~/components/ui/navigation-menu';
 import { NAV_ITEMS_LIST } from '~/lib/navigation/content';
-import { cn } from '~/lib/utils';
-
-export function MobileMenuTrigger({
-  open,
-  setOpen,
-}: {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-}) {
-  return (
-    <button
-      onClick={() => setOpen(!open)}
-      className={cn(
-        `z-30 cursor-pointer rounded-full p-2 text-white transition-colors
-        duration-200 hover:bg-black/20 md:hidden`,
-      )}
-    >
-      {open ? <XIcon className="size-5" /> : <MenuIcon className="size-5" />}
-    </button>
-  );
-}
 
 export function MobileMenu() {
   return (
@@ -44,8 +23,7 @@ export function MobileMenu() {
       </NavigationMenuTrigger>
       <NavigationMenuContent
         className="absolute inset-x-0 top-0 z-12 mt-0 flex max-h-[80svh]
-          flex-col overflow-auto px-4 sm:flex-row sm:items-center sm:justify-end
-          md:w-fit"
+          flex-col overflow-auto px-4"
       >
         <ul className="grid divide-y divide-white/10">
           {NAV_ITEMS_LIST.map(({ title, to, icon: Icon }, index) => (
