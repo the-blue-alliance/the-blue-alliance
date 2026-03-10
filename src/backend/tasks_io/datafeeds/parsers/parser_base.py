dbeebc793eb5c17118bd276616a74719d9b14eb9
Phil Lopreiato <plnyyanks@gmail.com>
2026-03-10 11:35:12 -0500

    removed aliased files

--- src/backend/tasks_io/datafeeds/parsers/parser_base.py
+++
@@ -1,7 +0,0 @@
-from backend.common.datafeeds.parsers.parser_base import (
-    ParserBase,
-    TParsedResponse,
-    TParserInput,
-)
-
-__all__ = ["ParserBase", "TParsedResponse", "TParserInput"]
