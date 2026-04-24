e55aa38f138698f3a00fc78a9bed9c9262003e0d
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2026-04-24 19:50:40 -0400

    [cached-query] Compress API dicts (#9771)

--- src/backend/common/models/cached_query_result.py
+++ src/backend/common/models/cached_query_result.py
@@ -12,7 +12,7 @@ class CachedQueryResult(ndb.Model):
 
     # Only one of result or result_dict should ever be populated for one model
     result = ndb.PickleProperty(compressed=True)  # Raw models
-    result_dict = ndb.JsonProperty()  # Dict version of models
+    result_dict = ndb.JsonProperty(compressed=True)  # Dict version of models
 
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
