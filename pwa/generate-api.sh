1e4b6fdda03e80083337d43da731b15db5d57dcf
Justin Tervay <justin@tervay.com>
2026-07-25 23:33:43 -0400

    Replace PWA nexus client with TBA Nexus endpoint

--- pwa/generate-api.sh
+++ pwa/generate-api.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 npx @hey-api/openapi-ts -f openapi-ts/read.config.ts
 npx @hey-api/openapi-ts -f openapi-ts/mobile.config.ts
-npx @hey-api/openapi-ts -f openapi-ts/nexus.config.ts
 npx @hey-api/openapi-ts -f openapi-ts/colors.config.ts
 
-pnpm exec prettier --write app/api/tba/read app/api/tba/mobile app/api/nexus app/api/colors
+pnpm exec prettier --write app/api/tba/read app/api/tba/mobile app/api/colors
