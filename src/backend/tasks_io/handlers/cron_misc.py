47ee13f0c243cf67aab13a42824e264a332c314d
Brian Rozmierski <bdaroz@users.noreply.github.com>
2026-09-09 22:11:32 -0400

    Add Beta Link to Mod Cron Nag Text (#10641)
    * Add Beta Link to Mod Cron Nag Text
    
    * Making linting happy....

--- src/backend/tasks_io/handlers/cron_misc.py
+++ src/backend/tasks_io/handlers/cron_misc.py
@@ -36,9 +36,7 @@ def nag_pending_suggestions() -> str:
             )
 
     if suggestions_to_nag:
-        nag_text += (
-            "_Review them on <https://www.thebluealliance.com/suggest/review|TBA>_"
-        )
+        nag_text += "_Review them on <https://www.thebluealliance.com/suggest/review|TBA> or on <https://beta.thebluealliance.com/suggest/review|Beta>_"
         OutgoingNotificationHelper.send_slack_alert(channel_url, nag_text)
 
     return ""
